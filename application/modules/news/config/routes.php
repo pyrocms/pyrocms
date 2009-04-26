2500b069fcade7a6e1205a9450098ab22ed29d75
Philip Sturgeon <email@philsturgeon.co.uk>
2009-04-26 13:01:46 +0100

    Support for Coda previews on controllers.

--- application/modules/news/config/routes.php
+++ application/modules/news/config/routes.php
@@ -20,6 +20,9 @@
 |	http://www.codeigniter.com/user_guide/general/routing.html
 */
 
+$route['news/index'] = "news/index";
+$route['news/admin'] = "news/admin";
+
 $route['news/(:num)/(:num)/(:any)'] = "news/view/$3";
 $route['news/page/(:num)'] = "news/index/$1";
 
@@ -33,7 +36,6 @@ $route['news/category/(:any)'] = "news/category/$1"; // Main category page
 $route['news/archive/(:num)/(:num)/(:num)'] = "news/archive/$1/$2"; // Paginated pages
 $route['news/archive/(:num)/(:num)'] = "news/archive/$1/$2"; // Main archive pages
 
-$route['news/index'] = "news/index";
 $route['news/(:any)'] = "news/view/$1";
 
 ?>
