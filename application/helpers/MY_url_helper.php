4b415ad7a486a0d674cb702519bd053948ec4a2e
Phil Sturgeon <email@philsturgeon.co.uk>
2009-05-05 19:05:40 +0100

    Ticket closed #24 - Extended url_helper and inflector_helper to allow cyrillic character support in url_title() and humanize().

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -11,6 +11,64 @@
 
 // ------------------------------------------------------------------------
 
+/**
+ * Create URL Title - modified version
+ *
+ * Takes a "title" string as input and creates a
+ * human-friendly URL string with either a dash
+ * or an underscore as the word separator.
+ * 
+ * Added support for Cyrillic characters.
+ *
+ * @access	public
+ * @param	string	the string
+ * @param	string	the separator: dash, or underscore
+ * @return	string
+ */
+if ( ! function_exists('url_title'))
+{
+	function url_title($str, $separator = 'dash', $lowercase = FALSE)
+    {
+        if ($separator == 'dash')
+        {
+            $search        = '_';
+            $replace    = '-';
+        }
+        else
+        {
+            $search        = '-';
+            $replace    = '_';
+        }
+
+        $trans = array(
+                        '&\#\d+?;'                => '',
+                        '&\S+?;'                => '',
+                        '\s+'                    => $replace,
+                        '[^a-zАБВГҐДЕЄЁЖЗИІЫЇЙКЛМНОПРСТУФХЦЧШЩЮЯЬЪабвгґдеєёжзиіыїйклмнопрстуфхцчшщюяьъ0-9\-\._]' => '',
+                        $replace.'+'            => $replace,
+                        $replace.'$'            => $replace,
+                        '^'.$replace            => $replace,
+                        '\.+$'                    => ''
+                      );
+
+        $str = strip_tags($str);
+
+        foreach ($trans as $key => $val)
+        {
+            $str = preg_replace("#".$key."#i", $val, $str);
+        }
+
+        if ($lowercase === TRUE)
+        {
+            $str = mb_convert_case($str, MB_CASE_LOWER, "UTF-8");
+        }
+        
+        return trim(stripslashes($str));
+    }
+}
+
+// ------------------------------------------------------------------------
+
 function shorten_url($url = '')
 {
 	$CI =& get_instance();
