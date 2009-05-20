ae5464a04312168c86de7de4148823d257b2b657
Phil Sturgeon <email@philsturgeon.co.uk>
2009-05-20 23:44:23 +0100

    feature: Trialing use of Ajaxify for speeding up some parts of admin backend.

--- application/views/admin/layout_fragments/sidebar.php
+++ application/views/admin/layout_fragments/sidebar.php
@@ -3,7 +3,7 @@
 	
 		<? foreach($admin_modules as $admin_module): ?>
 		<li class="inactive">
-			<a href="<?= site_url('admin/'.$admin_module['slug']); ?>" class="button">
+			<a href="<?= site_url('admin/'.$admin_module['slug']); ?>" class="button ajax">
 				<strong>
 					<?= image('admin/icons/'.(!empty($admin_module['icon']) ? $admin_module['icon'] : 'folder_48.png'), NULL, array('alt' => $admin_module['name'] .' icon', 'class' => 'icon') ); ?>
 					<?=$admin_module['name'];?><span class="expand expanded"></span>
