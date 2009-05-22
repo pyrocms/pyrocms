13512746a52bc7522fabd5729f24870009686df3
Yorick Peterse <yorickpeterse@Yorick.lan>
2009-05-22 20:12:19 +0200

    Just testing

---
+++ codeigniter/logs/log-2009-05-19.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2009-05-19 18:42:44 --> Severity: Warning  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: Access denied for user 'root'@'localhost' (using password: NO) /Applications/MAMP/htdocs/Big_Projects/pyrocms/codeigniter/database/drivers/mysql/mysql_driver.php 88
+ERROR - 2009-05-19 18:42:44 --> Unable to connect to the database
