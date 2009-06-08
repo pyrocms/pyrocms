19bbf31d7059de7ebd30d60e8aab13c86d85d670
Yorick Peterse <info@yorickpeterse.com>
2009-06-08 20:51:19 +0200

    Removed a few comments from the layout.php file

--- application/themes/pyrocms/views/layout.php
+++ application/themes/pyrocms/views/layout.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<!-- Find the hidden easter eggs ! -->
 		<!-- Meta information -->
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 		<?php echo "\n"; echo $extra_head_content; ?>
@@ -14,7 +13,6 @@
 		<title><?php echo $this->settings->item('site_name'); ?></title>		
 	</head>
 	<body>
-		<!-- We shit on CakePHP -->
 		<!-- Main wrapper -->
 		<div id="wrapper">
 			<!-- Header -->
