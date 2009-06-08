bd351dc04109a9300e10822c4a21041f0f0e835c
Yorick Peterse <info@yorickpeterse.com>
2009-06-08 21:23:17 +0200

    Forgot to add a very important notice in the layout file :)

--- application/themes/pyrocms/views/layout.php
+++ application/themes/pyrocms/views/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
+		<!-- Main template for the PyroCMS website : http://www.pyrocms.com/ Please refrain from using this template yourself, else we'll be forced to send a few 4Chan freaks to your house ;] -->
 		<!-- Meta information -->
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 		<?php echo "\n"; echo $extra_head_content; ?>
