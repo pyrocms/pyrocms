dc281561d98140aff1e8cd38fa9fbadf2d466229
BenneX <Administrator@.(none)>
2009-06-10 12:16:13 +0200

    Well, I've added the new language-files now. Hope that's all for now

---
+++ application/language/english/admin_lang.php
@@ -0,0 +1,73 @@
+<?php
+
+// The defaul title for the cp
+$lang['cp_title'] = 'Control Panel';
+
+//
+//// admin/cpanel
+//
+$lang['cp_welcome'] = 'Welcome to the %s Control Panel. Using the links above and to the left you can control almost every aspect of your website. For a full list of modules, please <a href="#modules">see below</a>.';
+
+// general strings
+$lang['title_label'] 			= 'Title';
+$lang['name_label'] 			= 'Name';
+$lang['desc_label'] 			= 'Description';
+$lang['version_label'] 		= 'Version';
+$lang['author_label'] 		= 'Author';
+$lang['confirm'] 					= 'Confirm';
+$lang['yes'] 							= 'Yes';
+$lang['no'] 							= 'No';
+
+//
+//// admin/result_messages
+//
+
+// general strings
+$lang['close_message'] 						= 'Close this message';
+$lang['general_error_label'] 			= 'Error';
+$lang['required_error_label'] 		= 'Required field missing';
+$lang['note_label'] 							= 'Note';
+$lang['success_label'] 						= 'Success';
+
+//
+//// admin/login
+//
+
+$lang['login_title'] = 'Log In';
+
+//general strings
+$lang['login_error_label'] 				= 'Login Error';
+$lang['forgot_password_label'] 		= 'Forgot your password ?';
+$lang['email_label'] 							= 'E-mail';
+$lang['password_label'] 					= 'Password';
+$lang['login_label'] 							= 'Log In';
+
+//
+//// admin/layout_fragments/
+//
+
+// breadcrumbs
+$lang['breadcrumb_home_title'] = 'Admin';
+
+// header
+$lang['cp_to_home'] 								= 'Back to Control Panel home';
+$lang['cp_view_frontend'] 					= 'View website';
+$lang['cp_logged_in_as'] 						= 'You\'re logged in as %s.';
+$lang['cp_logout'] 									= '%s, or %s';
+$lang['cp_logout_label'] 						= 'logout';
+$lang['cp_edit_profile_label'] 			= 'Edit profile';
+
+// inner_header
+
+// table_buttons
+$lang['save_label'] 		= 'Save';
+$lang['cancel_label'] 	= 'Cancel';
+$lang['delete_label'] 	= 'Delete';
+$lang['activate_label'] = 'Activate';
+$lang['publish_label'] 	= 'Publish';
+$lang['upload_label'] 	= 'Upload';
+
+
+//$lang[''] = '';
+
+?>
