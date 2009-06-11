606525741e1d24cd14d0ece6622bfa50ac398093
Yorick Peterse <info@yorickpeterse.com>
2009-06-11 14:32:21 +0200

    Fixed the installer, it now works fine on Windows

--- installer/includes/core_class.php
+++ installer/includes/core_class.php
@@ -63,14 +63,16 @@ class Core {
 		chmod($output_path,0777);
 		
 		// Verify file permissions
-		if(substr(sprintf('%o', fileperms($output_path)), -3) != '777') {
-			return false;
-		}
-		else {
+		if(is_writable($output_path))
+		{
 			// Write the file
 			fwrite($handle,$new);
 			return true;
-		}		
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
 ?>
