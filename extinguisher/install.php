aa53f87912084b4194a58d245af9b84344ff8e4a
Yorick Peterse <info@yorickpeterse.com>
2009-06-16 23:54:59 +0200

    Renamed Euphoria to Extinguisher as that is more appealing to PyroCMS :)

--- extinguisher/install.php
+++ extinguisher/install.php
@@ -42,14 +42,14 @@ if($_POST) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<!-- Euphoria 1.0, by Yorick Peterse : http://www.yorickpeterse.com -->
+		<!-- Extinguisher 1.0, by Yorick Peterse : http://www.yorickpeterse.com -->
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		
 		<!-- Stylesheets -->
 		<link rel="stylesheet" href="css/reset.css" type="text/css" />
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 		
-		<title>Euphoria 1.0 | Installer</title>
+		<title>Extinguisher 1.0 | Installer</title>
 	</head>
 	<body>
 		<!-- Main wrapper -->
