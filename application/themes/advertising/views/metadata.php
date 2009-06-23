bc34898de1f4a8cadef74a833133ef19d00e14e7
Phil Sturgeon <email@philsturgeon.co.uk>
2009-06-23 19:03:17 +0100

    feature: Added multiple layouts to all themes to allow different layouts or wrappers to each page.

---
+++ application/themes/advertising/views/metadata.php
@@ -0,0 +1,18 @@
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?=$page_title;?> | <?=$this->settings->item('site_name'); ?></title>
+	
+	<!-- Language: <?=DEFAULT_LANGUAGE ?> -->
+	
+    <script type="text/javascript">
+	    var APPPATH_URI = "<?=$this->config->item('asset_dir');?>";
+	    var BASE_URI = "<?=BASE_URI;?>";
+    </script>
+	
+	<?= css('style.css').css('layout.css', '_theme_');?>
+	
+	<?= js('jquery/jquery.js'); ?>
+	<?= js('facebox.js').css('facebox.css');?>
+	
+	<?= js('front.js'); ?>
+
+    <?=$extra_head_content; ?>
