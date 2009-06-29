240df4897d9cda5ae1673cfdcebe102f45c91603
Phil Sturgeon <email@philsturgeon.co.uk>
2009-06-29 18:22:50 +0100

    Getting multi-color admin CSS ready.

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<title><?=$page_title;?> | <?=$this->settings->item('site_name'); ?> Control Panel</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -30,7 +30,9 @@
 			
 	    <?=$extra_head_content; ?>        
 	    <?= js('functions.js'); ?>
-		<?= js('admin.js').css('admin/admin.css');?>        
+		<?= js('admin.js').css('admin/admin.css');?>
+		
+		<?//= css('admin/orange.css'); ?>      
 	</head>
 	<body>
 	
