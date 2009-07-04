3382eb12bcb0848ba1be73d2b42f3458f7cd90dd
Phil Sturgeon <email@philsturgeon.co.uk>
2009-07-04 20:29:54 +0100

    feature: Added flag icons for all supported languages top right of admin so users can switch languages.

--- application/views/admin/fragments/header.php
+++ application/views/admin/fragments/header.php
@@ -6,4 +6,15 @@
 		<?= sprintf(lang('cp_logged_in_as'), $user->first_name.' '.$user->last_name);?> 
 		<?= sprintf(lang('cp_logout'), anchor('edit-profile', lang('cp_edit_profile_label')), anchor('admin/logout', lang('cp_logout_label')));?>
 	</div>
+	
+	<div class="languageSelector">
+	
+		<? foreach( $this->config->item('supported_languages') as $code => $lang ): ?>
+			<a href="?lang=<?=$code;?>">
+				<?=image('icons/flags/'.strtolower($code).'.gif', NULL, array('alt' => $lang)); ?>
+			</a>
+		<? endforeach; ?>
+		
+	</div>
+	
 </div>
