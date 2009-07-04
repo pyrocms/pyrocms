c8c3d9389c19ffab5ba3607f4aff6d4f19e7d0b0
Phil Sturgeon <email@philsturgeon.co.uk>
2009-07-04 20:29:54 +0100

    feature: Added flag icons for all supported languages top right of admin so users can switch languages.

--- application/language/english/admin_lang.php
+++ application/language/english/admin_lang.php
@@ -1,16 +1,16 @@
 <?php
 
-$lang['cp_welcome'] 								= 'Welcome to the %s Control Panel. Using the links above and to the left you can control almost every aspect of your website. For a full list of modules, please <a href="#modules">see below</a>.';
+$lang['cp_welcome'] 					= 'Welcome to the %s Control Panel. Using the links above and to the left you can control almost every aspect of your website. For a full list of modules, please <a href="#modules">see below</a>.';
 // breadcrumbs
-$lang['cp_breadcrumb_home_title'] 	= 'Admin';
+$lang['cp_breadcrumb_home_title'] 		= 'Admin';
 // header
-$lang['cp_to_home'] 								= 'Back to Control Panel home';
-$lang['cp_view_frontend'] 					= 'View website';
-$lang['cp_logged_in_as'] 						= 'You\'re logged in as %s.';
-$lang['cp_logout'] 									= '%s, or %s';
-$lang['cp_logout_label'] 						= 'logout';
+$lang['cp_to_home'] 					= 'Back to Control Panel home';
+$lang['cp_view_frontend'] 				= 'View website';
+$lang['cp_logged_in_as'] 				= 'You\'re logged in as %s.';
+$lang['cp_logout'] 						= '%s, or %s';
+$lang['cp_logout_label'] 				= 'logout';
 $lang['cp_edit_profile_label'] 			= 'Edit profile';
 
-$lang['cp_access_denied'] 					= 'You do not have sufficient permissions to view this page.'
+$lang['cp_access_denied'] 				= 'You do not have sufficient permissions to view this page.'
 
 ?>
