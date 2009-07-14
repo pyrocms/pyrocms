57075d21931e4624a733f0bd74db9ac2b9bfceba
Phil Sturgeon <email@philsturgeon.co.uk>
2009-07-14 17:07:45 +0100

    bug: Fixed multiple displays of comments fromnon-registered users.

--- application/modules/comments/models/comments_m.php
+++ application/modules/comments/models/comments_m.php
@@ -11,14 +11,7 @@ class Comments_m extends Model
 	{  	
 		if(isset($param['is_active']))
 		{
-			if($param['is_active'] == 1)
-			{
-				$this->db->where('is_active', 1);
-			}
-			else
-			{
-				$this->db->where('is_active = 0');
-			}			
+			$this->db->where('is_active', $param['is_active']);
 		}
 		
 		if(!empty($params['module']))
@@ -58,108 +51,95 @@ class Comments_m extends Model
 		
 	public function getComments($params = array())
 	{
-    $sql = '
-			SELECT
-				c.id, c.is_active, c.body, c.created_on, c.module, c.module_id, c.user_id,
-				IF(c.user_id > 0, IF(u.last_name = "", u.first_name, CONCAT(u.first_name, " ", u.last_name)), c.name) as name, 
-				IF(c.user_id > 0, u.email, c.email) as email
-			FROM
-				comments c, users u
-			WHERE
-				IF(c.user_id > 0, c.user_id = u.id, true = true)
-		';
+    	$this->db->select('c.id, c.is_active, c.body, c.created_on, c.module, c.module_id, c.user_id');
+    	$this->db->select('IF(c.user_id > 0, IF(u.last_name = "", u.first_name, CONCAT(u.first_name, " ", u.last_name)), c.name) as name');
+    	$this->db->select('IF(c.user_id > 0, u.email, c.email) as email');
+
+    	$this->db->from('comments c');
+    	
+    	$this->db->join('users u', 'c.user_id = u.id', 'left');
+	
+    	// If there is a comment user id, make sure the user still exists
+    	$this->db->where('IF(c.user_id > 0, c.user_id = u.id, TRUE)');
 		
 		if(isset($params['id']))
 		{
-			$sql .= ' AND c.id = "'. $params['id'] .'"';
+			$this->db->where('c.id', $params['id']);
 		}
 		
 		if(isset($params['is_active']))
 		{
-			if($params['is_active'] == 1)
-			{
-				$sql .= ' AND c.is_active = '. $params['is_active'];
-			}
-			else
-			{
-				$sql .= ' AND c.is_active = '. $params['is_active'];
-			}			
+			$this->db->where('c.is_active', $params['is_active']);
 		}
 		
 		if(!empty($params['module']))
 		{			
-			$sql .= ' AND c.module = "'. $params['module'] .'"';
+			$this->db->where('c.module', $params['module']);
     	}
 		
 		if(!empty($params['module_id']))
 		{
-			$sql .= ' AND c.module_id = "'. $params['module_id'] .'"';
+			$this->db->where('c.module_id', $params['module_id']);
 		}
 				
 		if(!empty($params['month']))
 		{
-			$sql .= ' AND MONTH(FROM_UNIXTIME(c.created_on)) = "'. $params['month'] .'"';
+			$this->db->where('MONTH(FROM_UNIXTIME(c.created_on))', $params['month']);
 		}
 		
 		if(!empty($params['year']))
 		{
-			$sql .= ' AND YEAR(FROM_UNIXTIME(c.created_on)) = "'. $params['year'] .'"';
+			$this->db->where('YEAR(FROM_UNIXTIME(c.created_on))', $params['year']);
 		}
 		
 		if(!empty($params['user']))
 		{
 			if(is_numeric($params['user']))
 			{
-				$sql .= ' AND c.user_id = "'. $params['user'] .'"';
+				$this->db->where('c.user_id', $params['user']);
 			}
 			else
 			{
-				$sql .= ' AND c.name = "'. $params['user'] .'"';
+				$this->db->where('c.name', $params['user']);
 			}
 		}
     		
 		// Sorting the results based on a param
-		if(!empty($params['order']))
+		switch(@$params['order'])
 		{
-			switch($params['order'])
-			{
-				// Specific sorting options goes here
-				case 'byNameDESC':
-					$sql .= ' ORDER BY c.name DESC';
-				break;
-				
-				// Default sorting of comments by wrong param
-				default:
-					$sql .= ' ORDER BY c.created_on DESC';
-			}
-		}
-		else
-		{
-			// Default sorting of comments by no param
-			$sql .= ' ORDER BY c.created_on DESC';
+			// Specific sorting options goes here
+			case 'byNameDESC':
+				$this->db->orderby('c.name DESC');
+			break;
+			
+			// Default sorting of comments by wrong param
+			default:
+				$this->db->orderby('c.created_on DESC');
+			break;
 		}
 		
 		// Limit the results based on 1 number or 2 (2nd is offset)
-    if(isset($params['limit']) && is_array($params['limit']))
+    	if(isset($params['limit']) && is_array($params['limit']))
 		{
-			$sql .= ' LIMIT '.$params['limit'][1].', '.$params['limit'][0];
+			$this->db->limit($params['limit'][0], $params['limit'][1]);
 		}
-    elseif(isset($params['limit']))
+		
+    	elseif(isset($params['limit']))
 		{
-			$sql .= ' LIMIT '.$params['limit'];
+			$this->db->limit($params['limit']);
 		}
 		  
-    $query = $this->db->query($sql);
+    	$query = $this->db->get();
     
 		if ($query->num_rows() == 0)
 		{
-    	return FALSE;
-    }
+    		return FALSE;
+    	}
 		else
 		{
-    	return $query->result();
-    }
-  }
+    		return $query->result();
+   		}
+  	}
 	
 	public function getModule($id)
 	{
@@ -178,11 +158,11 @@ class Comments_m extends Model
 		if ($query->num_rows() == 0)
 		{
     	return FALSE;
-    }
+    	}
 		else
 		{
-    	return $query->result();
-    }
+    		return $query->result();
+    	}
 	}
 	
 	public function getModuleComments($module, $limit)
@@ -209,12 +189,12 @@ class Comments_m extends Model
 		$this->load->helper('date');
 		
 		$this->db->insert('comments', array(
-			'user_id'			=> isset($input['user_id']) 	? 	$input['user_id'] 											:  0,
-			'is_active'		=> isset($input['is_active']) ? 	$input['is_active'] 										:  0,
-			'name'				=> isset($input['name']) 			? 	ucwords(strtolower($input['name'])) 		: '',
-			'email'				=> isset($input['email']) 		? 	strtolower($input['email']) 						: '',
-			'body'				=> strip_tags($input['body']),
-			'module'			=> $input['module'],
+			'user_id'		=> isset($input['user_id']) 		? 	$input['user_id'] 							:  0,
+			'is_active'		=> isset($input['is_active']) 		? 	$input['is_active'] 						:  0,
+			'name'			=> isset($input['name']) 			? 	ucwords(strtolower($input['name'])) 		: '',
+			'email'			=> isset($input['email']) 			? 	strtolower($input['email']) 				: '',
+			'body'			=> strip_tags($input['body']),
+			'module'		=> $input['module'],
 			'module_id'		=> $input['module_id'],
 			'created_on' 	=> now()
 		));
@@ -223,17 +203,17 @@ class Comments_m extends Model
 	}
 	
 	public function updateComment($input, $id = 0)
-  {
-  	$this->load->helper('date');
+	{
+  		$this->load->helper('date');
 		
 		$this->db->where('id', $id);		
 		$set = array(
-			'user_id'			=> isset($input['user_id']) 	? 	$input['user_id'] 											:  0,
-			'is_active'		=> isset($input['is_active']) ? 	$input['is_active'] 										:  0,
-			'name'				=> isset($input['name']) 			? 	ucwords(strtolower($input['name'])) 		: '',
-			'email'				=> isset($input['email']) 		? 	strtolower($input['email']) 						: '',
-			'body'				=> strip_tags($input['body']),
-			'module'			=> $input['module'],
+			'user_id'		=> isset($input['user_id']) 		? 	$input['user_id'] 							:  0,
+			'is_active'		=> isset($input['is_active']) 		? 	$input['is_active'] 						:  0,
+			'name'			=> isset($input['name']) 			? 	ucwords(strtolower($input['name'])) 		: '',
+			'email'			=> isset($input['email']) 			? 	strtolower($input['email']) 				: '',
+			'body'			=> strip_tags($input['body']),
+			'module'		=> $input['module'],
 			'module_id'		=> $input['module_id'],
 			'created_on' 	=> now()
 		);	
@@ -242,8 +222,8 @@ class Comments_m extends Model
 	}
 	
 	public function activateComment($id, $is_active = 0)
-  {
-  	$this->db->where('id', $id);
+	{
+  		$this->db->where('id', $id);
 		return $this->db->update('comments', array('is_active' => $is_active));
 	}
 
