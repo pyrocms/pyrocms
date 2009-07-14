d0d13ed4b2678a85dfe2fa6f3928bfa27329e7c6
Yorick Peterse <info@yorickpeterse.com>
2009-07-14 20:24:41 +0200

    Fixed issue #31 and the MySQLi connection issue. The installer will now show nicely formatted messages instead of the ugly default PHP errors :)

--- installer/server.php
+++ installer/server.php
@@ -7,7 +7,6 @@ $php_ver 	= phpversion();
 $mysql_ver 	= $core->mysql_version();
 $GDArray 	= gd_info();
 $gd_ver		= ereg_replace('[[:alpha:][:space:]()]+', '', $GDArray['GD Version']);
-	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
