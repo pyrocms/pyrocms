a0e2a992dcd1324dad16b2c77b17fe3f6b753d57
Yorick Peterse <info@yorickpeterse.com>
2009-07-17 15:34:48 +0200

    Started working on a completely new default PyroCMS template. In fact, I'll make multiple templates :)

--- application/themes/pyrocms/views/layouts/default.php
+++ application/themes/pyrocms/views/layouts/default.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<!-- Main template for the PyroCMS website : http://www.pyrocms.com/ Please refrain from using this template yourself, else we'll be forced to send a few 4Chan freaks to your house ;] -->
+		<!-- And no, we do not support Microsoft Internet Explorer 6 :] -->
 		<!-- Meta information -->
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 		<?php echo "\n"; echo $extra_head_content; ?>
@@ -9,17 +9,12 @@
 		<!-- Stylesheets -->
 		<link href="<?php echo base_url(); ?>application/themes/pyrocms/css/reset.css" rel="stylesheet" media="screen" type="text/css" />
 		<link href="<?php echo base_url(); ?>application/themes/pyrocms/css/style.css" rel="stylesheet" media="screen" type="text/css" />
-		<!-- Javascript -->
 		
 		<title><?php echo $this->settings->item('site_name'); ?></title>		
 	</head>
 	<body>
 		<!-- Main wrapper -->
 		<div id="wrapper">
-			<!-- Header -->
-			<div id="header">
-				<?php echo "\n"; $this->load->view($theme_view_folder.'header'); echo "\n"; ?>
-			</div>
 			<!-- Flash data messages will appear below -->
 			<?php if ($this->session->flashdata('notice')) {
 		                  echo '<div class="notice notification_box"><p>' . strip_tags($this->session->flashdata('notice')) . '</p></div>';
@@ -31,11 +26,42 @@
 		                  echo '<div class="error notification_box"><p>' . strip_tags($this->session->flashdata('error')) . '</p></div>';
 			}
 			?>
+			
+			<!-- Header -->
+			<div id="header">						
+				<!-- Site title -->
+				<div id="site_title">
+					<h1><img src="<?php echo image_path('logo.png', '_theme_'); ?>" alt="PyroCMS" width="170" height="80" /></h1>
+				</div>								
+				<!-- Navigation -->
+				<div id="navigation">
+					<ul>
+						<?php if(!empty($navigation['header'])) foreach($navigation['header'] as $nav_link): ?>					
+						<li><?php echo anchor($nav_link->url, $nav_link->title, $nav_link->current_link ? 'id="current"' : ''); ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>	
+				<div class="clear"></div>			
+			</div>
 
 			<!-- Content -->
 			<div id="content">
 				<?php echo $page_output; ?>
-				<div class="clear"></div>
+			</div>
+			
+			<!-- Sidebar -->
+			<div id="sidebar">
+				<? if(is_module('news')): ?>
+			  	<h2>Latest News</h2>
+				<ul>
+					<?php foreach ($this->news_m->getNews() as $news): ?>
+					<li>
+						<h3><?php echo anchor('news/' . date('Y/m') . '/'. strtolower($news->slug), $news->title); ?></h3>
+						<p class="post-info">Posted in category : <?php echo anchor('news/category/'.$news->category_slug, $news->category_title);?></p>
+					</li>
+					<?php endforeach ?>
+				</ul>
+				<? endif; ?>
 			</div>
 			
 			<!-- Footer -->
