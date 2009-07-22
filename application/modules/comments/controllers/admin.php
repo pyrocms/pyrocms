f98ee69db15a794e5588f53d1bb33078fc2e3b7c
BenneX <Administrator@.(none)>
2009-07-22 20:11:22 +0200

    Second Part of issue_33 fixed.

--- application/modules/comments/controllers/admin.php
+++ application/modules/comments/controllers/admin.php
@@ -7,7 +7,7 @@ class Admin extends Admin_Controller
 		parent::Admin_Controller();
 		$this->load->library('validation');
 		$this->load->model('comments_m');
-		$this->lang->load('comments');		
+		$this->lang->load('comments');	
 	}
 	
 	// Admin: List all comments
@@ -195,4 +195,5 @@ class Admin extends Admin_Controller
 		$this->layout->create('admin/preview', $this->data);
 	}
 }
+
 ?>
