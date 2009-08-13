f240e96e9359bff6a7367a002b446622a6004fb8
Phil Sturgeon <email@philsturgeon.co.uk>
2009-08-13 09:16:23 +0000

    tweak: added is_backend_sidebar to the details.xml to control which modules show on left navigation for admin.

--- application/libraries/Admin_Controller.php
+++ application/libraries/Admin_Controller.php
@@ -60,13 +60,13 @@ class Admin_Controller extends MY_Controller
 	    {
 	  		$this->data->admin_modules = $this->cache->model('modules_m', 'getModules', array(
 	    		array(
-					'is_backend'=>true, 
+					'is_backend_sidebar'=>true, 
 					'role' => @$this->data->user->role,
 	    			'lang' => CURRENT_LANGUAGE
 				) // This function does NOT need role OR language, that is to give it a unique md5 hash
 	    	), $this->config->item('navigation_cache'));
 	}
-		
+
 	    $this->data->toolbar = $this->modules_m->getModuleToolbar($this->module);        
 	    
 	    $this->layout->wrapper('admin/layout');
