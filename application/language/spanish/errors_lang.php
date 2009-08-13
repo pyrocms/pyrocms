cc31ff1140235b1d404dab87e33c07353f116127
Phil Sturgeon <email@philsturgeon.co.uk>
2009-08-13 23:44:02 +0100

    bug: Apparently it was bad translation. Fuck knows, I don't speak the language.

--- application/language/spanish/errors_lang.php
+++ application/language/spanish/errors_lang.php
@@ -1,8 +1,8 @@
 <?php
 
 // Error 404
-$lang['error_404_title'] 			= 'P&aacute;gina estraviada';
-$lang['error_404_message'] 		= 'No pudimos encontrar la p&aacute;gina que estabas buscando, por favor ve a la <a href="%s">p&aacute;gina principal</a>.';
+$lang['error_404_title'] 			= 'Extraviada';
+$lang['error_404_message'] 			= 'No pudimos encontrar la p&aacute;gina que estabas buscando, por favor ve a la <a href="%s">p&aacute;gina principal</a>.';
 
 // Database
 $lang['error_invalid_db_group'] 	= 'La base de datos esta tratando de utilizar un grupo de configuraci&oacute;n inv&aacute;lido "%s".';
