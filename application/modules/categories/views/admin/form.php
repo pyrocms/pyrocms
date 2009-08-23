0aa9c32aa4c8fe76ce70c6771d1b17193739659f
YorickPeterse <info@yorickpeterse.com>
2009-08-23 16:19:32 +0200

    Started cleaning up the modules

--- application/modules/categories/views/admin/form.php
+++ application/modules/categories/views/admin/form.php
@@ -1,13 +1,13 @@
-<? if($this->uri->segment(3,'create') == 'create'): ?>
-	<h2><?=lang('cat_create_title');?></h2>	
-<? else: ?>
-	<h2><?=sprintf(lang('cat_edit_title'), $this->data->category->title);?></h2>
-<? endif; ?>
-<?= form_open($this->uri->uri_string()); ?>
+<?php if($this->uri->segment(3,'create') == 'create'): ?>
+	<h2><?php echo lang('cat_create_title');?></h2>	
+<?php else: ?>
+	<h2><?php echo sprintf(lang('cat_edit_title'), $this->data->category->title);?></h2>
+<?php endif; ?>
+<?php echo form_open($this->uri->uri_string()); ?>
 	<div class="field">
-		<label for="title"><?=lang('cat_title_label');?></label>
-		<?= form_input('title', $category->title, 'class="text"'); ?>
-		<span class="required-icon tooltip"><?=lang('cat_required_label');?></span>
+		<label for="title"><?php echo lang('cat_title_label');?></label>
+		<?php echo  form_input('title', $category->title, 'class="text"'); ?>
+		<span class="required-icon tooltip"><?php echo lang('cat_required_label');?></span>
 	</div>
-	<? $this->load->view('admin/fragments/table_buttons', array('buttons' => array('save', 'cancel') )); ?>
-<?= form_close(); ?>
+	<?php $this->load->view('admin/fragments/table_buttons', array('buttons' => array('save', 'cancel') )); ?>
+<?php echo form_close(); ?>
