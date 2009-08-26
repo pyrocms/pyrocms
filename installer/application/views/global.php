edf3bdecba4c887d531492dfb8f99515e12c96e5
Phil Sturgeon <email@philsturgeon.co.uk>
2009-08-26 23:05:06 +0100

    tweak: Interface improvements for the new installer.

--- installer/application/views/global.php
+++ installer/application/views/global.php
@@ -18,10 +18,10 @@
 			<!-- The header -->
 			<div id="header">
 				<ul>
-					<li><?php echo anchor('', 'Introduction'); ?></li>
-					<li><?php echo anchor('installer/step_1', 'Step #1'); ?></li>
-					<li><span>Step #2</span></li>
-					<li><span>Step #3</span></li>
+					<li><?php echo anchor('', 'Introduction', $this->uri->segment(2, '') == '' ? 'id="current"' : ''); ?></li>
+					<li><?php echo anchor('installer/step_1', 'Step #1', $this->uri->segment(2, '') == 'step_1' ? 'id="current"' : ''); ?></li>
+					<li><span id="<?php echo $this->uri->segment(2, '') == 'step_2' ? 'current' : ''?>">Step #2</span></li>
+					<li><span id="<?php echo $this->uri->segment(2, '') == 'step_3' ? 'current' : ''?>">Step #3</span></li>
 				</ul>
 			</div>
 			<!-- The content -->
