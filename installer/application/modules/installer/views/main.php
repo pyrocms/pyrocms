acf0a71837b6006eff1a5a89a38bbf177a00bb18
YorickPeterse <info@yorickpeterse.com>
2009-08-26 23:08:56 +0200

    Updated the main view file. It still had some dummy text in it. (more text is required)

--- installer/application/modules/installer/views/main.php
+++ installer/application/modules/installer/views/main.php
@@ -1,12 +1,3 @@
 <!-- Welcome -->
 <h2>Welcome</h2>
-<p>Thank you for choosing PyroCMS ! Installing PyroCMS is very easy, thanks to the installer you are currently viewing. In case you do have problems installing the system don't worry, the installer has enough tools to deal with a wide range of problems.</p>
-<!-- Installing PyroCMS -->
-<h2>Installing</h2>
-<p>Lorem ipsum</p>
-<!-- Upgrading PyroCMS -->
-<h2>Upgrading</h2>
-<p>Lorem ipsum</p>
-<!-- Maintenance -->
-<h2>Maintenance</h2>
-<p>Lorem ipsum</p>
+<p>Thank you for choosing PyroCMS ! Installing PyroCMS is very easy, thanks to the installer you are currently viewing. Simply hit the "Install" button above to start the installation process</p>
