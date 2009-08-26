e3876ea728dee7677344178831e07711bc7cd58f
Phil Sturgeon <email@philsturgeon.co.uk>
2009-08-26 23:05:06 +0100

    tweak: Interface improvements for the new installer.

--- installer/application/views/main.php
+++ installer/application/views/main.php
@@ -2,6 +2,6 @@
 <h2>Welcome</h2>
 <p>Thank you for choosing PyroCMS ! Installing PyroCMS is very easy, thanks to the installer you are currently viewing. In case you do have problems installing the system don't worry, the installer has enough tools to deal with a wide range of problems.</p>
 
-<?php echo form_open('installer/step_1')?>
-<input type="submit" value="Step #1" />
-<?php echo form_close(); ?>
+<p id="next_step"><a href="<?php echo site_url('installer/step_1'); ?>" title="Proceed to the first step">Step 1</a></p>
+
+<br class="clear" />
