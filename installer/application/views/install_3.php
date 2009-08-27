fe958a01621336800709011f489f27d37f545cd7
YorickPeterse <info@yorickpeterse.com>
2009-08-27 15:21:45 +0200

    Changed the way the installer works. Also fixed some bugs that were introduced by Phil's changes.

--- installer/application/views/install_3.php
+++ installer/application/views/install_3.php
@@ -1,12 +1,8 @@
 <h2>Step 3: Create database</h2>
 <p>Complete the form below and hit the button labelled "Finish" to install PyroCMS. Be sure to install PyroCMS into the right database since all existing changes will be lost!</p>
-<form id="install_frm" method="post" action="">
+<form id="install_frm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 	<h3>Database Settings</h3>
-	<p><label for="server">Server</label><input type="text" id="server" class="input_text" name="server" /></p>
-	<p><label for="username">Username</label><input type="text" id="username" class="input_text" name="username" /></p>
-	<p><label for="password">Password</label><input type="password" id="password" class="input_text" name="password" /></p>
 	<p><label for="database">Database</label><input type="text" id="database" class="input_text" name="database" /></p>
-	<h3>Optional Settings</h3>
 	<p><label for="dummy_data">Install dummy data</label><input type="checkbox" name="dummy_data" value="true" id="dummy_data" /></p>
 	<p><label for="create_db">Create Database</label><input type="checkbox" name="create_db" value="true" id="create_db" /><small>In some cases you might have to create the database yourself.</small></p>
 	<p id="next_step"><input type="submit" id="submit" value="Finish" /></p>
--- installer/application/views/main.php
+++ installer/application/views/main.php
@@ -1,7 +1,12 @@
 <!-- Welcome -->
 <h2>Welcome</h2>
 <p>Thank you for choosing PyroCMS! Installing PyroCMS is very easy, just follow the steps and messages on the screen. In case you have any problems installing the system don't worry, the installer will explain what you need to do.</p>
-
-<p id="next_step"><a href="<?php echo site_url('installer/step_1'); ?>" title="Proceed to the first step">Step 1</a></p>
-
-<br class="clear" />
+<h3>Database Settings</h3>
+<p>In order for the installer to check your MySQL server version it requires you to enter the hostname, username and password in the form below. These settings will also be used when installing the database.</p>
+<form id="install_frm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<p><label for="server">Server</label><input type="text" id="server" name="server" class="input_text" /></p>
+	<p><label for="username">Username</label><input type="text" id="username" name="username" class="input_text" /></p>
+	<p><label for="password">Password</label><input type="password" id="password" name="password" class="input_text" /></p>
+	<p id="next_step"><input type="submit" id="submit" value="Step 1" /></p>
+	<div class="clear" />
+</form>
