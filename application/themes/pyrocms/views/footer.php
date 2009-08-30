c298f6be2526411b4554d34a24371f3854d33b13
Phil Sturgeon <email@philsturgeon.co.uk>
2009-08-30 13:13:43 +0100

    Removed half-finished buggy default template.

--- application/themes/pyrocms/views/footer.php
+++
@@ -1,6 +0,0 @@
-
-<p id="copyrights">Copyright 2009<?php if(date('Y') != '2009') {echo ' - '.date('Y');} ?> <?php echo $this->settings->item('site_name'); ?>, all rights reserved.</p><p id="site_notice">Site powered by PyroCMS</p>
-    
-<?php if($this->settings->item('google_analytic')): ?>
-	<?php $this->load->view('fragments/google_analytic'); ?>
-<?php endif; ?>
