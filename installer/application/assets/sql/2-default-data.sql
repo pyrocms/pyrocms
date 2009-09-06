27d3cee1f1d55cc2292608c90807a5884171b6c3
YorickPeterse <info@yorickpeterse.com>
2009-09-06 19:51:18 +0200

    Users can now manage their dashboard RSS feeds using the settings panel. Don't forget to update your database table

--- installer/application/assets/sql/2-default-data.sql
+++ installer/application/assets/sql/2-default-data.sql
@@ -21,6 +21,7 @@ insert into `settings` values
  ('meta_topic','Meta Topic','Two or three words describing this type of company/website.','text','Content Management','','','0','1',''),
  ('currency','Currency','The currency symbol for use on products, services, etc.','text','&pound;','','','1','1',''),
  ('dashboard_rss', 'Dashboard RSS Feed', 'Link to an RSS feed that will be displayed on the dashboard.', 'text', 'http://pyrocms.com/news/rss/all.rss', '', '', 0, 0, ''),
+ ('dashboard_rss_count', 'Dashboard RSS Items', 'How many RSS items would you like to display on the dashboard ? ', 'text', '5', '5', '', 1, 1, ''),
  ('captcha_enabled','Use Captcha','Captcha boxes are used to make sure spammers and other unwanted fake guests do not abuse input forms.','radio','1','','1=Enabled|0=Disabled','0','1',''),
  ('captcha_folder','Captcha Folder','Where should captcha image files be stored?','text','application/cache/captcha/','','','0','1',''),
  ('frontend_enabled','Site Status','Use this option to the user-facing part of the site on or off. Useful when you want to take the site down for maintenence','radio','1','','1=Open|0=Closed','1','1',''),
