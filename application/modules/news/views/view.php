b9bbbafa3f1ede042703ce2fe3dd113585d1e8a0
YorickPeterse <info@yorickpeterse.com>
2009-09-18 20:29:25 +0200

    Redesigned the frontend of the comments. They use my idea of a global module structure to give an idea of how it would look like. I also modified the language files, but they might need to be cleaned up

--- application/modules/news/views/view.php
+++ application/modules/news/views/view.php
@@ -1,27 +1,42 @@
-<? $this->load->helper('typography'); ?>
-<h2><?= $article->title; ?></h2>
-<p>
-	<strong><?=lang('news_posted_label');?>:</strong> <?= date('M d, Y', $article->created_on); ?><br/>		
-	<? if($article->category_slug): ?>
-		<strong><?=lang('news_category_label');?>:</strong> <?=anchor('news/category/'.$article->category_slug, $article->category_title);?>
-	<? endif; ?>
-</p>
-<hr/>
-<?=stripslashes($article->body);?> 
-<hr/>
-<?php if( $this->settings->item('enable_social_bookmarks')): ?>
-<?= $this->load->view('fragments/social_bookmarking/toolbar', array('bookmark' => array('title' => $article->title))); ?>
-<hr/>
-<?php endif; ?>
-
-<h3><?=lang('news_comments_title');?></h3>
-    
-<fieldset class="alternative float-left width-half">
-	<legend><?=lang('news_other_comments_label');?></legend>
-	<?= $this->load->module_view('comments', 'comments', array('comments' => $this->comments_m->getComments(array('module' => $this->module, 'module_id' => $article->id, 'is_active' => 1)))); ?>
-</fieldset>
-                
-<fieldset class="float-right width-half">
-	<legend><?=lang('news_your_comments_label');?></legend>
-	<?= $this->load->module_view('comments', 'form', array('module'=>$this->module, 'id' => $article->id)); ?> 
-</fieldset>
+<?php $this->load->helper('typography'); ?>
+<!-- News module -->
+<div class="module news_module">
+	<!-- Module heading -->
+	<div class="module_heading">
+		<h2><?php echo $article->title; ?></h2>	
+		<?php if($article->category_slug): ?>
+			<p><?php echo lang('news_posted_label'); ?> <?php echo anchor('news/category/'.$article->category_slug, $article->category_title); ?> <?php echo lang('news_date_at'); ?> <?php echo date('M d, Y', $article->created_on); ?></p>
+		<?php else: ?>
+			<p><?php echo lang('news_posted_label_alt'); ?> <?php echo date('M d, Y', $article->created_on); ?></p>
+		<?php endif; ?>
+	</div>
+	<!-- Module body -->
+	<div class="module_body">
+		<?php echo stripslashes($article->body); ?>
+	</div>
+	<!-- Module bottom -->
+	<div class="module_bottom">
+		<?php
+		if( $this->settings->item('enable_social_bookmarks'))
+		{
+			echo $this->load->view('fragments/social_bookmarking/toolbar', array('bookmark' => array('title' => $article->title)));
+		}		
+		?>
+	</div>
+</div>
+<!-- Comments module -->
+<div class="module comments_module">
+	<!-- Module heading -->
+	<div class="module_heading">
+		<h3><?php echo lang('news_comments_title');?></h3>
+	</div>
+	<!-- Module body -->
+	<div class="module_body">
+		<?php echo $this->load->module_view('comments', 'comments', array('comments' => $this->comments_m->getComments(array('module' => $this->module, 'module_id' => $article->id, 'is_active' => 1)))); ?>
+	</div>
+	<!-- Module bottom -->
+	<div class="module_bottom">
+		<h3><?php echo lang('news_your_comments_label');?></h3>
+		<?php echo $this->load->module_view('comments', 'form', array('module'=>$this->module, 'id' => $article->id)); ?> 
+	</div>
+</div>
