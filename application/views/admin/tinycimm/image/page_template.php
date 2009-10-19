002a0cbf6f206e90826e9d7bdc88402aea47b51f
badsyntax <willis.rh@gmail.com>
2009-10-20 00:24:39 +0100

    tinycimm - fixed updating image bug

--- application/views/admin/tinycimm/image/page_template.php
+++ application/views/admin/tinycimm/image/page_template.php
@@ -1,7 +1,7 @@
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
 <tr>
 	<td width="178" valign="top">
-		<?= $this->load->View($this->view_path.'leftpane');?>
+		<?= $this->load->niew($this->view_path.'leftpane');?>
 	</td>
 	<td width="5">&nbsp;</td>
 	<td valign="top">
