cf3396bb89cf8d140dafe5c8fe2a87f51207b04b
YorickPeterse <info@yorickpeterse.com>
2009-10-20 14:44:01 +0200

    Fixed issue #24 : MySQLi checking on installer

--- installer/application/views/step_1.php
+++ installer/application/views/step_1.php
@@ -7,6 +7,7 @@
 	<p><label for="server">Server</label><?php echo form_input('server', $this->session->userdata('server'), 'class="input-text"'); ?></p>
 	<p><label for="username">Username</label><?php echo form_input('username', $this->session->userdata('username'), 'class="input-text"'); ?></p>
 	<p><label for="password">Password</label><?php echo form_password('password', $this->session->userdata('password'), 'class="input-text"'); ?></p>
+	<input type="hidden" name="installation_step" value="step_1" />
 	<p id="next_step"><input type="submit" id="submit" value="Step 2" /></p>
 	<br class="clear" />
 </form>
