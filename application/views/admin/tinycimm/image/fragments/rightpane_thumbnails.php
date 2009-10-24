b693cff4d940cb800f72b102363a58d5c40d6001
badsyntax <willis.rh@gmail.com>
2009-10-24 19:04:02 +0100

    tinycimm - added asset name to thumbnail view

--- application/views/admin/tinycimm/image/fragments/rightpane_thumbnails.php
+++ application/views/admin/tinycimm/image/fragments/rightpane_thumbnails.php
@@ -7,6 +7,8 @@
 				<span class="thumb" onclick="TinyCIMMImage.loadResizer('<?=$image['id'].$image['extension'];?>', event);" style="background:url(<?=$this->config->item('tinycimm_image_controller');?>get/<?=$image['id'];?>/92/92) no-repeat center center;">
 					<span class="loader"></span>
 				</span>
+				<span class="name-bg"></span>
+				<span class="name"><span><?=htmlspecialchars($image['name']);?></span></span>
 				<span class="controls-bg"></span>
 				<span class="controls">
 					<a href="#" title="edit image" class="edit" onclick="TinyCIMMImage.showManager(this, <?=$image['id'];?>);return false;">&nbsp;</a>
