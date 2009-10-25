17c7d1e3e8d93831619695042074d445c944260f
badsyntax <willis.rh@gmail.com>
2009-10-25 14:24:58 +0000

    tinycimm - updated the manager panel

--- application/views/admin/tinycimm/image/manager.php
+++ application/views/admin/tinycimm/image/manager.php
@@ -1,46 +1,39 @@
 <fieldset style="padding:0em 1em">
 	<legend>Manage Image</legend>
 	<input type="hidden" id="image-name" value="<?=$image->name;?>" />
-	<div style="display:none">
-		<b>Image Name:</b>
-		<br/><input id="image_caption" type="text" style="width:160px" value="" disabled="disabled" />
-		<br/>
-	</div>
-	<div style="margin-top:4px;">
-		<p>
+	<div class="left" style="margin-right:20px">
+		<div style="margin-top:4px;">
 			<a id="image-preview-popup" href="<?=$this->config->item('tinycimm_asset_path').$image->id.$image->extension;?>" title="view larger version">
-				<img id="image-preview" src="<?=$this->config->item('tinycimm_image_controller');?>get/<?=$image->id;?>/280/200" />
+				<img id="image-preview" src="<?=$this->config->item('tinycimm_image_controller');?>get/<?=$image->id;?>/270/320" />
 			</a>
+		</div>
+		<?php
+			$image->filesize = $image->filesize/1024;
+		?>
+		<p>
+			<?=$image->width.' x '.$image->height;?>px - <?=($image->filesize > 1024 ? round($image->filesize/1024, 2).'mb' : round($image->filesize).'kb');?>
 		</p>
 	</div>
-	<?php
-		$image->filesize = $image->filesize/1024;
-	?>
-	<p>
-		<strong><?=$image->name;?></strong><br/>
-		<?=$image->width.' x '.$image->height;?>px - <?=($image->filesize > 1024 ? round($image->filesize/1024, 2).'mb' : round($image->filesize).'kb');?>
-	</p>
-	<strong>Image Description:</strong>
-	<p id="alttext-container">
-		<textarea id="image-alttext" rows="4" cols="20"><?=$image->description;?></textarea>
-	</p>
-	<p id="folder-select-list">
-		<strong>Image Folder:</strong><br/>
-		<?=$this->tinycimm->get_folders('select', $image->folder_id);?>
-	</p>
-	<p id="manager-actions">
-		<a href="javascript:;" id="update-image">
-			<img src="img/image_save.png" alt="update" />[Update]
-		</a>&nbsp;
-		<a href="javascript:;" id="download-image" class="hidden">
-			<img src="img/image_restore.png" alt="download" />[Download]
-		</a>
-		<a href="javascript:;" id="delete-image">
-			<img src="img/image_delete.png" alt="delete" />[Delete]
-		</a>&nbsp;
-		<a href="javascript:;" id="insert-image">
-			<img src="img/insertthumb.gif" alt="insert" />[Insert]
-		</a>
-
-	</p>
+	<div class="left">
+		<strong>Filename:</strong>
+		<p>
+			<input type="text" value="<?=$image->filename;?>" />
+		</p>
+		<strong>Image Description:</strong>
+		<p id="alttext-container">
+			<textarea id="image-alttext" rows="4" cols="12"><?=$image->description;?></textarea>
+		</p>
+		<p id="folder-select-list" style="margin-bottom:20px">
+			<strong>Image Folder:</strong><br/>
+			<?=$this->tinycimm->get_folders('select', $image->folder_id);?>
+		</p>
+		<input class="button" value="Update" type="button" id="update-image" />
+		or
+		<select id="manager-actions">
+			<option value="">select..</option>
+			<option value="delete">Delete</option>
+			<option value="insert">Insert</option>
+			<option value="download">Download</option>
+		</select>
+	</div>
 </fieldset>
