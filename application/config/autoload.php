696ff7aff528caa119324816a706a340338959dd
Phil Sturgeon <email@philsturgeon.co.uk>
2009-10-28 22:39:27 +0000

    bug: Pages threw a wobbly on delete and now improved HelpfulParser support. Use page_anchor and page_url to link to your pages by id.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -51,7 +51,7 @@ $autoload['libraries'] = array('database', 'session', 'cache', 'layout', 'settin
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'form', 'language', 'matchbox', 'xhtml', 'asset', 'pagination', 'debug');
+$autoload['helper'] = array('url', 'form', 'language', 'pages' => 'pages', 'matchbox', 'xhtml', 'asset', 'pagination', 'debug');
 
 
 /*
