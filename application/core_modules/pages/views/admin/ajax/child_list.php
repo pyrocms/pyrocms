d4198d82746489ea9f5bd51aed0b97b017929933
badsyntax <willis.rh@gmail.com>
2009-10-31 18:24:38 +0000

    feature: cleaning up persisent tree methods

--- application/core_modules/pages/views/admin/ajax/child_list.php
+++ application/core_modules/pages/views/admin/ajax/child_list.php
@@ -1,7 +1,13 @@
-	
+<?php foreach($pages as $page):?>
+<li>	
 	<span class="<?php echo $page->has_children ? 'folder' : 'file' ?>" onclick="javascript:void(0);">
 		<?php // <a href="<?php echo site_url('admin/pages/edit/'.$page->id); " rel="page-<?php echo $page->id; " onclick="javascript:window.location = this.href;"> ?>
 		<a href="#" rel="page-<?php echo $page->id; ?>">
 			<?php echo $page->title; ?> <small>(#<?php echo $page->id; ?>)</small>
 		</a>
 	</span>
+	<?php if ($page->has_children) :?>
+		<ul></ul>
+	<?php endif;?>
+</li>
+<?php endforeach;?>
