90fe000b620463a3b4b5890e92d6311873b22ab2
Phil Sturgeon <email@philsturgeon.co.uk>
2009-10-31 11:17:46 +0000

    tweak: Got the user form using tabs.

--- application/modules/users/views/admin/form.php
+++ application/modules/users/views/admin/form.php
@@ -9,57 +9,76 @@
 })(jQuery);
 </script>
 
-<? if($this->uri->segment(3,'add') == 'add'): ?>
-	<h2><?= lang('user_add_title');?></h2>
-	
-<? else: ?>
-	<h2><?= sprintf(lang('user_edit_title'), $member->full_name);?></h2>
-<? endif; ?>
-
 <?=form_open($this->uri->uri_string()); ?>
-	<fieldset>
-		<legend><?= lang('user_details_label');?></legend>
+
+	<div class="fieldset fieldsetBlock active tabs">
+		<div class="header">
+			
+		<? if($this->uri->segment(3,'add') == 'add'): ?>
+			<h3><?= lang('user_add_title');?></h3>
+			
+		<? else: ?>
+			<h3><?= sprintf(lang('user_edit_title'), $member->full_name);?></h3>
+		<? endif; ?>
 		
-		<div class="field">
-			<label for="first_name"><?= lang('user_first_name_label');?></label>
-			<?= form_input('first_name', $member->first_name); ?>
 		</div>
 		
-		<div class="field">
-			<label for="first_name"><?= lang('user_last_name_label');?></label>
-			<?= form_input('last_name', $member->last_name); ?>
-		</div>
+		<div class="tabs">
+			
+			<ul class="clear-both">
+				<li><a href="#fieldset1"><span><?=lang('user_details_label');?></span></a></li>
+				<li><a href="#fieldset2"><span><?=lang('user_password_label');?></span></a></li>
+			</ul>
+			
+			<!-- Content tab -->
+			<fieldset id="fieldset1">
+				<legend><?= lang('user_details_label');?></legend>
+				
+				<div class="field">
+					<label for="first_name"><?= lang('user_first_name_label');?></label>
+					<?= form_input('first_name', $member->first_name); ?>
+				</div>
+				
+				<div class="field">
+					<label for="first_name"><?= lang('user_last_name_label');?></label>
+					<?= form_input('last_name', $member->last_name); ?>
+				</div>
+				
+				<div class="field">
+					<label for="email"><?= lang('user_email_label');?></label>
+					<?= form_input('email', $member->email); ?>
+				</div>
+				
+				<div class="field">
+					<label for="active"><?= lang('user_role_label');?></label>
+					<?= form_dropdown('role', $roles, $member->role); ?>
+				</div>
+				
+				<div class="field">
+					<label for="active"><?= lang('user_activate_label');?></label>
+					<?= form_checkbox('is_active', 1, $member->is_active == 1); ?>
+				</div>
 		
-		<div class="field">
-			<label for="email"><?= lang('user_email_label');?></label>
-			<?= form_input('email', $member->email); ?>
-		</div>
-		
-		<div class="field">
-			<label for="active"><?= lang('user_role_label');?></label>
-			<?= form_dropdown('role', $roles, $member->role); ?>
-		</div>
-		
-		<div class="field">
-			<label for="active"><?= lang('user_activate_label');?></label>
-			<?= form_checkbox('is_active', 1, $member->is_active == 1); ?>
-		</div>
-		
-	</fieldset>
+			</fieldset>
 	
-	<fieldset>
-		<legend><?= lang('user_password_label');?></legend>
-	
-		<div class="field">
-			<label for="password"><?= lang('user_password_label');?></label>
-			<?= form_password('password'); ?>
+			<fieldset id="fieldset2">
+				<legend><?= lang('user_password_label');?></legend>
+			
+				<div class="field">
+					<label for="password"><?= lang('user_password_label');?></label>
+					<?= form_password('password'); ?>
+				</div>
+				
+				<div class="field">
+					<label for="confirm_password"><?= lang('user_password_confirm_label');?></label>
+					<?= form_password('confirm_password'); ?>
+				</div>
+				
+			</fieldset>
+			
 		</div>
-		
-		<div class="field">
-			<label for="confirm_password"><?= lang('user_password_confirm_label');?></label>
-			<?= form_password('confirm_password'); ?>
-		</div>
-		
-	</fieldset>
+	</div>
+			
 	<? $this->load->view('admin/fragments/table_buttons', array('buttons' => array('save', 'cancel') )); ?>
+	
 <?=form_close(); ?>
