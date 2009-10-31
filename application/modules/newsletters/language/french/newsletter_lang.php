0b85f8a6b7ea6350f97162c0766bae014a8715e4
Frédéric Quié <frederic.quie@gmail.com>
2009-10-31 13:41:08 +0100

    French Version Added

---
+++ application/modules/newsletters/language/french/newsletter_lang.php
@@ -0,0 +1,38 @@
+<?php
+
+// labels
+$lang['letter_created_on_label'] 							= 'Créé le';
+$lang['letter_sent_on_label'] 								= 'Envoyé le';
+$lang['letter_subject_label'] 								= 'Sujet';
+$lang['letter_created_label'] 								= 'Créé';
+$lang['letter_date_label'] 										= 'Date';
+$lang['letter_actions_label'] 								= 'Actions';
+$lang['letter_not_sent_label'] 								= 'Non envoyé';
+$lang['letter_view_label'] 										= 'Visualiser';
+$lang['letter_send_label'] 										= 'Envouyer';
+$lang['letter_export_subscribers_label'] 			= 'Exporter les abonnés';
+$lang['letter_title_label'] 									= 'Titre';
+$lang['letter_required_label'] 								= 'Requis';
+$lang['letter_email_label'] 									= 'Email';
+$lang['letter_subscripe_label'] 							= 'S\'abonner';
+$lang['letter_back_label'] 										= 'Retour';
+$lang['letter_subject_suffix'] 								= 'Bulletin';
+
+// titles
+$lang['letter_subscriped_title'] 							= 'Abonné';
+$lang['letter_letter_title']									= 'Newsletter';
+$lang['letter_letters_title'] 								= 'Newsletters';
+
+// messages
+$lang['letter_no_letters_error'] 							= 'Il n\'y a pas bulletins.';
+$lang['letter_subscribed_success'] 						= 'Vous êtes maintenant abonné. Vous aurez de nos nouvelles prochainement quand nous vous enverrons la future Newsletter.';
+$lang['letter_subscripe_desc'] 								= 'Abonnez-vous à notre bulletin pour recevoir des emails et autres articles d\'informations utiles.';
+$lang['letter_add_mail_success'] 							= 'Une adresse identique a déjà été enregistrée.';
+$lang['letter_delete_mail_success'] 					= 'Vous avez été enlevé de la liste d\'envoi des bulletins.';
+$lang['letter_delete_mail_error'] 						= 'Une erreur est survenue.';
+$lang['letter_add_success'] 									= 'Le bulletin "%s" a été enregistré.';
+$lang['letter_add_error'] 										= 'Une erreur est survenue.';
+$lang['letter_sent_success'] 									= 'Le bulletin a été envoyé.';
+$lang['letter_sent_error'] 										= 'Une erreur est survenue.';
+
+?>
