ef71acdb77c1c794f7cb4821a6fd1fd1a97155da
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-01 17:54:36 +0000

    Reverted accidental commits made in commit 86a87b22c037b705cc687c4bf363ea92d4390582

--- application/core_modules/modules/language/french/modules_lang.php
+++ application/core_modules/modules/language/french/modules_lang.php
@@ -1,4 +1,3 @@
 <?php
-// TODO: Translate this motherfucker below.
 $lang['modules_welcome'] = 'Ci-desous la liste des modules installés et leur rôle, description ou version.'; #translate
 ?>
---
+++ application/core_modules/modules/language/spanish/modules_lang.php
@@ -0,0 +1,3 @@
+<?php
+$lang['modules_welcome'] = 'Below is a list of installed modules and their information, such as the description or version.'; #translate
+?>
