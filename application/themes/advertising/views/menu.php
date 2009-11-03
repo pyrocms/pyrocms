30e2164b6542e8fa9c135f5eb0fd1bfd798deb5b
YorickPeterse <info@yorickpeterse.com>
2009-11-03 13:17:31 +0100

    Fixed issue #32: Target support in link navigation.

--- application/themes/advertising/views/menu.php
+++ application/themes/advertising/views/menu.php
@@ -2,7 +2,7 @@
 	<h2><?= lang('navigation_headline');?></h2>
 	<ul class="spacer-left-dbl">
 		<? if(!empty($navigation['sidebar'])) foreach($navigation['sidebar'] as $nav_link): ?>
-		<li><?=anchor($nav_link->url, $nav_link->title); ?></li>
+		<li><?=anchor($nav_link->url, $nav_link->title,array('target' => $nav_link->target)); ?></li>
 		<? endforeach; ?>
 	</ul>
 	
