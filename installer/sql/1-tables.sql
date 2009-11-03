3da641f19735a041b4b2755a358954a4cde6e613
YorickPeterse <info@yorickpeterse.com>
2009-11-03 12:37:12 +0100

    Fixed a bug in the installer that would prevent it from inserting the data into the tables and showing a message when it was done. Bug was caused by duplicate table entries in the SQL file and a missing else statement. Thanks to: Jeroen van der Gulik for the patch.

--- installer/sql/1-tables.sql
+++ installer/sql/1-tables.sql
@@ -17,16 +17,6 @@ CREATE TABLE `asset` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Assets used in the wysiwyg image manager';
 
 
-CREATE TABLE `asset_folder` (
-  `id` int(5) NOT NULL auto_increment,
-  `user_id` int(5) NOT NULL default '1',
-  `name` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `smart` int(1) NOT NULL default '0',
-  `dateadded` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Asset folder categories';
-
-
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL auto_increment,
   `slug` varchar(20) collate utf8_unicode_ci NOT NULL default '',
@@ -317,24 +307,6 @@ CREATE TABLE `users` (
   PRIMARY KEY  (`id`),
   KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Registered User Information';
-
-
-CREATE TABLE `asset` (
-  `id` int(5) NOT NULL auto_increment,
-  `folder_id` int(5) NOT NULL default '0',
-  `user_id` int(5) NOT NULL default '1',
-  `name` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `filename` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `description` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `extension` varchar(5) collate utf8_unicode_ci NOT NULL,
-  `mimetype` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `width` int(5) default NULL COMMENT 'Width of type image in pixels',
-  `height` int(5) default NULL COMMENT 'Height of type image in pixels',
-  `filesize` int(11) NOT NULL default '0',
-  `dateadded` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Assets used in the wysiwyg image manager';
-
 
 CREATE TABLE `asset_folder` (
   `id` int(5) NOT NULL auto_increment,
