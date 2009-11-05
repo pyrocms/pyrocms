f7bda8376b734372e835e056a4c9433bf6690b42
badsyntax <willis.rh@gmail.com>
2009-11-05 11:03:11 +0000

    tinycimm bugix: added missing view files

---
+++ application/views/admin/tinycimm/link/dialog.php
@@ -0,0 +1,29 @@
+<div class="tabs">
+	<ul>
+		<li id="browser_tab" class="current">
+			<span>
+				<a href="javascript:TinyCIMMImage.showBrowser();" onmousedown="return false;">
+					Browse
+				</a>
+			</span>
+		</li>
+	</ul>
+</div>
+
+<div id="link_wrapper_panel" class="panel_wrapper">
+
+	<!-- BROWSER PANEL -->
+	<div id="browser_panel" class="panel">
+		<fieldset>
+			<legend>Browser</legend>
+			<div id="filebrowser">
+				<span id="loading">loading</span>
+			</div>
+		</fieldset>
+	</div>
+
+<!-- END PANELS -->
+</div>
+<div id="flash-msg-wrapper">
+	<span id="flash-msg"></span>
+</div>
