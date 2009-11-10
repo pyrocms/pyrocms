42136921807ae2bb7e752ed6f7cb25fdcdbdb573
Nguyễn Đình Trung <nguyendinhtrung141@gmail.com>
2009-11-10 19:01:51 +0700

    - Successfully implement the Permission for User feature. - TODO : Modify Admin_Controller to check for user permission override role permission. I didn't do it yet because I'm not sure where should I do this,     in the permission_m (function hasAdminAccess) or in Admin_Controller.

--- application/core_modules/permissions/js/permissions.js
+++ application/core_modules/permissions/js/permissions.js
@@ -38,52 +38,52 @@
 		});
 		
 	});
+
+	function get_controllers(module, selected) {
+	
+		controller_select = "select[name='controller']";
+		
+		$(controller_select).hide().empty();
+		get_methods();
+		
+		$.getJSON(BASE_URI + "admin/permissions/module_controllers/" + module, function(data){
+	         
+	         $("<option/>").attr("value", '*').text(permControllerSelectDefault).appendTo(controller_select);
+	         
+	         $.each(data, function(i,controller){
+	           if(controller == module) label = controller + ' (default)';
+	           else { label = controller; }
+	           option = $("<option/>").attr("value", controller).text(label);
+	           
+	           // If the current one is the supplied selected method
+	           if(selected == controller) option.attr("selected", "selected");
+	           
+	           option.appendTo(controller_select);
+	         });
+	         
+	         $(controller_select).fadeIn('slow');
+	    });
+	}
+	
+	function get_methods(module, controller, selected) {
+		
+		method_select = "select[name='method']";
+		
+		$(method_select).hide().empty();
+		
+	    $("<option/>").attr("value", '*').text(permMethodSelectDefault).appendTo(method_select);
+	         
+		$.getJSON(BASE_URI + "admin/permissions/controller_methods/" + module + "/" + controller, function(data){
+	         $.each(data, function(i,method){
+	           option = $("<option/>").attr("value", method).text(method);
+				
+				// If the current one is the supplied selected method
+	           if(selected == method) option.attr("selected", "selected");
+	
+	           option.appendTo(method_select);
+	         });
+	         
+	         $(method_select).fadeIn('slow');
+	    });
+}
 })(jQuery);
-
-function get_controllers(module, selected) {
-
-	controller_select = "select[name='controller']";
-	
-	$(controller_select).hide().empty();
-	get_methods();
-	
-	$.getJSON(BASE_URI + "admin/permissions/module_controllers/" + module, function(data){
-         
-         $("<option/>").attr("value", '*').text(permControllerSelectDefault).appendTo(controller_select);
-         
-         $.each(data, function(i,controller){
-           if(controller == module) label = controller + ' (default)';
-           else { label = controller; }
-           option = $("<option/>").attr("value", controller).text(label);
-           
-           // If the current one is the supplied selected method
-           if(selected == controller) option.attr("selected", "selected");
-           
-           option.appendTo(controller_select);
-         });
-         
-         $(controller_select).fadeIn('slow');
-    });
-}
-
-function get_methods(module, controller, selected) {
-	
-	method_select = "select[name='method']";
-	
-	$(method_select).hide().empty();
-	
-    $("<option/>").attr("value", '*').text(permMethodSelectDefault).appendTo(method_select);
-         
-	$.getJSON(BASE_URI + "admin/permissions/controller_methods/" + module + "/" + controller, function(data){
-         $.each(data, function(i,method){
-           option = $("<option/>").attr("value", method).text(method);
-			
-			// If the current one is the supplied selected method
-           if(selected == method) option.attr("selected", "selected");
-
-           option.appendTo(method_select);
-         });
-         
-         $(method_select).fadeIn('slow');
-    });
-}
