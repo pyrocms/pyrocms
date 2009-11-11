d018f1271bb945ec03a6978bc4e74522d748aac5
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-11 20:11:17 +0000

    Fixed up MY_Router for pages support and deleted Matchbox library.

--- application/libraries/MY_Router.php
+++ application/libraries/MY_Router.php
@@ -108,7 +108,49 @@ class MY_Router extends CI_Router
 		}
 		
 		/* not a module controller */
-		return parent::_validate_request($segments);
+			// Does the requested controller exist in the root folder?
+		if (file_exists(APPPATH.'controllers/'.$segments[0].EXT))
+		{
+			return $segments;
+		}
+
+		// Is the controller in a sub-folder?
+		if (is_dir(APPPATH.'controllers/'.$segments[0]))
+		{		
+			// Set the directory and remove it from the segment array
+			$this->set_directory($segments[0]);
+			$segments = array_slice($segments, 1);
+			
+			if (count($segments) > 0)
+			{
+				// Does the requested controller exist in the sub-folder?
+				if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$segments[0].EXT))
+				{
+					show_404($this->fetch_directory().$segments[0]);
+				}
+			}
+			else
+			{
+				$this->set_class($this->default_controller);
+				$this->set_method('index');
+			
+				// Does the default controller exist in the sub-folder?
+				if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$this->default_controller.EXT))
+				{
+					$this->directory = '';
+					return array();
+				}
+			
+			}
+
+			return $segments;
+		}
+		
+		// Not a module or a controller? Must be a page!
+		$this->module = 'pages';
+		$this->directory = '../core_modules/pages/controllers/';
+		
+		return array('pages', 'index', $segments[0]);
 	}
 	/*
 	// Phil magic
