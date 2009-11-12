b1467504a741cc5edc8295754c5601c71dc8d89e
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-12 22:53:42 +0000

    Simplified and fixed news routes.

--- application/modules/news/config/routes.php
+++ application/modules/news/config/routes.php
@@ -20,24 +20,10 @@
 |	http://www.codeigniter.com/user_guide/general/routing.html
 */
 
-$route['news/index'] = "news/index";
-$route['news/admin'] = "news/admin";
-
 $route['news/(:num)/(:num)/(:any)'] = "news/view/$3";
 $route['news/page/(:num)'] = "news/index/$1";
 
-$route['news/rss/index'] = "news/rss";
 $route['news/rss/all.rss'] = "news/rss";
 $route['news/rss/(:any).rss'] = "news/rss/category/$1";
-
-// Category routes
-$route['news/category/(:any)/(:num)'] = "news/category/$1"; // Paginated category routes
-$route['news/category/(:any)'] = "news/category/$1"; // Main category page
-
-// Archive routes
-$route['news/archive/(:num)/(:num)/(:num)'] = "news/archive/$1/$2"; // Paginated pages
-$route['news/archive/(:num)/(:num)'] = "news/archive/$1/$2"; // Main archive pages
-
-$route['news/(:any)'] = "news/view/$1";
 
 ?>
