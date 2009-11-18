d98f0877b1ff6828977605991ae4033c450110e6
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-18 23:00:23 +0000

    application/core_modules/

--- application/core_modules/navigation/config/routes.php
+++ application/core_modules/navigation/config/routes.php
@@ -20,6 +20,6 @@
 |	http://www.codeigniter.com/user_guide/general/routing.html
 */
 
-$route['admin/navigation/groups(:any)'] = "navigation/groups$1";
+$route['navigation/admin/groups/(:any)'] = 'groups/$1';
 
 ?>
