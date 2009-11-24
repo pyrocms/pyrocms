337a814b4a21ed4b9c9773863f63b59af5506e6a
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-24 23:37:46 +0000

    Added German language translations. PyroCMS translation provided by @the_loops.

---
+++ application/core_modules/navigation/language/german/navigation_lang.php
@@ -0,0 +1,46 @@
+<?php
+
+// labels
+$lang['nav_note_label'] 						= 'Notiz';
+$lang['nav_title_label'] 						= 'Titel';
+$lang['nav_position_label'] 					= 'Position';
+$lang['nav_target_label']						= 'Ziel'; #translate
+$lang['nav_url_label'] 							= 'URL';
+$lang['nav_actions_label'] 						= 'Aktionen';
+$lang['nav_details_label'] 						= 'Details';
+$lang['nav_text_label'] 						= 'Text';
+$lang['nav_group_label'] 						= 'Gruppe';
+$lang['nav_location_label'] 					= 'Ort';
+$lang['nav_uri_label'] 							= 'Seiten-Link (URI)';
+$lang['nav_page_label'] 						= 'Seite';
+$lang['nav_module_label'] 						= 'Modul';
+$lang['nav_abbrev_label'] 						= 'Abkürzung';
+$lang['nav_edit_label'] 						= 'Bearbeiten';
+$lang['nav_delete_label'] 						= 'Löschen';
+$lang['nav_group_delete_label']					= 'Gruppe "%s" löschen';
+$lang['nav_group_add_label']					= 'Gruppe hinzufügen';
+$lang['nav_link_module_select_default'] 		= '-- Auswählen --';
+$lang['nav_link_page_select_default'] 			= '-- Auswählen --';
+
+// titles
+$lang['nav_link_create_title'] 					= 'Navigations-Link hinzufügen';
+$lang['nav_link_edit_title'] 					= 'Navigations-Link "%s" bearbeiten';
+
+// messages
+$lang['nav_group_note'] 						= 'Navigations-Gruppen werden z.Z. in den Layout-Dateien eingetragen und können nicht dynamisch geändert werden. Du kannst sie hinzufügen oder löschen, musst jedoch den Quellcode von Hand anpassen um die neue Gruppe auch auf deiner Seite anzeigen zu lassen.';
+$lang['nav_group_no_links'] 					= 'Es existieren keine Links in dieser Gruppe.';
+$lang['nav_no_groups'] 							= 'Es existieren keine Navigations-Gruppen.';
+$lang['nav_group_delete_confirm'] 				= 'Diese Gruppe wirklich löschen? Dies wird alle Navigation-Links innerhalb der Gruppe löschen und die Layout-Dateien werden entsprechend angepasst werden müssen.';
+$lang['nav_group_create_title'] 				= 'Neue Navigation-Gruppe';
+$lang['nav_group_add_success'] 					= 'Navigation-Gruppe wurde gesichert.';
+$lang['nav_group_add_error'] 					= 'Ein Fehler ist aufgetreten.';
+$lang['nav_group_mass_delete_success'] 			= 'Navigation-Gruppe wurde gelöscht.';
+$lang['nav_link_add_success'] 					= 'Navigation-Link wurde hinzugefügt.';
+$lang['nav_link_add_error'] 					= 'Ein Fehler ist aufgetreten.';
+$lang['nav_link_not_exist_error'] 				= 'Navigation-Link existiert nicht.';
+$lang['nav_link_edit_success'] 					= 'Navigation-Link wurde gesichert.';
+$lang['nav_link_delete_success'] 				= 'Navigation-Link wurde gelöscht.';
+
+$lang['nav_link_type_desc'] 					= 'Bitte wähle <strong>einen</strong> Link-Typen.';
+
+?>
