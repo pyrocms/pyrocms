d4e0f355ac6054382b2ea3f1c4fc62628aa60b07
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-25 20:41:51 +0000

    Removed utilities module, that was the module for my random play and as no real purpose anymore.

--- application/modules/utilities/views/sneak/frameset.php
+++
@@ -1,8 +0,0 @@
-<html>
-	
-	<frameset rows="10%,90%">
-	  <frame name="topnav" src="<?=site_url('utilities/sneak/topnav/'.$url) ?>" />
-	  <frame name="fetch" src="<?=site_url('utilities/sneak/fetch/'.$url) ?>" />
-	</frameset>
-	
-</html>
