d3ec123a5eda81be107ee93a4f95be5d2d2c6a3c
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-25 21:19:57 +0000

    Renamed navigation roles admin and permission group admin controllers to have more logical names.

--- application/core_modules/navigation/controllers/admin_groups.php
+++ application/core_modules/navigation/controllers/admin_groups.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Groups extends Admin_Controller
+class Admin_groups extends Admin_Controller
 {
 	function __construct()
 	{
