c894745fd77f977b18a4a11f936848a98db36db0
Phil Sturgeon <email@philsturgeon.co.uk>
2009-11-27 07:29:30 +0000

    missed a commit. @badsyntax, I have possibly broken persistent tree from working, but it had a weird double view which should just be the one.

--- application/core_modules/pages/views/admin/index.php
+++ application/core_modules/pages/views/admin/index.php
@@ -3,11 +3,11 @@
 <?php echo js('index.js', 'pages'); ?>
 <?php echo css('index.css', 'pages'); ?>
 
-<p class="float-right">[ <?php echo anchor('admin/pages/layouts', lang('page')); ?> ]</p>		
+<p class="float-right">[ <?php echo anchor('admin/pages/layouts', lang('page_layouts_heading')); ?> ]</p>		
 
 <br class="clear-both" />
 
-<div id="page-tree" class="float-left" style=" width:49%;">
+<div id="page-tree" class="float-left" style="width:49%;">
 	<ul class="filetree">
 		<?php echo $page_tree_html; ?>  
 	</ul>
