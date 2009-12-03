17cd01b732728d7f61c550c20799ddd96fe9a56a
Phil Sturgeon <email@philsturgeon.co.uk>
2009-12-03 22:34:17 +0000

    Removed non-core modules. These are being moved to http://github.com/philsturgeon/pyrocms-extras.

--- application/modules/services/views/index.php
+++
@@ -1,8 +0,0 @@
-<h2><?php echo lang('service_title');?></h2>
-<?php if ($services): ?>		
-	<?php foreach ($services as $service): ?>
-		<p><?php echo anchor('services/view/' . $service->slug, $service->title);?><br /><?php echo word_limiter($service->description, 30);?></p>
-	<?php endforeach; ?>
-<?php else: ?>
-	<p><?php echo lang('service_no_offered_services');?></p>
-<?php endif; ?>
