fce90b89118e56902a804b1b3423c9ae1ddd4791
Phil Sturgeon <email@philsturgeon.co.uk>
2009-12-03 22:34:17 +0000

    Removed non-core modules. These are being moved to http://github.com/philsturgeon/pyrocms-extras.

--- application/modules/services/views/view.php
+++
@@ -1,3 +0,0 @@
-<h2><?php echo $service->title; ?></h2>
-<strong><?php echo lang('service_price_label');?>: <?php echo sprintf(lang('service_price_format'), $this->settings->item('currency'), $service->price, $service->pay_per);?></strong>
-<p><?php echo $service->description; ?></p>
