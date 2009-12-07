afca9daf95bb4260f8669ef36d3751fb06973697
Phil Sturgeon <email@philsturgeon.co.uk>
2009-12-07 16:23:21 +0000

    Tagged release v0.9.7.3.

--- application/config/constants.php
+++ application/config/constants.php
@@ -111,7 +111,7 @@ unset($base_uri, $base_url);
 |
 */
 
-define('CMS_VERSION', '0.9.7.3-dev');
+define('CMS_VERSION', '0.9.7.3');
 
 
 /*
@@ -123,7 +123,7 @@ define('CMS_VERSION', '0.9.7.3-dev');
 |
 */
 
-define('CMS_DATE','29/11/2009');
+define('CMS_DATE','07/12/2009');
 
 /* End of file constants.php */
 /* Location: ./system/application/config/constants.php */
