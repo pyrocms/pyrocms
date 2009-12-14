52e73fb3aa4b4ce94942355177476bb66376e6bf
Phil Sturgeon <email@philsturgeon.co.uk>
2009-12-14 22:05:32 +0000

    Fixed  busted merge.

--- application/modules/third_party/news/views/view.php
+++ application/modules/third_party/news/views/view.php
@@ -18,7 +18,7 @@ if( $this->settings->item('enable_social_bookmarks'))
 <div class="comments">
 	<!-- Module heading -->
 	<h3><?php echo lang('news_comments_title');?></h3>
-	<?php echo $this->load->view('comments/comments', array('comments' => $this->comments_m->get(array('module' => $this->module, 'module_id' => $article->id, 'is_active' => 1)))); ?>
+	<?php echo $this->load->view('comments/comments', array('comments' => $this->comments_m->get_comments(array('module' => $this->module, 'module_id' => $article->id, 'is_active' => 1)))); ?>
 	
 	<h3><?php echo lang('news_your_comments_label');?></h3>
 	<?php echo $this->load->view('comments/form', array('module'=>$this->module, 'id' => $article->id)); ?> 
