e0758dc97a3a2af03112b6d0effde69f90a91519
Phil Sturgeon <email@philsturgeon.co.uk>
2010-01-17 21:09:29 +0000

    Updated newsletter module for v0.9.8. This module still sucks, would love some work to be done if anyone is interested.

--- application/modules/third_party/newsletters/language/french/newsletter_lang.php
+++ application/modules/third_party/newsletters/language/french/newsletter_lang.php
@@ -1,29 +1,29 @@
 <?php
 
 // labels
-$lang['letter_created_on_label'] 					= 'Créé le';
-$lang['letter_sent_on_label'] 						= 'Envoyé le';
-$lang['letter_subject_label'] 						= 'Sujet';
-$lang['letter_created_label'] 						= 'Créé';
-$lang['letter_date_label'] 							= 'Date';
-$lang['letter_actions_label'] 						= 'Actions';
-$lang['letter_not_sent_label'] 						= 'Non envoyé';
-$lang['letter_view_label'] 							= 'Visualiser';
-$lang['letter_send_label'] 							= 'Envouyer';
-$lang['letter_export_subscribers_label'] 			= 'Exporter les abonnés';
+$lang['newsletter.sent'] 							= 'Envoyé le';
+$lang['newsletter.subject'] 						= 'Sujet';
+$lang['newsletter.created'] 						= 'Créé';
+$lang['newsletter.date'] 							= 'Date';
+$lang['newsletter.actions'] 						= 'Actions';
+$lang['newsletter.not_sent_label'] 					= 'Non envoyé';
+$lang['newsletter.view'] 							= 'Visualiser';
+$lang['newsletters.send'] 							= 'Envouyer';
+$lang['newsletters.export_title'] 					= 'Exporter les abonnés';
 $lang['letter_title_label'] 						= 'Titre';
 $lang['letter_email_label'] 						= 'Email';
-$lang['letter_subscripe_label'] 					= 'S\'abonner';
+$lang['newsletters.subscribe'] 						= 'S\'abonner';
 $lang['letter_back_label'] 							= 'Retour';
 $lang['letter_subject_suffix'] 						= 'Bulletin';
 
 // titles
 $lang['letter_subscriped_title'] 					= 'Abonné';
 $lang['letter_letter_title']						= 'Newsletter';
-$lang['letter_letters_title'] 						= 'Newsletters';
+$lang['newsletters.add_title']						= 'Create newsletter'; #translate
+$lang['newsletters.list_title'] 					= 'List newsletters'; #translate
 
 // messages
-$lang['letter_no_letters_error'] 					= 'Il n\'y a pas bulletins.';
+$lang['newsletter.no_newsletters_error'] 					= 'Il n\'y a pas bulletins.';
 $lang['letter_subscribed_success'] 					= 'Vous êtes maintenant abonné. Vous aurez de nos nouvelles prochainement quand nous vous enverrons la future Newsletter.';
 $lang['letter_subscripe_desc'] 						= 'Abonnez-vous à notre bulletin pour recevoir des emails et autres articles d\'informations utiles.';
 $lang['letter_add_mail_success'] 					= 'Une adresse identique a déjà été enregistrée.';
--- application/modules/third_party/newsletters/views/admin/view.php
+++ application/modules/third_party/newsletters/views/admin/view.php
@@ -1,10 +1,18 @@
-<h3><?php echo $newsletter->title; ?></h3>
-<p>
-	<em><?php echo lang('letter_created_on_label');?>: <?php echo date('M d, Y', $newsletter->created_on); ?></em>	
-	<?php if($newsletter->sent_on): ?>
-		<br />
-		<em><?php echo lang('letter_sent_on_label');?>: <?php echo date('M d, Y', $newsletter->sent_on); ?></em>
-	<?php endif; ?>
-</p>
-<p><?php echo $newsletter->body; ?></p>
-<p><?php echo anchor('admin/newsletters/index', lang('letter_back_label'));?></p>
+<div class="box">
+	
+	<h3><?php echo $newsletter->title; ?></h3>
+	
+	<div class="box-container">
+		<p>
+			<em><?php echo lang('newsletter.created');?>: <?php echo date('M d, Y', $newsletter->created_on); ?></em>	
+			<?php if($newsletter->sent_on): ?>
+				<br />
+				<em><?php echo lang('newsletter.sent');?>: <?php echo date('M d, Y', $newsletter->sent_on); ?></em>
+			<?php endif; ?>
+		</p>
+		
+		<hr />
+		
+		<?php echo $newsletter->body; ?>
+	</div>
+</div>
