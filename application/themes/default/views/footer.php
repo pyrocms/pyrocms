5e23e0748207801e4e37244c2db84d26a6d331a9
Phil Sturgeon <email@philsturgeon.co.uk>
2010-01-28 21:00:53 +0000

    Upgraded to Google Analytics Async.

--- application/themes/default/views/footer.php
+++ application/themes/default/views/footer.php
@@ -7,8 +7,3 @@
 	
 	&copy;2008 - <?php echo date('Y');?> by <?php echo $this->settings->item('site_name'); ?>. All Rights Reserved. &nbsp;&bull;&nbsp; Designed by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.
 </p>
-
-<?php // Google Tracker ?>
-<?php if($this->settings->item('google_analytic')): ?>
-	<?php $this->load->view('fragments/google_analytic'); ?>
-<?php endif; ?>
