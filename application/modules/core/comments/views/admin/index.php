e1a9107c0bad68be6779ac0433e37be908eddb77
Phil Sturgeon <phil@philbook-pro.home>
2010-02-02 20:52:33 +0000

    Tidied category admin index when there are no categories.

--- application/modules/core/comments/views/admin/index.php
+++ application/modules/core/comments/views/admin/index.php
@@ -9,71 +9,71 @@
 	<?php endif; ?>
 	
 	<div class="box-container">
-	
-	<?php echo form_open('admin/comments/action');?>
-		<?php echo form_hidden('redirect', $this->uri->uri_string()); ?> 
-		<table border="0" class="table-list clear-both">    
-			<thead>
-				<tr>
-					<th><?php echo form_checkbox('action_to_all');?></th>
-					<th class="width-20"><?php echo lang('comments.teaser_label');?></th>
-					<th class="width-10"><?php echo lang('comments.author_label');?></th>
-					<th><?php echo lang('comments.date_label');?></th>
-					<th><?php echo lang('comments.actions_label');?></th>
-				</tr>
-			</thead>
-			<tfoot>
-				<tr>
-					<td colspan="6">
-						<div class="inner"><?php $this->load->view('admin/partials/pagination'); ?></div>
-					</td>
-				</tr>
-			</tfoot>
-			<tbody>
-			<?php if (!empty($comments)): ?>
-					<?php foreach ($comments as $comment): ?>
+		
+		<?php if (!empty($comments)): ?>
+					
+			<?php echo form_open('admin/comments/action');?>
+				<?php echo form_hidden('redirect', $this->uri->uri_string()); ?> 
+				<table border="0" class="table-list clear-both">    
+					<thead>
 						<tr>
-							<td><?php echo form_checkbox('action_to[]', $comment->id);?></td>
-							<td>
-								<?php if( strlen($comment->body) > 30 ): ?>
-									<?php echo character_limiter($comment->body, 30); ?>...
-								<?php else: ?>
-									<?php echo $comment->body; ?>
-								<?php endif; ?>
-							</td>
-							<td>
-								<?php if($comment->user_id > 0): ?>
-									<?php echo anchor('admin/users/edit/' . $comment->user_id, $comment->name); ?>
-								<?php else: ?>
-									<?php echo $comment->name;?>
-								<?php endif; ?>
-							</td>
-							<td><?php echo date('M d, Y', $comment->created_on);?></td>						
-							<td>
-								<?php echo anchor('admin/comments/preview/'. $comment->id, lang('comments.preview_label'), 'rel="modal" target="_blank"'); ?> | 
-								<?php if($comment->is_active == 0): ?>
-									<?php echo anchor('admin/comments/approve/' . $comment->id, lang('comments.activate_label'),array('class' => 'ajax'));?>
-								<?php else: ?>
-									<?php echo anchor('admin/comments/unapprove/' . $comment->id, lang('comments.deactivate_label'),array('class' => 'ajax'));?>
-								<?php endif; ?> | 
-								<?php echo anchor('admin/comments/edit/' . $comment->id, lang('comments.edit_label'));?> | 
-								<?php echo anchor('admin/comments/delete/' . $comment->id, lang('comments.delete_label'), array('class'=>'confirm')); ?>
+							<th><?php echo form_checkbox('action_to_all');?></th>
+							<th class="width-20"><?php echo lang('comments.teaser_label');?></th>
+							<th class="width-10"><?php echo lang('comments.author_label');?></th>
+							<th><?php echo lang('comments.date_label');?></th>
+							<th><?php echo lang('comments.actions_label');?></th>
+						</tr>
+					</thead>
+					<tfoot>
+						<tr>
+							<td colspan="6">
+								<div class="inner"><?php $this->load->view('admin/partials/pagination'); ?></div>
 							</td>
 						</tr>
-				<?php endforeach; ?>
-			<?php else: ?>
-					<tr>
-						<td colspan="6"><?php echo lang('comments.no_comments');?></td>
-					</tr>
-			<?php endif; ?>
-			</tbody>	
-		</table>
-		
-		<?php if( $method == 'index' ): ?>
-		<?php $this->load->view('admin/partials/buttons', array('buttons' => array('approve','delete'))); ?>
+					</tfoot>
+					<tbody>
+							<?php foreach ($comments as $comment): ?>
+								<tr>
+									<td><?php echo form_checkbox('action_to[]', $comment->id);?></td>
+									<td>
+										<?php if( strlen($comment->body) > 30 ): ?>
+											<?php echo character_limiter($comment->body, 30); ?>...
+										<?php else: ?>
+											<?php echo $comment->body; ?>
+										<?php endif; ?>
+									</td>
+									<td>
+										<?php if($comment->user_id > 0): ?>
+											<?php echo anchor('admin/users/edit/' . $comment->user_id, $comment->name); ?>
+										<?php else: ?>
+											<?php echo $comment->name;?>
+										<?php endif; ?>
+									</td>
+									<td><?php echo date('M d, Y', $comment->created_on);?></td>						
+									<td>
+										<?php echo anchor('admin/comments/preview/'. $comment->id, lang('comments.preview_label'), 'rel="modal" target="_blank"'); ?> | 
+										<?php if($comment->is_active == 0): ?>
+											<?php echo anchor('admin/comments/approve/' . $comment->id, lang('comments.activate_label'),array('class' => 'ajax'));?>
+										<?php else: ?>
+											<?php echo anchor('admin/comments/unapprove/' . $comment->id, lang('comments.deactivate_label'),array('class' => 'ajax'));?>
+										<?php endif; ?> | 
+										<?php echo anchor('admin/comments/edit/' . $comment->id, lang('comments.edit_label'));?> | 
+										<?php echo anchor('admin/comments/delete/' . $comment->id, lang('comments.delete_label'), array('class'=>'confirm')); ?>
+									</td>
+								</tr>
+						<?php endforeach; ?>
+					</tbody>	
+				</table>
+				
+				<?php if( $method == 'index' ): ?>
+				<?php $this->load->view('admin/partials/buttons', array('buttons' => array('approve','delete'))); ?>
+				<?php else: ?>
+				<?php $this->load->view('admin/partials/buttons', array('buttons' => array('unapprove','delete'))); ?>
+				<?php endif; ?>
+			<?php echo form_close();?>
+	
 		<?php else: ?>
-		<?php $this->load->view('admin/partials/buttons', array('buttons' => array('unapprove','delete'))); ?>
+			<p><?php echo lang('comments.no_comments');?></p>
 		<?php endif; ?>
-	<?php echo form_close();?>
 	</div>
 </div>
