00db377bba5020e5cad3875ba7ec5c474a79478c
Phil Sturgeon <phil@philbook-pro.local>
2010-02-03 14:54:42 +0000

    Fixed up the last broken part of photos frontend.

--- application/modules/third_party/photos/views/index.php
+++ application/modules/third_party/photos/views/index.php
@@ -1,15 +1,12 @@
 <h2><?php echo lang('photos.title');?></h2>
 
 <?php if (!empty($photo_albums)): ?>	
-	<ul class="photos">
-		<?php foreach ($photos as $gallery): ?>
-			<li<?php echo $gallery->slug == 'home' ? 'class="box-hidden"' : '' ?>>
-				<?php echo anchor('galleries/' . $gallery->slug, $gallery->title);?><br />
-				<?php echo $gallery->description; ?><br />
-				<?php echo $this->galleries_m->galleryPhotosList($gallery->slug);?>
-			</li>
+	<dl class="photos">
+		<?php foreach ($photo_albums as $album): ?>
+			<dt><?php echo anchor('photos/' . $album->slug, $album->title);?></dt>
+			<dd><?php echo $album->description; ?></dd>
 		<?php endforeach; ?>
-	</ul>
+	</dl>
 <?php else: ?>
 	<p><?php echo lang('gal_currently_no_photos_error');?></p>
 <?php endif; ?>
