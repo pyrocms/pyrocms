4bdaae192d95e0d4a18b74d11b1d1629ffff7848
Phil Sturgeon <phil@philbook-pro.local>
2010-02-03 09:23:40 +0000

    Tagged release v0.9.8-beta2.

--- application/config/constants.php
+++ application/config/constants.php
@@ -111,7 +111,7 @@ unset($base_uri, $base_url);
 |
 */
 
-define('CMS_VERSION', '0.9.8-beta1');
+define('CMS_VERSION', '0.9.8-beta2');
 
 
 /*
@@ -123,7 +123,7 @@ define('CMS_VERSION', '0.9.8-beta1');
 |
 */
 
-define('CMS_DATE','24/01/2010');
+define('CMS_DATE','03/02/2010');
 
 /* End of file constants.php */
 /* Location: ./system/application/config/constants.php */
