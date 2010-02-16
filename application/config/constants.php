a81d3d83cd0f900d6be74ea3d5535ea134f11bf0
philsturgeon <email@philsturgeon.co.uk>
2010-02-16 21:14:38 +0000

    Bumped version numbers for current master. Next release is a final folks!

--- application/config/constants.php
+++ application/config/constants.php
@@ -111,7 +111,7 @@ unset($base_uri, $base_url);
 |
 */
 
-define('CMS_VERSION', '0.9.8-beta2');
+define('CMS_VERSION', '0.9.8-prefinal');
 
 
 /*
@@ -123,7 +123,7 @@ define('CMS_VERSION', '0.9.8-beta2');
 |
 */
 
-define('CMS_DATE','03/02/2010');
+define('CMS_DATE','15/02/2010');
 
 /* End of file constants.php */
 /* Location: ./system/application/config/constants.php */
