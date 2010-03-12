237a66b63ea54a6d923679ec46597b5cc18c0b62
philsturgeon <email@philsturgeon.co.uk>
2010-03-12 09:27:47 +0000

    Removed refferences to scaffolding.

--- application/config/routes.php
+++ application/config/routes.php
@@ -31,17 +31,9 @@
 | URI contains no data. In the above example, the "welcome" class
 | would be loaded.
 |
-|	$route['scaffolding_trigger'] = 'scaffolding';
-|
-| This route lets you set a "secret" word that will trigger the
-| scaffolding feature for added security. Note: Scaffolding must be
-| enabled in the controller in which you intend to use it.   The reserved 
-| routes must come before any wildcard or regular expression routes.
-|
 */
 
 $route['default_controller'] = 'pages';
-$route['scaffolding_trigger'] = '';
 
 $route['admin/([a-zA-Z_-]+)/(:any)'] = "$1/admin/$2";
 $route['admin/(login|logout)'] = "admin/$1";
