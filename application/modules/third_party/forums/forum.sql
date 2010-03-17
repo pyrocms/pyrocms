3a50ba0be2b14f65072e01b8a6211412844d794f
philsturgeon <email@philsturgeon.co.uk>
2010-03-17 23:29:01 +0000

    Updated forum schema.

--- application/modules/third_party/forums/forum.sql
+++ application/modules/third_party/forums/forum.sql
@@ -1,56 +1,63 @@
-CREATE TABLE `ForumCategories` (
-  `categoryID` int(11) NOT NULL auto_increment,
-  `category_name` varchar(100) NOT NULL default '',
-  `permission` mediumint(2) NOT NULL default '0',
-  PRIMARY KEY  (`categoryID`)
-) ENGINE=MyISAM  COMMENT='Splits forums into categories';
+-- ----------------------------
+--  Table structure for `forum_categories`
+-- ----------------------------
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `forum_categories`;
 
--- 
--- Table structure for table `Forums`
--- 
+CREATE TABLE `forum_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
+  `permission` mediumint(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Splits forums into categories';
 
-CREATE TABLE `Forums` (
-  `forumID` int(11) NOT NULL auto_increment,
-  `forum_name` varchar(100) NOT NULL default '',
-  `forum_description` varchar(255) NOT NULL default '',
-  `categoryID` int(11) NOT NULL default '0',
-  `permission` int(2) NOT NULL default '0',
-  PRIMARY KEY  (`forumID`)
-) ENGINE=MyISAM  COMMENT='Forums are the containers for threads and topics.';
+-- ----------------------------
+--  Table structure for `forum_posts`
+-- ----------------------------
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `forum_posts`;
 
--- 
--- Table structure for table `ForumPosts`
--- 
+CREATE TABLE `forum_posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `forum_id` int(11) NOT NULL DEFAULT '0',
+  `author_id` int(11) NOT NULL DEFAULT '0',
+  `parent_id` int(11) NOT NULL DEFAULT '0',
+  `title` varchar(100) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `content` text CHARACTER SET utf8 NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT '0',
+  `is_locked` tinyint(1) NOT NULL DEFAULT '0',
+  `is_hidden` tinyint(1) NOT NULL DEFAULT '0',
+  `created_on` int(11) NOT NULL DEFAULT '0',
+  `updated_on` int(11) NOT NULL DEFAULT '0',
+  `view_count` int(11) NOT NULL DEFAULT '0',
+  `smileys_enabled` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `ForumPosts` (
-  `postID` int(11) NOT NULL auto_increment,
-  `forumID` int(11) NOT NULL default '0',
-  `authorID` int(11) NOT NULL default '0',
-  `parentID` int(11) NOT NULL default '0',
-  `post_title` varchar(100) character set latin1 NOT NULL default '',
-  `post_text` text character set latin1 NOT NULL,
-  `post_type` tinyint(1) NOT NULL default '0',
-  `post_locked` tinyint(1) NOT NULL default '0',
-  `post_hidden` tinyint(1) NOT NULL default '0',
-  `post_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `post_viewcount` int(11) NOT NULL default '0',
-  `smileys` tinyint(1) NOT NULL default '1',
-  PRIMARY KEY  (`postID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+-- ----------------------------
+--  Table structure for `forum_subscriptions`
+-- ----------------------------
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `forum_subscriptions`;
 
--- 
--- Table structure for table `ForumSubscriptions`
--- 
+CREATE TABLE `forum_subscriptions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `ForumSubscriptions` (
-  `id` int(11) NOT NULL auto_increment,
-  `topicID` int(11) NOT NULL default '0',
-  `userID` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+-- ----------------------------
+--  Table structure for `forums`
+-- ----------------------------
+
+DROP TABLE IF EXISTS `forums`;
+
+CREATE TABLE `forums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `category_id` int(11) NOT NULL DEFAULT '0',
+  `permission` int(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Forums are the containers for threads and topics.';
