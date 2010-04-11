7f5a5002c1c2f7cceaf985556476120d9c82d14b
philsturgeon <email@philsturgeon.co.uk>
2010-04-11 13:30:08 +0100

    Removed a bunch of crap from the CrytsalX metadata, should work with print now too.

--- third_party/themes/CrystalX/views/metadata.php
+++ third_party/themes/CrystalX/views/metadata.php
@@ -1,17 +1,11 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta http-equiv="content-language" content="cs" />
-<meta name="author" content="All: ... [Nazev webu - www.url.cz]; e-mail: info@url.cz" />
-<meta name="copyright" content="Design/Code: Vit Dlouhy [Nuvio - www.nuvio.cz]; e-mail: vit.dlouhy@nuvio.cz" />
-
-<link rel="index" href="./" title="Home" />
-<link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
-<link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
-<link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
 	
 <title>{$template.title} | {$ci->settings->item('site_name')}</title>
 
 {css('style.css')}
 {theme_css('main.css')}
+{theme_css('print.css', 'rel="print"')}
+{theme_css('aural.css', 'rel="aural"')}
 
 {js('jquery/jquery.js')}
 {js('jquery/jquery.fancybox.js')}
