4f0f0da9fa1a2e0896771516e476cb6a4d08fb4e
Dan Horrigan <dan@dhorrigan.com>
2010-04-28 19:48:49 -0400

    Added a bunch of comments.  Added visibility modifiers to all the admin controller functions.

--- third_party/modules/forums/config/forums.php
+++ third_party/modules/forums/config/forums.php
@@ -1,4 +1,11 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-//The title of the forums
+/*
+|--------------------------------------------------------------------------
+| Forums Title
+|--------------------------------------------------------------------------
+|
+| This will (be default) be visable on all the forum pages.
+|
+*/
 $config['forums_title'] = 'Forums';
