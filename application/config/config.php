edd459a177503ef1a6c82dad88d8a3722d98a2d5
Dan Horrigan <dan@dhorrigan.com>
2010-04-28 09:21:22 -0400

    Added cookie_domain to config file.

--- application/config/config.php
+++ application/config/config.php
@@ -239,8 +239,8 @@ $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update'] 		= 300;
-
+$config['sess_time_to_update']	= 300;
+$config['cookie_domain']		= preg_replace("/^www\./", "", $_SERVER['HTTP_HOST']);
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
