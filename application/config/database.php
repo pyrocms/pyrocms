9076c9a5eeef478f24eac2828ff7be067649db9c
Dan Horrigan <dan@dhorrigan.com>
2010-04-30 16:41:20 -0400

    Restoring original database config.  oops.

--- application/config/database.php
+++ application/config/database.php
@@ -34,8 +34,8 @@
 // Local
 $db['local']['hostname'] = "localhost";
 $db['local']['username'] = "root";
-$db['local']['password'] = "root";
-$db['local']['database'] = "pyro_dev";
+$db['local']['password'] = "";
+$db['local']['database'] = "pyrocms";
 $db['local']['dbdriver'] = "mysql";
 $db['local']['dbprefix'] = "";
 $db['local']['active_r'] = TRUE;
