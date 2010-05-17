daee8fb237bab38276d20a2b7bb81a67904927ba
Dan Horrigan <dan@dhorrigan.com>
2010-05-16 21:25:21 -0400

    Redid Login box HTML code to allow easier styling.

--- application/modules/users/views/login.php
+++ application/modules/users/views/login.php
@@ -5,23 +5,24 @@
 <?php if (validation_errors()): ?>
 	<div class="error-box"><?php echo validation_errors();?></div>
 <?php endif; ?>
-
-<p>
-	<label for="email"><?php echo lang('user_email')?></label>
-	<input type="text" name="email" maxlength="120" value="<?php echo $user_data->email; ?>" />
-</p>
-
-<p>
-	<label for="password"><?php echo lang('user_password')?></label>
-	<input type="password" name="password" maxlength="20" value="<?php echo $user_data->password; ?>" />
-</p>
-
-<p>
-	<label for="remember"><?php echo lang('user_remember')?></label>
-	<?php echo form_checkbox('remember', '1', FALSE); ?>
-</p>
-
-<input type="submit" value="<?php echo lang('user_login_btn') ?>" name="btnLogin" />
+<ul id="login">
+	<li>
+		<label for="email"><?php echo lang('user_email'); ?></label>
+		<input type="text" id="email" name="email" maxlength="120" />
+	</li>
+	<li>
+		<label for="password"><?php echo lang('user_password'); ?></label>
+		<input type="password" id="password" name="password" maxlength="20" />
+	</li>
+	<li id="remember-me">
+		<?php echo form_checkbox('remember', '1', FALSE); ?>
+		<label for="remember"><?php echo lang('user_remember')?></label>
+	</li>
+	<li class="form-buttons">
+		<label for="nothing">&nbsp;</label>
+		<input type="submit" value="<?php echo lang('user_login_btn') ?>" name="btnLogin" /> or <?php echo anchor('register', lang('user_register_btn'));?>
+	</li>
+</ul>
 	
 <?php echo form_close(); ?>
 
--- application/modules/users/views/login_small.php
+++ application/modules/users/views/login_small.php
@@ -1,21 +1,21 @@
+<h2><?php echo lang('user_login_header') ?></h2>
 <?php echo form_open('users/login', array('id'=>'login_small')); ?>
-
-<div class="float-left" style="width:20em">
-	<label for="email"><?php echo lang('user_email'); ?></label>
-	<input type="text" id="email" name="email" maxlength="120" />
-	
-	<div class="spacer-bottom"></div>
-	
-	<label for="password"><?php echo lang('user_password'); ?></label>
-	<input type="password" id="password" name="password" maxlength="20" />
-	
-	<label for="remember"><?php echo lang('user_remember')?></label>
-	<?php echo form_checkbox('remember', '1', FALSE); ?>
-</div>
-
-<div class="float-right align-center spacer-left">
-	<input type="submit" value="<?php echo lang('user_login_btn') ?>" name="btnLogin" /><br />
-	or <?php echo anchor('register', lang('user_register_btn'));?>
-</div>
-
+<ul id="small-login">
+	<li>
+		<label for="email"><?php echo lang('user_email'); ?></label>
+		<input type="text" id="email" name="email" maxlength="120" />
+	</li>
+	<li>
+		<label for="password"><?php echo lang('user_password'); ?></label>
+		<input type="password" id="password" name="password" maxlength="20" />
+	</li>
+	<li id="remember-me">
+		<?php echo form_checkbox('remember', '1', FALSE); ?>
+		<label for="remember"><?php echo lang('user_remember')?></label>
+	</li>
+	<li class="form-buttons">
+		<label for="nothing">&nbsp;</label>
+		<input type="submit" value="<?php echo lang('user_login_btn') ?>" name="btnLogin" /> or <?php echo anchor('register', lang('user_register_btn'));?>
+	</li>
+</ul>
 <?php echo form_close(); ?>
