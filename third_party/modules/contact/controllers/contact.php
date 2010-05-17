6677613415ca79f7237a3e0cf8e87f97be405b7c
Dan Horrigan <dan@dhorrigan.com>
2010-05-17 15:41:42 -0400

    Started documentation of Contact module, migrated to form_validation, fixed lang files.

--- third_party/modules/contact/controllers/contact.php
+++ third_party/modules/contact/controllers/contact.php
@@ -1,110 +1,133 @@
-<?php
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * PyroCMS
+ *
+ * An open source CMS based on CodeIgniter
+ *
+ * @package		PyroCMS
+ * @author		PyroCMS Dev Team
+ * @license		Apache License v2.0
+ * @link		http://pyrocms.com
+ * @since		Version 0.9.8
+ * @filesource
+ */
+
 class Contact extends Public_Controller
-{	
+{
 	var $subjects = array(
 		'support'	=>	'Support',
 		'sales'		=>	'Sales',
 		'payments'	=>	'Payments',
 		'business'	=>	'Business Development',
 		'feedback'	=>	'Feedback/Suggestions',
-		'other'		=>	'Other'				
+		'other'		=>	'Other'
 	);
-	
+
 	// Fields must match this certain criteria
-	var $rules = array(
-		'contact_name'	=>	'required|trim|max_length[80]',
-		'contact_email'	=>	'required|trim|valid_email|max_length[80]',
-		'company_name'	=>	'trim|max_length[80]',
-		'subject'		=>	'required|trim',
-		'message'		=>	'required'
-	);
-	
+	private $rules = array();
+
 	function __construct()
 	{
-		parent::Public_Controller();        
+		parent::Public_Controller();
 		$this->lang->load('contact');
+
+		$this->rules = array(
+			array(
+				'field'	=> 'contact_name',
+				'label'	=> lang('contact_name_label'),
+				'rules'	=> 'required|trim|max_length[80]'
+			),
+			array(
+				'field'	=> 'contact_email',
+				'label'	=> lang('contact_email_label'),
+				'rules'	=> 'required|trim|valid_email|max_length[80]'
+			),
+			array(
+				'field'	=> 'company_name',
+				'label'	=> lang('contact_company_name_label'),
+				'rules'	=> 'trim|max_length[80]'
+			),
+			array(
+				'field'	=> 'subject',
+				'label'	=> lang('contact_subject_label'),
+				'rules'	=> 'required|trim'
+			),
+			array(
+				'field'	=> 'message',
+				'label'	=> lang('contact_message_label'),
+				'rules'	=> 'required'
+			)
+		);
+
 	}
-	
+
 	function index()
 	{
-		$this->load->library('user_agent');
-		$this->load->library('session');
-		
+		$this->load->library('form_validation');
 		$this->load->helper('form');
 		
-		// If the user has provided valid information and isnt a robot
-		if(!empty($_POST) && $this->_validate())
-		{		
+		$this->form_validation->set_rules($this->rules);
+
+		// If the user has provided valid information
+		if($this->form_validation->run())
+		{
 			// The try to send the email
 			if($this->_send_email())
 			{
 				// Store this session to limit useage
 				$this->session->set_flashdata('sent_contact_form', TRUE);
-				
-				// Now redirec
-				redirect('contact/sent');	
-			}	
+
+				// Now redirect
+				redirect('contact/sent');
+			}
 		}
-		
+
 		$this->data->subjects =& $this->subjects;
-		
+
 		// Set the values for the form inputs
-		foreach(array_keys($this->rules) as $field_name)
+		foreach($this->rules as $rule)
 		{
-			$this->data->form_values->$field_name = isset($this->validation->$field_name) ? $this->validation->$field_name : '';
-		}		
+			$this->data->form_values->{$rule['field']} = set_value($rule['field']);
+		}
 		$this->template->build('index', $this->data);
 	}
-	
-	
+
+
 	function sent()
 	{
 		$this->template->build('sent', $this->data);
 	}
-	
-	
-	function _validate()
-	{
-		$this->load->library('validation');		
-		// Get the language keys for the field
-		foreach(array_keys($this->rules) as $field_name)
-		{
-			$fields[$field_name] = $this->lang->line('contact_'.$field_name.'_label');
-		}		
-		$this->validation->set_fields($fields);
-		$this->validation->set_rules($this->rules);		
-		return ($this->validation->run());
-	}
-	
-	
+
+
 	function _send_email()
 	{
 		$this->load->library('email');
-		$this->load->library('user_agent');		
+		$this->load->library('user_agent');
 		$this->email->from($this->input->post('contact_email'), $this->input->post('contact_name'));
 		$this->email->to($this->settings->item('contact_email'));
-		
+
 		// If "other subject" exists then use it, if not then use the selected subject
 		$subject = ($this->input->post('other_subject')) ? $this->input->post('other_subject') : $this->subjects[$this->input->post('subject')];
-		$this->email->subject($this->settings->item('site_name') .' '.$subject);
-		
+		$this->email->subject($this->settings->item('site_name') .' - '.$subject);
+
 		// Loop through cleaning data and inputting to $data
 		foreach(array_keys($_POST) as $field_name)
 		{
 			$data[$field_name] = $this->input->post($field_name, TRUE);
 		}
-		
-		// Add in some extra details		
+
+		// Add in some extra details
 		$data['sender_agent']	=	$this->agent->browser().' '.$this->agent->version();
 		$data['sender_ip']		=	$this->input->ip_address();
 		$data['sender_os']		=	$this->agent->platform();
-		
+
 		$this->email->message($this->load->view('email/contact_html', $data, TRUE));
 		$this->email->set_alt_message($this->load->view('email/contact_plain', $data, TRUE));
-		
+
 		// If the email has sent with no known erros, show the message
 		return ( $this->email->send() );
 	}
-	
-}	
-?>
+
+}
+
+/* End of file contact.php */
