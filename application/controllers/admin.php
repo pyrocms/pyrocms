f53e59498519ab5a3fccf731ae194a92376f4e37
zackkitzmiller <zackkitzmiller@gmail.com>
2010-06-06 10:07:46 -0500

    fixed delete installer message

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -57,7 +57,7 @@ class Admin extends Admin_Controller
 		
 		else if(is_dir('./installer'))
 		{
-			$this->data->messages['notice'] = lang('delete_installer_message');
+			$this->data->messages['notice'] = lang('cp_delete_installer_message');
 		}
 		
 		// Load stuff
