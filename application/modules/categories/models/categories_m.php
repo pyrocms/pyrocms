c809f121a2d56cb15191e24cc633176e51763f83
Tumas Bajoras <hanniph@gmail.com>
2010-07-05 04:05:34 +0800

    categories model check_title bug fix. Now it actually validates whether category with given slug is already in db

--- application/modules/categories/models/categories_m.php
+++ application/modules/categories/models/categories_m.php
@@ -46,11 +46,11 @@ class Categories_m extends MY_Model
 	 * Callback method for validating the title
 	 * @access public
 	 * @param string $title The title to validate
-	 * @return mixed
+	 * @return integer Number of categories already in the database
 	 */
 	public function check_title($title = '')
 	{
-		return parent::count_by('slug', url_title($title)) === 0;
+		return parent::count_by('slug', url_title($title));
 	}
 }
 ?>
