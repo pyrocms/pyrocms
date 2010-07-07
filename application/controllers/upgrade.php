a8cf8d064a76c5f6d5c6d4b242705e2c66eb2cbe
Phil Sturgeon <email@philsturgeon.co.uk>
2010-07-07 14:48:01 +0100

    Fixed issue #251: "The installer could not insert the data into the database. Field 'theme_layout' doesn't have a default value"

--- application/controllers/upgrade.php
+++ application/controllers/upgrade.php
@@ -7,7 +7,7 @@
  */
 class Upgrade extends Controller
 {
-	private $versions = array('0.9.8-rc1', '0.9.8-rc2', '0.9.8', '0.9.9', '0.9.9.1', '0.9.9.2', '0.9.9.3', '0.9.9.4');
+	private $versions = array('0.9.8', '0.9.9', '0.9.9.1', '0.9.9.2', '0.9.9.3', '0.9.9.4', '0.9.9.5');
 
 	function _remap()
 	{
@@ -28,8 +28,7 @@ class Upgrade extends Controller
 
 		if ( ! $db_version)
 		{
-			show_error('We have no idea what version you are using, which means it must be v0.9.8-rc1 or before.
-				Please manually upgrade everything to v0.9.8-rc1 then you can use this upgrade script past that. Look at /docs/UPGRADE to see how.');
+			show_error('We have no idea what version you are using, which means something has gone seriously wrong. Please contact support@pyrocms.com.');
 		}
 
 		// Upgrade is already done
@@ -74,6 +73,22 @@ class Upgrade extends Controller
 
 		echo "<p>The upgrade is complete, please " . anchor('admin', 'click here') . ' to go back to the Control Panel.</p>';
  	}
+
+	function upgrade_0995()
+	{
+		echo 'Fixed theme_layout in strict mode.<br />';
+		$this->dbforge->modify_column('page_layouts', array(
+			'theme_layout' => array(
+				'name' => 'theme_layout',
+				'type' => 'VARCHAR',
+				'constraint' => '100',
+				'null' => FALSE,
+				'default' => ''
+			),
+		));
+
+		return FALSE; // Set to TRUE when this version goes live
+	}
 
 	function upgrade_0994()
 	{
@@ -268,295 +283,4 @@ class Upgrade extends Controller
 		return TRUE;
 	}
 
- 	// Upgrade
- 	function upgrade_098rc2()
-	{
-		echo 'Moving existing "photo" comments to photo-album comments.<br/>';
-		$this->db->where('module', 'photos');
-		$this->db->update('comments', array('module' => 'photos-album'));
-
-		// Create a "unsorted" widget area
-		echo 'Adding unsorted widget area.<br/>';
-		$this->db->insert('widget_areas', array('slug' => 'unsorted', 'title' => 'Unsorted'));
-
-		echo 'Adding ip_address to comments.<br/>';
-		$this->dbforge->add_column('comments', array(
-			'ip_address' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '15'
-			)
-		));
-
-		//if the groups tables doesnt exist lets do some magic
-		if ( $this->db->table_exists('groups'))
-		{
-			show_error('The table "groups" already exists.');
-		}
-
-		$this->db->select(array('users.id as user_id, users.first_name, users.last_name, users.lang, profiles.bio, profiles.dob, profiles.gender, profiles.phone, profiles.mobile, profiles.address_line1, profiles.address_line2, profiles.address_line3, profiles.postcode, profiles.msn_handle, profiles.aim_handle, profiles.yim_handle, profiles.gtalk_handle, profiles.gravatar, profiles.updated_on'));
-		$this->db->join('profiles', 'profiles.user_id = users.id', 'left');
-		$profile_result = $this->db->get('users')->result_array();
-
-		//drop the profiles table
-		echo 'Dropping the profiles table.<br/>';
-		$this->dbforge->drop_table('profiles');
-
-		//create the meta table
-		$this->dbforge->add_field('id');
-		$profiles_fields = array(
-			'user_id' => array(
-				'type' 	  	  => 'INT',
-				'constraint' 	  => 11,
-				'unsigned' 	  => TRUE,
-				'auto_increment' => FALSE,
-				'null' => FALSE,
-			),
-				'first_name' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '50',
-				'null' => FALSE,
-			),
-				'last_name' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '50',
-				'null' => FALSE,
-			),
-				'company' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '100',
-				'null' => FALSE,
-			),
-			'lang' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '2',
-				'null' => FALSE,
-				'default' => 'en',
-			),
-			'bio' => array(
-				'type' 	  => 'text',
-				'null' => TRUE,
-			),
-			'dob' => array(
-				'type' 	  => 'INT',
-				'constraint' => '11',
-				'null' => TRUE,
-			),
-
-			'gender' => array(
-				'type' 	  => "set('m','f','')",
-				'null' => TRUE,
-			),
-			'phone' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '20',
-				'null' => TRUE,
-			),
-			'mobile' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '20',
-				'null' => TRUE,
-			),
-			'address_line1' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '255',
-				'null' => TRUE,
-			),
-			'address_line2' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '255',
-				'null' => TRUE,
-			),
-			'address_line3' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '255',
-				'null' => TRUE,
-			),
-			'postcode' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '20',
-				'null' => TRUE,
-			),
-			'msn_handle' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '100',
-				'null' => TRUE,
-			),
-			'yim_handle' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '100',
-				'null' => TRUE,
-			),
-			'aim_handle' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '100',
-				'null' => TRUE,
-			),
-			'gtalk_handle' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '100',
-				'null' => TRUE,
-			),
-			'gravatar' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => '100',
-				'null' => TRUE,
-			),
-			'updated_on' => array(
-				'type' 	  => 'INT',
-				'constraint' => '11',
-				'unsigned' 	  => TRUE,
-				'auto_increment' => FALSE,
-			),
-		);
-
-		$this->dbforge->add_field($profiles_fields);
-		echo 'Creating profiles table...<br/>';
-		$this->dbforge->create_table('profiles');
-
-		//insert the profile data
-		foreach ($profile_result as $profile_data)
-		{
-			echo 'Inserting user ' . $profile_data['user_id'] . ' into profiles table...<br/>';
-
-			$this->db->insert('profiles', $profile_data);
-		}
-		echo '<br/>';
-
-			$this->db->select(array('id, role'));
-			$role_user_query = $this->db->get('users');
-
-			$role_user_result = $role_user_query->result_array();
-
-			//update roles to group_id
-			echo 'Converting roles to group_ids<br/>';
-			foreach ($role_user_result as $role)
-			{
-				$role_query = $this->db->select(array('id'))->where('abbrev', $role['role'])->get('permission_roles');
-				$current_role = $role_query->row_array();
-
-				$this->db->where('id', $role['id'])->update('users', array('role' => $current_role['id']));
-			}
-
-			//rename permission_roles table
-			echo 'Renaming permission_roles to groups <br/>';
-			$this->dbforge->rename_table('permission_roles', 'groups');
-
-			//add new groups field
-			echo 'Adding columns to groups table <br />';
-			$this->dbforge->add_column('groups', array(
-				'description' => array(
-					'type' 	  	=> 'VARCHAR',
-					'constraint' => 100,
-					'null' 		=> TRUE,
-				  ),
-			));
-
-			//rename the groups columns
-			echo 'Renaming the groups columns <br/>';
-			$this->dbforge->modify_column('groups', array(
-				'abbrev' => array(
-					'name' 	  => 'name',
-					'type' 	  => 'VARCHAR',
-					'constraint' => '100',
-				)
-			));
-
-			//rename the users columns
-			echo 'Renaming the users columns <br/>';
-			$this->dbforge->modify_column('users', array(
-				'is_active' => array(
-					'name' 	  => 'active',
-					'type' 	  => 'INT',
-					'constraint' => '1',
-				),
-				'ip' => array(
-					'name' 	   => 'ip_address',
-					'type' 	   => 'VARCHAR',
-					'constraint' => '16',
-				 ),
-				'activation_code' => array(
-					'name' 	    => 'activation_code',
-					'type' 	    => 'VARCHAR',
-					'constraint' => '40',
-					'null' 	    => TRUE
-				),
-				'role' => array(
-					'name' 	     => 'group_id',
-					'type' 	     => 'INT',
-					'constraint' => '11',
-					'null' 	     => TRUE,
-				)
-			));
-
-			// add new users fields
-			echo 'Adding columns to users table <br/>';
-
-		$this->dbforge->add_column('users', array(
-			'username' => array(
-				'type' 	  	=> 'VARCHAR',
-				'constraint' => 20,
-				'null' 		=> TRUE,
-			),
-			'forgotten_password_code' => array(
-				'type' => 'VARCHAR',
-				'constraint' => 40,
-				'null' 		=> TRUE
-			),
-			'remember_code' => array(
-				'type' 	  => 'VARCHAR',
-				'constraint' => 40,
-				'null' 	  => TRUE
-			)
-        ));
-
-		//removing columns from users table
-		echo 'Removing columns from users table <br/>';
-		$this->dbforge->drop_column('users', 'first_name');
-		$this->dbforge->drop_column('users', 'last_name');
-		$this->dbforge->drop_column('users', 'lang');
-
-		echo 'Creating forum tables...<br/>';
-
-		$this->db->query("CREATE TABLE `forum_categories` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `title` varchar(100) NOT NULL DEFAULT '',
-		  `permission` mediumint(2) NOT NULL DEFAULT '0',
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Splits forums into categories'");
-
-		$this->db->query("CREATE TABLE `forum_posts` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `forum_id` int(11) NOT NULL DEFAULT '0',
-		  `author_id` int(11) NOT NULL DEFAULT '0',
-		  `parent_id` int(11) NOT NULL DEFAULT '0',
-		  `title` varchar(100) NOT NULL DEFAULT '',
-		  `content` text NOT NULL,
-		  `type` tinyint(1) NOT NULL DEFAULT '0',
-		  `is_locked` tinyint(1) NOT NULL DEFAULT '0',
-		  `is_hidden` tinyint(1) NOT NULL DEFAULT '0',
-		  `created_on` int(11) NOT NULL DEFAULT '0',
-		  `updated_on` int(11) NOT NULL DEFAULT '0',
-		  `view_count` int(11) NOT NULL DEFAULT '0',
-		  `sticky` tinyint(1) NOT NULL DEFAULT '0',
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
-
-		$this->db->query("CREATE TABLE `forum_subscriptions` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `topic_id` int(11) NOT NULL DEFAULT '0',
-		  `user_id` int(11) NOT NULL DEFAULT '0',
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
-
-		$this->db->query("CREATE TABLE `forums` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `title` varchar(100) NOT NULL DEFAULT '',
-		  `description` varchar(255) NOT NULL DEFAULT '',
-		  `category_id` int(11) NOT NULL DEFAULT '0',
-		  `permission` int(2) NOT NULL DEFAULT '0',
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Forums are the containers for threads and topics.'");
-
-		return TRUE;
-	}
 }
