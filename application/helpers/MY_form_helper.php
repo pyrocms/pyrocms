309da1de8d958950a3690614e7950cd8bc148ab2
Phil Sturgeon <email@philsturgeon.co.uk>
2010-07-12 13:22:27 +0100

    Fixed the recent Form_validation fix.

--- application/helpers/MY_form_helper.php
+++ application/helpers/MY_form_helper.php
@@ -1,30 +1,40 @@
 <?php
+
 /*
  * Helper (helpers/MY_form_helper.php)
  */
-function form_open($action = '', $attributes = '', $hidden = array()) {
-    $CI = & get_instance();
 
-    if ($attributes == '') {
-        $attributes = 'method="post"';
-    }
+function form_open($action = '', $attributes = '', $hidden = array())
+{
+	$_ci = & get_instance();
+	$_ci->load->library('form_validation');
 
-    $action = ( strpos($action, '://') === FALSE) ? $CI->config->site_url($action) : $action;
+	if ($attributes == '')
+	{
+		$attributes = 'method="post"';
+	}
 
-    $form = '<form action="' . $action . '"';
-    $form .= _attributes_to_string($attributes, TRUE);
-    $form .= '>';
+	$action = ( strpos($action, '://') === FALSE) ? $_ci->config->site_url($action) : $action;
 
-    if($CI->form_validation->has_nonce()) {
-        $value = set_value('nonce');
-        if($value == '')
-            $value = $CI->form_validation->create_nonce();
-        $hidden['nonce'] = set_value('nonce', $value);
-    }
+	$form = '<form action="' . $action . '"';
+	$form .= _attributes_to_string($attributes, TRUE);
+	$form .= '>';
 
-    if (is_array($hidden) && count($hidden) > 0) {
-        $form .= form_hidden($hidden);
-    }
+	if ($_ci->form_validation->has_nonce())
+	{
+		$value = set_value('nonce');
+		if ($value == '')
+		{
+			$value = $_ci->form_validation->create_nonce();
+		}
 
-    return $form;
+		$hidden['nonce'] = set_value('nonce', $value);
+	}
+
+	if (is_array($hidden) && count($hidden) > 0)
+	{
+		$form .= form_hidden($hidden);
+	}
+
+	return $form;
 }
