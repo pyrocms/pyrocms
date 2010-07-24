8d3521839caf70d88027d5b92549ca8369f7f638
Adam Fairholm <adam.fairholm@gmail.com>
2010-07-24 00:01:53 -0400

    List chunks.

--- third_party/modules/chunks/language/english/chunks_lang.php
+++ third_party/modules/chunks/language/english/chunks_lang.php
@@ -7,6 +7,8 @@
 	$lang['chunks.list_chunks']							= 'List Chunks';
 	$lang['chunks.add_chunk']							= 'Add Chunk';
 
+	$lang['chunks.no_chunks']							= 'There are no chunks in the system.';
+
 
 /* End of file chunks_lang.php */
 /* Location: ./third_party/modules/chunks/language/english/chunks_lang.php */
