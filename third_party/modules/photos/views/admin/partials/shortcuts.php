313e04bec07580ee21538f62dc84f32bb60baae7
Phil Sturgeon <email@philsturgeon.co.uk>
2010-08-05 18:47:03 +0100

    Killed off old photos module, we now have a galleries module instead.

--- third_party/modules/photos/views/admin/partials/shortcuts.php
+++
@@ -1,8 +0,0 @@
-<nav id="shortcuts">
-	<h6><?php echo lang('cp_shortcuts_title'); ?></h6>
-	<ul>
-		<li><?php echo anchor('admin/photos/create', lang('photo_albums.add_title'), 'class="add"') ?></li>
-		<li><?php echo anchor('admin/photos', lang('photo_albums.list_title')); ?></li>
-	</ul>
-	<br class="clear-both" />
-</nav>
