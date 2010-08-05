72fc3a5c518c51b9db9366e516414054988b6faa
Phil Sturgeon <email@philsturgeon.co.uk>
2010-08-05 18:47:11 +0100

    Added files schema to installer.

--- installer/sql/1-tables.sql
+++ installer/sql/1-tables.sql
@@ -47,6 +47,44 @@ CREATE TABLE `emails` (
 
 -- command split --
 
+DROP TABLE IF EXISTS `files`;
+
+-- command split --
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `folder_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '1',
+  `type` enum('a','v','d','i','o') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `extension` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `mimetype` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `width` int(5) DEFAULT NULL,
+  `height` int(5) DEFAULT NULL,
+  `filesize` int(11) NOT NULL DEFAULT 0,
+  `date_added` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- command split --
+
+DROP TABLE IF EXISTS `file_folders`;
+
+-- command split --
+
+CREATE TABLE `file_folders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT '0',
+  `slug` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `date_added` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- command split --
+
 DROP TABLE IF EXISTS `galleries`;
 
 -- command split --
