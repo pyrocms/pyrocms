c4bb3e36f1d50f98006f9c00d3e53545127fd5eb
David Ruiz <wupsbr@gmail.com>
2010-08-24 20:21:47 -0300

    New: Module Pages translated to Brazilian Portuguese.

---
+++ system/pyrocms/modules/pages/language/brazilian/pages_lang.php
@@ -0,0 +1,67 @@
+<?php
+
+// tabs
+$lang['pages.content_label'] 					= 'Conteúdo de Página';
+$lang['pages.meta_label'] 						= 'Meta dados';
+$lang['pages.design_label'] 					= 'Design';
+$lang['pages.script_label'] 					= 'Script';
+$lang['pages.options_label'] 					= 'Opções';
+
+$lang['pages.detail_label'] 					= 'Detalhes';
+
+// labels
+$lang['pages.page_label'] 						= 'Página';
+$lang['pages.language_label'] 					= 'Idioma';
+$lang['pages.updated_label'] 					= 'Atualizado';
+$lang['pages.actions_label'] 					= 'Ações';
+$lang['pages.unknown_label'] 					= 'Desconhecido';
+$lang['pages.title_label'] 						= 'Título';
+$lang['pages.slug_label'] 						= 'URL';
+$lang['pages.body_label'] 						= 'Corpo da Página';
+$lang['pages.no_selection_label'] 				= '-- Nenhum --';
+$lang['pages.meta_title_label'] 				= 'Meta Título';
+$lang['pages.meta_keywords_label'] 				= 'Meta palavras-chave';
+$lang['pages.meta_desc_label'] 					= 'Meta descrição';
+$lang['pages.layout_id_label'] 					= 'Layout da Página';
+$lang['pages.css_label'] 						= 'CSS';
+$lang['pages.js_label'] 						= 'JavaScript';
+$lang['pages.access_label'] 					= 'Acesso';
+$lang['pages.rss_enabled_label'] 				= 'RSS ativo';
+$lang['pages.comments_enabled_label']			= 'Comentários ativo';
+
+$lang['pages.status_label'] 					= 'Status';
+$lang['pages.draft_label'] 						= 'Rascunho';
+$lang['pages.live_label'] 						= 'Produção';
+$lang['pages.revisions_label']					= 'Revisões';
+
+$lang['pages.view_label'] 						= 'Ver';
+$lang['pages.create_label'] 					= 'Adicionar filho';
+$lang['pages.edit_label'] 						= 'Editar';
+$lang['pages.delete_label'] 					= 'Deletar';
+
+// titles
+$lang['pages.create_title'] 					= 'Adicionar página';
+$lang['pages.edit_title'] 						= 'Editar página "%s"';
+$lang['pages.list_title'] 						= 'Listar páginas';
+$lang['pages.layouts_create_title'] 			= 'Adicionar página de layout';
+$lang['pages.layouts_list_title'] 				= 'Listar páginas de layout';
+
+// messages
+$lang['pages.no_pages'] 						= 'Nenhuma página encontrada.';
+$lang['pages_create_success'] 					= 'A página foi criada.';
+$lang['pages_create_error'] 					= 'A página não foi criada.';
+$lang['pages_page_not_found_error'] 			= 'A página não existe.';
+$lang['pages_edit_success'] 					= 'A página "%s" foi salva.';
+$lang['pages_delete_home_error'] 				= 'Você não pode deletar a  home page!';
+$lang['pages_delete_success']					= 'Página #%s foi deletada.';
+$lang['pages_mass_delete_success'] 				= '%s páginas foram deletadas.';
+$lang['pages_delete_none_notice'] 				= 'Nenhuma página foi deletada.';
+$lang['pages_page_already_exist_error'] 		= 'A página com a URL "%s" já existe em %s.';
+$lang['pages_parent_not_exist_error'] 			= 'A página pai que você selecionou não existe.';
+
+$lang['pages.tree_explanation'] 				= 'A lista da esquerda representa as páginas em seu website. 
+Clique no ícone "+" próximo da página para exibir as páginas associadas. Quando você clicar na página você visualizará várias informações úteis nesta caixa.';
+
+$lang['pages.rss_explanation']					= 'Ativando o Feed RSS para esta página dará acesso aos usuários a chance de se inscreverem a este Feed e receberem uma atualização imediata quando uma nova página filho for adicionada.';
+
+?>
