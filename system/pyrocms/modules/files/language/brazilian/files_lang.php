3c39b1a08682e0ce6f691a7200d137aa7c8d6a4b
David Ruiz <wupsbr@gmail.com>
2010-08-25 06:15:58 +0800

    New: Module Files translated to Brazilian Portuguese.

---
+++ system/pyrocms/modules/files/language/brazilian/files_lang.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * PyroCMS
+ *
+ * An open source CMS based on CodeIgniter
+ *
+ * @package		PyroCMS
+ * @author		wupsbr
+ * @license		Apache License v2.0
+ * @link		http://pyrocms.com
+ * @since		Version 1.0-dev
+ * @filesource
+ */
+
+$lang['files.title']						= 'Arquivos';
+$lang['files.upload.title']					= 'Upload de Arquivos';
+$lang['files.labels.action']				= 'Ações';
+$lang['files.labels.edit']					= 'Editar';
+$lang['files.labels.delete']				= 'Deletar';
+$lang['files.labels.upload']				= 'Upload';
+$lang['files.labels.parent']				= 'Topo';
+$lang['files.no_files']						= 'Não existem arquivos atualmente.';
+
+$lang['files.folders.title']				= 'Pasta de Arquivos';
+$lang['files.folders.manage_title']			= 'Gerenciar Pastas';
+$lang['files.folders.label']				= 'Pastas';
+$lang['files.subfolders.label']				= 'Sub-Pastas';
+$lang['files.folders.name']					= 'Nome';
+$lang['files.folders.slug']					= 'URL Curta';
+$lang['files.folders.created']				= 'Criado Em';
+$lang['files.folders.create']				= 'Nova Pasta';
+$lang['files.folders.not_exists']			= 'Uma pasta inválida foi selecionada.';
+$lang['files.folders.no_subfolders']		= 'Vazio';
+$lang['files.folders.no_folders']			= 'Não existem pastas atualmente.';
+$lang['files.folders.delete_success']		= 'A pasta "%s" foi deletada.';
+$lang['files.folders.delete_title']			= 'Confirmar Deleção';
+$lang['files.folders.confirm_delete']		= 'Você tem certeza que deseja deletar "%s"?<br /><br />Ao deletar você irá deletar todos os arquivo e pastas dentro deste diretório.';
+
+$lang['files.dropdown.no_subfolders']		= '-- Vazio --';
+$lang['files.dropdown.root']				= '-- Raiz --';
+
+
+/* End of file file_lang.php */
