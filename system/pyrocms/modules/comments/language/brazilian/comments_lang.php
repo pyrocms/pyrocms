f87831fe222f5dc273a95f0a4b6348da7276b4e8
David Ruiz <wupsbr@gmail.com>
2010-08-24 19:10:34 -0300

    new: Module Comments translated to Brazilian Portuguese.

---
+++ system/pyrocms/modules/comments/language/brazilian/comments_lang.php
@@ -0,0 +1,56 @@
+<?php
+
+$lang['comments.title'] 					= 'Comentários';
+$lang['comments.your_comment'] 				= 'Postar um comentário';
+$lang['comments.user_comments'] 			= 'Comentários de usuários';
+
+$lang['comments.list_comment'] 				= '%s comentou %s dizendo:';
+$lang['comments.recent_comments'] 			= 'Comentários recentes';
+$lang['comments.view_more'] 				= 'Ver mais...';
+
+// labels
+$lang['comments.name_label'] 				= 'Seu Nome';
+$lang['comments.email_label'] 				= 'Endereço de Email';
+$lang['comments.website_label'] 			= 'Website';
+$lang['comments.message_label'] 			= 'Mensagem';
+$lang['comments.status_label'] 				= 'Status';
+$lang['comments.activate_label'] 			= 'Aprovar';
+$lang['comments.deactivate_label'] 			= 'Desaprovar';
+$lang['comments.author_label'] 				= 'Autor';
+$lang['comments.module_label'] 				= 'Modulo';
+$lang['comments.item_label'] 				= 'Item';
+$lang['comments.date_label'] 				= 'Data';
+$lang['comments.actions_label'] 			= 'Ações';
+$lang['comments.edit_label'] 				= 'Editar';
+$lang['comments.delete_label'] 				= 'Deletar';
+$lang['comments.posted_label'] 				= 'Postar';
+$lang['comments.from_label'] 				= 'De';
+$lang['comments.preview_label'] 			= 'Pré-visualizar';
+$lang['comments.send_label'] 				= 'Comentar';
+
+$lang['comments.inactive_title'] 			= 'Comentários aguardando aprovação';
+$lang['comments.active_title'] 				= 'Comentários aprovados';
+$lang['comments.edit_title'] 				= 'Editar comentários';
+
+// messages
+$lang['comments.add_success'] 				= 'O comentário foi salvo.';
+$lang['comments.add_approve'] 				= 'O comentário foi salvo e está aguardando aprovação.';
+$lang['comments.add_error']					= 'Ocorreu um problema ao postar seu comentário. Por favor, tente novamente mais tarde.';
+
+$lang['comments.edit_success'] 				= 'O comentário foi editado com sucesso.';
+$lang['comments.edit_error'] 				= 'Não foi possível editar o comentário.';
+
+$lang['comments.approve_success'] 			= 'Este comentário foi aprovado.';
+$lang['comments.approve_success_multiple'] 	= 'Os comentários selecionados foram aprovados.';
+$lang['comments.approve_error'] 			= 'Ocorreu um problema ao tentar aprovar este comentário.';
+$lang['comments.approve_error_multiple'] 	= 'Ocorreu um problema ao tentar aprovar estes comentários.';
+
+$lang['comments.unapprove_success'] 		= 'Este comentário foi desaprovado e não aparecerá mais no site em produção.';
+$lang['comments.unapprove_success_multiple'] = 'Estes comentários foram desaprovados e não serão mais exibidos no site em produção.';
+$lang['comments.unapprove_error'] 			= 'Ocorreu um problema ao tentar desaprovar este comentário. Deleta-lo poderá funcionar?';
+$lang['comments.unapprove_error_multiple']	= 'Ocorreu um problema ao tentar desaprovar estes comentários. Deleta-los poderá funcionar?';
+
+$lang['comments.delete_single_success'] 	= 'O comentário #%s foi deletado e não será mais possível visualiza-lo no site em produção.';
+$lang['comments.delete_multi_success'] 		= 'Os comentários #%s foram deletados e não será mais possível visualiza-los no site em produção.';
+
+$lang['comments.no_comments'] 				= 'Não existem comentários';
