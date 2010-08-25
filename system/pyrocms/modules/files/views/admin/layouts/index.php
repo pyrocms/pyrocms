aa0aca20195cf6675b43b0cf7eae573484d9982d
Eric <eric@ericlbarnes.com>
2010-08-25 09:17:48 -0400

    Fixed bug in files and made edit folders open in colorbox.

--- system/pyrocms/modules/files/views/admin/layouts/index.php
+++ system/pyrocms/modules/files/views/admin/layouts/index.php
@@ -98,7 +98,10 @@ h3 span {
             $(this).colorbox().click();
             return false;
         });
-
+		$('.edit_folder').livequery('click', function(){
+            $(this).colorbox().click();
+            return false;
+        });
 	});
 })(jQuery);
 </script>
