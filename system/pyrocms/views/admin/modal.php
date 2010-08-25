a350547e61f16f6aa0e1e5de66b4911edab2b0fa
Eric <eric@ericlbarnes.com>
2010-08-25 13:04:31 -0400

    Added new modal layout file for use in iframe modal windows.

---
+++ system/pyrocms/views/admin/modal.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo lang('cp_admin_title').' - '.$template['title'];?></title>
+	<?php echo $template['partials']['metadata']; ?>
+	<style type="text/css">
+	body {
+		background: none;
+	}
+	</style>
+</head>
+
+<body>
+	<?php echo $template['body']; ?>
+</body>
+</html>
