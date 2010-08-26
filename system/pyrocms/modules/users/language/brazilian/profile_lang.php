29c401e079a34848ea8b09834391009c42383c50
David Ruiz <wupsbr@gmail.com>
2010-08-27 06:11:59 +0800

    New: Module "Users" translated to Brazilian Portuguese.

---
+++ system/pyrocms/modules/users/language/brazilian/profile_lang.php
@@ -0,0 +1,57 @@
+<?php
+
+$lang['profile_of_title'] 				= '%s\'s Perfis';
+
+$lang['profile_user_details_label'] 	= 'Detalhes do Usuário';
+$lang['profile_role_label'] 			= 'Papel';
+$lang['profile_registred_on_label'] 	= 'Registrado em';
+$lang['profile_last_login_label'] 		= 'Última entrada';
+$lang['profile_male_label'] 			= 'Masculino';
+$lang['profile_female_label'] 			= 'Feminino';
+
+$lang['profile_not_set_up'] 			= 'Este usuário não possui um perfil configurado.';
+
+$lang['profile_edit'] 					= 'Editae seu perfil';
+
+$lang['profile_personal_section'] 		= 'Pessoal';
+
+$lang['profile_display_name']			= 'Nome de Exibição';  
+$lang['profile_dob']					= 'Data de Nascimento';
+$lang['profile_dob_day']				= 'Dia';
+$lang['profile_dob_month']				= 'Mês';
+$lang['profile_dob_year']				= 'Ano';
+$lang['profile_gender']					= 'Sexo';
+$lang['profile_bio']					= 'Sobre mim';
+
+$lang['profile_contact_section'] 		= 'Contato';
+
+$lang['profile_phone']					= 'Telefone';
+$lang['profile_mobile']					= 'Celular';
+$lang['profile_address']				= 'Endereço';
+$lang['profile_address_line1'] 			= 'Linha #1';
+$lang['profile_address_line2'] 			= 'Linha #2';
+$lang['profile_address_line3'] 			= 'Linha #3';
+$lang['profile_address_postcode'] 		= 'CEP/Código Postal';
+$lang['profile_website']				= 'Website'; #translate
+
+$lang['profile_messenger_section'] 		= 'Mensageiros Instantâneos';
+
+$lang['profile_msn_handle'] 			= 'MSN';
+$lang['profile_aim_handle'] 			= 'AIM';
+$lang['profile_yim_handle'] 			= 'Yahoo! messenger';
+$lang['profile_gtalk_handle'] 			= 'GTalk';
+
+$lang['profile_avatar_section'] 		= 'Avatar';
+$lang['profile_social_section'] 		= 'Social';
+
+$lang['profile_gravatar'] 				= 'Gravatar';
+$lang['profile_twitter'] 				= 'Twitter';
+
+$lang['profile_edit_success'] 			= 'Seu perfil foi salvo.';
+$lang['profile_edit_error'] 			= 'Ocorreu um erro.';
+
+// -- Buttons ------------------------------------------------------------------------------------------------
+
+$lang['profile_save_btn'] 				= 'Salvar perfil';
+
+?>
