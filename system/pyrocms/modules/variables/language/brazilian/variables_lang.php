c6c16b140598a0a827f0243e0e94e671932391f8
David Ruiz <wupsbr@gmail.com>
2010-08-27 06:15:22 +0800

    New: Module "Variables" translated to Brazilian Portuguese.

---
+++ system/pyrocms/modules/variables/language/brazilian/variables_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+// labels
+$lang['variables.name_label'] 			    = 'Nome';
+$lang['variables.syntax_label'] 			    = 'Sintaxe';
+$lang['variables.data_label'] 			    = 'Valor';
+$lang['variables.actions_label'] 				= 'Ações';
+$lang['variables.edit_label'] 				= 'Editae';
+$lang['variables.delete_label'] 				= 'Deletar';
+
+// titles
+$lang['variables.create_title'] 				= 'Adicionar Variável';
+$lang['variables.edit_title'] 				= 'Editar variáveis "%s"';
+$lang['variables.list_title'] 				= 'Listar variáveis';
+
+// messages
+$lang['variables.no_variables'] 				= 'Não existem variáveis.';
+$lang['variables.add_success'] 				= 'Sua variável foi salva.';
+$lang['variables.add_error'] 					= 'Ocorreu um erro.';
+$lang['variables.edit_success'] 				= 'A variável foi criada.';
+$lang['variables.edit_error'] 				= 'Ocorreu um erro.';
+$lang['variables.mass_delete_error'] 			= 'Ocorreu um erro ao tentar deletar a variável "%s".';
+$lang['variables.mass_delete_success']		= '%s de %s variáveis foram deletadas com sucesso.';
+$lang['variables.no_select_error'] 			= 'Você precisa selecionar as variáveis primeiro.';
+$lang['variables.already_exist_error']		= 'A variável com o nome "%s" já existe.';
