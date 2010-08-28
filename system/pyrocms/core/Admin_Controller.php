75c53185b21f45b0666b9210fb0a12fb3386277c
Phil Sturgeon <email@philsturgeon.co.uk>
2010-08-28 10:21:38 +0100

    Had a major tidy up of the Admin_Controller, especially the stupid check access code.

--- system/pyrocms/core/Admin_Controller.php
+++ system/pyrocms/core/Admin_Controller.php
@@ -11,85 +11,41 @@ class Admin_Controller extends MY_Controller
 	    $this->lang->load('admin');
 	    $this->lang->load('buttons');
 
-	    $allow_access = FALSE;
-
-	    // These pages get past permission checks
-	    $ignored_pages = array('admin/login', 'admin/logout');
-
-	    // Check if the current page is to be ignored
-	    $current_page = $this->uri->segment(1, '') . '/' . $this->uri->segment(2, 'index');
-	    $is_ignored_page = in_array($current_page, $ignored_pages);
-
-	    // Check the user is an admin
-	    $is_admin = $this->ion_auth->is_admin();
-
-	    // Login: If not logged in and its not an ignored page, force login
-	    if( ! $this->user && ! $is_ignored_page)
-	    {
-			if($current_page != 'admin/index')
-			{
-				$this->session->set_flashdata('error', lang('cp_must_login'));
-			}
-	    	redirect('admin/login');
-	    }
-	    // Logged in or ignored page
-	    else
-	    {
-	    	$allow_access = TRUE;
-	    }
-
-	    // We are looking at the index page. Show it if they have ANY admin access at all
-	    if( $current_page == 'admin/index' && $this->permissions_m->has_admin_access($this->user->group_id) )
-	    {
-	    	$allow_access = TRUE;
-	    }
-
-	    // Check Perms: Not an admin and this is not a page to ignore
-	    elseif( ! $is_admin && ! $is_ignored_page )
-	    {
-		  	// Check if the current user can view that page
-		    $allow_access = $this->permissions_m->check_rule_by_role( $this->user->group_id, array(
-				'module' => $this->module,
-				'controller' =>$this->controller,
-				'method' =>$this->method
-			));
-	    }
-
 	    // Show error and exit if the user does not have sufficient permissions
-	    if( ! $allow_access )
+	    if( ! self::_check_access() )
 	    {
 		  	show_error($this->lang->line('cp_access_denied'));
 		    exit;
 	    }
 
-	    // Get a list of all modules available to this role
-	    if($current_page != 'admin/login')
-	    {
-	  		$this->data->core_modules = $this->cache->model('modules_m', 'get_all', array(
-	    		array(
+	    // Get a list of all modules available to this user / group
+		if ($this->user)
+		{
+			$this->template->core_modules = $this->cache->model('modules_m', 'get_all', array(
+				array(
 					'is_backend_menu' => TRUE,
 					'is_backend' => TRUE,
 					'group' => $this->user->group,
-	    			'lang' => CURRENT_LANGUAGE
+					'lang' => CURRENT_LANGUAGE
 				) // This function does NOT need role OR language, that is to give it a unique md5 hash
-	    	), $this->config->item('navigation_cache'));
+			), $this->config->item('navigation_cache'));
 
-	    	$this->data->addon_modules = $this->cache->model('modules_m', 'get_all', array(
-	    		array(
+			$addon_modules = $this->cache->model('modules_m', 'get_all', array(
+				array(
 					'is_core' => FALSE,
 					'is_backend' => TRUE,
 					'group' => $this->user->group,
-	    			'lang' => CURRENT_LANGUAGE
+					'lang' => CURRENT_LANGUAGE
 				) // This function does NOT need role OR language, that is to give it a unique md5 hash
-	    	), $this->config->item('navigation_cache'));
+			), $this->config->item('navigation_cache'));
 
 			// This takes the modules array and creates a keyed array with the slug as the key.
 			$modules_keyed = array();
-			foreach($this->data->addon_modules as $mod)
+			foreach($addon_modules as $mod)
 			{
 				$modules_keyed[$mod['slug']] = $mod;
 			}
-			$this->data->addon_modules = $modules_keyed;
+			$this->template->addon_modules = $modules_keyed;
 		}
 
 	    // Template configuration
@@ -117,5 +73,50 @@ class Admin_Controller extends MY_Controller
 	    $this->template->set_partial('footer', 'admin/partials/footer', FALSE);
 
 	    //$this->output->enable_profiler(TRUE);
+	}
+
+	private function _check_access()
+	{
+	    // These pages get past permission checks
+	    $ignored_pages = array('admin/login', 'admin/logout');
+
+	    // Check if the current page is to be ignored
+	    $current_page = $this->uri->segment(1, '') . '/' . $this->uri->segment(2, 'index');
+
+	    // Dont need to log in, this is an open page
+		if(in_array($current_page, $ignored_pages))
+		{
+			return TRUE;
+		}
+
+		// Admins can go straight in
+		else if ($this->user->group !== 'admin')
+		{
+			return TRUE;
+		}
+
+		// Well they at least better have permissions!
+		else if ($this->user)
+		{
+			// We are looking at the index page. Show it if they have ANY admin access at all
+			if($current_page == 'admin/index' && $this->permissions_m->has_admin_access($this->user->group_id))
+			{
+				return TRUE;
+			}
+
+			// Check Perms: Not an admin and this is not a page to ignore
+			else
+			{
+				// Check if the current user can view that page
+				return $this->permissions_m->check_rule_by_role($this->user->group_id, array(
+					'module' => $this->module,
+					'controller' =>$this->controller,
+					'method' =>$this->method
+				));
+			}
+		}
+
+		// god knows what this is... erm...
+		return FALSE;
 	}
 }
