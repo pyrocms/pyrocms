4df134f605a3622f08f0f0f1175ff261abdfa4b8
Eric <eric@ericlbarnes.com>
2010-08-30 13:11:18 -0400

    Added ability to edit files.

--- system/pyrocms/modules/files/views/admin/folders/form.php
+++ system/pyrocms/modules/files/views/admin/folders/form.php
@@ -7,6 +7,7 @@
 	});
 })(jQuery);
 </script>
+<p><?php echo $messages['success']; ?></p>
 <?php else: ?>
 
 <?php echo form_open($this->uri->uri_string(), array('class' => 'crud', 'id' => 'folders_crud')); ?>
