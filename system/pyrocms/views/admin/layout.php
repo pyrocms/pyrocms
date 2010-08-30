a0d8272dc55478879dd670cb95753636d3ca4f7b
Lonnie Ezell <lonnieje@gmail.com>
2010-08-30 12:05:06 -0500

    Added base_url() to jQuery fallback.

--- system/pyrocms/views/admin/layout.php
+++ system/pyrocms/views/admin/layout.php
@@ -14,7 +14,7 @@
 	
 	<!-- Grab Google CDNs jQuery, fall back if necessary -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-	<script>!window.jQuery && document.write('<script src="/system/pyrocms/assets/js/jquery/jquery-1.4.2.min.js"><\/script>')</script>
+	<script>!window.jQuery && document.write('<script src="<?php echo base_url() ?>system/pyrocms/assets/js/jquery/jquery-1.4.2.min.js"><\/script>')</script>
 	
 	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]--> 
 	
