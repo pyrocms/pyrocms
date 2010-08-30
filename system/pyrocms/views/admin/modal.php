c0071d1bc09413e13978fba9ced78c7780e03082
Eric <eric@ericlbarnes.com>
2010-08-30 12:51:02 -0400

    Added jquery as it has been removed from metadata partial.

--- system/pyrocms/views/admin/modal.php
+++ system/pyrocms/views/admin/modal.php
@@ -2,6 +2,11 @@
 <html>
 <head>
 	<title><?php echo lang('cp_admin_title').' - '.$template['title'];?></title>
+
+	<!-- Grab Google CDNs jQuery, fall back if necessary -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+	<script>!window.jQuery && document.write('<script src="<?php echo base_url(); ?>system/pyrocms/assets/js/jquery/jquery-1.4.2.min.js"><\/script>')</script>
+
 	<?php echo $template['partials']['metadata']; ?>
 	<style type="text/css">
 	body {
