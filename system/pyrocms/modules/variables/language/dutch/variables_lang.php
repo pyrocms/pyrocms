99a2480119c22608c7053f2df40c71c99043e280
jbertoen <jbertoen@gmail.com>
2010-08-31 01:51:06 -0700

    dutch language edits

--- system/pyrocms/modules/variables/language/dutch/variables_lang.php
+++ system/pyrocms/modules/variables/language/dutch/variables_lang.php
@@ -1,27 +1,27 @@
 <?php
 
 // labels
-$lang['var_name_label'] 			    = 'Naam';
-$lang['var_syntax_label'] 			    = 'Syntax';
-$lang['var_data_label'] 			    = 'Waarde';
-$lang['var_actions_label'] 				= 'Acties';
-$lang['var_edit_label'] 				= 'Wijzig';
-$lang['var_delete_label'] 				= 'Verwijder';
+$lang['variables.name_label'] 			    = 'Naam';
+$lang['variables.syntax_label'] 			    = 'Syntax';
+$lang['variables.data_label'] 			    = 'Waarde';
+$lang['variables.actions_label'] 				= 'Acties';
+$lang['variables.edit_label'] 				= 'Wijzig';
+$lang['variables.delete_label'] 				= 'Verwijder';
 
 // titles
-$lang['var_create_title'] 				= 'Voeg variabele toe';
-$lang['var_edit_title'] 				= 'Wijzig variabele "%s"';
-$lang['var_list_title'] 				= 'Toon alle variabelen';
+$lang['variables.create_title'] 				= 'Voeg variabele Toe';
+$lang['variables.edit_title'] 				= 'Bewerk variabele "%s"';
+$lang['variables.list_title'] 				= 'Toon alle variabelen';
 
 // messages
-$lang['var_no_variables'] 				= 'Er zijn geen variabelen.';
-$lang['var_add_success'] 				= 'Uw variabele is opgeslagen.';
-$lang['var_add_error'] 					= 'Er is een fout opgetreden.';
-$lang['var_edit_success'] 				= 'Uw variabele is opgeslagen.';
-$lang['var_edit_error'] 				= 'Er is een fout opgetreden.';
-$lang['var_mass_delete_error'] 			= 'Er is een fout opgetreden bij het verwijderen van variabele "%s".';
-$lang['var_mass_delete_success']		= '%s variabelen van %s zijn verwijderd.';
-$lang['var_no_select_error'] 			= 'U moet eerst variabelen selecteren.';
-$lang['var_already_exist_error']		= 'De variabele "%s" bestaat al.';
+$lang['variables.no_variables'] 				= 'Er zijn geen variabelen.';
+$lang['variables.add_success'] 				= 'Uw variabel is opgeslagen.';
+$lang['variables.add_error'] 					= 'Er is een fout opgetreden.';
+$lang['variables.edit_success'] 				= 'Uw variabele is opgeslagen.';
+$lang['variables.edit_error'] 				= 'Er is een fout opgetreden.';
+$lang['variables.mass_delete_error'] 			= 'Er is een fout opgetreden bij het verwijderen van variabele "%s".';
+$lang['variables.mass_delete_success']		= '%s variabelen van de %s zijn verwijderd.';
+$lang['variables.no_select_error'] 			= 'U moet eerst variabelen selecteren.';
+$lang['variables.already_exist_error']		= 'De variabel "%s" bestaat al.';
 
 ?>
