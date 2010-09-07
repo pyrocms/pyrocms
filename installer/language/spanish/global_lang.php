85fcba2f3fdc33d0475e2af23d0dbbf6a751c5d4
jago <jago@jagobook.site>
2010-09-08 03:38:38 +0800

    Step 1 translated

---
+++ installer/language/spanish/global_lang.php
@@ -0,0 +1,8 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['intro'] = 'Inicio';
+$lang['step1'] = 'Paso #1';
+$lang['step2'] = 'Paso #2';
+$lang['step3'] = 'Paso #3';
+$lang['step4'] = 'Paso #4';
+$lang['final'] = 'Paso final';
--- installer/language/spanish/step_1_lang.php
+++ installer/language/spanish/step_1_lang.php
@@ -1,20 +1,21 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 // labels
-$lang['header']			=	'Step 1: Configure Database and Server';
-$lang['intro_text']		=	'Before we can check the database, we need to know where it is and what the login details are.';
+$lang['header']          = 'Paso 1: Configurar la Base de datos y el Servidor';
+$lang['intro_text']      = 'Antes de configurar la base de datos, es necesario saber donde se ubica y los datos de ingreso.';
 
-$lang['db_settings']	=	'Database Settings';
-$lang['db_text']		=	'In order for the installer to check your MySQL server version it requires you to enter the hostname, username and password in the form below. These settings will also be used when installing the database.';
+$lang['db_settings']     = 'Configuración de la Base de datos';
+$lang['db_text']         = 'Para que el instalador compruebe su versión del servidor MySQL necesita que ingrese el nombre de host, nombre de usuario y contraseña en el siguiente formulario. Estos datos serán usados cuando se instale la base de datos.';
 
-$lang['server']			=	'Server';
-$lang['username']		=	'Username';
-$lang['password']		=	'Password';
-$lang['portnr']			=	'Port';
-$lang['server_settings']=	'Server Settings';
-$lang['httpserver']		=	'HTTP Server';
-$lang['step2']			=	'Step 2';
+$lang['server']          = 'Servidor';
+$lang['username']        = 'Nombre de  usuario';
+$lang['password']        = 'Contraseña';
+$lang['portnr']          = 'Puerto';
+$lang['server_settings'] = 'Configuración del Servidor';
+$lang['httpserver']      = 'Servidor HTTP';
 
 // messages
-$lang['db_success']		=	'The database settings are tested and working fine.';
-$lang['db_failure']		=	'Problem connecting to the database: ';
+$lang['db_success']      = 'La configuración de la base de datos fueron probadas y funcionan bien.';
+$lang['db_failure']      = 'Hay problemas conectando a la base de datos: ';
+
+// $lang['step2']='Step 2'; //¿Used?
