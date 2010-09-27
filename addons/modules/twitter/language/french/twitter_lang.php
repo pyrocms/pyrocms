948e5dd6fdffb69eaa11cdea295e6c5de399639c
= <=>
2010-09-27 18:18:15 -0400

    -merge

--- addons/modules/twitter/language/french/twitter_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$lang['twitter_more'] 							= 'Suite &raquo;';
-$lang['twitter_no_tweets'] 					= 'Aucun message pour le moment.';
-
-?>
--- addons/modules/twitter/language/polish/twitter_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$lang['twitter_more'] 				= 'Więcej &raquo;';
-$lang['twitter_no_tweets'] 		= 'Brak tweetów.';
-
-?>
