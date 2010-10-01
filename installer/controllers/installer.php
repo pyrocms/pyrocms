1a80c1c90474764ee5ed1899ea18d28cfbfca5c7
jacksun101 <sunhsilu@gmail.com>
2010-10-01 16:18:11 +0800

    chinese_traditional is added to the $languages.

--- installer/controllers/installer.php
+++ installer/controllers/installer.php
@@ -13,7 +13,7 @@ class Installer extends Controller
 	/**
 	 * Array of languages supported by the installer
 	 */
-	private $languages	= array ('english','dutch','brazilian','polish');
+	private $languages	= array ('english','dutch','brazilian','polish','chinese_traditional');
 
 	/**
 	 * Array containing the directories that need to be writeable
