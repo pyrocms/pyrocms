586677209e56196601de134c78d6522f2dcaa735
jacksun101 <sunhsilu@gmail.com>
2010-10-01 21:49:30 +0800

    This is Chinese Traditional language files for CI.

---
+++ system/codeigniter/language/chinese_traditional/imglib_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['imglib_source_image_required'] = "請在設定中指定來源圖形.";
+$lang['imglib_gd_required'] = "此功能必須使用GD函式庫.";
+$lang['imglib_gd_required_for_props'] = "主機必須支援GD函式庫才能取得圖形屬性.";
+$lang['imglib_unsupported_imagecreate'] = "主機GD函式庫不支援此圖形類型.";
+$lang['imglib_gif_not_supported'] = "因授權限制關係 GIF 檔案已普遍不支援. 請使用 JPG 或 PNG 作為替代用.";
+$lang['imglib_jpg_not_supported'] = "不支援 JPG 檔案.";
+$lang['imglib_png_not_supported'] = "不支援 PNG 檔案.";
+$lang['imglib_jpg_or_png_required'] = "設定中所指定圖形調整尺寸功能只接受 JPEG 或 PNG 檔案格式.";
+$lang['imglib_copy_error'] = "無法更換檔案. 請確定資料夾可以寫入.";
+$lang['imglib_rotate_unsupported'] = "主機不支援圖片旋轉功能.";
+$lang['imglib_libpath_invalid'] = "圖形函式庫路徑不正確. 請在圖形設定中更正.";
+$lang['imglib_image_process_failed'] = "圖形處理失敗. 請確定主機是否支援及圖形函式庫路徑是否正確.";
+$lang['imglib_rotation_angle_required'] = "旋轉圖形必須包含旋轉角度.";
+$lang['imglib_writing_failed_gif'] = "GIF 檔案.";
+$lang['imglib_invalid_path'] = "圖形檔案路徑不正確.";
+$lang['imglib_copy_failed'] = "圖形複製程式錯誤.";
+$lang['imglib_missing_font'] = "無法找到可用字型.";
+$lang['imglib_save_failed'] = "無法儲存檔案.  請確認圖片或目錄是可寫入.";
+
+
+/* End of file imglib_lang.php */
+/* Location: ./system/language/chinese_traditional/imglib_lang.php */
