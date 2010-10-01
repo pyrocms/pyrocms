b5012a7da1049bab89d162ef0ceb8e9a08460134
jacksun101 <sunhsilu@gmail.com>
2010-10-01 16:28:28 +0800

    chinese_traditional is added.

---
+++ system/pyrocms/language/chinese_traditional/buttons_lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PyroCMS
+ *
+ * An open source CMS based on CodeIgniter
+ *
+ * @package		PyroCMS
+ * @author		PyroCMS Dev Team
+ * @license		Apache License v2.0
+ * @link		http://pyrocms.com
+ * @since		Version 1.0
+ * @filesource
+ */
+
+$lang['buttons.submit']		= '送出'; 		//'Save';
+$lang['buttons.cancel']		= '取消'; 		//'Cancel';
+$lang['buttons.upload']		= '上傳'; 		//'Upload';
+$lang['buttons.save']		= '儲存'; 		//'Save';
+$lang['buttons.save_exit']	= '儲存離開'; 	//'Save &amp; Exit';
+$lang['buttons.delete']		= '刪除'; 		//'Delete';
+$lang['buttons.publish']	= '發佈'; 		//'Publish';
+$lang['buttons.approve']	= '核准'; 		//'Approve';
+$lang['buttons.unapprove']	= '取消核准'; 	//'Un-approve';
+$lang['buttons.install']	= '安裝'; 		//'Install';
+$lang['buttons.yes']		= '是'; 			//'Yes';
+$lang['buttons.no']			= '否'; 			//'No';
+$lang['buttons.activate']   = '啟動'; 		//'Activate';
+
+/* End of file buttons_lang.php */
---
+++ system/pyrocms/language/chinese_traditional/errors_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+// Error 404
+$lang['error_404_title'] 			= '找不到頁面'; //'Page missing';
+$lang['error_404_message'] 			= '我們無法找到您要檢視的頁面，請  <a href="%s">點選這裡</a> 以回到首頁。'; //'We cannot find the page you are looking for, please click <a href="%s">here</a> to go to the homepage.';
+
+// Database
+$lang['error_invalid_db_group'] 	= '資料庫目前正嘗試使用的設定 "%s" 是不正確的。'; //'The database is trying to use an invalid configuration group "%s".';
+
+?>
