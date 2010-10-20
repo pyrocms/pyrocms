d5731c4409d15d3981a36b2b566e2d72ab73a642
diederik.eijgelaar <diederik.eijgelaar@gmail.com>
2010-10-20 21:55:55 +0200

    updated Dutch language pack

--- system/pyrocms/modules/pages/language/dutch/pages_lang.php
+++ system/pyrocms/modules/pages/language/dutch/pages_lang.php
@@ -42,26 +42,26 @@ $lang['pages.delete_label'] 					= 'Verwijder';
 // titles
 $lang['pages.create_title'] 					= 'Voeg pagina toe';
 $lang['pages.edit_title'] 						= 'Wijzig pagina "%s"';
-$lang['pages.list_title'] 						= 'Toon alle pagina\'s';
+$lang['pages.list_title'] 						= 'Toon alle pagina&apos;s';
 $lang['pages.layouts_create_title'] 			= 'Voeg een paginalayout toe';
 $lang['pages.layouts_list_title'] 				= 'Toon alle paginalayouts';
 
 // messages
-$lang['pages.no_pages'] 						= 'Er zijn geen pagina\'s';
+$lang['pages.no_pages'] 						= 'Er zijn geen pagina&apos;s';
 $lang['pages_create_success'] 					= 'De pagina is aangemaakt.';
 $lang['pages_create_error'] 					= 'De pagina is niet aangemaakt.';
 $lang['pages_page_not_found_error'] 			= 'Die pagina bestaat niet';
 $lang['pages_edit_success'] 					= 'De pagina "%s" is opgeslagen.';
 $lang['pages_delete_home_error'] 				= 'U kunt de hoofdpagina niet verwijderen!';
 $lang['pages_delete_success']					= 'Pagina #%s is verwijderd.';
-$lang['pages_mass_delete_success'] 				= '%s pagina\'s zijn verwijderd.';
-$lang['pages_delete_none_notice'] 				= 'Er zijn geen pagina\'s verwijderd.';
+$lang['pages_mass_delete_success'] 				= '%s pagina&apos;s zijn verwijderd.';
+$lang['pages_delete_none_notice'] 				= 'Er zijn geen pagina&apos;s verwijderd.';
 $lang['pages_page_already_exist_error'] 		= 'De pagna met URL "%s" bestaat al in %s.';
 $lang['pages_parent_not_exist_error'] 			= 'De geselecteerde bovenliggende pagina bestaat niet.';
 
-$lang['pages.tree_explanation'] 				= 'De lijst aan de linkerkant stellen de pagina\'s op uw website voor. 
-Klik op het "+" icoon naast de pagina om de onderliggende pagina\'s te zien. Als u een pagina selecteerd ziet u  de informatie over de pagina in dit veld.';
+$lang['pages.tree_explanation'] 				= 'De lijst aan de linkerkant stellen de pagina&apos;s op uw website voor. 
+Klik op het "+" icoon naast de pagina om de onderliggende pagina&apos;s te zien. Als u een pagina selecteerd ziet u  de informatie over de pagina in dit veld.';
 
-$lang['pages.rss_explanation']					= 'Het inschakelen van RSS voor deze pagina laat gebruikers een abonnement op een RSS-feed nemen waarin onderliggende pagina\'s direct getoond worden.';
+$lang['pages.rss_explanation']					= 'Het inschakelen van RSS voor deze pagina laat gebruikers een abonnement op een RSS-feed nemen waarin onderliggende pagina&apos;s direct getoond worden.';
 
 ?>
