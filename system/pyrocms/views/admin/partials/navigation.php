16f60949f2a6a08d87d1f88eb3cf6949f1248ff9
jerel <mail@unruhdesigns.com>
2010-10-21 10:51:20 -0500

    Menu items in the admin panel are now hidden if user does not have permissions to view module.

--- system/pyrocms/views/admin/partials/navigation.php
+++ system/pyrocms/views/admin/partials/navigation.php
@@ -8,7 +8,11 @@
 					ksort($modules['content']);
 					foreach ($modules['content'] as $module):
 				?>
+				
+				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group_id === '1'): ?>
 				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], ($this->module == $module['slug']) ? 'class="current"' : '');?></li>
+				<?php endif; ?>
+				
 				<?php endforeach; ?>
 			</ul>
 		</li>
@@ -19,7 +23,11 @@
 					ksort($modules['design']);
 					foreach ($modules['design'] as $module):
 				?>
+				
+				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group_id === '1'): ?>
 				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], (($this->module == $module['slug']) ? 'class="current"' : ''));?></li>
+				<?php endif; ?>
+				
 				<?php endforeach; ?>
 			</ul>
 		</li>
@@ -27,13 +35,19 @@
 		<li>
 			<a href="#" class="top-link <?php echo (($this->module_details AND $this->module_details['menu'] == 'users') OR $this->module == 'users') ? 'current' : ''; ?>"><?php echo lang('cp_nav_users');?></a>
 			<ul>
+				<?php if(in_array('users', $this->permissions) OR $this->user->group_id === '1'): ?>
 				<li><?php echo anchor('admin/users', lang('cp_manage_users'), array('style' => 'font-weight: bold;', 'class' => $module == 'modules' ? 'current' : ''));?></li>
-
+				<?php endif; ?>
+				
 				<?php
 					ksort($modules['users']);
 					foreach ($modules['users'] as $module):
 				?>
+				
+				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group_id === '1'): ?>
 				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], (($this->module == $module['slug']) ? 'class="current"' : ''));?></li>
+				<?php endif; ?>
+				
 				<?php endforeach; ?>
 			</ul>
 		</li>
@@ -45,13 +59,21 @@
 					ksort($modules['utilities']);
 					foreach ($modules['utilities'] as $module):
 				?>
+				
+				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group_id === '1'): ?>
 				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], (($this->module == $module['slug']) ? 'class="current"' : ''));?></li>
+				<?php endif; ?>
+				
 				<?php endforeach; ?>
 			</ul>
 		</li>
 		
-
+		<?php if(in_array('settings', $this->permissions) OR $this->user->group_id === '1'): ?>
 		<li><?php echo anchor('admin/settings', lang('cp_nav_settings'), 'class="top-link no-submenu' . (($this->module == 'settings') ? ' current"' : '"'));?></li>
+		<?php endif; ?>
+		
+		<?php if(in_array('modules', $this->permissions) OR $this->user->group_id === '1'): ?>
 		<li><?php echo anchor('admin/modules', lang('cp_nav_addons'), 'class="last top-link no-submenu' . (($this->module == 'modules') ? ' current"' : '"'));?></li>
+		<?php endif; ?>
 	</ul>
 </nav>
