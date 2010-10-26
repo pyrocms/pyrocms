1fd836c7026878f0cd628ac93dbe5024dfccc1af
asturyan <mlemarchand@hubchannel.fr>
2010-10-26 15:55:37 +0200

    updated French language pack

--- system/pyrocms/modules/settings/language/french/settings_lang.php
+++ system/pyrocms/modules/settings/language/french/settings_lang.php
@@ -1,48 +1,48 @@
 <?php
 
-$lang['settings_save_success'] 		= 'Vos paramètres ont été enregistrés!';
-$lang['settings_edit_title'] 			= 'Modifier les paramètres';
+$lang['settings_save_success'] 					= 'Vos paramètres ont été enregistrés!';
+$lang['settings_edit_title'] 					= 'Modifier les paramètres';
 
 
 #section settings
-$lang['settings_site_name'] 					= 'Nom du site';#translate
-$lang['settings_site_name_desc'] 				= 'Le nom du site. Ce nom sera utilisé dans le titre des pages ainsi que dans d\'autres emplacemnts';#translate
+$lang['settings_site_name'] 					= 'Nom du site';
+$lang['settings_site_name_desc'] 				= 'Le nom du site. Ce nom sera utilisé dans le titre des pages ainsi que dans d\'autres emplacemnts';
 
-$lang['settings_site_slogan'] 					= 'Site Slogan';#translate
-$lang['settings_site_slogan_desc'] 				= 'The slogan of the website for page titles and for use around the site.';#translate
+$lang['settings_site_slogan'] 					= 'Slogan du site';
+$lang['settings_site_slogan_desc'] 				= 'Le slogan du site. Ce nom sera utilisé dans le titre des pages ainsi que dans d\'autres emplacements.';
 
-$lang['settings_contact_email'] 				= 'E-mail de contact';#translate
-$lang['settings_contact_email_desc'] 			= 'Tous les emails provenant des utilisateurs, des invités et du site seront adressés à cette adresse.';#translate
+$lang['settings_contact_email'] 				= 'E-mail de contact';
+$lang['settings_contact_email_desc'] 			= 'Tous les emails provenant des utilisateurs, des invités et du site seront adressés à cette adresse.';
 
-$lang['settings_server_email'] 					= 'E-mail du serveur';#translate
-$lang['settings_server_email_desc'] 			= 'Tous les emails seront envoyés depuis le site auront pour expéditeur cette adresse email.';#translate
+$lang['settings_server_email'] 					= 'E-mail du serveur';
+$lang['settings_server_email_desc'] 			= 'Tous les emails seront envoyés depuis le site auront pour expéditeur cette adresse email.';
 
-$lang['settings_meta_topic']					= 'Balise Meta';#translate
-$lang['settings_meta_topic_desc']				= 'Two or three words describing this type of company/website.';#translate
+$lang['settings_meta_topic']					= 'Balise Meta';
+$lang['settings_meta_topic_desc']				= 'Deux ou trois mots décrivant le type de société / site web.';
 
-$lang['settings_currency'] 						= 'Currency';#translate
-$lang['settings_currency_desc'] 				= 'The currency symbol for use on products, services, etc.';#translate
+$lang['settings_currency'] 						= 'Devise';
+$lang['settings_currency_desc'] 				= 'Le symbole de la monnaie pour une utilisation sur les produits, services, etc.';
 
-$lang['settings_dashboard_rss'] 				= 'Dashboard RSS Feed';#translate
-$lang['settings_dashboard_rss_desc'] 			= 'Link to an RSS feed that will be displayed on the dashboard.';#translate
+$lang['settings_dashboard_rss'] 				= 'Flux RSS du tableau de bord';
+$lang['settings_dashboard_rss_desc'] 			= 'Lien vers un flux RSS qui sera affiché sur le tableau de bord.';
 
 $lang['settings_dashboard_rss_count'] 			= 'Dashboard RSS Items';#translate
 $lang['settings_dashboard_rss_count_desc'] 		= 'How many RSS items would you like to display on the dashboard ?';#translate
 
-$lang['settings_frontend_enabled'] 				= 'Site Status';#translate
-$lang['settings_frontend_enabled_desc'] 		= 'Use this option to the user-facing part of the site on or off. Useful when you want to take the site down for maintenence';#translate
+$lang['settings_frontend_enabled'] 				= 'Statut du site';
+$lang['settings_frontend_enabled_desc'] 		= 'Utilisez cette option pour mettre en ligne ou hors ligne le site. Utile lorsque vous voulez mettre le site en maintenance.';
 
-$lang['settings_unavailable_message']			= 'Unavailable Message';#translate
-$lang['settings_unavailable_message_desc'] 		= 'When the site is turned off or there is a major problem, this message will show to users.';#translate
+$lang['settings_unavailable_message']			= 'Message non disponible';
+$lang['settings_unavailable_message_desc'] 		= 'Lorsque le site est en mode "hors ligne" ou s\'il ya un problème majeur, ce message sera indiqué aux utilisateurs.';
 
-$lang['settings_default_theme'] 				= 'Default Theme';#translate
-$lang['settings_default_theme_desc'] 			= 'Select the theme you want users to see by default.';#translate
+$lang['settings_default_theme'] 				= 'Thème par défaut';
+$lang['settings_default_theme_desc'] 			= 'Sélectionnez le thème que vous souhaitez que les utilisateurs voie par défaut.';
 
-$lang['settings_activation_email'] 				= 'Activation Email';#translate
-$lang['settings_activation_email_desc'] 		= 'Send out an e-mail when a user signs up with an activation link. Disable this to let only admins activate accounts.';#translate
+$lang['settings_activation_email'] 				= 'E-mail d\'activation';
+$lang['settings_activation_email_desc'] 		= 'Envoyer un e-mail quand un utilisateur s\'inscrit avec un lien d\'activation. Désactiver cette option pour laisser les admins activer les comptes.';
 
-$lang['settings_records_per_page'] 				= 'Records Per Page';#translate
-$lang['settings_records_per_page_desc'] 		= 'How many records should we show per page in the admin section?';#translate
+$lang['settings_records_per_page'] 				= 'Enregistrements par page';
+$lang['settings_records_per_page_desc'] 		= 'Combien d\'enregistrements devons nous montrer par page dans la section admin?';
 
 $lang['settings_rss_feed_items'] 				= 'Feed item count';#translate
 $lang['settings_rss_feed_items_desc'] 			= 'How many items should we show in RSS/news feeds?';#translate
@@ -50,14 +50,14 @@ $lang['settings_rss_feed_items_desc'] 			= 'How many items should we show in RSS
 $lang['settings_require_lastname'] 				= 'Require last names?';#translate
 $lang['settings_require_lastname_desc'] 		= 'For some situations, a last name may not be required. Do you want to force users to enter one or not?';#translate
 
-$lang['settings_enable_profiles'] 				= 'Enable profiles';#translate
-$lang['settings_enable_profiles_desc'] 			= 'Allow users to add and edit profiles.';#translate
+$lang['settings_enable_profiles'] 				= 'Activer les profils';
+$lang['settings_enable_profiles_desc'] 			= 'Permettre aux utilisateurs d\'ajouter et de modifier leurs profils.';#
 
-$lang['settings_google_analytic'] 				= 'Google Analytic';#translate
+$lang['settings_google_analytic'] 				= 'Google Analytic';
 $lang['settings_google_analytic_desc'] 			= 'Enter your analytic key to activate Google Analytic.';#translate
 
-$lang['settings_twitter_username'] 				= 'Username';#translate
-$lang['settings_twitter_username_desc'] 		= 'Twitter username.';#translate
+$lang['settings_twitter_username'] 				= 'Nom d\'utilisateur';
+$lang['settings_twitter_username_desc'] 		= 'Nom d\'utilisateur Twitter.';
 
 $lang['settings_twitter_consumer_key'] 			= 'Consumer Key';#translate
 $lang['settings_twitter_consumer_key_desc'] 	= 'Twitter consumer key.';#translate
@@ -77,25 +77,25 @@ $lang['settings_twitter_cache_desc'] 			= 'How many minutes should your Tweets b
 $lang['settings_akismet_api_key'] 				= 'Akismet API Key';#translate
 $lang['settings_akismet_api_key_desc'] 			= 'Akismet is a spam-blocker from the WordPress team. It keeps spam under control without forcing users to get past human-checking CAPTCHA forms.';#translate
 
-$lang['settings_moderate_comments'] 			= 'Modérer les commentaires';#translate
-$lang['settings_moderate_comments_desc']		= 'Exige l\'approbation des commentaires avant leur publication sur le site.';#translate
+$lang['settings_moderate_comments'] 			= 'Modérer les commentaires';
+$lang['settings_moderate_comments_desc']		= 'Exige l\'approbation des commentaires avant leur publication sur le site.';
 
-$lang['settings_version'] 						= 'Version';#translate
+$lang['settings_version'] 						= 'Version';
 $lang['settings_version_desc'] 					= '';
 
 
 #section titles
-$lang['settings_section_general']				= 'Général';#translate
-$lang['settings_section_integration']			= 'Integratie';#translate
-$lang['settings_section_users']					= 'Utilisateurs';#translate
-$lang['settings_section_statistics']			= 'Statistiques';#translate
-$lang['settings_section_twitter']				= 'Twitter';#translate
+$lang['settings_section_general']				= 'Général';
+$lang['settings_section_integration']			= 'Intégration';
+$lang['settings_section_users']					= 'Utilisateurs';
+$lang['settings_section_statistics']			= 'Statistiques';
+$lang['settings_section_twitter']				= 'Twitter';
 
 #checkbox and radio options
-$lang['settings_form_option_Open']				= 'Ouvrir';#translate
-$lang['settings_form_option_Closed']			= 'Fermé';#translate
-$lang['settings_form_option_Enabled']			= 'Actif';#translate
-$lang['settings_form_option_Disabled']			= 'Inactif';#translate
-$lang['settings_form_option_Required']			= 'Requis';#translate
-$lang['settings_form_option_Optional']			= 'Optionel';#translate
+$lang['settings_form_option_Open']				= 'Ouvrir';
+$lang['settings_form_option_Closed']			= 'Fermé';
+$lang['settings_form_option_Enabled']			= 'Actif';
+$lang['settings_form_option_Disabled']			= 'Inactif';
+$lang['settings_form_option_Required']			= 'Requis';
+$lang['settings_form_option_Optional']			= 'Optionnel';
 ?>
