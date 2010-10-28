33fa4b05b8e9562d5084cd3c487f8ad998ea9dc3
jerel <mail@unruhdesigns.com>
2010-10-28 18:59:49 -0500

    Made the galleries module compatible with installs that are not using mod_rewrite.

--- addons/modules/galleries/views/admin/edit.php
+++ addons/modules/galleries/views/admin/edit.php
@@ -4,7 +4,7 @@
 	<ul>
 		<li class="<?php echo alternator('', 'even'); ?>">
 			<label for="current_thumbnail"><?php echo lang('gallery_images.thumbnail_label'); ?></label>
-			<img id="current_thumbnail" src="<?php echo site_url('uploads/galleries/' . $gallery_image->slug . '/thumbs/' . $gallery_image->filename . '_thumb' . $gallery_image->extension); ?>" alt="<?php echo $gallery_image->title; ?>" />
+			<img id="current_thumbnail" src="<?php echo BASE_URL.'uploads/galleries/' . $gallery_image->slug . '/thumbs/' . $gallery_image->filename . '_thumb' . $gallery_image->extension; ?>" alt="<?php echo $gallery_image->title; ?>" />
 			<input type="hidden" id="thumb_width" name="thumb_width" />
 			<input type="hidden" id="thumb_height" name="thumb_height" />
 			<input type="hidden" id="thumb_x" name="thumb_x" />
@@ -12,7 +12,7 @@
 			<input type="hidden" id="scaled_height" name="scaled_height" />
 			<br />
 			<label for="crop-button"></label>
-			<a class="colorbox button" name="crop-button" href="<?php echo site_url('uploads/galleries/' . $gallery_image->slug . '/full/' . $gallery_image->filename . $gallery_image->extension); ?>"><?php echo lang('gallery_images.crop_label'); ?></a>
+			<a class="colorbox button" name="crop-button" href="<?php echo BASE_URL.'uploads/galleries/' . $gallery_image->slug . '/full/' . $gallery_image->filename . $gallery_image->extension; ?>"><?php echo lang('gallery_images.crop_label'); ?></a>
 			<br />
 			<br />
 			<span class="crop_options" style="display:none;">
