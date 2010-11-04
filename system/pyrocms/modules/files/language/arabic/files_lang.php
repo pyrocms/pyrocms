be3633e80899cc1b511ab3f121a9010230aed5ef
okhayat <okhayat@gmail.com>
2010-11-04 14:24:44 +0300

    Initial Arabic translation for 'Files' module.

---
+++ system/pyrocms/modules/files/language/arabic/files_lang.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * PyroCMS
+ *
+ * An open source CMS based on CodeIgniter
+ *
+ * @package		PyroCMS
+ * @author		PyroCMS Dev Team
+ * @license		Apache License v2.0
+ * @link		http://pyrocms.com
+ * @since		Version 1.0-dev
+ * @filesource
+ */
+
+$lang['files.title']						= 'الملفات';
+$lang['files.upload.title']					= 'رفع الملفات';
+$lang['files.labels.action']				= 'الإجراء';
+$lang['files.labels.edit']					= 'تعديل';
+$lang['files.labels.delete']				= 'حذف';
+$lang['files.labels.upload']				= 'رفع';
+$lang['files.labels.parent']				= 'المحتوي';
+$lang['files.no_files']						= 'لا يوجد أية ملفات.';
+
+$lang['files.folders.title']				= 'مجلدات الملفات';
+$lang['files.folders.manage_title']			= 'إدارة المجلدات';
+$lang['files.folders.label']				= 'مجلدات';
+$lang['files.folder.label']					= 'مجلد';
+$lang['files.subfolders.label']				= 'مجلدات فرعية';
+$lang['files.folders.name']					= 'الإسم';
+$lang['files.folders.slug']					= 'مختصر URL';
+$lang['files.folders.created']				= 'أنشئ في';
+$lang['files.folders.create']				= 'مجلد جديد';
+$lang['files.folders.success']				= 'تم حفظ المجلد.';
+$lang['files.folders.not_exists']			= 'تم اختيار مجلد غير صالح.';
+$lang['files.folders.no_subfolders']		= 'لاشيء';
+$lang['files.folders.no_folders']			= 'ملفاتك مرتبة ضمن مجلدات، وليس لديك حالياً أية مجلدات مُعدّة.';
+$lang['files.folders.delete_success']		= 'تم حذف المجلد "%s".';
+$lang['files.folders.delete_title']			= 'تأكيد الحذف';
+$lang['files.folders.confirm_delete']		= 'متأكد أنك تريد حذف "%s"؟<br /><br />هذا الأمر سيحذف أيضاً جميع الملفات والمجلدات التي ضمن هذا المجلد.';
+$lang['files.folders.mkdir']				= 'تعذر إنشاء دليل الملفات المرفوعة';
+$lang['files.folders.chmod']				= 'تعذر تنفيذ chmod على دليل الملفات المرفوعة';
+
+$lang['files.dropdown.no_subfolders']		= '-- لاشيء --';
+$lang['files.dropdown.root']				= '-- رئيسي --';
+
+$lang['files.description']					= 'الوصف';
+$lang['files.type']							= 'النوع';
+$lang['files.file']							= 'الملف';
+$lang['files.file_name']							= 'إسم الملف';
+$lang['files.success']				= 'تم حفظ الملف.';
+$lang['files.not_exists']			= 'تم اختيار مجلد غير صالح.';
+$lang['files.delete.error']			= 'تعذر حذف الملف.';
+$lang['files.delete.success']			= 'تم حذف الملف';
+
+// Types
+$lang['files.i'] = 'صورة';
+$lang['files.a'] = 'صوت';
+$lang['files.v'] = 'فيديو';
+$lang['files.d'] = 'مستند';
+$lang['files.o'] = 'آخر';
+
+/* End of file file_lang.php */
