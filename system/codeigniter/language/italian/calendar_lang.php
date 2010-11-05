69d5adcb33e80e7967b5ac1c8d808167494cf25a
tudo75 <tudo75@hotmail.com>
2010-11-05 19:58:59 +0100

    Italian translation of Codeigniter system
    Signed-off-by: tudo75 <tudo75@hotmail.com>

---
+++ system/codeigniter/language/italian/calendar_lang.php
@@ -0,0 +1,55 @@
+<?php
+/* Translation made By Alessandro Piconi: all update on invernomuto.net */
+/* Date 18/09/2009 */
+/* Modified by Nicola Tudino */
+/* Date 04/11/2010 */
+
+$lang['cal_su']			= "Do";
+$lang['cal_mo'] 		= "Lu";
+$lang['cal_tu'] 		= "Ma";
+$lang['cal_we'] 		= "Me";
+$lang['cal_th'] 		= "Gi";
+$lang['cal_fr'] 		= "Ve";
+$lang['cal_sa'] 		= "Sa";
+$lang['cal_sun'] 		= "Dom";
+$lang['cal_mon'] 		= "Lun";
+$lang['cal_tue'] 		= "Mar";
+$lang['cal_wed'] 		= "Mer";
+$lang['cal_thu'] 		= "Gio";
+$lang['cal_fri'] 		= "Ven";
+$lang['cal_sat'] 		= "Sab";
+$lang['cal_sunday']		= "Domenica";
+$lang['cal_monday']		= "Luned&igrave;";
+$lang['cal_tuesday']	= "Marted&igrave;";
+$lang['cal_wednesday']	= "Mercoled&igrave;";
+$lang['cal_thursday']	= "Gioved&igrave;";
+$lang['cal_friday']		= "Venerd&igrave;";
+$lang['cal_saturday']	= "Sabato";
+$lang['cal_jan'] 		= "Gen";
+$lang['cal_feb'] 		= "Feb";
+$lang['cal_mar'] 		= "Mar";
+$lang['cal_apr'] 		= "Apr";
+$lang['cal_may'] 		= "Mag";
+$lang['cal_jun'] 		= "Giu";
+$lang['cal_jul'] 		= "Lug";
+$lang['cal_aug'] 		= "Ago";
+$lang['cal_sep'] 		= "Set";
+$lang['cal_oct'] 		= "Ott";
+$lang['cal_nov'] 		= "Nov";
+$lang['cal_dec'] 		= "Dic";
+$lang['cal_january'] 	= "Gennaio";
+$lang['cal_february'] 	= "Febbraio";
+$lang['cal_march'] 		= "Marzo";
+$lang['cal_april']		= "Aprile";
+$lang['cal_mayl'] 		= "Maggio";
+$lang['cal_june'] 		= "Giugno";
+$lang['cal_july'] 		= "Luglio";
+$lang['cal_august']		= "Agosto";
+$lang['cal_september']	= "Settembre";
+$lang['cal_october'] 	= "Ottobre";
+$lang['cal_november']	= "Novembre";
+$lang['cal_december'] 	= "Dicembre";
+
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/italian/calendar_lang.php */
---
+++ system/codeigniter/language/italian/number_lang.php
@@ -0,0 +1,14 @@
+<?php
+/* Translation made By Alessandro Piconi: all update on invernomuto.net */
+/* Date 18/09/2009 */
+/* Modified by Nicola Tudino */
+/* Date 04/11/2010 */
+
+$lang['terabyte_abbr'] = "TB";
+$lang['gigabyte_abbr'] = "GB";
+$lang['megabyte_abbr'] = "MB";
+$lang['kilobyte_abbr'] = "KB";
+$lang['bytes'] = "Bytes";
+
+/* End of file number_lang.php */
+/* Location: ./system/language/italian/number_lang.php */
