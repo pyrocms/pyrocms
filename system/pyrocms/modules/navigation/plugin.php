3b3f5806d64ca09caba6584be35be571183848b6
Ben Edmunds <ben.edmunds@gmail.com>
2010-11-08 20:06:20 -0800

    Cache sucks ;)

--- system/pyrocms/modules/navigation/plugin.php
+++ system/pyrocms/modules/navigation/plugin.php
@@ -37,8 +37,8 @@ class Plugin_Navigation extends Plugin
 			$i = 1;
 			foreach ($links as $link)
 			{
-				$attributes['target'] = (isset($link->target)) ? $link->target : '';
-				$attributes['class']  = (isset($link->class))  ? $link->class  : '';
+				$attributes['target'] = $link->target;
+				$attributes['class']  = $link->class;
 
 				if (current_url() == $link->url)
 				{
