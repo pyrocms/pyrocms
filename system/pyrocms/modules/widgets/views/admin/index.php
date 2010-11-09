8a1fad7fe6f5bff43df3384f3be45dde560550d3
jerel <mail@unruhdesigns.com>
2010-11-09 00:21:22 -0600

    Made some tweaks to the Available Widgets view since I didn't care for this look all that much :P http://ubuntuone.com/p/ORK/

--- system/pyrocms/modules/widgets/views/admin/index.php
+++ system/pyrocms/modules/widgets/views/admin/index.php
@@ -95,7 +95,7 @@
 	<h3><?php echo lang('widgets.available_title')?></h3>
 
 		<?php foreach($available_widgets as $widget): ?>
-		<section id="widget-<?php echo $widget->slug; ?>" class="box widget-box" style="<?php echo alternator('clear:both;', '', '', ''); ?>">
+		<section id="widget-<?php echo $widget->slug; ?>" class="box widget-box" >
 			<header>
 				<h3 class="title"><?php echo $widget->title; ?></h3>
 			</header>
