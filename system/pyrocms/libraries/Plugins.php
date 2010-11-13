01e50896b5f5dcfc045bf9f2a78a7b6d54197ecf
Phil Sturgeon <email@philsturgeon.co.uk>
2010-11-13 21:09:29 +0000

    Stop plugins checking for method. Quicker, allows support for __call() and gives a more useful error message when a non-existent method is called.

--- system/pyrocms/libraries/Plugins.php
+++ system/pyrocms/libraries/Plugins.php
@@ -19,7 +19,7 @@ abstract class Plugin
 
     function __get($var)
     {
-		return CI_Base::get_instance()->$var;
+		return get_instance()->$var;
     }
 
 	// ------------------------------------------------------------------------
@@ -136,7 +136,7 @@ class Plugins
 	private function _process($path, $class, $method, $data)
 	{
 		$class_name = 'Plugin_'.ucfirst(strtolower($class));
-		
+
 		if ( ! isset($this->instances[$class]))
 		{
 			// Load it up
@@ -152,7 +152,7 @@ class Plugins
 			$this->instances[$class]->set_data($data);
 		}
 
-		if ( ! class_exists($class_name) OR ! method_exists($class_name, $method))
+		if ( ! class_exists($class_name))
 		{
 			throw new Exception('Plugin "'.$class_name.'" does not exist.');
 			return FALSE;
