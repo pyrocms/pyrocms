d52d0fb3b90fa41bb61005e30e06dd408c79b112
Phil Sturgeon <email@philsturgeon.co.uk>
2010-11-14 00:16:05 +0000

    Added missing italian and polish lang files to galleries module.

---
+++ addons/modules/galleries/language/polish/galleries_lang.php
@@ -0,0 +1,53 @@
+<?php
+// Success notifications
+$lang['galleries.create_success'] 	= 'The gallery has been created successfully.';
+$lang['galleries.update_success'] 	= 'The gallery has been successfully updated.';
+$lang['galleries.delete_success']	= 'All galleries have been deleted successfully.';
+$lang['galleries.install_success']	= 'The module has been installed successfully.';
+
+// Error notifications
+$lang['galleries.create_error']			= 'The gallery could not be created.';
+$lang['galleries.exists_error']			= 'The specified gallery does not exist.';
+$lang['galleries.update_error']			= 'The gallery could not be updated.';
+$lang['galleries.id_error']				= 'No IDs have been specified.';
+$lang['galleries.folder_error']			= 'The "%s" gallery folders could not be deleted.';
+$lang['galleries.delete_error']			= 'The "%s" gallery could not be deleted.';
+$lang['galleries.no_galleries_error'] 	= 'No galleries have been created yet.';
+$lang['galleries.install_error']		= 'The module could not be installed.';
+
+// Labels
+$lang['galleries.galleries_label']			= 'Galleries';
+$lang['galleries.manage_gallery_label']		= 'Manage Gallery';
+$lang['galleries.new_gallery_label']		= 'New Gallery';
+
+$lang['galleries.album_label']				= 'Album';
+$lang['galleries.num_photos_label']			= 'Number of Photos';
+$lang['galleries.updated_label']			= 'Last Updated';
+$lang['galleries.actions_label']			= 'Actions';
+$lang['galleries.view_label']				= 'View';
+$lang['galleries.manage_label']				= 'Manage';
+$lang['galleries.delete_label']				= 'Delete';
+$lang['galleries.comments_enabled_label']	= 'Yes';
+$lang['galleries.comments_disabled_label']	= 'No';
+$lang['galleries.published_yes_label']		= 'Yes';
+$lang['galleries.published_no_label']		= 'No';
+
+// General labels
+$lang['galleries.title_label']				= 'Title';
+$lang['galleries.slug_label']				= 'Slug';
+$lang['galleries.parent_label']				= 'Parent';
+$lang['galleries.none_label']				= '-- NONE --';
+$lang['galleries.description_label']		= 'Album Description';
+$lang['galleries.thumbnail_label']			= 'Thumbnail';
+$lang['galleries.comments_label']			= 'Enable Comments';
+$lang['galleries.published_label']			= 'Published';
+$lang['galleries.current_label']			= 'Current Images (drag and drop to sort, click to edit)';
+
+// Sidebar labels
+$lang['galleries.list_label']				= 'List Galleries';
+$lang['galleries.new_gallery_label']		= 'Create a new gallery';
+$lang['galleries.upload_label']				= 'Upload Image';
+
+// Frontend labels
+$lang['galleries.sub-galleries_label']		= 'Sub Galleries';
+?>
