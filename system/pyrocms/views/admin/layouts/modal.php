9bafe208944e33261f8770820f76e4a1a22cca21
Phil Sturgeon <email@philsturgeon.co.uk>
2010-11-20 00:21:23 +0800

    A bunch of changes.

--- system/pyrocms/views/admin/layouts/modal.php
+++ system/pyrocms/views/admin/layouts/modal.php
@@ -6,8 +6,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<!-- Grab Google CDNs jQuery, fall back if necessary -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-	<script>!window.jQuery && document.write('<script src="<?php echo js_path('jquery/jquery-1.4.2.min.js'); ?>"><\/script>')</script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+	<script>!window.jQuery && document.write('<script src="<?php echo js_path('jquery/jquery.js'); ?>"><\/script>')</script>
 
 	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 
