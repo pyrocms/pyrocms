baf67452dc03b2a1969f617da3ced885d083e028
okhayat <okhayat@gmail.com>
2010-11-27 07:38:38 +0800

    - Pages module Arabic translation

---
+++ system/pyrocms/modules/pages/language/arabic/page_layouts_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+// tabs
+$lang['page_layouts.html_label'] 					= 'HTML';
+$lang['page_layouts.css_label'] 					= 'CSS';
+
+// labels
+$lang['page_layouts.updated_label'] 				= 'آخر تحديث';
+$lang['page_layouts.actions_label'] 				= 'إجراءات';
+$lang['page_layouts.title_label'] 					= 'العنوان';
+$lang['page_layouts.theme_layout_label'] 		= 'تخطيط السّمة';
+
+$lang['page_layouts.edit_label'] 					= 'تعديل';
+$lang['page_layouts.delete_label'] 					= 'حذف';
+
+// titles
+$lang['page_layouts.list_title'] 					= 'سرد مُخططات الصفحات';
+$lang['page_layouts.create_title'] 					= 'إضافة تخطيط صفحة';
+$lang['page_layouts.edit_title'] 					= 'تعديل تخطيط الصفحة "%s"';
+
+// messages
+$lang['page_layouts.no_pages'] 						= 'لا يوجد أي تخطيطات صفحات.';
+$lang['page_layouts.create_success'] 				= 'تم إنشاء تخطيط الصفحة.';
+$lang['page_layouts.create_error'] 					= 'لم يتم إنشاء تخطيط الصفحة.';
+$lang['page_layouts.page_layout.not_found_error']	= 'تخطيط الصفحة هذا غير موجود.';
+$lang['page_layouts.edit_success'] 					= 'تم حفظ تخطيط الصفحة "%s".';
+$lang['page_layouts.delete_home_error'] 			= 'لا يمكنك حذف التخطيط الإفتراضي.';
+$lang['page_layouts.delete_success']				= 'تم حذف تخطيط الصفحة #%s.';
+$lang['page_layouts.mass_delete_success'] 			= 'تم حذف %s تخطيط صفحة.';
+$lang['page_layouts.delete_none_notice'] 			= 'لم يتم حذف أي تخطيط صفحة.';
+$lang['page_layouts.already_exist_error'] 			= 'هناك تخطيط صفحة له اسم "%s" موجود مسبقاً.';
+
+$lang['page_layouts.variable_introduction']			= 'في مربّع الإدخال هذا يوجد متغيّران متوفّران.';
+$lang['page_layouts.variable_title']				= 'يحتوي عنوان الصفحة.';
+$lang['page_layouts.variable_body']					= 'يحتوي نص HTML للصفحة.';
