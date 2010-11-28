9a3f4f1d5b03b4de6a8323347660f10f7143b502
Eric <eric@ericlbarnes.com>
2010-11-27 23:26:52 -0500

    Fixed a bug in variables library where it would return the name but not the value.

--- system/pyrocms/modules/variables/libraries/Variables.php
+++ system/pyrocms/modules/variables/libraries/Variables.php
@@ -1,41 +1,48 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-
+/**
+ * Variable Library
+ *
+ * Handles the variables data
+ *
+ * @package		PyroCMS
+ * @author		PyroCMS Dev Team
+ * @copyright	Copyright (c) 2008 - 2010, PyroCMS
+ *
+ */
 class Variables {
 
 	private $CI;
-	
+
 	private $_vars = array();
-	
-	function __construct() {
+
+	public function __construct()
+	{
 		$this->CI =& get_instance();
 		$this->CI->load->model('variables/variables_m');
-		
+
 		$vars = $this->CI->variables_m->get_all();
-		
-		foreach($vars as $var)
+
+		foreach ($vars as $var)
 		{
-			$this->_vars[ $var->name ] = $var->data;
+			$this->_vars[$var->name] = $var->data;
 		}
-		
-		// unset($_items);
 	}
-    
-	function __get($name) {
-		
+
+	function __get($name)
+	{
 		// getting data
-        
-        if( isset( $this->_vars->$name ) )
+        if (isset($this->_vars->$name))
         {
-            return $name;
+            return $this->_vars[$name];
         }
-        
+
         return NULL;
-         
 	}
-    
+
     public function get_all()
     {
     	return $this->_vars;
     }
 
 }
+/* End of file Variables.php */
