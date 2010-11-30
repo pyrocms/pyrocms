aca03ff1d0dc30006097938eba77deb39b4c86a0
okhayat <okhayat@gmail.com>
2010-11-30 16:56:39 +0300

    - Language files

---
+++ addons/modules/galleries/language/arabic/galleries_lang.php
@@ -0,0 +1,53 @@
+<?php
+// Success notifications
+$lang['galleries.create_success'] 	= 'تم إنشاء معرض الصّور بنجاح.';
+$lang['galleries.update_success'] 	= 'تم تحديث معرض الصّور بنجاح.';
+$lang['galleries.delete_success']	= 'تم حذف جميع معارض الصّور بنجاح.';
+$lang['galleries.install_success']	= 'تم تثبيت الوحدة بنجاح.';
+
+// Error notifications
+$lang['galleries.create_error']			= 'تعذّر إنشاء معرض الصّور.';
+$lang['galleries.exists_error']			= 'معرض الصّور المُحدّد غير موجود.';
+$lang['galleries.update_error']			= 'تعذّر تحديث معرض الصّور.';
+$lang['galleries.id_error']				= 'لم يُحدّد أي رقم مُعرّف.';
+$lang['galleries.folder_error']			= 'تعذّر حذف مجلّدات معرض الصّور "%s".';
+$lang['galleries.delete_error']			= 'تعذّر حذف معرض الصّوَر "%s".';
+$lang['galleries.no_galleries_error'] 	= 'لم يُنشأ أي معرض صُوَر بعد.';
+$lang['galleries.install_error']		= 'تعذّر تثبيت الوحدة.';
+
+// Labels
+$lang['galleries.galleries_label']			= 'معارض الصّور';
+$lang['galleries.manage_gallery_label']		= 'إدارة معرض الصّور';
+$lang['galleries.new_gallery_label']		= 'معرض صوَر جديد';
+
+$lang['galleries.album_label']				= 'الألبوم';
+$lang['galleries.num_photos_label']			= 'عدد الصّور';
+$lang['galleries.updated_label']			= 'آخر تحديث';
+$lang['galleries.actions_label']			= 'الإجراءات';
+$lang['galleries.view_label']				= 'عرض';
+$lang['galleries.manage_label']				= 'إدارة';
+$lang['galleries.delete_label']				= 'حذف';
+$lang['galleries.comments_enabled_label']	= 'نعم';
+$lang['galleries.comments_disabled_label']	= 'لا';
+$lang['galleries.published_yes_label']		= 'نعم';
+$lang['galleries.published_no_label']		= 'لا';
+
+// General labels
+$lang['galleries.title_label']				= 'العنوان';
+$lang['galleries.slug_label']				= 'مُختصر الرابط';
+$lang['galleries.parent_label']				= 'المُحتوي';
+$lang['galleries.none_label']				= '-- لاشيء --';
+$lang['galleries.description_label']		= 'وصْف الألبوم';
+$lang['galleries.thumbnail_label']			= 'مُصغّر';
+$lang['galleries.comments_label']			= 'تمكين التعليقات';
+$lang['galleries.published_label']			= 'منشور';
+$lang['galleries.current_label']			= 'الصّوَر الحاليّة (حرك الصّور لإعادة ترتيبها، واضغط على أي صورة لتعديلها).';
+
+// Sidebar labels
+$lang['galleries.list_label']				= 'سرْد معارض الصّور';
+$lang['galleries.new_gallery_label']		= 'إنشاء معرض صوَر جديد';
+$lang['galleries.upload_label']				= 'رفع صورة';
+
+// Frontend labels
+$lang['galleries.sub-galleries_label']		= 'معارض الصّور الفرعيّة';
+?>
