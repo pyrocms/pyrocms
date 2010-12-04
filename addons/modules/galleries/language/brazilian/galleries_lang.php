30687a0e2d853ba0066d503a0c355250427b4b5d
MarcosCoelho <marcos@marcoscoelho.com>
2010-12-04 18:37:36 -0200

    Add brazilian language in module addon galleries

---
+++ addons/modules/galleries/language/brazilian/galleries_lang.php
@@ -0,0 +1,53 @@
+<?php
+// Success notifications
+$lang['galleries.create_success'] 	= 'A galeria foi criada com sucesso.';
+$lang['galleries.update_success'] 	= 'A galeria foi atualizada com sucesso.';
+$lang['galleries.delete_success']	= 'Todas as galerias foram atualizadas com sucesso.';
+$lang['galleries.install_success']	= 'O módulo foi instalado com sucesso.';
+
+// Error notifications
+$lang['galleries.create_error']			= 'A galeria não pode ser criada.';
+$lang['galleries.exists_error']			= 'A galeria especificada não existe.';
+$lang['galleries.update_error']			= 'A galeria não pode ser atualizada.';
+$lang['galleries.id_error']				= 'No IDs have been specified.'; #translate
+$lang['galleries.folder_error']			= 'The "%s" gallery folders could not be deleted.'; #translate
+$lang['galleries.delete_error']			= 'A galeria "%s" não pode ser removida.';
+$lang['galleries.no_galleries_error'] 	= 'Nenhuma galeria foi criada ainda.';
+$lang['galleries.install_error']		= 'O módulo não pode ser instalado.';
+
+// Labels
+$lang['galleries.galleries_label']			= 'Galerias';
+$lang['galleries.manage_gallery_label']		= 'Gerenciar galeria';
+$lang['galleries.new_gallery_label']		= 'Nova galeria';
+
+$lang['galleries.album_label']				= 'Álbum';
+$lang['galleries.num_photos_label']			= 'Número de fotos';
+$lang['galleries.updated_label']			= 'Última atualização';
+$lang['galleries.actions_label']			= 'Ações';
+$lang['galleries.view_label']				= 'Ver';
+$lang['galleries.manage_label']				= 'Gerenciar';
+$lang['galleries.delete_label']				= 'Remover';
+$lang['galleries.comments_enabled_label']	= 'Sim';
+$lang['galleries.comments_disabled_label']	= 'Não';
+$lang['galleries.published_yes_label']		= 'Sim';
+$lang['galleries.published_no_label']		= 'Não';
+
+// General labels
+$lang['galleries.title_label']				= 'Título';
+$lang['galleries.slug_label']				= 'Apelido';
+$lang['galleries.parent_label']				= 'Pai';
+$lang['galleries.none_label']				= '-- NENHUM --';
+$lang['galleries.description_label']		= 'Descrição do álbum';
+$lang['galleries.thumbnail_label']			= 'Mimiatura';
+$lang['galleries.comments_label']			= 'Ativar comentários';
+$lang['galleries.published_label']			= 'Publicada';
+$lang['galleries.current_label']			= 'Imagens atuais (arraste e solte para organizar, clique para editar)';
+
+// Sidebar labels
+$lang['galleries.list_label']				= 'Listar galerias';
+$lang['galleries.new_gallery_label']		= 'Criar nova galeria';
+$lang['galleries.upload_label']				= 'Enviar imagem';
+
+// Frontend labels
+$lang['galleries.sub-galleries_label']		= 'Sub-galerias';
+?>
