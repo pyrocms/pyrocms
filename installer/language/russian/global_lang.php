60956bbeaaaf328839567f6dd37965d0ceb01f01
Dark Preacher <dark@darklab.ru>
2010-12-16 23:34:47 +0300

    Translated missing strings for installer

--- installer/language/russian/global_lang.php
+++ installer/language/russian/global_lang.php
@@ -7,5 +7,5 @@ $lang['step3']	=	'Шаг #3';
 $lang['step4']	=	'Шаг #4';
 $lang['final']	=	'Завершение';
 
-$lang['installer.passwords_match']		= "Passwords Match."; #translate
-$lang['installer.passwords_dont_match']	= "Passwords Don\'t Match."; #translate
+$lang['installer.passwords_match']				= "Пароли совпадают.";
+$lang['installer.passwords_dont_match']		= "Пароли не совпадают.";
