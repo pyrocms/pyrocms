71f2df20352541a54331e40fc9416f164a4b85b6
J Bertoen <jbertoen@gmail.com>
2011-01-25 13:34:08 +0100

    It is now possible to add your own main menu button in the admin template navigation. Only thing you need to do is enter your title in the public function info() in details.php public function info(){
    		'menu' => 'Zmash IT'
    		);
    	}

--- system/pyrocms/views/admin/partials/navigation.php
+++ system/pyrocms/views/admin/partials/navigation.php
@@ -3,90 +3,70 @@
 	<ul>
 		<li><?php echo anchor('admin', lang('cp_admin_home_title'), 'class="top-link no-submenu' . (!$this->module > '' ? ' current' : '').'"');?></li>
 		
-		<?php $display = ''; foreach($modules['content'] as $module) if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin') $display = 1; ?>
-		<?php if($display === 1): ?>
-		<li><a href="#" class="top-link <?php echo ($this->module_details AND $this->module_details['menu'] == 'content') ? 'current' : ''; ?>"><?php echo lang('cp_nav_content');?></a>
-			<ul>
-				<?php
-					ksort($modules['content']);
-					foreach ($modules['content'] as $module):
-				?>
-				
-				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin'): ?>
-				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], ($this->module == $module['slug']) ? 'class="current"' : '');?></li>
-				<?php endif; ?>
-				
-				<?php endforeach; ?>
-			</ul>
-		</li>
-		<?php endif; ?>
+        
+        <?php
 		
-		<?php $display = ''; foreach($modules['design'] as $module) if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin') $display = 1; ?>
-		<?php if($display === 1): ?>
-		<li><a href="#" class="top-link <?php echo ($this->module_details AND $this->module_details['menu'] == 'design') ? 'current' : ''; ?>"><?php echo lang('cp_nav_design');?></a>
-			<ul>
-				<?php
-					ksort($modules['design']);
-					foreach ($modules['design'] as $module):
+		foreach($menu_items as $menu_item){
+			
+			$display = '';
+			
+			foreach($modules[$menu_item] as $module){
+				
+				if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin'){
+					
+					$display = 1;
+				
+				}
+				
+			}
+				
+			if($display === 1){
+				
 				?>
 				
-				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin'): ?>
-				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], (($this->module == $module['slug']) ? 'class="current"' : ''));?></li>
-				<?php endif; ?>
+				<li><a href="#" class="top-link <?php echo ($this->module_details AND $this->module_details['menu'] == $menu_item) ? 'current' : ''; ?>">
+				<?php
 				
-				<?php endforeach; ?>
-			</ul>
-		</li>
-		<?php endif; ?>
+					if(lang('cp_nav_'.$menu_item)!=''&&lang('cp_nav_'.$menu_item)!=NULL){
+						echo lang('cp_nav_'.$menu_item);	
+					}
+					else{
+						echo $menu_item;
+					}				
+				?>
+                </a>
+                    <ul>
+                    <?php
+                    
+                    ksort($modules[$menu_item]);
+                    foreach ($modules[$menu_item] as $module){
+                        
+                        if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin'){
+                        
+                            if(lang('cp_nav_'.$module['name'])!=''&&lang('cp_nav_'.$module['name'])!=NULL){
+                                $module['name'] = lang('cp_manage_'.$module['slug']);	
+                            }
+                            ?>
+                            <li><?php echo anchor('admin/'.$module['slug'], $module['name'], ($this->module == $module['slug']) ? 'class="current"' : '');?></li>                                         
+                        <?php
+                        
+                        }					
+                    }
+                    
+                    ?>
+                    </ul>
+                </li>
+                <?php
+			}
+		}
 		
-		<?php $display = ''; foreach($modules['users'] as $module) if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin') $display = 1; ?>
-		<?php if($display === 1): ?>
-		<li>
-			<a href="#" class="top-link <?php echo (($this->module_details AND $this->module_details['menu'] == 'users') OR $this->module == 'users') ? 'current' : ''; ?>"><?php echo lang('cp_nav_users');?></a>
-			<ul>
-				<?php if(in_array('users', $this->permissions) OR $this->user->group == 'admin'): ?>
-				<li><?php echo anchor('admin/users', lang('cp_manage_users'), array('style' => 'font-weight: bold;', 'class' => $module == 'modules' ? 'current' : ''));?></li>
-				<?php endif; ?>
-				
-				<?php
-					ksort($modules['users']);
-					foreach ($modules['users'] as $module):
-				?>
-				
-				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin'): ?>
-				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], ($this->module == $module['slug']) ? 'class="current"' : '');?></li>
-				<?php endif; ?>
-				
-				<?php endforeach; ?>
-			</ul>
-		</li>
-		<?php endif; ?>
-
-		<?php $display = ''; foreach($modules['utilities'] as $module) if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin') $display = 1; ?>
-		<?php if($display === 1): ?>
-		<li>
-			<a href="#" class="top-link <?php echo (($this->module_details AND $this->module_details['menu'] == 'utilities') OR $this->module == 'utilities') ? 'current' : ''; ?>"><?php echo lang('cp_nav_utilities');?></a>
-			<ul>
-				<?php
-					ksort($modules['utilities']);
-					foreach ($modules['utilities'] as $module):
-				?>
-				
-				<?php if(in_array($module['slug'], $this->permissions) OR $this->user->group == 'admin'): ?>
-				<li><?php echo anchor('admin/'.$module['slug'], $module['name'], (($this->module == $module['slug']) ? 'class="current"' : ''));?></li>
-				<?php endif; ?>
-				
-				<?php endforeach; ?>
-			</ul>
-		</li>
-		<?php endif; ?>
-		
-		<?php if(in_array('settings', $this->permissions) OR $this->user->group == 'admin'): ?>
+		if(in_array('settings', $this->permissions) OR $this->user->group == 'admin'): ?>
 		<li><?php echo anchor('admin/settings', lang('cp_nav_settings'), 'class="top-link no-submenu' . (($this->module == 'settings') ? ' current"' : '"'));?></li>
 		<?php endif; ?>
 		
 		<?php if(in_array('modules', $this->permissions) OR $this->user->group == 'admin'): ?>
 		<li><?php echo anchor('admin/modules', lang('cp_nav_addons'), 'class="last top-link no-submenu' . (($this->module == 'modules') ? ' current"' : '"'));?></li>
-		<?php endif; ?>
+		<?php endif; ?>        
+        
 	</ul>
 </nav>
