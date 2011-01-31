ea408ac397173a920f2e05810bd8df848d3413a3
Kieran Klaassen <kieranklaassen@gmail.com>
2011-01-31 15:20:00 +0100

    added the html5boilerplate of Paul Irish as a PyroCMS theme to start of working
    Signed-off-by: Kieran Klaassen <kieranklaassen@gmail.com>

---
+++ system/pyrocms/themes/html5boilerplate/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_html5boilerplate extends Theme {
+
+    public $name = 'HTML 5 Boilerplate';
+    public $author = 'Paul Irish';
+    public $author_website = 'http://html5boilerplate.com/';
+    public $website = 'http://www.pyrocms.com/';
+    public $description = 'Ported the html5boilerplate as a starting point for your own theme';
+    public $version = '0.9.5';
+
+}
+
+/* End of file theme.php */
