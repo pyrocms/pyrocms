00cd5572aa2b0b15aef4f623af3190791916e697
jerel <mail@unruhdesigns.com>
2011-02-28 21:44:55 -0600

    Added widget database record updates to the News->Blog switch to make upgrading existing sites simpler

--- system/pyrocms/migrations/013_Rename_news_to_blog.php
+++ system/pyrocms/migrations/013_Rename_news_to_blog.php
@@ -11,6 +11,15 @@ class Migration_Rename_news_to_blog extends Migration {
 		$this->db->query("UPDATE modules SET `name` = 'a:1:{s:2:\"en\";s:4:\"Blog\";}', slug = 'blog', `version` = 1.1 WHERE slug = 'news' ");
 
 		$this->db->query("UPDATE navigation_links SET module_name = 'blog' WHERE module_name = 'news' ");
+		
+		//rename the widgets
+		$this->db->where('slug', 'news_categories')
+				 ->update('widgets', array('slug' => 'blog_categories',
+										   'description' => 'Show a list of blog categories.'));
+				 
+		$this->db->where('slug', 'latest_news')
+				 ->update('widgets', array('slug' => 'latest_articles',
+										   'description' => 'Display latest blog articles with a widget.'));
 
 		$this->cache->delete_all('modules_m');
 		$this->cache->delete_all('navigation_m');
@@ -25,6 +34,15 @@ class Migration_Rename_news_to_blog extends Migration {
 		$this->db->query("UPDATE modules SET `name` = 'a:1:{s:2:\"en\";s:4:\"News\";}', slug = 'news', `version` = 1.0 WHERE slug = 'blog' ");
 		
 		$this->db->query("UPDATE navigation_links SET module_name = 'news' WHERE module_name = 'blog' ");
+		
+		//rename the widgets
+		$this->db->where('slug', 'blog_categories')
+				 ->update('widgets', array('slug' => 'news_categories',
+										   'description' => 'Show a list of news categories.'));
+				 
+		$this->db->where('slug', 'latest_articles')
+				 ->update('widgets', array('slug' => 'latest_news',
+										   'description' => 'Display latest news articles with a widget.'));
 
 		$this->cache->delete_all('modules_m');
 		$this->cache->delete_all('navigation_m');
