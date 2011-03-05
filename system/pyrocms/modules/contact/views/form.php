551b130cf29064d43ed51eca215cce40b0321a00
alfierivera <alfredo_rivera340@hotmail.com>
2011-03-05 10:52:45 -0800

    Fixed typo on the form view.

--- system/pyrocms/modules/contact/views/form.php
+++ system/pyrocms/modules/contact/views/form.php
@@ -6,7 +6,7 @@
 <?php echo form_open(current_url());?>
 
 	<p>
-		<label for="contact_email"><?php echo lang('contact_name_label');?></label>
+		<label for="contact_name"><?php echo lang('contact_name_label');?></label>
 		<?php echo form_input('contact_name', $form_values->contact_name);?>
 	</p>
 	<p>
@@ -14,11 +14,11 @@
 		<?php echo form_input('contact_email', $form_values->contact_email);?>
 	</p>
 	<p>
-		<label for="contact_email"><?php echo lang('contact_company_name_label');?></label>
+		<label for="company_name"><?php echo lang('contact_company_name_label');?></label>
 		<?php echo form_input('company_name', $form_values->company_name);?>
 	</p>
 	<p>
-		<label for="contact_email"><?php echo lang('contact_subject_label');?></label>
+		<label for="subject"><?php echo lang('contact_subject_label');?></label>
 		<?php echo form_dropdown('subject', $subjects, $form_values->subject, 'id="subject"'); ?>
 		<input id="other_subject" name="other_subject" type="text" />
 	</p>
