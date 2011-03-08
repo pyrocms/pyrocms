b49e55e54670fe4e1a263f2f28c51975e2755bf2
okhayat <okhayat@gmail.com>
2011-03-09 02:56:23 +0300

    - Chunks Arabic translation

---
+++ language/arabic/chunks_lang.php
@@ -0,0 +1,39 @@
+<?php
+
+	// -------------------------------------
+	// Titles
+	// -------------------------------------
+
+	$lang['chunks.list_chunks']							= 'سرد القصاصات';
+	$lang['chunks.add_chunk']							= 'أضف قصاصة';
+	$lang['chunks.edit_chunk']							= 'عدّل القصاصة "%s"';
+
+	// -------------------------------------
+	// Messages
+	// -------------------------------------
+
+	$lang['chunks.no_chunks']							= 'لا يوج أي قصاصات في هذا النظام.';
+	$lang['chunks.new_chunk_success']					= 'تمت إضافة القصاصة بنجاح';
+	$lang['chunks.new_chunk_error']						= 'حدث خطأ أثناء إضافة هذه القصاصة';
+	$lang['chunks.delete_chunk_success']				= 'تم حذف القصاصة بنجاح';
+	$lang['chunks.delete_chunk_error']					= 'حدث خطأ أثناء حذف هذه القصاصة';
+	$lang['chunks.update_chunk_success']				= 'تم تحديث القصاصة بنجاح';
+	$lang['chunks.update_chunk_error']					= 'حدث خطأ أثناء تحديث هذه القصاصة';
+
+	// -------------------------------------
+	// Validation
+	// -------------------------------------
+
+	$lang['chunks.slug_unique']							= 'يجب أن يكون عنوان القصاصة المختصر مُميزاً.';
+
+	// -------------------------------------
+	// Form Tiles
+	// -------------------------------------
+
+	$lang['chunks.chunk_name']							= 'إسم القصاصة';
+	$lang['chunks.chunk_slug']							= 'عنوان القصاصة المختصر';
+	$lang['chunks.chunk_type']							= 'نوع القصاصة';
+	$lang['chunks.chunk_content']						= 'محتوى القصاصة';
+
+/* End of file chunks_lang.php */
+/* Location: ./third_party/modules/chunks/language/arabic/chunks_lang.php */
