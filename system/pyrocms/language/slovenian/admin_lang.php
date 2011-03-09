479af375f4666e40851030e0526a9402572d4c1e
imamo <info@imamo.si>
2011-03-09 01:08:30 +0100

    Slovenian translation of language folder

---
+++ system/pyrocms/language/slovenian/admin_lang.php
@@ -0,0 +1,54 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cp_admin_title'] 				= 'Nadzorna plošča';
+$lang['cp_admin_home_title'] 			= 'Namizje';
+$lang['cp_admin_quick_links']			= 'Hitre povezave';
+
+$lang['cp_welcome_title'] 				= 'Dobrodošli';
+$lang['cp_welcome_message'] 			= 'Welcome to the %s Control Panel. Using the links above and to the left you can control almost every aspect of your website. If you happen to have any problems with the system, please submit your issue to our bugtracker.';
+
+$lang['cp_blog_feed_title'] 			= 'Zadnje novice';
+
+// Shortcuts
+$lang['cp_manage_comments']				= 'Uredi komentarje';
+$lang['cp_manage_pages']				= 'Uredi strani';
+$lang['cp_manage_files']				= 'Uredi datoteke';
+$lang['cp_manage_users']				= 'Uredi uporabnike';
+
+// Header
+$lang['cp_to_home'] 					= 'Nazaj na namizje';
+$lang['cp_view_frontend'] 				= 'Ogled strani';
+$lang['cp_logged_in_welcome'] 			= 'Zdravo %s';
+$lang['cp_logout_label'] 				= 'Odjava';
+$lang['cp_edit_profile_label'] 			= 'Uredi profil';
+
+// Footer
+$lang['cp_change_language'] 			= 'Spremeni jezik';
+$lang['cp_documentation'] 				= 'Dokumentacija';
+$lang['cp_report_bug'] 					= 'Prijava napake';
+$lang['cp_contact_support'] 			= 'Podpora';
+
+$lang['cp_nav_settings']				= 'Nastavitve';
+$lang['cp_nav_addons']					= 'Razširitve';
+$lang['cp_nav_modules']					= 'Moduli';
+$lang['cp_nav_widgets']					= 'Vtičniki';
+$lang['cp_nav_users']					= 'Uporabniki';
+$lang['cp_nav_design']					= 'Izgled';
+$lang['cp_nav_content']					= 'Vsebina';
+$lang['cp_nav_utilities']				= 'Pripomočki';
+
+$lang['cp_must_login']	 				= 'Morate biti prijavljeni za ogled te strani.';
+$lang['cp_access_denied'] 				= 'Nimate dovolj dovoljenj za ogled te strani';
+$lang['cp_delete_installer_message']	= 'Please delete the installer directory from your server. Leaving it online means control of this site could be granted to somebody else.';
+$lang['cp_upgrade_message']				= 'Trenutno imate verzijo %s, vaša podatkovna baza pa je verzije %s. Prosimo <a href="%s" title="Nadgradnja">kliknite tukaj</a> Za nadgradnjo vaše podatkovne baze.';
+$lang['cp_google_analytics_missing']	= 'Nastavitve za Analytics manjkajo. Dodajte jih na %s ali kontaktirajte administratorja.';
+$lang['cp_google_analytics_no_connect']	= 'Ne morem se povezati na Analytics. Preverite v %s.';
+
+// Admin-wide text
+$lang['cp_shortcuts_title'] 			= 'Bližnjice';
+
+$lang['required_label'] 				= 'Zahtevano';
+$lang['sb_recent_users'] 				= 'Zadnji uporabniki';
+
+/* End of file admin_lang.php */
+/* Location: ./system/pyrocms/language/slovensko/admin_lang.php */
