1c1d2220a338a2840ac8260f5b9867d1abcf7bf5
imamo <info@imamo.si>
2011-03-12 11:10:09 +0100

    Slovenian translation

---
+++ system/pyrocms/modules/pages/language/slovenian/pages_lang.php
@@ -0,0 +1,70 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// tabs
+$lang['pages.content_label']               = 'Vsebina strani';
+$lang['pages.meta_label']                  = 'Meta podatki';
+$lang['pages.design_label']                = 'Izgled';
+$lang['pages.script_label']                = 'Koda';
+$lang['pages.options_label']               = 'Možnosti';
+
+$lang['pages.detail_label']                = 'Podrobnosti';
+
+// labels
+$lang['pages.page_label']                  = 'Stran';
+$lang['pages.language_label']              = 'Jezik';
+$lang['pages.updated_label']               = 'Posodobljeno';
+$lang['pages.actions_label']               = 'Akcije';
+$lang['pages.unknown_label']               = 'Neznan';
+$lang['pages.title_label']                 = 'Naslov';
+$lang['pages.slug_label']                  = 'URL';
+$lang['pages.navigation_label']                  = 'Dodaj v meni';
+$lang['pages.body_label']                  = 'Telo strani';
+$lang['pages.meta_title_label']            = 'Meta naslov';
+$lang['pages.meta_keywords_label']         = 'Meta ključ. besed.';
+$lang['pages.meta_desc_label']             = 'Meta opis';
+$lang['pages.layout_id_label']             = 'Postavitev strani';
+$lang['pages.css_label']                   = 'CSS';
+$lang['pages.js_label']                    = 'JavaScript';
+$lang['pages.access_label']                = 'Dostop';
+$lang['pages.rss_enabled_label']           = 'RSS omogočen';
+$lang['pages.comments_enabled_label']      = 'Komentarji omogočeni';
+
+$lang['pages.status_label']                = 'Stanje';
+$lang['pages.draft_label']                 = 'Osnutek';
+$lang['pages.live_label']                  = 'Objavljeno';
+$lang['pages.revisions_label']             = 'Pregled';
+$lang['pages.compare_label']               = 'Primerjaj';
+$lang['pages.preview_label']               = 'Predogled';
+$lang['pages.current_label']               = 'Trenutno';
+
+$lang['pages.view_label']                  = 'Ogled';
+$lang['pages.create_label']                = 'Dodaj podstran';
+$lang['pages.edit_label']                  = 'Uredi';
+$lang['pages.delete_label']                = 'Izbriši';
+
+// titles
+$lang['pages.create_title']                = 'Dodaj stran';
+$lang['pages.edit_title']                  = 'Uredi stran "%s"';
+$lang['pages.list_title']                  = 'Seznam strani';
+$lang['pages.layouts_create_title']        = 'Dodaj postavitev strani';
+$lang['pages.layouts_list_title']          = 'Seznam postavitev strani';
+$lang['pages.compare_revisions_title']     = 'Primerjaj v pregledu';
+$lang['pages.preview_revision_title']      = 'Predogled pregleda';
+
+// messages
+$lang['pages.no_pages']                    = 'Ni strani.';
+$lang['pages_create_success']              = 'Stran je bila ustvarjena.';
+$lang['pages_create_error']                = 'Ta stran ni bila ustvarjena.';
+$lang['pages_page_not_found_error']        = 'Ta stran ne obstaja.';
+$lang['pages_edit_success']                = 'Stran "%s" je bila shranjena.';
+$lang['pages_delete_home_error']           = 'Ne morete odstraniti prve strani!';
+$lang['pages_delete_success']              = 'Stran #%s je bila odstranjena.';
+$lang['pages_mass_delete_success']         = '%s strani so bile izbrisane.';
+$lang['pages_delete_none_notice']          = 'Nobena stran ni bila izbrisana.';
+$lang['pages_page_already_exist_error']    = 'Stran s URL-jem "%s" že obstaja v %s.';
+$lang['pages_parent_not_exist_error']      = 'Glavna stran katero ste izbrali ne obstaja.';
+
+$lang['pages.tree_explanation']            = 'Seznam na levi predstavlja strani na vaši spletni strani. 
+Kliknite "+" ikono zraven strani da se prikaže seznam podstrani. Če kliknete na stran se vam bo tukaj prikazal seznam vse uporabnih informacij in možnosti.';
+
+$lang['pages.rss_explanation']             = 'Objava RSS ja za o stran bo dala uporabnikom možnost za prijavo na RSS podajalec ki bo prikazal vsako podstran kot nov vnos.';
