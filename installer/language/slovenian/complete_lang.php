c3bf42f05bb8ceeb35ec3662681c486aecb39d24
imamo <info@imamo.si>
2011-03-12 17:14:17 +0100

    Some corrections and installer translation into slovenian language

---
+++ installer/language/slovenian/complete_lang.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['congrats']			= 'Čestitamo';
+$lang['intro_text']			= 'PyroCMS je sedaj nameščen in pripravlje za delo. Prosimo prijavite se v administracijo s naslednjimi podatki.';
+$lang['email']				= 'E-mail';
+$lang['password']			= 'Geslo';
+$lang['outro_text']			= 'Za konec, <strong>izbrišite mapo installer</strong>, če jo boste pustili vam lahko vdrejo na vašo stran.';
+
+$lang['go_website']			= 'Pojdi na stran';
+$lang['go_control_panel']	= 'Pojdi v administracijo';
+
+/* End of file complete_lang.php */
+/* Location: ./installer/language/slovenian/complete_lang.php */
