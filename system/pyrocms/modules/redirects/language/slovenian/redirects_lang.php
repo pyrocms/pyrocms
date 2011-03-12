d4b788b9c4ab472c3feaaa0433eda8d0791b11d4
imamo <info@imamo.si>
2011-03-12 14:07:51 +0100

    Slovenian Translation

---
+++ system/pyrocms/modules/redirects/language/slovenian/redirects_lang.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['redirects.from']                      = 'Od';
+$lang['redirects.to']                        = 'Za';
+$lang['redirects.edit']                      = 'Uredi';
+$lang['redirects.delete']                    = 'Izbriši';
+
+// titles
+$lang['redirects.add_title']                 = 'Dodaj preusmeritev';
+$lang['redirects.edit_title']                = 'Uredi preusmeritev';
+$lang['redirects.list_title']                = 'Preusmeritve';
+
+// messages
+$lang['redirects.no_redirects']              = 'Trenutno ni preusmeritev.';
+$lang['redirects.add_success']               = 'Nova preusmeritev je bila shranjena.';
+$lang['redirects.add_error']                 = 'Nova preusmeritev ne more biti shranjena prosimo poizkusite malo kasneje.';
+$lang['redirects.edit_success']              = 'Spremembe pri preusmeritvi so bile uspešno shranjene.';
+$lang['redirects.edit_error']                = 'Spremembe pri tej preusmeritvi ni mogoče shraniti, prosimo poizkuste kasneje.';
+$lang['redirects.mass_delete_error']         = 'Pri poizkusu izbrisa preusmeritve "%s" je prišlo do napake.';
+$lang['redirects.mass_delete_success']       = '%s preusmeritev od %s uspešno izbrisane.';
+$lang['redirects.no_select_error']           = 'Izbrati morate najmanj eno preusmeritev za izbris.';
+$lang['redirects.request_conflict_error']    = 'Preusmeritev že obstaja za "%s".';
