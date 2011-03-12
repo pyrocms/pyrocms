e6e1dd79fedf139112b93338bbb7876207896a41
imamo <info@imamo.si>
2011-03-12 01:05:36 +0100

    Slovenian Translation

---
+++ system/codeigniter/language/slovenian/db_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['db_invalid_connection_str'] = 'Na podalagi vnešenih podatkov ni mogoče pridobiti nastavitev podatkovne baze.';
+$lang['db_unable_to_connect'] = 'Na podatkovno bazo se ni možno povezati s vnešenimi podatki.';
+$lang['db_unable_to_select'] = 'Željene podatkovne baze ni mogoče izbrati: %s';
+$lang['db_unable_to_create'] = 'Željene podatkovne baze ni mogoče skreirati: %s';
+$lang['db_invalid_query'] = 'Query katerega ste vnesli ni veljaven.';
+$lang['db_must_set_table'] = 'Nastaviti morate tabelo podatkovne baze da jo lahko uporabite s query-em.';
+$lang['db_must_use_set'] = 'Uporabiti morate "set" metodo za podosobitev vnosa.';
+$lang['db_must_use_index'] = 'Določiti morate index za posodobitev večih.';
+$lang['db_batch_missing_index'] = 'One or more rows submitted for batch updating is missing the specified index.';
+$lang['db_must_use_where'] = 'Updates are not allowed unless they contain a "where" clause.';
+$lang['db_del_must_use_where'] = 'Deletes are not allowed unless they contain a "where" or "like" clause.';
+$lang['db_field_param_missing'] = 'To fetch fields requires the name of the table as a parameter.';
+$lang['db_unsupported_function'] = 'This feature is not available for the database you are using.';
+$lang['db_transaction_failure'] = 'Transaction failure: Rollback performed.';
+$lang['db_unable_to_drop'] = 'Unable to drop the specified database.';
+$lang['db_unsuported_feature'] = 'Unsupported feature of the database platform you are using.';
+$lang['db_unsuported_compression'] = 'The file compression format you chose is not supported by your server.';
+$lang['db_filepath_error'] = 'Unable to write data to the file path you have submitted.';
+$lang['db_invalid_cache_path'] = 'The cache path you submitted is not valid or writable.';
+$lang['db_table_name_required'] = 'A table name is required for that operation.';
+$lang['db_column_name_required'] = 'A column name is required for that operation.';
+$lang['db_column_definition_required'] = 'A column definition is required for that operation.';
+$lang['db_unable_to_set_charset'] = 'Unable to set client connection character set: %s';
+$lang['db_error_heading'] = 'A Database Error Occurred';
+
+/* End of file db_lang.php */
+/* Location: ./system/language/slovenian/db_lang.php */
---
+++ system/codeigniter/language/slovenian/unit_test_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['ut_test_name']		= 'Testno ime';
+$lang['ut_test_datatype']	= 'Testni podatki';
+$lang['ut_res_datatype']	= 'Pričakovani podatki';
+$lang['ut_result']			= 'Rezultat';
+$lang['ut_undefined']		= 'Neimenovano ime testa';
+$lang['ut_file']			= 'Naziv datoteke';
+$lang['ut_line']			= 'Št. Vrstice';
+$lang['ut_passed']			= 'Izdelano';
+$lang['ut_failed']			= 'Neuspelo';
+$lang['ut_boolean']			= 'Boolean';
+$lang['ut_integer']			= 'Integer';
+$lang['ut_float']			= 'Float';
+$lang['ut_double']			= 'Float'; // can be the same as float
+$lang['ut_string']			= 'String';
+$lang['ut_array']			= 'Array';
+$lang['ut_object']			= 'Object';
+$lang['ut_resource']		= 'Resource';
+$lang['ut_null']			= 'Null';
+$lang['ut_notes']			= 'Notes';
+
+
+/* End of file unit_test_lang.php */
+/* Location: ./system/language/slovenian/unit_test_lang.php */
