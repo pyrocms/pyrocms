5f51a4b3515d7753d8bb2e2670e9f47404c2f83e
imamo <info@imamo.si>
2011-03-12 10:23:28 +0100

    Slovenian translation

---
+++ system/pyrocms/modules/files/language/slovenian/files_lang.php
@@ -0,0 +1,71 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * PyroCMS
+ *
+ * An open source CMS based on CodeIgniter
+ *
+ * @package		PyroCMS
+ * @author		PyroCMS Dev Team
+ * @license		Apache License v2.0
+ * @link		http://pyrocms.com
+ * @since		Version 1.0-dev
+ * @translation IMAMO, Danijel Ocepek s.p.
+ * @filesource
+ */
+
+$lang['files.title']						= 'Datoteke';
+$lang['files.upload.title']					= 'Naloži datoteke';
+$lang['files.labels.action']				= 'Akcija';
+$lang['files.labels.download']				= 'Prenosi na računalnik';
+$lang['files.labels.edit']					= 'Uredi';
+$lang['files.labels.delete']				= 'Izbriši';
+$lang['files.labels.upload']				= 'Naloži';
+$lang['files.labels.parent']				= 'Starš';
+$lang['files.no_files']						= 'Trenutno ni datotek.';
+
+$lang['files.folders.title']				= 'Mape datotek';
+$lang['files.folders.manage_title']			= 'Uredi mape';
+$lang['files.folders.label']				= 'Mape';
+$lang['files.folder.label']					= 'Mapa';
+$lang['files.subfolders.label']				= 'Pod-Mapa';
+$lang['files.folders.name']					= 'Naziv';
+$lang['files.folders.slug']					= 'URL pot';
+$lang['files.folders.created']				= 'Ustvarjeno';
+$lang['files.folders.create']				= 'Nova mapa';
+$lang['files.folders.success']				= 'Mapa je bila shranjena.';
+$lang['files.folders.not_exists']			= 'Izbrana je bila neveljavna mapa.';
+$lang['files.folders.no_subfolders']		= 'Brez';
+$lang['files.folders.no_folders']			= 'Vaše datoteke so razvrščene po mapah, trenutno nimate usvarjene nobene mape.';
+$lang['files.folders.delete_success']		= 'Mapa "%s" je bila izbrisana';
+$lang['files.folders.delete_title']			= 'Potrdi izbris';
+$lang['files.folders.confirm_delete']		= 'Ali ste prepričani da želite izbrisati "%s"?<br /><br /> Če to storite prav tako pobrišete vse datoteke in mape ki so v tej mapi.';
+$lang['files.folders.mkdir']				= 'Naložitev datotek v mapo ni možno';
+$lang['files.folders.chmod']				= 'Sprememba lastništva pri naloženih datotekah ni možno';
+
+$lang['files.dropdown.no_subfolders']		= '-- Brez --';
+$lang['files.dropdown.root']				= '-- Root --';
+
+$lang['files.description']					= 'Opis';
+$lang['files.type']							= 'Tip';
+$lang['files.file']							= 'Datoteka';
+$lang['files.file_name']					= 'Naziv datoteke';
+$lang['files.success']						= 'Datoteka je bila shranjena.';
+$lang['files.file.no_upload']				= 'Datoteka mora biti naložena.';
+$lang['files.file.no_extension']			= 'Datoteka mora imeti končnico.';
+$lang['files.not_exists']					= 'Izbrana je bila neveljavna mapa';
+$lang['files.delete.error']					= 'Datoteko ni možno izbrisati.';
+$lang['files.delete.success']				= 'Datoteka je bila izbrisana';
+$lang['files.filter']						= 'Razvrstitev';
+$lang['files.loading']						= 'Nalagam...'; #translate
+
+// Types
+$lang['files.i'] = 'Slika';
+$lang['files.a'] = 'Glasba';
+$lang['files.v'] = 'Video';
+$lang['files.d'] = 'Dokument';
+$lang['files.o'] = 'Ostalo';
+
+$lang['files.no_permissions'] = 'Nimate dovolj dovoljen za ogled tega modula.';
+
+/* End of file files_lang.php */
