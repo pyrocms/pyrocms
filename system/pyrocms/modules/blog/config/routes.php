b1393d8278eba0d09aaa1f178e1a6fb7fe5d37af
MarcosCoelho <marcos@marcoscoelho.com>
2011-03-12 22:47:49 -0300

    make more clean the config routes map news to blog

--- system/pyrocms/modules/blog/config/routes.php
+++ system/pyrocms/modules/blog/config/routes.php
@@ -20,10 +20,10 @@
 |	http://www.codeigniter.com/user_guide/general/routing.html
 */
 
-$route['(blog|news)/(:num)/(:num)/(:any)']		= 'blog/view/$4';
-$route['(blog|news)/page(/:num)?']				= 'blog/index$2';
-
-$route['(blog|news)/rss/all.rss']				= 'rss/index';
-$route['(blog|news)/rss/(:any).rss']			= 'rss/category/$2';
-
-$route['(blog|news)/admin/categories(/:any)?']	= 'admin_categories$2';
+// public
+$route['(blog|news)/(:num)/(:num)/(:any)']	= 'blog/view/$4';
+$route['(blog|news)/page(/:num)?']			= 'blog/index$2';
+$route['(blog|news)/rss/all.rss']			= 'rss/index';
+$route['(blog|news)/rss/(:any).rss']		= 'rss/category/$2';
+// admin
+$route['blog/admin/categories(/:any)?']		= 'admin_categories$1';
