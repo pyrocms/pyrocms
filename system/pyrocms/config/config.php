ca1425cdba3fcad085bac5aaa00cdad34a68ba06
Alec Cumming <alec.cumming@gmail.com>
2011-03-13 16:03:51 -0400

    Added codeigniter cache directory and index.html file; updated config.php and .gitignore accordingly.

--- system/pyrocms/config/config.php
+++ system/pyrocms/config/config.php
@@ -207,7 +207,7 @@ $config['log_date_format'] = 'Y-m-d H:i:s';
 | system/cache/ folder.  Use a full server path with trailing slash.
 |
 */
-$config['cache_path'] = APPPATH . 'cache';
+$config['cache_path'] = APPPATH . 'cache/codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
