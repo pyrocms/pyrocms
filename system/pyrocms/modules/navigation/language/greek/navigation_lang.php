8c2630b3623332929a150e5343859032ae395d1b
Petsagourakis George <petsagouris@gmail.com>
2011-03-21 16:27:40 +0200

    Added Greek translation for modules: blog, comments, contact, files, groups, modules, navigation, pages, permissions, redirects, settings

---
+++ system/pyrocms/modules/navigation/language/greek/navigation_lang.php
@@ -0,0 +1,45 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['nav_title_label']                        = 'Τίτλος';
+$lang['nav_parent_label']						= 'Γονέας';
+$lang['nav_target_label']                       = 'Προορισμός';
+$lang['nav_class_label']                        = 'Class';
+$lang['nav_url_label']                          = 'URL';
+$lang['nav_actions_label']                      = 'Ενέργεια';
+$lang['nav_details_label']                      = 'Λεπτομέρειες';
+$lang['nav_text_label']                         = 'Κείμενο';
+$lang['nav_group_label']                        = 'Ομάδα';
+$lang['nav_location_label']                     = 'Τοποθεσία';
+$lang['nav_type_label']                         = 'Τύπος συνδέσμου';
+$lang['nav_uri_label']                          = 'Σύνδεσμος ιστοτόπου (URI)';
+$lang['nav_page_label']                         = 'Σελίδα';
+$lang['nav_module_label']                       = 'Πρόσθετο';
+$lang['nav_abbrev_label']                       = 'Συντόμευση';
+$lang['nav_edit_label']                         = 'Επεξεργασία';
+$lang['nav_delete_label']                       = 'Διαγραφή';
+$lang['nav_group_delete_label']                 = 'Διαγραφή ομάδας';
+
+$lang['nav_link_target_self']                   = 'Τρέχων παράθυρο (προεπιλογή)';
+$lang['nav_link_target_blank']                  = 'Νέο παράθυρο (_blank)';
+
+// titles
+$lang['nav_link_create_title']                  = 'Προσθήκη συνδέμου περιήγησης';
+$lang['nav_group_create_title']                 = 'Προσθήκη ομάδας';
+$lang['nav_link_edit_title']                    = 'Επεξεργασία συνδέσμου περιήγησης "%s"';
+$lang['nav_link_list_title']                    = 'Λίστα συνδέσμων';
+
+// messages
+$lang['nav_group_no_links']                     = 'Δεν υπάρχουν συνδέσμοι σε αυτήν την ομάδα.';
+$lang['nav_no_groups']                          = 'Δεν υπάρχουν ομάδες συνδέσμων περιήγησης.';
+$lang['nav_group_delete_confirm']               = 'Είσαστε σίγουροι ότι θέλετε να διαγράψετε αυτήν την ομάδα συνδέσμων περιήγησης; Αυτό θα διαγράψει και ΌΛΟΥΣ τους συνδέσμους που περιέχονται τα αρχεία διάταξης και θα χρειαστεί επεξεργασία για να αφαιρεθούν αναφορές σε αυτό.';
+$lang['nav_group_add_success']                  = 'Η ομάδα συνδέσμων περιήγησης αποθηκεύτηκε.';
+$lang['nav_group_add_error']                    = 'Συνέβη κάποιο σφάλμα.';
+$lang['nav_group_mass_delete_success']          = 'Η ομάδα συνδέσμων περιήγησης διαγράφηκε.';
+$lang['nav_link_add_success']                   = 'Ο σύνδεσμος περιήγησης προστέθηκε.';
+$lang['nav_link_add_error']                     = 'Συνέβη ένα αναπάντεχο σφάλμα.';
+$lang['nav_link_not_exist_error']               = 'Αυτός ο σύνδεσμος περιήγησης δεν υπάρχει.';
+$lang['nav_link_edit_success']                  = 'Ο σύνδεσμος περιήγησης αποθηκεύτηκε.';
+$lang['nav_link_delete_success']                = 'Ο σύνδεσμος περιήγησης διαγράφηκε.';
+
+$lang['nav_link_type_desc']                     = 'Παρακαλώ επιλέξτε ένα τύπο για τον σύνδεσμό σας έτσι ώστε να έχετε περισσότερες επιλογές κατά την δημιουργία του.';
---
+++ system/pyrocms/modules/pages/language/greek/page_layouts_lang.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// tabs
+$lang['page_layouts.html_label']                     = 'HTML';
+$lang['page_layouts.css_label']                      = 'CSS';
+
+// labels
+$lang['page_layouts.updated_label']                  = 'Ενημερώθηκε';
+$lang['page_layouts.actions_label']                  = 'Ενέργειες';
+$lang['page_layouts.title_label']                    = 'Τίτλος';
+$lang['page_layouts.theme_layout_label']             = 'Διάταξη Θέματος';
+
+$lang['page_layouts.edit_label']                     = 'Επεξεργασία';
+$lang['page_layouts.delete_label']                   = 'Διαγραφή';
+
+// titles
+$lang['page_layouts.list_title']                     = 'Λίστα διατάξεων σελίδων';
+$lang['page_layouts.create_title']                   = 'Προσθήκη διάταξης σελίδας';
+$lang['page_layouts.edit_title']                     = 'Επεξεργασία διάταξης σελίδας "%s"';
+
+// messages
+$lang['page_layouts.no_pages']                       = 'Δεν υπάρχουν διατάξεις σελίδων.';
+$lang['page_layouts.create_success']                 = 'Η διάταξη σελίδας δημιουργήθηκε.';
+$lang['page_layouts.create_error']                   = 'Αυτή η διάταξη σελίδας δεν έχει δημιουργηθεί.';
+$lang['page_layouts.page_layout.not_found_error']    = 'Αυτή η διάταξη σελίδας δεν υπάρχει.';
+$lang['page_layouts.edit_success']                   = 'Η διάταξη σελίδας "%s" αποθηκεύτηκε.';
+$lang['page_layouts.delete_home_error']              = 'Δεν μπορείτε να διαγράψετε την προεπιλεγμένη διάταξη.';
+$lang['page_layouts.delete_success']                 = 'Η διάταξη σελίδας #%s έχει διαγράφηκε.';
+$lang['page_layouts.mass_delete_success']            = '%s διατάξεις σελίδας διαγράφηκαν.';
+$lang['page_layouts.delete_none_notice']             = 'Δεν διαγράφηκε καμία διάταξη σελίδας.';
+$lang['page_layouts.already_exist_error']            = 'Υπάρχει ήδη μια διάταξη σελίδας με σύντομη ονομασία "%s".';
+
+$lang['page_layouts.variable_introduction']          = 'Για αυτό το πεδίο, υπάχουν δύο διαθέσιμες μεταβλητές';
+$lang['page_layouts.variable_title']                 = 'Περιέχει τον τίτλο της σελίδας.';
+$lang['page_layouts.variable_body']                  = 'Περιέχει το HTML περιεχόμενο της σελίδας.';
