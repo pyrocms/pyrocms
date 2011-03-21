08fe7c6a14bab1ed458f3b0f61ad351425ec8503
Petsagourakis George <petsagouris@gmail.com>
2011-03-21 09:06:28 +0200

    Added Greek translation for modules: templates, themes, users, variables, widgets, wysiwyg

---
+++ system/pyrocms/modules/users/language/greek/ion_auth_lang.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - English
+* 
+* Author: Ben Edmunds
+*           ben.edmunds@gmail.com
+*         @benedmunds
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+*          
+* Created:  03.14.2010 
+* 
+* Description:  English language file for Ion Auth messages and errors
+* 
+*/
+
+// Account Creation
+$lang['account_creation_successful']            = 'Ο Λογαριασμός Δημιουργήθηκε Με Επιτυχία';
+$lang['account_creation_unsuccessful']          = 'Δεν Ήταν Δυνατό Να Δημιουργηθεί Ο Λογαριασμός';
+$lang['account_creation_duplicate_email']       = 'Το Email Χρησιμοποιείται Ήδη ή Δεν Είναι Έγκυρο';
+$lang['account_creation_duplicate_username']    = 'Το Όνομα Χρήστη Χρησιμοποιείται Ήδη ή Δεν Είναι Έγκυρο';  
+
+
+// Password
+$lang['password_change_successful']             = 'Το Συνθηματικό Άλλαχτηκε Με Επιτυχία';
+$lang['password_change_unsuccessful']           = 'Δεν Ήταν Δυνατό Να Αλλαχθεί Το Συνθηματικό';
+$lang['forgot_password_successful']             = 'Εστάλη Το Email Για Τον Επανακαθορισμό Του Συνθηματικού';
+$lang['forgot_password_unsuccessful']           = 'Δεν Ήταν Δυνατό Να Επανακαθοριστεί Το Συνθηματικό';
+
+// Activation
+$lang['activate_successful']                    = 'Ο Λογαριασμός Ενεργοποιήθηκε';
+$lang['activate_unsuccessful']                  = 'Δεν Ήταν Δυνατό Να Ενεργοποιηθεί Ο Λογαριασμός';
+$lang['deactivate_successful']                  = 'Ο Λογαριασμός Απενεργοποιήθηκε';
+$lang['deactivate_unsuccessful']                = 'Δεν Ήταν Δυνατό Να Απενεργοποιηθεί Ο Λογαριασμός';
+$lang['activation_email_successful']            = 'Εστάλη Το Email Της Ενεργοποίησης';
+$lang['activation_email_unsuccessful']          = 'Δεν Ήταν Δυνατό Να Σταλεί Το Email Της Ενεργοποίησης';
+
+// Login / Logout
+$lang['login_successful']                       = 'Συνδεθήκατε Με Επιτυχία';
+$lang['login_unsuccessful']                     = 'Μη Έγκυρα Στοιχεία Σύνδεσης';
+$lang['logout_successful']                      = 'Απόσυνδεθήκατε Με Επιτυχία';
+
+// Account Changes
+$lang['update_successful']                      = 'Οι Πληροφορίες Του Λογαριασμού Ενημερώθηκαν Με Επιτυχία';
+$lang['update_unsuccessful']                    = 'Δεν Ήταν Δυνατό Να Ενημερωθούν Οι Πληροφορίες Του Λογαριασμού';
+$lang['delete_successful']                      = 'Ο Χρήστης Διαγράφηκε';
+$lang['delete_unsuccessful']                    = 'Δεν Ήταν Δυνατό Να Διαγραφεί Ο Χρήστης';
---
+++ system/pyrocms/modules/wysiwyg/language/greek/wysiwyg_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang = array(
+    'wysiwyg.meta.width'    =>  'Πλάτος',
+    'wysiwyg.meta.height'   =>  'Ύψος',
+    'wysiwyg.meta.size'     =>  'Μέγεθος',
+    'wysiwyg.label.insert_width'    => 'Εισαγωγή Πλάτους Εικόνας',
+    'wysiwyg.label.float'   =>  'Στοίχιση',
+    'wysiwyg.label.left'    =>  'Αριστερά',
+    'wysiwyg.label.right'   =>  'Δεξιά',
+    'wysiwyg.label.none'    =>  'Καμία',
+    'wysiwyg.meta.mime'     =>  'Τύπος Mime'
+    
+);
+
+/* end of file wysiwyg_lang.php */
