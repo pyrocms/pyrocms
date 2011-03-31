0e0e584774db85a74044e820cd24158336db4172
Anatoly Khelmer <admin@etqw.co.il>
2011-03-31 18:58:20 +0200

    hebrew translation update

---
+++ system/pyrocms/modules/files/language/hebrew/files_lang.php
@@ -0,0 +1,72 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * PyroCMS
+ *
+ * Hebrew translation by anatolykhelmer
+ *
+ * @package		PyroCMS
+ * @author		Anatoly Khelmer
+ * @license		Apache License v2.0
+ * @link		http://pyrocms.com
+ * @since		Version 1.0-dev
+ * @filesource
+ */
+
+$lang['files.title']						= 'קבצים';
+$lang['files.upload.title']					= 'העלאת קבצים';
+$lang['files.labels.action']				= 'פעולה';
+$lang['files.labels.download']				= 'הורדה';
+$lang['files.labels.edit']					= 'ערוך';
+$lang['files.labels.delete']				= 'מחק';
+$lang['files.labels.upload']				= 'העלא';
+$lang['files.labels.parent']				= 'אב';
+$lang['files.no_files']						= 'כעת אין קבצים.';
+
+$lang['files.folders.title']				= 'תיקיות';
+$lang['files.folders.manage_title']			= 'ניהול תיקיות';
+$lang['files.folders.label']				= 'תיקיות';
+$lang['files.folder.label']					= 'תיקיה';
+$lang['files.subfolders.label']				= 'תת-תיקיות';
+$lang['files.folders.name']					= 'שם';
+$lang['files.folders.slug']					= 'URL Slug';
+$lang['files.folders.created']				= 'נוצר ב';
+$lang['files.folders.create']				= 'תיקיה חדשה';
+$lang['files.folders.success']				= 'תיקיה נשמרה בהצלחה.';
+$lang['files.folders.not_exists']			= 'נבחר קובץ שאינו קיים.';
+$lang['files.folders.no_subfolders']		= 'כלום';
+$lang['files.folders.no_folders']			= 'הקבצים שלך ממוינים לפי תיקיות, כרגע אין לך שום Setup תיקיות.';
+$lang['files.folders.delete_success']		= 'תיקיה "%s" נמחקה בהצלחה.';
+$lang['files.folders.delete_title']			= 'אישור מחיקה';
+$lang['files.folders.confirm_delete']		= 'אתם בתוח רוצים למחוק את "%s"?<br /><br />זה ימחוק את כל הקבצים ותיקיות בתוך התיקיה.';
+$lang['files.folders.mkdir']				= 'אין אפשרות ליצור תיקיה uploads/files';
+$lang['files.folders.chmod']				= 'אין אפשרות לשינוי הרשאות בתיקיה uploads/files';
+
+$lang['files.dropdown.no_subfolders']		= '-- אף לא אחד --';
+$lang['files.dropdown.root']				= '-- שורש --';
+
+$lang['files.description']					= 'תיאור';
+$lang['files.type']							= 'סוג';
+$lang['files.file']							= 'קובץ';
+$lang['files.file_name']					= 'שם קובץ';
+$lang['files.success']						= 'הקובץ נשמר בהצלחה.';
+$lang['files.file.no_upload']				= 'הקובץ חייב להיות נטען.';
+$lang['files.file.no_extension']			= 'לקובץ חייבת להיות סיומת.';
+$lang['files.not_exists']					= 'An invalid folder has been selected.';
+$lang['files.delete.error']					= 'הקובץ לא ניתן למחיקה.';
+$lang['files.delete.success']				= 'הקובץ נמחק בהצלחה';
+$lang['files.filter']						= 'מסנן';
+$lang['files.loading']						= 'טעינה...'; #translate
+
+// Types
+$lang['files.i'] = 'תמונה';
+$lang['files.a'] = 'אודיו';
+$lang['files.v'] = 'וידיאו';
+$lang['files.d'] = 'מסמך';
+$lang['files.o'] = 'אחר';
+
+$lang['files.no_permissions'] = 'אין לך הרשאות כדי לראות המודול.';
+$lang['files.grid'] = 'רשת';
+$lang['files.list'] = 'רשימה';
+
+/* End of file files_lang.php */
