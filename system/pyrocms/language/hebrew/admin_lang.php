15687f6ec46529eea6020686c4f30e5566fb3336
Anatoly Khelmer <admin@etqw.co.il>
2011-03-31 17:44:39 +0200

    hebrew translation update

--- system/pyrocms/language/hebrew/admin_lang.php
+++ system/pyrocms/language/hebrew/admin_lang.php
@@ -1,18 +1,19 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['cp_admin_title'] 			= 'לוח מנהל';
+$lang['cp_admin_title'] 			= 'לוח בקרה';
 $lang['cp_admin_home_title'] 			= 'לוח בקרה';
 $lang['cp_admin_quick_links']			= 'קישורים מהירים';
 
 $lang['cp_welcome_title'] 			= 'ברוכים הבאים';
-$lang['cp_welcome_message'] 			= 'ברוכים הבאים ל% s בלוח הבקרה.
+$lang['cp_welcome_message'] 			= '
+ברוכים הבאים ל%s בלוח הבקרה.
 על יד שימוש בקישורים למעלה אתה יכול לשלוט כמעט בכל היבט של אתר האינטרנט שלך.
-אם במקרה יש לך בעיות עם המערכת, בבקשה לשלוח את הבעיה שלך ל bugtracker שלנו.';
+אם במקרה יש לך בעיות עם המערכת, בבקשה לשלוח את הבעיה שלך לbugtracker שלנו.';
 
 $lang['cp_news_feed_title'] 			= 'סרט חדשות';
 
 // Shortcuts
-$lang['cp_manage_comments']			= 'ניהול תגובות';
+$lang['cp_manage_comments']		= 'ניהול תגובות';
 $lang['cp_manage_pages']			= 'ניהול עמודים';
 $lang['cp_manage_files']			= 'ניהול קבצים';
 $lang['cp_manage_users']			= 'ניהול משתמשים';
@@ -31,20 +32,27 @@ $lang['cp_report_bug']                          = 'דווח על שגיה';
 $lang['cp_contact_support'] 			= 'תמיכה';
 
 $lang['cp_nav_settings']				= 'הגדרות';
-$lang['cp_nav_addons']					= 'Add-ons';
-$lang['cp_nav_modules']					= 'Modules';
-$lang['cp_nav_widgets']					= 'Widgets';
+$lang['cp_nav_addons']					= 'תוספות';
+$lang['cp_nav_modules']					= 'מודולים';
+$lang['cp_nav_widgets']					= 'ויג\'טים';
 $lang['cp_nav_users']					= 'משתמשים';
 $lang['cp_nav_design']					= 'עיצוב';
-$lang['cp_nav_content']					= 'Content';
+$lang['cp_nav_content']					= 'קונטנט';
 $lang['cp_nav_utilities']				= 'כלי עזר';
 
-$lang['cp_must_login']	 				= 'חייב להיות מחובר כדי לצפות בדף זה';
-$lang['cp_access_denied'] 				= 'אין לך הרשאות מתאימות כדי לצפות בדף זה.';
-$lang['cp_delete_installer_message']            = 'נא למחוק את ספריית ההתקנה מהשרת שלך. השארתה באתר יכול להעניק למישהו אחר שליטה על האתר.';
-$lang['cp_upgrade_message']				= 'אתא מפעיל כעת גירסה %s, אבל מסד הנתונים שלך גירסה %s. נא <a href="%s" title="Upgrade">לחץ כאן</a> לחידוש.';
-$lang['cp_google_analytics_missing']            = 'הגדרות גוגל אנליטיקס חסרות. הוסף אותם ל %s או פנה למנהל המערכת.';
-$lang['cp_google_analytics_no_connect']         = 'אין אפשרות להתחבר לגוגל אנליטיקס. אנא בדוק ב%s';
+$lang['cp_must_login']	 				= 'חייבים להיות מחוברים כדי לצפות בעמוד זה';
+$lang['cp_access_denied'] 				= 'אין לך הרשאות מתאימות כדי לצפות בעמוד זה.';
+
+$lang['cp_delete_installer_message'] = '
+נא למחוק את ספריית ההתקנה מהשרת. השארתה באתר יכולה להעניק למישהו אחר שליטה על האתר.';
+
+$lang['cp_upgrade_message']= '
+אתא מפעיל כעת גירסה %s, אבל מסד הנתונים שלך גירסה %s. נא <a href="%s" title="Upgrade">לחץ כאן</a> לחידוש.';
+
+$lang['cp_google_analytics_missing']= '
+הגדרות גוגל אנליטיקס חסרות. הוסף אותם ל %s או פנה למנהל המערכת.';
+$lang['cp_google_analytics_no_connect']= '
+אין אפשרות להתחבר לגוגל אנליטיקס. אנא בדוק ב%s';
 
 // Admin-wide text
 $lang['cp_shortcuts_title'] 			= 'קיצורי דרך';
