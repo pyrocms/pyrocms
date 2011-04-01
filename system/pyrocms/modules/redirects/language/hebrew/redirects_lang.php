0a1815a4e4753e36335212561bc32a6059a525bf
Anatoly Khelmer <admin@etqw.co.il>
2011-04-01 10:06:50 +0300

    hebrew translation update

---
+++ system/pyrocms/modules/redirects/language/hebrew/redirects_lang.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['redirects.from']                      = 'מ';
+$lang['redirects.to']                        = 'אל';
+$lang['redirects.edit']                      = 'ערוך';
+$lang['redirects.delete']                    = 'מחק';
+
+// titles
+$lang['redirects.add_title']                 = 'הוסף הפניה';
+$lang['redirects.edit_title']                = 'ערוך הפניה';
+$lang['redirects.list_title']                = 'הפניות';
+
+// messages
+$lang['redirects.no_redirects']              = 'כרגע אין הפניות.';
+$lang['redirects.add_success']               = 'ההפניה נשמרה בהצלחה.';
+$lang['redirects.add_error']                 = 'אין אפשרות לשמור את ההפניה, נסו שוב מאוחר יותר';
+$lang['redirects.edit_success']              = 'שינויים להפניה זו נשמרו בהצלחה';
+$lang['redirects.edit_error']                = 'אין אפשרות לשמור את ההפניה, נסו שוב מאוחר יותר';
+$lang['redirects.mass_delete_error']         = 'אירעה שגיאה בעת הניסיון למחוק הפניה "%s".';
+$lang['redirects.mass_delete_success']       = '%s הפניות מתוך %s נמחקו בהצלחה.';
+$lang['redirects.no_select_error']           = 'עליכם לבחוב הפניה אחת לפחות כדי למחוק.';
+$lang['redirects.request_conflict_error']    = 'כבר קיימת הפניה ל "%s".';
---
+++ system/pyrocms/modules/users/language/hebrew/ion_auth_lang.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Account Creation
+$lang['account_creation_successful']            = 'החשבון נוצר בהצלחה';
+$lang['account_creation_unsuccessful']          = 'לא ניתן ליצור חשבון';
+$lang['account_creation_duplicate_email']       = 'כתובת דואר האלקטרוני כבר בשימוש או לא חוקית';
+$lang['account_creation_duplicate_username']    = 'שם משתמש כבר בשישוש או לא חוקי';  
+
+
+// Password
+$lang['password_change_successful']             = 'הסיסמה שונתה בהצלחה';
+$lang['password_change_unsuccessful']           = 'לא ניתן לשנות סיסמה';
+$lang['forgot_password_successful']             = 'אימייל עם חידוש סיסמה נשלח בהצלחה';
+$lang['forgot_password_unsuccessful']           = 'לא ניתן לחדש סיסמה';
+
+// Activation
+$lang['activate_successful']                    = 'החשבון הופעל';
+$lang['activate_unsuccessful']                  = 'לא ניתן להפעיל את החשבון';
+$lang['deactivate_successful']                  = 'החשבון בוטל';
+$lang['deactivate_unsuccessful']                = 'לא ניתן לבטל את החשבון';
+$lang['activation_email_successful']            = 'אימייל עם הפעלת חשבון נשלח בהצלחה';
+$lang['activation_email_unsuccessful']          = 'לא ניתן לשלוח אימייל עם הפעלת חשבון';
+
+// Login / Logout
+$lang['login_successful']                       = 'התחברת בהצלחה';
+$lang['login_unsuccessful']                     = 'התחברות לא נכונה';
+$lang['logout_successful']                      = 'התנתקת בהצלחה';
+
+// Account Changes
+$lang['update_successful']                      = 'מידע על החשבון עודכן בהצלחה';
+$lang['update_unsuccessful']                    = 'לא ניתן לעדכן מידע על החשבון';
+$lang['delete_successful']                      = 'משתמש נמחק בהצלחה';
+$lang['delete_unsuccessful']                    = 'לא ניתן למחוק את המשתמש';
