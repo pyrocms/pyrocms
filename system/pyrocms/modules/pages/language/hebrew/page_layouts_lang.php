f15430c604f191747488028adf091aec50276fad
Anatoly Khelmer <admin@etqw.co.il>
2011-04-01 10:01:06 +0300

    hebrew translation update

---
+++ system/pyrocms/modules/pages/language/hebrew/page_layouts_lang.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// tabs
+$lang['page_layouts.html_label']                     = 'HTML';
+$lang['page_layouts.css_label']                      = 'CSS';
+
+// labels
+$lang['page_layouts.updated_label']                  = 'עודכן';
+$lang['page_layouts.actions_label']                  = 'פעולות';
+$lang['page_layouts.title_label']                    = 'כותרת';
+$lang['page_layouts.theme_layout_label']             = 'Theme Layout';
+
+$lang['page_layouts.edit_label']                     = 'ערוך';
+$lang['page_layouts.delete_label']                   = 'מחק';
+
+// titles
+$lang['page_layouts.list_title']                     = 'רשימת page layouts';
+$lang['page_layouts.create_title']                   = 'הוסף page layout';
+$lang['page_layouts.edit_title']                     = 'ערוך page layout "%s"';
+
+// messages
+$lang['page_layouts.no_pages']                       = 'אין page layouts.';
+$lang['page_layouts.create_success']                 = 'page layout נוצר בהצלחה.';
+$lang['page_layouts.create_error']                   = 'page layout לא נוצר.';
+$lang['page_layouts.page_layout.not_found_error']    = 'page layout אינו קיים.';
+$lang['page_layouts.edit_success']                   = 'page layout "%s" נשמר בהצלחה.';
+$lang['page_layouts.delete_home_error']              = 'אין אפשרות למחוק default layout.';
+$lang['page_layouts.delete_success']                 = 'Page layout #%s נמחק בהצלחה.';
+$lang['page_layouts.mass_delete_success']            = '%s page layouts נמחקו בהצלחה.';
+$lang['page_layouts.delete_none_notice']             = 'אף page layout לא נמחק.';
+$lang['page_layouts.already_exist_error']            = 'page layout עם שבלול "%s" כבר קיים.';
+
+$lang['page_layouts.variable_introduction']          = 'בתיבת הקלט הזה ישנם שני משתנים זמינים';
+$lang['page_layouts.variable_title']                 = 'מכיל את כותרת הדף.';
+$lang['page_layouts.variable_body']                  = 'מכיל את הגוף HTML של הדף.';
---
+++ system/pyrocms/modules/permissions/language/hebrew/permissions_lang.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['permissions.group']                  = 'קבוצה';
+$lang['permissions.roles']                  = 'תפקידים';
+$lang['permissions.edit']                   = 'ערוך הרשאות';
+
+$lang['permissions.introduction']           = 'באפשרותך ליצור הרשאות מותאמות אישית עבור משתמשים שונים על ידי הקצאת אותם לקבוצות באזור משתמשים> ניהול משתמשים. לאחר מכן תוכל לערוך הרשאות עבור כל קבוצה ולשלוט איזו מודולים או "תפקידים" הקבוצה יכול להיות.';
+
+$lang['permissions.message_group_saved']    = 'ההרשאות עבור קבוצה זו נשמרו בהצלחה.';
