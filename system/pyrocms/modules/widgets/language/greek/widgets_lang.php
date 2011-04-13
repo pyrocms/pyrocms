ff60d40bb4e4e952cd547d2d9e69a80139610b3f
Petsagourakis George <petsagouris@gmail.com>
2011-04-13 22:50:13 +0300

    Brought the Greek translation files up to date.

--- system/pyrocms/modules/widgets/language/greek/widgets_lang.php
+++ system/pyrocms/modules/widgets/language/greek/widgets_lang.php
@@ -22,12 +22,13 @@ $lang['widgets.widget_area_title']      = 'Όνομα περιοχής';
 $lang['widgets.widget_area_slug']       = 'Σύντομο όνομα περιοχής';
 
 // Widget instance titles
-$lang['widgets.add_instance']           = 'Add widget instance';
-$lang['widgets.edit_instance']          = 'Edit widget instance';
-$lang['widgets.delete_instance']        = 'Delete widget instance';
+$lang['widgets.add_instance']           = 'Προσθήκη widget instance';
+$lang['widgets.edit_instance']          = 'Επεξεργασία widget instance';
+$lang['widgets.delete_instance']        = 'Διαγραφή widget instance';
 
 $lang['widgets.instance_title']         = 'Τίτλος';
-$lang['widgets.tag_title']              = 'Tag';
+$lang['widgets.tag_title']              = 'Ετικέτα';
 
 $lang['widgets.instance_edit']          = 'Επεξεργασία';
 $lang['widgets.instance_delete']        = 'Διαγραφή';
+$lang['widgets.no_available_widgets']		= 'Δεν υπάρχουν διαθέσιμα widgets.';
