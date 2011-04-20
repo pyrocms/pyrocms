e3cef3f692bb370cc3c137ff8e4e4a069d74b422
MarcosCoelho <marcos@marcoscoelho.com>
2011-04-20 16:19:12 -0300

    fixes widgets parser bug on control panel ,, now parser is enabled only for your display.php .. your form and db data not are changed (parsed)

--- system/pyrocms/modules/widgets/libraries/Widgets.php
+++ system/pyrocms/modules/widgets/libraries/Widgets.php
@@ -395,11 +395,19 @@ class Widgets {
 	{
 		$path = isset($this->_widget->path) ? $this->_widget->path : $this->path;
 
-		return $this->parser->parse_string($this->load->_ci_load(array(
-			'_ci_path'		=> $path . 'views/' . $view . EXT,
-			'_ci_vars'		=> $data,
-			'_ci_return'	=> TRUE
-		)), array(), TRUE);
+		return $view == 'display'
+
+			? $this->parser->parse_string($this->load->_ci_load(array(
+				'_ci_path'		=> $path . 'views/' . $view . EXT,
+				'_ci_vars'		=> $data,
+				'_ci_return'	=> TRUE
+			)), array(), TRUE)
+
+			: $this->load->_ci_load(array(
+				'_ci_path'		=> $path . 'views/' . $view . EXT,
+				'_ci_vars'		=> $data,
+				'_ci_return'	=> TRUE
+			));
 	}
 
 	private function _serialize_options($options)
