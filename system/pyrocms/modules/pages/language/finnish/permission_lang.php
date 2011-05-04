82a615509c760fb9ba7cad85a4107740e9186b32
Mikael Kundert <miksu@kundert.fi>
2011-05-04 20:04:00 +0300

    Translated permission related strings to finnish and added wysiwyg finnish translations

---
+++ system/pyrocms/modules/pages/language/finnish/permission_lang.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Page Permissions
+$lang['pages.role_put_live']	= 'Sivujen julkaiseminen';
+$lang['pages.role_edit_live']	= 'Julkaistujen sivujen muokkaaminen';
+$lang['pages.role_delete_live']	= 'Julkaistujen sivujen poistaminen';
