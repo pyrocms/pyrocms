684f6ec5afd5c933206ffe0d05f32f43533fb753
jerel <mail@unruhdesigns.com>
2011-05-06 10:56:32 -0500

    Added SITE_URL to admin javascript. BASE_URL = pyrocms.dev/, BASE_URI = /, and SITE_URL = pyrocms.dev/index.php/ unless the install is using mod_rewrite then it's pyrocms.dev/

--- system/pyrocms/views/admin/partials/metadata.php
+++ system/pyrocms/views/admin/partials/metadata.php
@@ -1,6 +1,7 @@
 <script type="text/javascript">
     var APPPATH_URI = "<?php echo APPPATH_URI;?>";
-    var BASE_URL = "<?php echo rtrim(site_url(), '/').'/';?>";
+	var SITE_URL = "<?php echo rtrim(site_url(), '/').'/';?>";
+    var BASE_URL = "<?php echo BASE_URL;?>";
     var BASE_URI = "<?php echo BASE_URI;?>";
     var DEFAULT_TITLE = "<?php echo $this->settings->site_name; ?>";
 	var DIALOG_MESSAGE = "<?php echo lang('dialog.delete_message'); ?>";
