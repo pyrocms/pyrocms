230df91e2fe369bb10f0c3097ef876f11ffbb6cc
MarcosCoelho <marcos@marcoscoelho.com>
2011-05-11 11:39:55 -0300

    source code formatation

--- addons/widgets/twitter_feed/twitter_feed.php
+++ addons/widgets/twitter_feed/twitter_feed.php
@@ -1,16 +1,15 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * @package 		MizuCMS
+ * @package 		PyroCMS
  * @subpackage 		Twitter Feed Widget
- * @author			Phil Sturgeon - MizuCMS Development Team
+ * @author			Phil Sturgeon - PyroCMS Development Team
  * 
  * Show Twitter streams in your site
  */
+class Widget_Twitter_feed extends Widgets {
 
-class Widget_Twitter_feed extends Widgets
-{
-	public $title		= array(
+	public $title = array(
 		'en' => 'Twitter Feed',
 		'pt' => 'Feed do Twitter',
 	);
@@ -51,22 +50,19 @@ class Widget_Twitter_feed extends Widgets
 
 		$patterns = array(
 			// Detect URL's
-			'((https?|ftp|gopher|telnet|file|notes|ms-help):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)'     => '<a href="$0" target="_blank">$0</a>',
-
+			'((https?|ftp|gopher|telnet|file|notes|ms-help):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)' => '<a href="$0" target="_blank">$0</a>',
 			// Detect Email
 			'|[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,6}|i' => '<a href="mailto:$0">$0</a>',
-
 			// Detect Twitter @usernames
-			'|@([a-z0-9-_]+)|i'     => '<a href="http://twitter.com/$1" target="_blank">$0</a>',
-
+			'|@([a-z0-9-_]+)|i' => '<a href="http://twitter.com/$1" target="_blank">$0</a>',
 			// Detect Twitter #tags
-			'|#([a-z0-9-_]+)|i'     => '<a href="http://twitter.com/search?q=%23$1" target="_blank">$0</a>'
+			'|#([a-z0-9-_]+)|i' => '<a href="http://twitter.com/search?q=%23$1" target="_blank">$0</a>'
 		);
 
-		foreach($tweets as &$tweet)
+		foreach ($tweets as &$tweet)
 		{
 			$tweet->id		= sprintf('%.0f', $tweet->id);
-			$tweet->text	= str_replace($options['username'].': ', '', $tweet->text);
+			$tweet->text	= str_replace($options['username'] . ': ', '', $tweet->text);
 			$tweet->text	= preg_replace(array_keys($patterns), $patterns, $tweet->text);
 		}
 
@@ -75,5 +71,6 @@ class Widget_Twitter_feed extends Widgets
 			'username'	=> $options['username'],
 			'tweets'	=> $tweets
 		);
-	}	
+	}
+
 }
