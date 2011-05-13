8419ac95149722ad3e8d67981674fc749e83d924
Khalil TABBAL <khalil_tabbal@yahoo.fr>
2011-05-12 22:46:11 -0700

    Edited system/pyrocms/language/french/migrations_lang.php via GitHub

--- system/pyrocms/language/french/migrations_lang.php
+++ system/pyrocms/language/french/migrations_lang.php
@@ -13,7 +13,7 @@
  */
 
 $lang['migrations.no_migrations_found']				= 'Aucune migration trouvée.';
-$lang['migrations.multiple_migrations_version']		= 'Une seule migration par étape est autorisée, ici plusieurs migrations à l'étape "%03d".';
+$lang['migrations.multiple_migrations_version']		= 'Une seule migration par étape est autorisée, ici plusieurs migrations à l\'étape "%03d".';
 $lang['migrations.migration_not_found']				= 'Migration à l\'étape "%03d" introuvable.';
 $lang['migrations.multiple_migrations_name']		= 'La migration ne peut être répétée à plusieurs étapes, ici plusieurs migrations sont nommées "%s".';
 $lang['migrations.migration_class_doesnt_exist']	= 'La classe Migration "%s" n\'existe pas.';
