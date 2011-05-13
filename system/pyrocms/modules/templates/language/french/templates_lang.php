58be97f026c9f9f981b119995ed9d47e4c4286fd
Khalil TABBAL <khalil_tabbal@yahoo.fr>
2011-05-12 23:22:10 -0700

    Edited system/pyrocms/modules/templates/language/french/templates_lang.php via GitHub

--- system/pyrocms/modules/templates/language/french/templates_lang.php
+++ system/pyrocms/modules/templates/language/french/templates_lang.php
@@ -1,36 +1,36 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // Labels
-$lang['templates.actions_label']			= 'Actions'; #translate
-$lang['templates.name_label']				= 'Name'; #translate
-$lang['templates.description_label']		= 'Description'; #translate
-$lang['templates.language_label']			= 'Language'; #translate
-$lang['templates.choose_lang_label']		= 'Choose language'; #translate
-$lang['templates.subject_label']			= 'Subject'; #translate
-$lang['templates.body_label']				= 'Body'; #translate
-$lang['templates.slug_label']				= 'Slug'; #translate
+$lang['templates.actions_label']			= 'Actions';
+$lang['templates.name_label']				= 'Nom';
+$lang['templates.description_label']		= 'Description';
+$lang['templates.language_label']			= 'Langue';
+$lang['templates.choose_lang_label']		= 'Choisir la langue';
+$lang['templates.subject_label']			= 'Sujet';
+$lang['templates.body_label']				= 'Corps';
+$lang['templates.slug_label']				= 'Slug';
 
 // Titles
-$lang['templates.create_title']				= 'Create template'; #translate
-$lang['templates.edit_title']				= 'Editing template "%s"'; #translate
-$lang['templates.clone_title']				= 'Coping template "%s"'; #translate
-$lang['templates.list_title']				= 'List templates'; #translate
-$lang['templates.default_title']			= 'Default templates'; #translate
-$lang['templates.user_defined_title']		= 'User defined templates'; #translate
+$lang['templates.create_title']				= 'Créer un template';
+$lang['templates.edit_title']				= 'Éditer le template "%s"';
+$lang['templates.clone_title']				= 'Copier le template "%s"';
+$lang['templates.list_title']				= 'Lister les templates';
+$lang['templates.default_title']			= 'Templates par défaut';
+$lang['templates.user_defined_title']		= 'Template défini par l\utilisateur';
 
 // Messages
-$lang['templates.tmpl_create_success']		= 'Email template "%s" has been saved.'; #translate
-$lang['templates.tmpl_create_error']		= 'Email template "%s" was not saved.'; #translate
-$lang['templates.tmpl_edit_success']		= 'Changes made to email template "%s" has been saved.'; #translate
-$lang['templates.tmpl_edit_error']			= 'Changes made to email template "%s" was not saved.'; #translate
-$lang['templates.tmpl_clone_success']		= '"%s" has been cloned. You may now edit the template to your liking.'; #translate
-$lang['templates.tmpl_clone_error']			= '"%s" was unable to be cloned.  Please try again.'; #translate
-$lang['templates.single_delete_success']	= 'The email template has been deleted.'; #translate
-$lang['templates.mass_delete_success']		= '%s email templates out of %s successfully deleted.'; #translate
-$lang['templates.mass_delete_error'] 		= 'Error occurred while trying to delete email template "%s".'; #translate
-$lang['templates.default_delete_error'] 	= 'Error occurred, default email templates can not be removed.'; #translate
-$lang['templates.no_select_error'] 			= 'You need to select email templates first.'; #translate
-$lang['templates.already_exist_error']		= 'A email template with the name "%s" already exists.'; #translate
+$lang['templates.tmpl_create_success']		= 'Le template d\'Email "%s" a été sauvegardé.';
+$lang['templates.tmpl_create_error']		= 'Le template d\'Email "%s" n'a pas été sauvegardé.';
+$lang['templates.tmpl_edit_success']		= 'Les modifications apportées au template d\'email "%s" ont été sauvées.';
+$lang['templates.tmpl_edit_error']			= 'Les modifications apportées au template d\'email "%s" n'ont pas été sauvées.';
+$lang['templates.tmpl_clone_success']		= '"%s" a été dupliqué. Vous pouvez maintenant éditer le template comme vous souhaitez.';
+$lang['templates.tmpl_clone_error']			= '"%s" n'as pas pu être dupliqué.  Merci de réessayer.';
+$lang['templates.single_delete_success']	= 'Le template d\'email a été supprimé.';
+$lang['templates.mass_delete_success']		= '%s templates d\'email sur %s supprimés avec succès.';
+$lang['templates.mass_delete_error'] 		= 'Une erreur à eu lieu pendant la suppression du template d\'email "%s".';
+$lang['templates.default_delete_error'] 	= 'Une erreur à eu lieu, les templates d\'email par défaut ne peuvent pas être supprimés.';
+$lang['templates.no_select_error'] 			= 'Vous devez tout d'abord sélectionner le template d\'email.';
+$lang['templates.already_exist_error']		= 'Un template d\email avec le nom  "%s" existe déjà.';
 
 /* End of file templates_lang.php */
 /* Location: ./system/pyrocms/modules/templates/language/french/templates_lang.php */
