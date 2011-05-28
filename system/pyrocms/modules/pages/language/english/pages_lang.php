4dc55a64d424650e62ef64cfca580832f7a0a380
MarcosCoelho <marcos@marcoscoelho.com>
2011-05-28 15:25:12 -0300

    change the indentation

--- system/pyrocms/modules/pages/language/english/pages_lang.php
+++ system/pyrocms/modules/pages/language/english/pages_lang.php
@@ -1,71 +1,70 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 // tabs
-$lang['pages.content_label']               = 'Page Content';
-$lang['pages.meta_label']                  = 'Meta data';
-$lang['pages.design_label']                = 'Design';
-$lang['pages.script_label']                = 'Script';
-$lang['pages.options_label']               = 'Options';
+$lang['pages.content_label']						= 'Page Content';
+$lang['pages.meta_label']							= 'Meta data';
+$lang['pages.design_label']							= 'Design';
+$lang['pages.script_label']							= 'Script';
+$lang['pages.options_label']						= 'Options';
 
-$lang['pages.detail_label']                = 'Details';
+$lang['pages.detail_label']							= 'Details';
 
 // labels
-$lang['pages.page_label']                  = 'Page';
-$lang['pages.language_label']              = 'Language';
-$lang['pages.updated_label']               = 'Updated';
-$lang['pages.actions_label']               = 'Actions';
-$lang['pages.unknown_label']               = 'Unknown';
-$lang['pages.title_label']                 = 'Title';
-$lang['pages.slug_label']                  = 'URL';
-$lang['pages.navigation_label']                  = 'Add to Navigation';
-$lang['pages.body_label']                  = 'Page body';
-$lang['pages.meta_title_label']            = 'Meta title';
-$lang['pages.meta_keywords_label']         = 'Meta keywords';
-$lang['pages.meta_desc_label']             = 'Meta description';
-$lang['pages.layout_id_label']             = 'Page Layout';
-$lang['pages.css_label']                   = 'CSS';
-$lang['pages.js_label']                    = 'JavaScript';
-$lang['pages.access_label']                = 'Access';
-$lang['pages.rss_enabled_label']           = 'RSS enabled?';
-$lang['pages.comments_enabled_label']      = 'Comments enabled?';
-$lang['pages.is_home_label']      = 'Is default (home) page?';
+$lang['pages.page_label']							= 'Page';
+$lang['pages.language_label']						= 'Language';
+$lang['pages.updated_label']						= 'Updated';
+$lang['pages.actions_label']						= 'Actions';
+$lang['pages.unknown_label']						= 'Unknown';
+$lang['pages.title_label']							= 'Title';
+$lang['pages.slug_label']							= 'URL';
+$lang['pages.navigation_label']						= 'Add to Navigation';
+$lang['pages.body_label']							= 'Page body';
+$lang['pages.meta_title_label']						= 'Meta title';
+$lang['pages.meta_keywords_label']					= 'Meta keywords';
+$lang['pages.meta_desc_label']						= 'Meta description';
+$lang['pages.layout_id_label']						= 'Page Layout';
+$lang['pages.css_label']							= 'CSS';
+$lang['pages.js_label']								= 'JavaScript';
+$lang['pages.access_label']							= 'Access';
+$lang['pages.rss_enabled_label']					= 'RSS enabled?';
+$lang['pages.comments_enabled_label']				= 'Comments enabled?';
+$lang['pages.is_home_label']						= 'Is default (home) page?';
 
-$lang['pages.status_label']                = 'Status';
-$lang['pages.draft_label']                 = 'Draft';
-$lang['pages.live_label']                  = 'Live';
-$lang['pages.revisions_label']             = 'Revisions';
-$lang['pages.compare_label']               = 'Compare';
-$lang['pages.preview_label']               = 'Preview';
-$lang['pages.current_label']               = 'Current';
+$lang['pages.status_label']							= 'Status';
+$lang['pages.draft_label']							= 'Draft';
+$lang['pages.live_label']							= 'Live';
+$lang['pages.revisions_label']						= 'Revisions';
+$lang['pages.compare_label']						= 'Compare';
+$lang['pages.preview_label']						= 'Preview';
+$lang['pages.current_label']						= 'Current';
 
-$lang['pages.view_label']                  = 'View';
-$lang['pages.create_label']                = 'Add child';
-$lang['pages.edit_label']                  = 'Edit';
-$lang['pages.delete_label']                = 'Delete';
+$lang['pages.view_label']							= 'View';
+$lang['pages.create_label']							= 'Add child';
+$lang['pages.edit_label']							= 'Edit';
+$lang['pages.delete_label']							= 'Delete';
 
 // titles
-$lang['pages.create_title']                = 'Add page';
-$lang['pages.edit_title']                  = 'Edit page "%s"';
-$lang['pages.list_title']                  = 'List pages';
-$lang['pages.layouts_create_title']        = 'Add page layout';
-$lang['pages.layouts_list_title']          = 'List page layouts';
-$lang['pages.compare_revisions_title']     = 'Compare Revisions';
-$lang['pages.preview_revision_title']      = 'Preview Revision';
+$lang['pages.create_title']							= 'Add page';
+$lang['pages.edit_title']							= 'Edit page "%s"';
+$lang['pages.list_title']							= 'List pages';
+$lang['pages.layouts_create_title']					= 'Add page layout';
+$lang['pages.layouts_list_title']					= 'List page layouts';
+$lang['pages.compare_revisions_title']				= 'Compare Revisions';
+$lang['pages.preview_revision_title']				= 'Preview Revision';
 
 // messages
-$lang['pages.no_pages']                    = 'There are no pages.';
-$lang['pages_create_success']              = 'The page was created.';
-$lang['pages_create_error']                = 'That page has not been created.';
-$lang['pages_page_not_found_error']        = 'That page does not exist.';
-$lang['pages_edit_success']                = 'The page "%s" was saved.';
-$lang['pages_delete_home_error']           = 'You can not delete the home page!';
-$lang['pages_delete_success']              = 'Page #%s has been deleted.';
-$lang['pages_mass_delete_success']         = '%s pages have been deleted.';
-$lang['pages_delete_none_notice']          = 'No pages were deleted.';
-$lang['pages_page_already_exist_error']    = 'A page with the URL "%s" already exists in %s.';
-$lang['pages_parent_not_exist_error']      = 'The parent page you have selected does not exist.';
+$lang['pages.no_pages']								= 'There are no pages.';
+$lang['pages_create_success']						= 'The page was created.';
+$lang['pages_create_error']							= 'That page has not been created.';
+$lang['pages_page_not_found_error']					= 'That page does not exist.';
+$lang['pages_edit_success']							= 'The page "%s" was saved.';
+$lang['pages_delete_home_error']					= 'You can not delete the home page!';
+$lang['pages_delete_success']						= 'Page #%s has been deleted.';
+$lang['pages_mass_delete_success']					= '%s pages have been deleted.';
+$lang['pages_delete_none_notice']					= 'No pages were deleted.';
+$lang['pages_page_already_exist_error']				= 'A page with the URL "%s" already exists in %s.';
+$lang['pages_parent_not_exist_error']				= 'The parent page you have selected does not exist.';
 
-$lang['pages.tree_explanation']            = 'The list on the left represents pages on your website. 
-Click the page item to show its children. You may drag pages to order them; drag horizontally to make it a child. When you click the page text you will see all sorts of handy information in this box.';
+$lang['pages.tree_explanation']						= 'The list on the left represents pages on your website. Click the page item to show its children. You may drag pages to order them; drag horizontally to make it a child. When you click the page text you will see all sorts of handy information in this box.';
 
-$lang['pages.rss_explanation']             = 'Enabling RSS for this page will give users a chance to subscribe to an RSS feed which shows each immediate page child as a new entry.';
+$lang['pages.rss_explanation']						= 'Enabling RSS for this page will give users a chance to subscribe to an RSS feed which shows each immediate page child as a new entry.';
