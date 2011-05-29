49529de17f8ff1912aff68628328744eb1b7c36d
jerel <mail@unruhdesigns.com>
2011-05-28 23:59:23 -0500

    Moved the RSS feed and analytics out of the core and into the admin theme. Restored the Recent Comments display.

--- system/pyrocms/themes/admin_theme/theme.php
+++ system/pyrocms/themes/admin_theme/theme.php
@@ -9,7 +9,32 @@ class Theme_Admin_theme extends Theme {
     public $description		= 'Default PyroCMS v1.0 Admin Theme - Vertical navigation, CSS3 styling.';
     public $version			= '1.0';
 	public $type			= 'admin';
-
+	public $options 		= array('recent_comments' => 	array('title' 		=> 'Recent Comments',
+																'description'   => 'Would you like to display recent comments on the dashboard?',
+																'default'       => 'yes',
+																'type'          => 'radio',
+																'options'       => 'yes=Yes|no=No',
+																'is_required'   => TRUE),
+									'news_feed' => 			array('title' => 'News Feed',
+																'description'   => 'Would you like to display the news feed on the dashboard?',
+																'default'       => 'yes',
+																'type'          => 'radio',
+																'options'       => 'yes=Yes|no=No',
+																'is_required'   => TRUE),
+									'quick_links' => 		array('title' => 'Quick Links',
+																'description'   => 'Would you like to display quick links on the dashboard?',
+																'default'       => 'yes',
+																'type'          => 'radio',
+																'options'       => 'yes=Yes|no=No',
+																'is_required'   => TRUE),
+									'analytics_graph' => 	array('title' => 'Analytics Graph',
+																'description'   => 'Would you like to display the graph on the dashboard?',
+																'default'       => 'yes',
+																'type'          => 'radio',
+																'options'       => 'yes=Yes|no=No',
+																'is_required'   => TRUE),
+								   );
+	
 	/**
 	 * Initialize is automatically ran when the theme is loaded for use
 	 *
@@ -21,6 +46,14 @@ class Theme_Admin_theme extends Theme {
 	public function initialize()
 	{
 		self::generate_menu();
+	
+		// only load these items on the dashboard
+		if ($this->module == '')
+		{
+			self::get_analytics();
+			self::get_rss_feed();
+			self::get_recent_comments();
+		}
 	}
 	
 	private function generate_menu()
@@ -59,6 +92,112 @@ class Theme_Admin_theme extends Theme {
 			$this->template->menu_items = $grouped_menu;
 			$this->template->modules = $grouped_modules;
 		}
+	}
+	
+	public function get_analytics()
+	{
+		if ($this->settings->ga_email AND $this->settings->ga_password AND $this->settings->ga_profile)
+		{
+			// Not FALSE? Return it
+			if ($cached_response = $this->pyrocache->get('analytics'))
+			{
+				$data['analytic_visits'] = $cached_response['analytic_visits'];
+				$data['analytic_views'] = $cached_response['analytic_views'];
+			}
+
+			else
+			{
+				try
+				{
+					$this->load->library('analytics', array(
+						'username' => $this->settings->ga_email,
+						'password' => $this->settings->ga_password
+					));
+
+					// Set by GA Profile ID if provided, else try and use the current domain
+					$this->analytics->setProfileById('ga:'.$this->settings->ga_profile);
+
+					$end_date = date('Y-m-d');
+					$start_date = date('Y-m-d', strtotime('-1 month'));
+
+					$this->analytics->setDateRange($start_date, $end_date);
+
+					$visits = $this->analytics->getVisitors();
+					$views = $this->analytics->getPageviews();
+
+					/* build tables */
+					if (count($visits))
+					{
+						foreach ($visits as $date => $visit)
+						{
+							$year = substr($date, 0, 4);
+							$month = substr($date, 4, 2);
+							$day = substr($date, 6, 2);
+
+							$utc = mktime(date('h') + 1, NULL, NULL, $month, $day, $year) * 1000;
+
+							$flot_datas_visits[] = '[' . $utc . ',' . $visit . ']';
+							$flot_datas_views[] = '[' . $utc . ',' . $views[$date] . ']';
+						}
+
+						$flot_data_visits = '[' . implode(',', $flot_datas_visits) . ']';
+						$flot_data_views = '[' . implode(',', $flot_datas_views) . ']';
+					}
+
+					$data['analytic_visits'] = $flot_data_visits;
+					$data['analytic_views'] = $flot_data_views;
+
+					// Call the model or library with the method provided and the same arguments
+					$this->pyrocache->write(array('analytic_visits' => $flot_data_visits, 'analytic_views' => $flot_data_views), 'analytics', 60 * 60 * 6); // 6 hours
+				}
+
+				catch (Exception $e)
+				{
+					$data['messages']['notice'] = sprintf(lang('cp_google_analytics_no_connect'), anchor('admin/settings', lang('cp_nav_settings')));
+				}
+			}
+		}
+
+		// Only mention this notice if no other notices are set
+		elseif (empty($data['messages']['notice']))
+		{
+			$data['messages']['notice'] = sprintf(lang('cp_google_analytics_missing'), anchor('admin/settings', lang('cp_nav_settings')));
+		}
+		
+		// make it available in the theme
+		$this->template->set($data);
+	}
+	
+	public function get_rss_feed()
+	{
+		// Dashboard RSS feed (using SimplePie)
+		$this->load->library('simplepie');
+		$this->simplepie->set_cache_location($this->config->item('simplepie_cache_dir'));
+		$this->simplepie->set_feed_url($this->settings->dashboard_rss);
+		$this->simplepie->init();
+		$this->simplepie->handle_content_type();
+
+		// Store the feed items
+		$data['rss_items'] = $this->simplepie->get_items(0, $this->settings->dashboard_rss_count);
+		
+		// you know
+		$this->template->set($data);
+	}
+	
+	public function get_recent_comments()
+	{
+		$this->load->model('comments/comments_m');
+		$this->load->model('pages/pages_m');
+		$this->load->model('users/users_m');
+
+		$this->lang->load('comments/comments');
+
+		$data['recent_users'] = $this->users_m->get_recent(5);
+
+		$recent_comments = $this->comments_m->get_recent(5);
+		$data['recent_comments'] = process_comment_items($recent_comments);
+		
+		$this->template->set($data);
 	}
 }
 
--- system/pyrocms/themes/admin_theme/views/admin/dashboard.php
+++ system/pyrocms/themes/admin_theme/views/admin/dashboard.php
@@ -1,4 +1,4 @@
-<?php if (isset($analytic_visits) OR isset($analytic_views)): ?>
+<?php if ((isset($analytic_visits) OR isset($analytic_views)) AND $theme_options->analytics_graph == 'yes'): ?>
 <script type="text/javascript">
 
 	jQuery(function($) {
@@ -64,72 +64,102 @@
 </script>
 
 <div id="analytics" class="line" style="padding-bottom: 10px"></div>
-<?php endif; ?>
-<div class="line">
-	<section class="box unit <?php echo $rss_items ? 'size1of3' : 'size10f1'; ?>">
-		<header>
-			<h3><?php echo lang('cp_admin_quick_links') ?></h3>
-		</header>
-		
-		<ul class="quick-links">
-			<?php if(array_key_exists('comments', $this->permissions) OR $this->user->group == 'admin'): ?>
-			<li class="clearfix">
-				<?php echo image('icons/comments.png'); ?>
-				<a href="<?php echo site_url('admin/comments') ?>"><h4><?php echo lang('cp_manage_comments'); ?></h4></a>
-			</li>
-			<?php endif; ?>
+<?php endif; ?><!-- End Analytics -->
+
+	<!-- Dashboard Widgets FTW -->
+		{pyro:widgets:area slug="dashboard"}
+	<!-- Then End -->
+	
+	
+	<!-- Begin Recent Comments -->
+	<?php if (isset($recent_comments) AND is_array($recent_comments) AND $theme_options->recent_comments == 'yes'): ?>
+		<section class="box unit size1of1">
+			<header>
+				<h3><?php echo lang('comments.recent_comments') ?></h3>
+			</header>
 			
-			<?php if(array_key_exists('pages', $this->permissions) OR $this->user->group == 'admin'): ?>
-			<li class="clearfix">
-				<?php echo image('icons/pages.png'); ?>
-				<a href="<?php echo site_url('admin/pages') ?>"><h4><?php echo lang('cp_manage_pages'); ?></h4></a>
-			</li>
-			<?php endif; ?>
-			
-			<?php if(array_key_exists('files', $this->permissions) OR $this->user->group == 'admin'): ?>
-			<li class="clearfix">
-				<?php echo image('icons/folder_open.png'); ?>
-				<a href="<?php echo site_url('admin/files') ?>"><h4><?php echo lang('cp_manage_files'); ?></h4></a>
-			</li>
-			<?php endif; ?>
-			
-			<?php if(array_key_exists('users', $this->permissions) OR $this->user->group == 'admin'): ?>
-			<li class="clearfix">
-				<?php echo image('icons/user.png'); ?>
-				<a href="<?php echo site_url('admin/users') ?>"><h4><?php echo lang('cp_manage_users'); ?></h4></a>
-			</li>
-			<?php endif; ?>
-		</ul>
-		
-		
-	</section>
-	<?php if ($rss_items) : ?>
-	<!-- News Feed -->
-	<section class="box unit size2of3 lastUnit">
-		<header>
-			<h3><?php echo lang('cp_news_feed_title'); ?></h3>
-		</header>
-			
-			<ul id="news-feed">
-				<?php foreach($rss_items as $rss_item): ?>
-				<li>
-					<h3><?php echo anchor($rss_item->get_permalink(), $rss_item->get_title(), 'target="_blank"'); ?></h3>
-					
-					<?php
-						$item_date	= strtotime($rss_item->get_date());
-						$item_month = date('M', $item_date);
-						$item_day	= date('j', $item_date);
-					?>
-					<div class="date">
-						<span><?php echo $item_month ?></span>
-						<?php echo $item_day; ?>
-					</div>
-										
-					<p class='item_body'><?php echo $rss_item->get_description(); ?></p>
-				</li>
+			<ul class="recent-comments">
+				<li class="clearfix icon"><a href="<?php echo site_url('admin/comments') . '">' . image('icons/comments.png'); ?></a></li>
+				<?php foreach ($recent_comments AS $rant): ?>
+					<li class="clearfix">
+						<p><?php echo sprintf(lang('comments.list_comment'), $rant->name, $rant->item); ?> <em><?php echo $rant->comment; ?></em></p>
+					</li>
 				<?php endforeach; ?>
 			</ul>
-		
-	</section>
+		</section>
 	<?php endif; ?>
-</div>
+	<!-- End Recent Comments -->
+
+
+	<!-- Begin Quick Links -->
+	<?php if ($theme_options->quick_links == 'yes'): ?>
+	<div class="line">
+		<section class="box unit <?php echo $rss_items ? 'size1of3' : 'size1of2'; ?>">
+			<header>
+				<h3><?php echo lang('cp_admin_quick_links') ?></h3>
+			</header>
+			
+			<ul class="quick-links">
+				<?php if(array_key_exists('comments', $this->permissions) OR $this->user->group == 'admin'): ?>
+				<li class="clearfix">
+					<?php echo image('icons/comments.png'); ?>
+					<a href="<?php echo site_url('admin/comments') ?>"><h4><?php echo lang('cp_manage_comments'); ?></h4></a>
+				</li>
+				<?php endif; ?>
+				
+				<?php if(array_key_exists('pages', $this->permissions) OR $this->user->group == 'admin'): ?>
+				<li class="clearfix">
+					<?php echo image('icons/pages.png'); ?>
+					<a href="<?php echo site_url('admin/pages') ?>"><h4><?php echo lang('cp_manage_pages'); ?></h4></a>
+				</li>
+				<?php endif; ?>
+				
+				<?php if(array_key_exists('files', $this->permissions) OR $this->user->group == 'admin'): ?>
+				<li class="clearfix">
+					<?php echo image('icons/folder_open.png'); ?>
+					<a href="<?php echo site_url('admin/files') ?>"><h4><?php echo lang('cp_manage_files'); ?></h4></a>
+				</li>
+				<?php endif; ?>
+				
+				<?php if(array_key_exists('users', $this->permissions) OR $this->user->group == 'admin'): ?>
+				<li class="clearfix">
+					<?php echo image('icons/user.png'); ?>
+					<a href="<?php echo site_url('admin/users') ?>"><h4><?php echo lang('cp_manage_users'); ?></h4></a>
+				</li>
+				<?php endif; ?>
+			</ul>
+		</section>
+		<?php endif; ?>
+		<!-- End Quick Links -->
+
+
+		<!-- Begin RSS Feed -->
+		<?php if ($rss_items AND $theme_options->news_feed == 'yes') : ?>
+		<section class="box unit size2of3 lastUnit">
+			<header>
+				<h3><?php echo lang('cp_news_feed_title'); ?></h3>
+			</header>
+				
+				<ul id="news-feed">
+					<?php foreach($rss_items as $rss_item): ?>
+					<li>
+						<h3><?php echo anchor($rss_item->get_permalink(), $rss_item->get_title(), 'target="_blank"'); ?></h3>
+						
+						<?php
+							$item_date	= strtotime($rss_item->get_date());
+							$item_month = date('M', $item_date);
+							$item_day	= date('j', $item_date);
+						?>
+						<div class="date">
+							<span><?php echo $item_month ?></span>
+							<?php echo $item_day; ?>
+						</div>
+											
+						<p class='item_body'><?php echo $rss_item->get_description(); ?></p>
+					</li>
+					<?php endforeach; ?>
+				</ul>
+		</section>
+		<?php endif; ?>
+		<!-- End RSS Feed -->
+	</div>
