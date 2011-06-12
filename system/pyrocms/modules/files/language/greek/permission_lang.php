ec5ae6d5f71416247506656d9fb8fa817983a2fe
Petsagourakis George <petsagouris@gmail.com>
2011-06-13 00:36:08 +0300

    Updated Greek translation in modules: blog, files, settings, widgets and the pyrocms general language file.

--- system/pyrocms/modules/files/language/greek/permission_lang.php
+++ system/pyrocms/modules/files/language/greek/permission_lang.php
@@ -1,9 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 // Blog Permissions
-$lang['files.role_download_file']	= 'Download File'; #translate
-$lang['files.role_edit_file']		= 'Edit File'; #translate
-$lang['files.role_delete_file'] 	= 'Delete File'; #translate
+$lang['files.role_download_file']	= 'Κατέβασμα Αρχείου';
+$lang['files.role_edit_file']		= 'Επεξεργασία Αρχείου';
+$lang['files.role_delete_file'] 	= 'Διαγραφή Αρχείου';
 
-$lang['files.role_edit_folder']		= 'Edit Folder'; #translate
-$lang['files.role_delete_folder'] 	= 'Delete Folder'; #translate
+$lang['files.role_edit_folder']		= 'Επεξεργασία Φακέλου';
+$lang['files.role_delete_folder'] 	= 'Διαγραφή Φακέλου';
