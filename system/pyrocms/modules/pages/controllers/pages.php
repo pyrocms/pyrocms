fe204e6c135fb07ae0fa4674f5d0e52e7d64e0b7
jerel <mail@unruhdesigns.com>
2011-06-15 23:18:32 -0500

    Migrate page revisions to page chunks.

--- system/pyrocms/modules/pages/controllers/pages.php
+++ system/pyrocms/modules/pages/controllers/pages.php
@@ -160,7 +160,7 @@ class Pages extends Public_Controller
 		$page_array['layout'] = (array) $page_array['layout'];
 
 		// Parse it so the content is parsed
-		$page_array['body'] = $this->parser->parse_string(str_replace('&quot;', '"', $page->body), $this->data, TRUE);
+		$page_array['body'] = $this->parser->parse_string(str_replace('&quot;', '"', $page->body), array(), TRUE);
 		
         // Create page output
 	    $this->template->title($page->meta_title)
@@ -191,7 +191,7 @@ class Pages extends Public_Controller
 					</script>');
 			}
 			
-        	$this->template->build('page', $this->data);
+        	$this->template->build('page');
     }
     
 	/**
