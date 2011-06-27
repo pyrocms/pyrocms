cc99580327a2062ba3dff596c879528ec43b85c0
Petsagourakis George <petsagouris@gmail.com>
2011-06-27 08:19:33 +0300

    Merged the language files too.
    Signed-off-by: Petsagourakis George <petsagouris@gmail.com>

--- system/pyrocms/modules/users/language/finnish/user_lang.php
+++ system/pyrocms/modules/users/language/finnish/user_lang.php
@@ -161,5 +161,63 @@ $lang['user_activated_email_content_line2']    = 'Jos sähköposti ohjelmasi ei 
 $lang['user_reset_pass_email_subject']         = 'Salasanan nollaaminen';
 $lang['user_reset_pass_email_body']            = 'Salasanasi sivustolla %s on nollattu. Jos et ole nollannut salasanaa, pyydämme Teitä ottamaan yhteyttä %s.';
 
+// Profile
+$lang['profile_of_title']             = 'Käyttäjän %s profiili';
+
+$lang['profile_user_details_label']   = 'Käyttäjän tiedot';
+$lang['profile_role_label']           = 'Rooli';
+$lang['profile_registred_on_label']   = 'Rekisteröitynyt';
+$lang['profile_last_login_label']     = 'Viimeksi kijautunut sisään';
+$lang['profile_male_label']           = 'Mies';
+$lang['profile_female_label']         = 'Nainen';
+
+$lang['profile_not_set_up']           = 'Tällä käyttäjällä ei ole profiilia.';
+
+$lang['profile_edit']                 = 'Muokkaa profiilia';
+
+$lang['profile_personal_section']     = 'Henkilökohtaiset tiedot';
+
+$lang['profile_display_name']         = 'Näyttönimi';  
+$lang['profile_dob']                  = 'Syntymäpäivä';
+$lang['profile_dob_day']              = 'Päivä';
+$lang['profile_dob_month']            = 'Kuukausi';
+$lang['profile_dob_year']             = 'Vuosi';
+$lang['profile_gender']               = 'Sukupuoli';
+$lang['profile_gender_nt']            = 'Not Telling'; #translate
+$lang['profile_gender_male']          = 'Male'; #translate
+$lang['profile_gender_female']        = 'Female'; #translate
+$lang['profile_bio']                  = 'Minä';
+
+$lang['profile_contact_section']      = 'Yhteydenotto';
+
+$lang['profile_phone']                = 'Puhelin';
+$lang['profile_mobile']               = 'Matkapuhelin';
+$lang['profile_address']              = 'Osoite';
+$lang['profile_address_line1']        = 'Osoite rivi #1';
+$lang['profile_address_line2']        = 'Osoite rivi #2';
+$lang['profile_address_line3']        = 'Osoite rivi #3';
+$lang['profile_address_postcode']     = 'Postinumero';
+$lang['profile_website']              = 'Kotisivut';
+
+$lang['profile_messenger_section']    = 'Pikaviestintä';
+
+$lang['profile_msn_handle']           = 'MSN';
+$lang['profile_aim_handle']           = 'AIM';
+$lang['profile_yim_handle']           = 'Yahoo! messenger';
+$lang['profile_gtalk_handle']         = 'GTalk';
+
+$lang['profile_avatar_section']       = 'Avatar'; // @todo Where we use these?
+$lang['profile_social_section']       = 'Social'; // ^
+
+$lang['profile_gravatar']             = 'Gravatar';
+$lang['profile_twitter']              = 'Twitter';
+
+$lang['profile_edit_success']         = 'Profiilisi on tallennettu.';
+$lang['profile_edit_error']           = 'Tapahtui virhe.';
+
+// -- Buttons ------------------------------------------------------------------------------------------------
+
+$lang['profile_save_btn']             = 'Tallenna profiili';
+
 /* End of file user_lang.php */
 /* Location: ./system/pyrocms/modules/users/language/finnish/user_lang.php */
