803df3d82aab338deb070766d77085275a1a923b
MarcosCoelho <marcos@marcoscoelho.com>
2011-06-30 00:32:59 -0300

    creates a cache directory for SimplePie, eliminating the error which is raised after clearing the cache or when it is done installing a new site.

--- system/pyrocms/config/pyrocache.php
+++ system/pyrocms/config/pyrocache.php
@@ -10,3 +10,8 @@ $config['rss_cache'] = 3600; // 1 hour
 
 // Set the location for simplepie cache
 $config['simplepie_cache_dir'] = APPPATH . 'cache/' . SITE_REF . '/simplepie/';
+
+if ( ! is_dir($config['simplepie_cache_dir']))
+{
+	mkdir($config['simplepie_cache_dir'], 0777, TRUE);
+}
