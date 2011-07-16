b9603ce9323774e081edc21d53e99854e57ed88a
jerel <mail@unruhdesigns.com>
2011-07-16 00:08:26 -0500

    Updated addon uploading permissions

--- system/pyrocms/modules/themes/views/admin/partials/shortcuts.php
+++ system/pyrocms/modules/themes/views/admin/partials/shortcuts.php
@@ -1,8 +1,11 @@
 <nav id="shortcuts">
 	<h6><?php echo lang('cp_shortcuts_title'); ?></h6>
 	<ul>
-		<li><?php echo anchor('admin/themes/upload', lang('themes.upload_title'), 'class="add"'); ?></li>
+		<?php if ($this->settings->addons_upload): ?>
+			<li><?php echo anchor('admin/themes/upload', lang('themes.upload_title'), 'class="add"'); ?></li>
+		<?php endif; ?>
 		<li><?php echo anchor('admin/themes', lang('themes.list_title')); ?></li>
+		<li><?php echo anchor('admin/themes/admin_themes', lang('themes.admin_list')); ?></li>
 	</ul>
 	<br class="clear-both" />
 </nav>
