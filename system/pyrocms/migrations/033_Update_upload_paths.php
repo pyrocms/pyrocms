3df0792eb78d1fca186365af0f4cfd29b3e5b7dc
Phil Sturgeon <email@philsturgeon.co.uk>
2011-07-22 21:57:51 -0600

    Update upload blog upload paths.

--- system/pyrocms/migrations/033_Update_upload_paths.php
+++ system/pyrocms/migrations/033_Update_upload_paths.php
@@ -9,7 +9,7 @@ class Migration_Update_upload_paths extends Migration {
 			foreach ($chunks as $chunk)
 			{
 				// Change upload paths to point images to the site ref folder
-				$chunk->body = str_replace('/uploads/', '/uploads/'.SITE_REF.'/', $chunk->body);
+				$chunk->body = str_replace('uploads/', 'uploads/'.SITE_REF.'/', $chunk->body);
 				
 				// Remove any crazy old 0.9.8 literal tags that might be sat around
 				$chunk->body = str_replace(array('{literal}', '{/literal}'), '', $chunk->body);
