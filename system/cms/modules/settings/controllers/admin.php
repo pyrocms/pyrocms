9893c94841ddd87e8d6c0bbeff604606a4341111
Don Myers <donmyers@mac.com>
2011-07-28 11:49:52 -0400

    updated to text area "unlimited" everything else max length 255

--- system/cms/modules/settings/controllers/admin.php
+++ system/cms/modules/settings/controllers/admin.php
@@ -77,7 +77,7 @@ class Admin extends Admin_Controller
 			$this->validation_rules[] = array(
 				'field' => $setting->slug . (in_array($setting->type, array('select-multiple', 'checkbox')) ? '[]' : ''),
 				'label' => 'lang:settings_' . $setting->slug,
-				'rules' => 'trim' . ($setting->is_required ? '|required' : '')
+				'rules' => 'trim' . ($setting->is_required ? '|required' : '') . ($setting->type !== 'textarea' ? '|max_length[255]' : '')
 			);
 
 			$settings_array[$setting->slug] = $setting->value;
