0639315223622f7d00d28d424a1133bb0157c20d
imamo <info@imamo.si>
2011-07-30 09:53:25 +0200

    Some completly missing translation and other changes
    Added completely missing translation for modules groups and templates.
    And missing important change in config.php

--- system/cms/modules/groups/language/slovenian/group_lang.php
+++ system/cms/modules/groups/language/slovenian/group_lang.php
@@ -4,13 +4,19 @@
 $lang['groups.title']					= 'Naslov';
 $lang['groups.name']				    = 'Ime';
 <<<<<<< HEAD
+<<<<<<< HEAD
 $lang['groups.short_name']				= 'Kratko ime';
 =======
+=======
+>>>>>>> bc19549... Some completly missing translation and other changes
 $lang['groups.short_name']				= 'Kratko ime'; 
 $lang['groups.description'] 		    = 'Opis';
 $lang['groups.actions']					= 'Akcije';
 $lang['groups.edit']					= 'Uredi';
 $lang['groups.delete']					= 'Izbriši';
+<<<<<<< HEAD
+>>>>>>> bc19549... Some completly missing translation and other changes
+=======
 >>>>>>> bc19549... Some completly missing translation and other changes
 
 // titles
@@ -22,7 +28,11 @@ $lang['groups.edit_title']              = 'Uredi skupino "%s"';
 $lang['groups.no_groups']               = 'Skupine še ne obstajajo.';
 $lang['groups.add_success']             = 'Skupina "%s" je bila dodana.';
 <<<<<<< HEAD
+<<<<<<< HEAD
 $lang['groups.add_error']               = 'Skupine "%s" ni mogoče dodati.';
+=======
+$lang['groups.edit_error']              = 'Skupine "%s" ni mogoče dodati.';
+>>>>>>> bc19549... Some completly missing translation and other changes
 =======
 $lang['groups.edit_error']              = 'Skupine "%s" ni mogoče dodati.';
 >>>>>>> bc19549... Some completly missing translation and other changes
@@ -31,9 +41,13 @@ $lang['groups.edit_error']              = 'Skupine "%s" ni mogoče shraniti.';
 $lang['groups.delete_success']          = 'Skuina je bila uspešno odstranjena.';
 $lang['groups.delete_error']            = 'Prišlo je do napake pri izbrisu te skupine. Najprej morate odstraniti vse uporabnike povezane iz te skupine predno izbrišete to skupino.';
 <<<<<<< HEAD
+<<<<<<< HEAD
 $lang['groups.already_exist_error']     = 'Skupina s tem imenom "%s" že obstaja.';
 
 /* End of file group_lang.php */
+=======
+$lang['groups.already_exist_error']     = 'Skupina s tem imenom "%s" že obstaja.';
+>>>>>>> bc19549... Some completly missing translation and other changes
 =======
 $lang['groups.already_exist_error']     = 'Skupina s tem imenom "%s" že obstaja.';
 >>>>>>> bc19549... Some completly missing translation and other changes
