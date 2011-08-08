8bfef7615754e89be44d2f0415481d6b0de7bd7a
MarcosCoelho <marcos@marcoscoelho.com>
2011-08-08 19:19:21 -0300

    fixes declaration of SITE_URL without end slash in the wysiwyg controller, then moves these declaration to views folder in admin theme to fix #663; extra are some code standards

--- system/cms/themes/admin_theme/views/admin/partials/metadata.php
+++ system/cms/themes/admin_theme/views/admin/partials/metadata.php
@@ -13,16 +13,18 @@
 <?php endif; ?>
 
 <script type="text/javascript">
-	var APPPATH_URI			= "<?php echo APPPATH_URI;?>";
-	var SITE_URL			= "<?php echo rtrim(site_url(), '/').'/';?>";
-	var BASE_URL			= "<?php echo BASE_URL;?>";
-	var BASE_URI			= "<?php echo BASE_URI;?>";
-	var UPLOAD_PATH			= "<?php echo UPLOAD_PATH;?>";
-	var DEFAULT_TITLE		= "<?php echo $this->settings->site_name; ?>";
-	var DIALOG_MESSAGE		= "<?php echo lang('dialog.delete_message'); ?>";
+	var APPPATH_URI			= "<?php echo APPPATH_URI; ?>",
+		SITE_URL			= "<?php echo rtrim(site_url(), '/') . '/'; ?>",
+		BASE_URL			= "<?php echo BASE_URL; ?>",
+		BASE_URI			= "<?php echo BASE_URI; ?>",
+		UPLOAD_PATH			= "<?php echo UPLOAD_PATH; ?>",
+		DEFAULT_TITLE		= "<?php echo $this->settings->site_name; ?>",
+		DIALOG_MESSAGE		= "<?php echo lang('dialog.delete_message'); ?>";
+
 	pyro.admin_theme_url 	= "<?php echo BASE_URL . $this->admin_theme->path; ?>";
 	pyro.apppath_uri		= "<?php echo APPPATH_URI; ?>";
 	pyro.base_uri			= "<?php echo BASE_URI; ?>";
+
 	jQuery.noConflict();
 </script>
 
