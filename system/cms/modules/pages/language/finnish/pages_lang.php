c96516b68232325b3d4a4738808c324f14bbd2b8
Don Myers <donmyers@mac.com>
2011-08-13 13:18:45 -0400

    translate

--- system/cms/modules/pages/language/finnish/pages_lang.php
+++ system/cms/modules/pages/language/finnish/pages_lang.php
@@ -73,6 +73,7 @@ $lang['pages_mass_delete_success']         = '%s sivua poistettiin.';
 $lang['pages_delete_none_notice']          = 'Yhtään sivua ei poistettu.';
 $lang['pages_page_already_exist_error']    = 'Sivu, jolla on URL osoite "%s" on jo olemassa paikassa %s.';
 $lang['pages_parent_not_exist_error']      = 'Yläkohdan sivua ei ole olemassa.';
+$lang['pages_root_folder']                = 'the top level'; #translate
 
 $lang['pages.tree_explanation']            = 'Vasemmalla oleva lista näyttää sivustosi sivut. 
 Klikkaa "+" ikonia, niin näet sen alasivut. Kun klikkaat sivua, näet sivuun liittyen käteviä toimintoja.';
