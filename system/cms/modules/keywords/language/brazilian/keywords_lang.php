0ad64f686c31cc867ca36e45c3911c5f767d2761
MarcosCoelho <marcos@marcoscoelho.com>
2011-08-17 01:17:44 -0300

    added remaining file of brazilian translation

---
+++ system/cms/modules/keywords/language/brazilian/keywords_lang.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['keywords:name']				    = 'Nome';
+
+// titles
+$lang['keywords:index_title']            = 'Listar de palavras-chave';
+$lang['keywords:add_title']              = 'Adicionar palavra-chave';
+$lang['keywords:edit_title']             = 'Editando a palavra-chave "%s"';
+
+// messages
+$lang['keywords:no_keywords']            = 'Nenhuma palavra-chave encontrada.';
+$lang['keywords:add_success']            = 'A palavra-chave "%s" foi adicionada com sucesso.';
+$lang['keywords:add_error']              = 'A palavra-chave "%s" não pôde ser adicionada.';
+$lang['keywords:edit_success']           = 'A palavra-chave "%s" foi alterada com sucesso.';
+$lang['keywords:edit_error']             = 'A palavra-chave "%s" não pôde ser alterada.';
+$lang['keywords:delete_success']         = 'A palavra-chave foi removida com êxito.';
+$lang['keywords:delete_error']           = 'Ocorreu um erro ao tentar remover esta palavra-chave.';
