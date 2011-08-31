de646bf5a0bef8092efdad9b20420d2e980751a6
MarcosCoelho <marcos@marcoscoelho.com>
2011-08-30 22:13:42 -0300

    fixes a conflict with translations to pt-br and pt (pt-pt) of ckeditor that uses codes diff of pyrocms codes on language filenames

--- system/cms/config/language.php
+++ system/cms/config/language.php
@@ -20,103 +20,120 @@ $config['supported_languages'] = array(
 		'name'		=> 'English',
 		'folder'	=> 'english',
 		'direction'	=> 'ltr',
-		'codes'		=> array('english', 'en_US')
+		'codes'		=> array('english', 'en_US'),
+		'ckeditor'	=> NULL
 	),
 	'de' => array(
 		'name'		=> 'Deutsch',
 		'folder'	=> 'german',
 		'direction'	=> 'ltr',
-		'codes'		=> array('deu', 'german', 'de_DE')
+		'codes'		=> array('deu', 'german', 'de_DE'),
+		'ckeditor'	=> NULL
 	),
 	'ar' => array(
 		'name'		=> 'العربية',
 		'folder'	=> 'arabic',
 		'direction'	=> 'rtl',
-		'codes'		=> array('arabic', 'ar_SA')
+		'codes'		=> array('arabic', 'ar_SA'),
+		'ckeditor'	=> NULL
 	),
 	'cs' => array(
 		'name'		=> 'Česky',
 		'folder'	=> 'czech',
 		'direction'	=> 'ltr',
-		'codes'		=> array('csy', 'czech', 'cs_CZ')
+		'codes'		=> array('csy', 'czech', 'cs_CZ'),
+		'ckeditor'	=> NULL
 	),
     'el' => array(
 		'name'		=> 'Ελληνικά',
 		'folder'	=> 'greek',
 		'direction'	=> 'ltr',
-		'codes'		=> array('ell', 'greek', 'el_GR')
+		'codes'		=> array('ell', 'greek', 'el_GR'),
+		'ckeditor'	=> NULL
 	),
 	'es' => array(
 		'name'		=> 'Espa&ntilde;ol',
 		'folder'	=> 'spanish',
 		'direction'	=> 'ltr',
-		'codes'		=> array('esp', 'spanish', 'es_ES')
+		'codes'		=> array('esp', 'spanish', 'es_ES'),
+		'ckeditor'	=> NULL
 	),
 	'fr' => array(
 		'name'		=> 'Français',
 		'folder'	=> 'french',
 		'direction'	=> 'ltr',
-		'codes'		=> array('fra', 'french', 'fr_FR')
+		'codes'		=> array('fra', 'french', 'fr_FR'),
+		'ckeditor'	=> NULL
 	),
 	'it' => array(
 		'name'		=> 'Italiano',
 		'folder'	=> 'italian',
 		'direction'	=> 'ltr',
-		'codes'		=> array('ita', 'italian', 'it_IT')
+		'codes'		=> array('ita', 'italian', 'it_IT'),
+		'ckeditor'	=> NULL
 	),
 	'nl' => array(
 		'name'		=> 'Nederlands',
 		'folder'	=> 'dutch',
 		'direction'	=> 'ltr',
-		'codes'		=> array('dutch', 'nld', 'nl-NL')
+		'codes'		=> array('dutch', 'nld', 'nl-NL'),
+		'ckeditor'	=> NULL
 	),
 	'sl' => array(
 		'name'		=> 'Slovensko',
 		'folder'	=> 'slovenian',
 		'direction'	=> 'ltr',
-		'codes'		=> array('slovenian', 'sl_SI')
+		'codes'		=> array('slovenian', 'sl_SI'),
+		'ckeditor'	=> NULL
 	),
 	'pl' => array(
 		'name'		=> 'Polski',
 		'folder'	=> 'polish',
 		'direction'	=> 'ltr',
-		'codes'		=> array('plk', 'polish', 'pl_PL')
+		'codes'		=> array('plk', 'polish', 'pl_PL'),
+		'ckeditor'	=> NULL
 	),
 	'br' => array(
 		'name'		=> 'Portugu&ecirc;s do Brasil',
 		'folder'	=> 'brazilian',
 		'direction'	=> 'ltr',
-		'codes'		=> array('portuguese-brazil', 'ptb', 'pt_BR')
+		'codes'		=> array('portuguese-brazil', 'ptb', 'pt_BR'),
+		'ckeditor'	=> 'pt-br'
 	),
 	'ru' => array(
 		'name'		=> 'Русский',
 		'folder'	=> 'russian',
 		'direction'	=> 'ltr',
-		'codes'		=> array('rus', 'russian', 'ru_RU')
+		'codes'		=> array('rus', 'russian', 'ru_RU'),
+		'ckeditor'	=> NULL
 	),
 	'zh' => array(
 		'name'		=> '繁體中文',
 		'folder'	=> 'chinese_traditional',
 		'direction'	=> 'ltr',
-		'codes'		=> array('chinese-traditional', 'cht', 'zh_HK')
+		'codes'		=> array('chinese-traditional', 'cht', 'zh_HK'),
+		'ckeditor'	=> NULL
 	),
 	'he' => array(
 		'name'		=> 'עברית',
 		'folder'	=> 'hebrew',
 		'direction'	=> 'rtl',
-		'codes'		=> array('he', 'hebrew', 'he_IL')
+		'codes'		=> array('he', 'hebrew', 'he_IL'),
+		'ckeditor'	=> NULL
 	),
 	'lt' => array(
 		'name'		=> 'Lietuvių',
 		'folder'	=> 'lithuanian',
 		'direction'	=> 'ltr',
-		'codes'		=> array('lt', 'lt_LT')
+		'codes'		=> array('lt', 'lt_LT'),
+		'ckeditor'	=> NULL
 	),
 	'fi' => array(
 		'name'		=> 'Suomi',
 		'folder'	=> 'finnish',
 		'direction'	=> 'ltr',
-		'codes'		=> array('fi', 'fi_FI')
+		'codes'		=> array('fi', 'fi_FI'),
+		'ckeditor'	=> NULL
 	)
 );
 
