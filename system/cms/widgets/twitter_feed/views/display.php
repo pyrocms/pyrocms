ad2435a5b06bf86afef9b30e164e26f8046de1ff
MarcosCoelho <marcos@marcoscoelho.com>
2011-09-01 10:50:03 -0300

    use id string

--- system/cms/widgets/twitter_feed/views/display.php
+++ system/cms/widgets/twitter_feed/views/display.php
@@ -2,7 +2,7 @@
     <?php foreach($tweets as $tweet): ?>
 	<li>
 	    <?php echo $tweet->text; ?>
-	    <p class="date"><em><?php echo anchor('https://twitter.com/' . $username . '/status/' . $tweet->id, format_date($tweet->created_at, Settings::get('date_format') . ' h:i'), 'target="_blank"'); ?></em></p>
+	    <p class="date"><em><?php echo anchor('https://twitter.com/' . $username . '/status/' . $tweet->id_str, format_date($tweet->created_at, Settings::get('date_format') . ' h:i'), 'target="_blank"'); ?></em></p>
 	</li>
     <?php endforeach; ?>
 </ul>
