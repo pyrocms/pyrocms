db03ef4020e6e483cba218dc39d32089561c424f
Phil Sturgeon <email@philsturgeon.co.uk>
2011-09-13 20:25:21 +0100

    Auto-loading of models for comment display now expects models to have a singular name.

--- system/cms/modules/comments/helpers/comments_helper.php
+++ system/cms/modules/comments/helpers/comments_helper.php
@@ -56,10 +56,10 @@ function display_comments($ref_id = '', $reference = NULL)
 	 **/
 	$view = 'comments';
 	
-	if (file_exists($ci->template->get_views_path() . 'modules/comments/' . $view . (pathinfo($view, PATHINFO_EXTENSION) ? '' : EXT)))
+	if (file_exists($ci->template->get_views_path().'modules/comments/'.$view.(pathinfo($view, PATHINFO_EXTENSION) ? '' : EXT)))
 	{
 		// look in the theme for overloaded views
-		$path = $ci->template->get_views_path() . 'modules/comments/';
+		$path = $ci->template->get_views_path().'modules/comments/';
 	}
 	else
 	{
@@ -116,7 +116,7 @@ function counter_comments($ref_id = '', $reference = NULL, $return_number = FALS
 		default	: $line = 'plural';
 	}
 
-	return sprintf(lang('comments.counter_' . $line . '_label'), $total);
+	return sprintf(lang('comments.counter_'.$line.'_label'), $total);
 }
 
 /**
@@ -134,7 +134,7 @@ function process_comment_items($comments)
 		// work out who did the commenting
 		if($comment->user_id > 0)
 		{
-			$comment->name = anchor('admin/users/edit/' . $comment->user_id, $comment->name);
+			$comment->name = anchor('admin/users/edit/'.$comment->user_id, $comment->name);
 		}
 
 		// What did they comment on
@@ -151,7 +151,7 @@ function process_comment_items($comments)
 				$ci->load->model('galleries/gallery_images_m');
 				if ($item = $ci->gallery_images_m->get($comment->module_id))
 				{
-					$comment->item = anchor('admin/' . $comment->module . '/image_preview/' . $item->id, $item->title, 'class="modal-large"');
+					$comment->item = anchor('admin/'.$comment->module.'/image_preview/'.$item->id, $item->title, 'class="modal-large"');
 					continue 2;
 				}
 				break;
@@ -159,14 +159,16 @@ function process_comment_items($comments)
 
 		if (module_exists($comment->module))
 		{
-			if ( ! isset($ci->{$comment->module . '_m'}))
+			$model_name = singular($comment->module).'_m';
+			
+			if ( ! isset($ci->{$model_name.'_m'}))
 			{
-				$ci->load->model($comment->module . '/' . $comment->module . '_m');
+				$ci->load->model($comment->module.'/'.$model_name);
 			}
 
-			if ($item = $ci->{$comment->module . '_m'}->get($comment->module_id))
+			if ($item = $ci->{$model_name}->get($comment->module_id))
 			{
-				$comment->item = anchor('admin/' . $comment->module . '/preview/' . $item->id, $item->title, 'class="modal-large"');
+				$comment->item = anchor('admin/'.$comment->module.'/preview/'.$item->id, $item->title, 'class="modal-large"');
 			}
 		}
 		else
