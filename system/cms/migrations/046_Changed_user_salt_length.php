45d63bd502a4b4602be97d4fd1e1dd52d01a91bc
jerel <mail@unruhdesigns.com>
2011-09-25 21:36:20 -0500

    Tweaked the migration so the core_users table gets updated to accept longer salts also.

--- system/cms/migrations/046_Changed_user_salt_length.php
+++ system/cms/migrations/046_Changed_user_salt_length.php
@@ -12,6 +12,21 @@ class Migration_Changed_user_salt_length extends CI_Migration {
 				'default'		=> ''
 			)
 		));
+		
+		// switch the prefix so we can update core_users
+		$this->db->set_dbprefix('core_');
+		
+		$this->dbforge->modify_column('users', array(
+			'salt' => array(
+				'type'			=> 'varchar',
+				'constraint'	=> 6,
+				'collate'		=> 'utf8_unicode_ci',
+				'default'		=> ''
+			)
+		));
+		
+		// now put it back for following migrations
+		$this->db->set_dbprefix(SITE_REF . '_');
 	}
 
 	public function down()
@@ -24,5 +39,18 @@ class Migration_Changed_user_salt_length extends CI_Migration {
 				'default'		=> ''
 			)
 		));
+		
+		$this->db->set_dbprefix('core_');
+		
+		$this->dbforge->modify_column('users', array(
+			'salt' => array(
+				'type'			=> 'varchar',
+				'constraint'	=> 5,
+				'collate'		=> 'utf8_unicode_ci',
+				'default'		=> ''
+			)
+		));
+		
+		$this->db->set_dbprefix(SITE_REF . '_');
 	}
 }
