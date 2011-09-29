a068dcd2f9924b151cc48342301a69a582f45294
Phil Sturgeon <email@philsturgeon.co.uk>
2011-09-29 13:12:51 +0100

    Added VIEWPATH to installer index.php

--- index.php
+++ index.php
@@ -262,23 +262,11 @@ else
 	define('SYSDIR', end($parts));
 	unset($parts);
 
-
 	// The path to the "application" folder
-//	if (is_dir($application_folder))
-//	{
-		define('APPPATH', $application_folder.'/');
-//	}
-//	else
-//	{
-//		if ( ! is_dir(BASEPATH.$application_folder.'/'))
-//		{
-//			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
-//		}
-//
-//		define('APPPATH', BASEPATH.$application_folder.'/');
-//	}
+	define('APPPATH', $application_folder.'/');
 	
-	define ('VIEWPATH', APPPATH.'views/' );	
+	// Path to the views folder
+	define ('VIEWPATH', APPPATH.'views/' );
 
 /*
  * --------------------------------------------------------------------
