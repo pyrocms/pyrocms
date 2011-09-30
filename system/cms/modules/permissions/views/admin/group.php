84617bd15323f2a8439d2650daa1062a9b5d20a5
jerel <mail@unruhdesigns.com>
2011-09-29 22:09:06 -0500

    Updated the views of Groups, Addons, Permissions, Settings, and Themes.

--- system/cms/modules/permissions/views/admin/group.php
+++ system/cms/modules/permissions/views/admin/group.php
@@ -1,39 +1,43 @@
-<h3><?php echo $group->description; ?></h3>
+<section class="title">
+	<h3><?php echo $group->description; ?></h3>
+</section>
 
-<?php echo form_open(uri_string(), 'class="crud"'); ?>
-
-<table>
-	<thead>
-		<tr>
-			<th><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all')); ?></th>
-			<th>Module</th>
-			<th>Roles</th>
-		</tr>
-	</thead>
-	<tbody>
-
-	<?php foreach ($permisison_modules as $module): ?>
-		<tr>
-			<td style="width: 30px"><?php echo form_checkbox('modules[' . $module['slug'] . ']', TRUE, array_key_exists($module['slug'], $edit_permissions)); ?></td>
-			<td>
-				<?php echo $module['name']; ?>
-			</td>
-			<td>
-				<?php if ( ! empty($module['roles'])): ?>
-					<?php foreach ($module['roles'] as $role): ?>
-						<?php echo form_checkbox('module_roles[' . $module['slug'] . ']['.$role.']', TRUE, isset($edit_permissions[$module['slug']]) AND array_key_exists($role, (array) $edit_permissions[$module['slug']])); ?>
-						<?php echo lang($module['slug'].'.role_'.$role); ?>
-					<?php endforeach; ?>
-				<?php endif; ?>
-			</td>
-		</tr>
-	<?php endforeach; ?>
-
-	</tbody>
-</table>
-
-<div class="buttons float-right padding-top">
-	<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel'))); ?>
-</div>
-
-<?php echo form_close(); ?>
+	<section class="item">
+	<?php echo form_open(uri_string(), 'class="crud"'); ?>
+	
+	<table>
+		<thead>
+			<tr>
+				<th><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all')); ?></th>
+				<th>Module</th>
+				<th>Roles</th>
+			</tr>
+		</thead>
+		<tbody>
+	
+		<?php foreach ($permisison_modules as $module): ?>
+			<tr>
+				<td style="width: 30px"><?php echo form_checkbox('modules[' . $module['slug'] . ']', TRUE, array_key_exists($module['slug'], $edit_permissions)); ?></td>
+				<td>
+					<?php echo $module['name']; ?>
+				</td>
+				<td>
+					<?php if ( ! empty($module['roles'])): ?>
+						<?php foreach ($module['roles'] as $role): ?>
+							<?php echo form_checkbox('module_roles[' . $module['slug'] . ']['.$role.']', TRUE, isset($edit_permissions[$module['slug']]) AND array_key_exists($role, (array) $edit_permissions[$module['slug']])); ?>
+							<?php echo lang($module['slug'].'.role_'.$role); ?>
+						<?php endforeach; ?>
+					<?php endif; ?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	
+		</tbody>
+	</table>
+	
+	<div class="buttons float-right padding-top">
+		<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel'))); ?>
+	</div>
+	
+	<?php echo form_close(); ?>
+</section>
--- system/cms/modules/settings/views/admin/index.php
+++ system/cms/modules/settings/views/admin/index.php
@@ -1,53 +1,57 @@
-<?php if ($setting_sections): ?>
-	<h3><?php echo lang('settings_edit_title');?></h3>
+<section class="title">
+	<h4><?php echo lang('settings_edit_title');?></h4>
+</section>
 
-	<?php echo form_open('admin/settings/edit', 'class="crud"');?>
-
-		<div class="tabs">
-
-			<ul class="tab-menu">
-				<?php foreach ($setting_sections as $section_slug => $section_name): ?>
-				<li>
-					<a href="#<?php echo $section_slug; ?>" title="<?php printf(lang('settings_section_title'), $section_name); ?>">
-						<span><?php echo $section_name; ?></span>
-					</a>
-				</li>
-				<?php endforeach; ?>
-			</ul>
-
-			<?php foreach ($setting_sections as $section_slug => $section_name): ?>
-			<div id="<?php echo $section_slug;?>">
-				<fieldset>
-					<ol>
-					<?php $section_count = 1; foreach ($settings[$section_slug] as $setting): ?>
-						<li id="<?php echo $setting->slug; ?>" class="<?php echo $section_count++ % 2 == 0 ? 'even' : ''; ?>">
-							<label for="<?php echo $setting->slug; ?>">
-								<?php echo $setting->title; ?>
-							</label>
-							<div class="width-40 <?php echo 'type-' . $setting->type; ?>">
-								<?php echo $setting->form_control; ?><br/>
-								<div class="clear-both text-small1" style="margin-left: 160px;">
-									<?php echo $setting->description; ?>
-								</div>
-							</div>
-							<br class="clear-both" />
-							<span class="move-handle"></span>
-						</li>
+	<section class="item">
+	<?php if ($setting_sections): ?>
+		<?php echo form_open('admin/settings/edit', 'class="crud"');?>
+	
+			<div class="tabs">
+	
+				<ul class="tab-menu">
+					<?php foreach ($setting_sections as $section_slug => $section_name): ?>
+					<li>
+						<a href="#<?php echo $section_slug; ?>" title="<?php printf(lang('settings_section_title'), $section_name); ?>">
+							<span><?php echo $section_name; ?></span>
+						</a>
+					</li>
 					<?php endforeach; ?>
-					</ol>
-				</fieldset>
+				</ul>
+	
+				<?php foreach ($setting_sections as $section_slug => $section_name): ?>
+				<div id="<?php echo $section_slug;?>">
+					<fieldset>
+						<ol>
+						<?php $section_count = 1; foreach ($settings[$section_slug] as $setting): ?>
+							<li id="<?php echo $setting->slug; ?>" class="<?php echo $section_count++ % 2 == 0 ? 'even' : ''; ?>">
+								<label for="<?php echo $setting->slug; ?>">
+									<?php echo $setting->title; ?>
+								</label>
+								<div class="width-40 <?php echo 'type-' . $setting->type; ?>">
+									<?php echo $setting->form_control; ?><br/>
+									<div class="clear-both text-small1" style="margin-left: 160px;">
+										<?php echo $setting->description; ?>
+									</div>
+								</div>
+								<br class="clear-both" />
+								<span class="move-handle"></span>
+							</li>
+						<?php endforeach; ?>
+						</ol>
+					</fieldset>
+				</div>
+				<?php endforeach; ?>
+	
 			</div>
-			<?php endforeach; ?>
-
+	
+			<div class="buttons float-right padding-top">
+				<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save') )); ?>
+			</div>
+	
+		<?php echo form_close(); ?>
+	<?php else: ?>
+		<div class="blank-slate">
+			<h2><?php echo lang('settings_no_settings');?></h2>
 		</div>
-
-		<div class="buttons float-right padding-top">
-			<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save') )); ?>
-		</div>
-
-	<?php echo form_close(); ?>
-<?php else: ?>
-	<div class="blank-slate">
-		<h2><?php echo lang('settings_no_settings');?></h2>
-	</div>
-<?php endif; ?>
+	<?php endif; ?>
+</section>
