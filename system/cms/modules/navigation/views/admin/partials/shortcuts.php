48c86ba260c4350c6e422e2df818d6f8ca981e27
Phil Sturgeon <email@philsturgeon.co.uk>
2011-09-30 01:23:00 +0100

    Removed some crufty shortcuts.

--- system/cms/modules/navigation/views/admin/partials/shortcuts.php
+++ system/cms/modules/navigation/views/admin/partials/shortcuts.php
@@ -3,7 +3,6 @@
 
 	<ul class="list-links">
 		<li><?php echo anchor('admin/navigation/groups/create', lang('nav_group_create_title'), 'class="add"') ?></li>
-		<li><?php echo anchor('admin/navigation', lang('nav_link_list_title')) ?></li>
 	</ul>
 	<br class="clear-both" />
 </nav>
