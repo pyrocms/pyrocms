727866b7b190241b8316a650b690f4dc147d51d1
Rakesh Tembhurne <rakesh@tembhurne.com>
2011-10-03 15:01:38 +0530

    Hindi language (India) files for pyrocms installer

---
+++ installer/language/hindi/complete_lang.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['congrats']			= 'बधाई';
+$lang['intro_text']			= 'पायरोसीएम्एस अब इंस्टाल और इस्तेमाल के लिए तैयार है. कृपया दी गयी जानकारी से एडमिनिस्ट्रेटर पैनल में लॉग इन करे.';
+$lang['email']				= 'इ-मेल';
+$lang['password']			= 'पासवर्ड';
+$lang['outro_text']			= 'अंततः, <strong>सर्वर से इंस्टालर को नष्ट कर दे</strong>, क्यों की अगर आपने इसे यही रहने दिया तो आपकी वेबसाईट हैक हो सकती है.';
+
+$lang['go_website']			= 'वेबसाईट पर जाएँ';
+$lang['go_control_panel']	= 'कंट्रोल पैनल पर जाएँ';
+
+/* End of file complete_lang.php */
---
+++ installer/language/hindi/global_lang.php
@@ -0,0 +1,13 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['intro']	=	'परिचय';
+$lang['step1']	=	'चरण #१';
+$lang['step2']	=	'चरण #२';
+$lang['step3']	=	'चरण #३';
+$lang['step4']	=	'चरण #४';
+$lang['final']	=	'अंतिम चरण';
+
+$lang['installer.passwords_match']		= "पासवर्ड्स मेल खाते है."; #translate
+$lang['installer.passwords_dont_match']	= "पासवर्ड्स मेल नहीं खाते"; #translate
+
+/* End of file global_lang.php */
