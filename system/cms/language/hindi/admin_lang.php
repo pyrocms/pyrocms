2ce9041e79826ec4ee10505e8fef8e79ac4c73ae
Rakesh Tembhurne <rakesh@tembhurne.com>
2011-10-03 15:04:04 +0530

    Hindi language (India) files for PyroCMS core

---
+++ system/cms/language/hindi/admin_lang.php
@@ -0,0 +1,50 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cp_admin_title'] 				= 'कंट्रोल पैनल';
+$lang['cp_admin_home_title'] 			= 'डैशबोर्ड';
+$lang['cp_admin_quick_links']			= 'तत्काल लिंक्स';
+
+$lang['cp_welcome_title'] 				= 'स्वागत';
+$lang['cp_welcome_message'] 			= '%s कंट्रोल पैनल में स्वागत है. ऊपर एवं बाएँ दी गयी लिंक्स की मदत से आप आपके वेबसाईट के लगभग सभी पहलुओं को नियंत्रित कर सकते है. अगर आपको कोई समस्या आये, तो कृपया समस्या को बगट्रॅकर में प्रस्तुत करे.';
+
+$lang['cp_news_feed_title'] 			= 'समाचार फीड';
+
+// Shortcuts
+$lang['cp_manage_comments']				= 'टिप्पणियाँ संभाले';
+$lang['cp_manage_pages']				= 'पृष्ट संभाले';
+$lang['cp_manage_files']				= 'फाइल्स संभाले';
+$lang['cp_manage_users']				= 'प्रयोक्ता संभाले';
+
+// Header
+$lang['cp_to_home'] 					= 'डैशबोर्ड पर वापस';
+$lang['cp_view_frontend'] 				= 'वेबसाईट देखे';
+$lang['cp_logged_in_welcome'] 			= 'नमस्ते %s';
+$lang['cp_logout_label'] 				= 'लॉग आउट';
+$lang['cp_edit_profile_label'] 			= 'प्रोफाइल संपादन';
+
+// Footer
+$lang['cp_change_language'] 			= 'भाषा बदले';
+$lang['cp_documentation'] 				= 'दस्तावेज';
+$lang['cp_report_bug'] 					= 'बग की सुचना दे';
+$lang['cp_contact_support'] 			= 'मदद';
+
+$lang['cp_nav_settings']				= 'सेटिंग्स';
+$lang['cp_nav_addons']					= 'अॅड-ऑन';
+$lang['cp_nav_modules']					= 'मोडयुल्स';
+$lang['cp_nav_widgets']					= 'विजेट्स';
+$lang['cp_nav_users']					= 'प्रयोक्ता';
+$lang['cp_nav_design']					= 'डिजाईन';
+$lang['cp_nav_content']					= 'सारांश';
+$lang['cp_nav_utilities']				= 'युटिलिटीज';
+
+$lang['cp_must_login']	 				= 'इस पृष्ट को देखने के लिए आपका लॉग इन होना जरुरी है.';
+$lang['cp_access_denied'] 				= 'इस पृष्ट को देखने के लिए आपको योग्य अनुमति नहीं है.';
+$lang['cp_delete_installer_message']	= 'कृपया installer डिरेक्टरी को सर्वर से नष्ट कर दे. इसे सर्वर पर रखने से आपके वेबसाईट का नियंत्रण किसी और को दिया जा सकता है. ';
+$lang['cp_upgrade_message']				= 'अभी आप %s वर्जन इस्तेमाल कर रहे है, किन्तु आपके डेटाबेस का वर्जन %s है. डेटाबेस को अपग्रेड करने के लिए कृपया <a href="%s" title="अपग्रेड">यहाँ क्लिक करे</a>.';
+$lang['cp_google_analytics_missing']	= 'गूगल अॅनालिटीक्स की सेटिंग्स गायब है. उन्हें %s में डाले या फिर अपने अॅडमिनीस्ट्रेटर को संपर्क करे';
+$lang['cp_google_analytics_no_connect']	= 'गूगल अॅनालिटीक्स को संपर्क नहीं हो पा रहा. %s को जाँच ले';
+
+$lang['required_label'] 				= 'जरुरी';
+$lang['sb_recent_users'] 				= 'हाल के प्रयोक्ता';
+
+/* End of file admin_lang.php */
---
+++ system/cms/language/hindi/errors_lang.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Error 404
+$lang['error_404_title'] = 'पृष्ट नामौजूद';
+$lang['error_404_message'] = 'आप जिस पृष्ट को खोज रहे है वह हम ढूंड नहीं पा रहे है, कृपया <a href="%s">यहाँ</a> क्लिक कर मुख पृष्ट पर जाएँ.';
+
+// Database
+$lang['error_invalid_db_group'] = 'डेटाबेस अमान्य कन्फिग्यरेशन वर्ग %s को इस्तेमाल करने की कोशिश कर रहा है.';
+
+/* End of file errors_lang.php */
