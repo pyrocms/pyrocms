78e0eedc25a6e22caae2526f477465d42bcad1c4
Scott Parry <hello@ikreativ.com>
2011-10-06 11:17:24 +0100

    gallery form

--- addons/shared_addons/modules/galleries/views/admin/form.php
+++ addons/shared_addons/modules/galleries/views/admin/form.php
@@ -17,43 +17,56 @@
 				<li><a href="#gallery-design"><span><?php echo lang('galleries.design_label'); ?></span></a></li>
 				<li><a href="#gallery-script"><span><?php echo lang('galleries.script_label'); ?></span></a></li>
 			</ul>
+			
+			<hr>
 	
-			<div id="gallery-content">
-				<ol>
+			<div id="content-body">
+				<ul>
 					<li class="<?php echo alternator('', 'even'); ?>">
-						<?php echo form_label(lang('galleries.folder_label'), 'folder_id'); ?>
 						<?php echo form_dropdown('folder_id', array(lang('global:select-pick')) + $folders_tree, $gallery->folder_id, 'id="folder_id" class="required"'); ?>
 					</li>
+					
+					<hr>
 	
 					<li class="<?php echo alternator('', 'even'); ?>">
-						<label for="title"><?php echo lang('galleries.title_label'); ?></label>
+						<label for="title"><?php echo lang('galleries.title_label'); ?></label><br>
 						<input type="text" id="title" name="title" maxlength="255" value="<?php echo $gallery->title; ?>" />
 						<span class="required-icon tooltip"><?php echo lang('required_label'); ?></span>
 					</li>
+					
+					<hr>
 	
 					<li class="<?php echo alternator('', 'even'); ?>">
-						<label for="slug"><?php echo lang('galleries.slug_label'); ?></label>
+						<label for="slug"><?php echo lang('galleries.slug_label'); ?></label><br>
 						<?php echo form_input('slug', $gallery->slug, 'class="width-15"'); ?>
 						<span class="required-icon tooltip"><?php echo lang('required_label'); ?></span>
 					</li>
+					
+					<hr>
 	
 					<li class="<?php echo alternator('', 'even'); ?>">
-						<label for="description"><?php echo lang('galleries.description_label'); ?></label><br />
+						<label for="description"><?php echo lang('galleries.description_label'); ?></label><br>
 						<?php echo form_textarea(array('id'=>'description', 'name'=>'description', 'value' => $gallery->description, 'rows' => 10, 'class' => 'wysiwyg-simple')); ?>
 					</li>
+					
+					<hr>
 	
 					<li class="<?php echo alternator('', 'even'); ?>">
-						<label for="comments"><?php echo lang('galleries.comments_label'); ?></label>
+						<label for="comments"><?php echo lang('galleries.comments_label'); ?></label><br>
 						<?php echo form_dropdown('enable_comments', array('1'=>lang('galleries.comments_enabled_label'), '0'=>lang('galleries.comments_disabled_label')), $gallery->enable_comments); ?>
 					</li>
+					
+					<hr>
 	
 					<li class="<?php echo alternator('', 'even'); ?>">
-						<label for="published"><?php echo lang('galleries.published_label'); ?></label>
+						<label for="published"><?php echo lang('galleries.published_label'); ?></label><br>
 						<?php echo form_dropdown('published', array('1'=>lang('galleries.published_yes_label'), '0'=>lang('galleries.published_no_label')), $gallery->published); ?>
 					</li>
+					
+					<hr>
 	
 					<li class="thumbnail-manage <?php echo alternator('', 'even'); ?>">
-						<label for="gallery_thumbnail"><?php echo lang('galleries.thumbnail_label'); ?></label>
+						<label for="gallery_thumbnail"><?php echo lang('galleries.thumbnail_label'); ?></label><br>
 						<select name="gallery_thumbnail" id="gallery_thumbnail">
 	
 							<?php if ( ! empty($gallery->thumbnail_id) ): ?>
@@ -109,7 +122,7 @@
 						<div class="clear-both"></div>
 					</li>
 	
-				</ol>
+				</ul>
 	
 			</div>
 	
