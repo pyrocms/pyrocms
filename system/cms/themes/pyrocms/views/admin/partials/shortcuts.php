e2931784010c28f90ab2f8cb8c62ea57a8f3c197
Scott Parry <hello@ikreativ.com>
2011-10-07 12:42:25 +0100

    shortcut button

--- system/cms/themes/pyrocms/views/admin/partials/shortcuts.php
+++ system/cms/themes/pyrocms/views/admin/partials/shortcuts.php
@@ -5,6 +5,7 @@
 			<li><?php echo anchor($shortcut['uri'], lang($shortcut['name'])); ?></li>
 			<?php endforeach; ?>
 		<?php endif; ?>
+		
 		<?php if ( ! empty($module_details['shortcuts'])): ?>
 			<?php foreach ($module_details['shortcuts'] as $shortcut): ?>
 			<li><?php echo anchor($shortcut['uri'], lang($shortcut['name'])); ?></li>
