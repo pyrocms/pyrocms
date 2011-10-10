9cee0e3def912e68610b1dba6dee4e1fadce51e6
Adam Fairholm <adam.fairholm@gmail.com>
2011-10-09 20:41:39 -0400

    Adding optional ability to choose secondary or primary buttons. Primary buttons are the default.

--- system/cms/themes/pyrocms/views/admin/partials/buttons.php
+++ system/cms/themes/pyrocms/views/admin/partials/buttons.php
@@ -1,4 +1,19 @@
 <?php if (isset($buttons) && is_array($buttons)): ?>
+
+	<?php 
+	
+		// What type of buttons?
+		if(isset($button_type) && $button_type == 'primary'):
+			$btn_class = 'btn';
+		elseif(isset($button_type) && $button_type == 'secondary'):
+			$btn_class = 'button';
+		else:
+			// Default to primary
+			$btn_class = 'btn';
+		endif;
+	
+	?>
+
 	<?php foreach ($buttons as $key => $button): ?>
 		<?php
 		/**
@@ -9,8 +24,11 @@
 		<?php $button	= ! is_numeric($key) && ($extra = $button) ? $key : $button; ?>
 
 		<?php switch ($button) :
-			case 'delete': ?>
-				<button type="submit" name="btnAction" value="delete" class="btn red confirm">
+			case 'delete': 
+			if($btn_class == 'btn') $btn_class .= ' gray';
+			
+			?>
+				<button type="submit" name="btnAction" value="delete" class="<?php echo $btn_class; ?> confirm">
 					<span><?php echo lang('buttons.delete'); ?></span>
 				</button>
 				<?php break;
@@ -27,14 +45,15 @@
 			case 'save_exit':
 			case 'unapprove':
 			case 'upload': ?>
-				<button type="submit" name="btnAction" value="<?php echo $button ?>" class="btn blue">
+				<button type="submit" name="btnAction" value="<?php echo $button ?>" class="<?php echo $btn_class; ?> blue">
 					<span><?php echo lang('buttons.' . $button); ?></span>
 				</button>
 				<?php break;
 			case 'cancel':
 			case 'close':
 			case 'preview':
-				echo anchor('admin/' . $this->module_details['slug'], lang('buttons.' . $button), 'class="btn gray ' . $button . '"');
+				if($btn_class == 'btn') $btn_class .= ' gray';
+				echo anchor('admin/' . $this->module_details['slug'], lang('buttons.' . $button), 'class="'.$btn_class. ' ' . $button . '"');
 				break;
 
 			/**
@@ -43,8 +62,9 @@
 			 */
 			case 'edit':
 				$id = is_array($extra) && array_key_exists('id', $extra) ? '/' . $button . '/' . $extra['id'] : NULL;
+				if($btn_class == 'btn') $btn_class .= ' gray';
 
-				echo anchor('admin/' . $this->module_details['slug'] . $id, lang('buttons.' . $button), 'class="btn gray ' . $button . '"');
+				echo anchor('admin/' . $this->module_details['slug'] . $id, lang('buttons.' . $button), 'class="'.$btn_class.' gray ' . $button . '"');
 				break; ?>
 
 		<?php endswitch; ?>
