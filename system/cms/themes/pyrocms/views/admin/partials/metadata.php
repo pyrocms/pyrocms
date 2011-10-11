98ed74ce65da2f3819036578c8c21b701065c0dc
Adam Fairholm <adam.fairholm@gmail.com>
2011-10-11 16:04:00 -0400

    Adding slashes to the js DEFAULT_TITLE var for cases where titles have double quotes, breaking the string. Closes #780.

--- system/cms/themes/pyrocms/views/admin/partials/metadata.php
+++ system/cms/themes/pyrocms/views/admin/partials/metadata.php
@@ -19,7 +19,7 @@
 	var BASE_URL			= "<?php echo BASE_URL;?>";
 	var BASE_URI			= "<?php echo BASE_URI;?>";
 	var UPLOAD_PATH			= "<?php echo UPLOAD_PATH;?>";
-	var DEFAULT_TITLE		= "<?php echo $this->settings->site_name; ?>";
+	var DEFAULT_TITLE		= "<?php echo addslashes($this->settings->site_name); ?>";
 	var DIALOG_MESSAGE		= "<?php echo lang('global:dialog:delete_message'); ?>";
 	pyro.admin_theme_url 	= "<?php echo BASE_URL . $this->admin_theme->path; ?>";
 	pyro.apppath_uri		= "<?php echo APPPATH_URI; ?>";
