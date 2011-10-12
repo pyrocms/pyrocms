af7bf8e278ba44e5924b56efbae50c1f415313ff
jerel <mail@unruhdesigns.com>
2011-10-12 09:17:25 -0500

    Modified shortcuts so that you can pass any number of attributes... rel, class, title, etc.

--- system/cms/themes/pyrocms/views/admin/partials/shortcuts.php
+++ system/cms/themes/pyrocms/views/admin/partials/shortcuts.php
@@ -1,14 +1,22 @@
 <nav id="shortcuts">
 	<ul>
 		<?php if ( ! empty($module_details['sections'][$active_section]['shortcuts'])): ?>
-			<?php foreach ($module_details['sections'][$active_section]['shortcuts'] as $shortcut): ?>
-			<li><?php echo anchor($shortcut['uri'], lang($shortcut['name'])); ?></li>
+			<?php foreach ($module_details['sections'][$active_section]['shortcuts'] as $shortcut):
+				$name 	= $shortcut['name'];
+				$uri	= $shortcut['uri'];
+				unset($shortcut['name']);
+				unset($shortcut['uri']); ?>
+			<li><a <?php foreach($shortcut AS $attr => $value) echo $attr.'="'.$value.'"'; echo 'href="' . site_url($uri) . '">' . lang($name) . '</a>'; ?></li>
 			<?php endforeach; ?>
 		<?php endif; ?>
 		
 		<?php if ( ! empty($module_details['shortcuts'])): ?>
-			<?php foreach ($module_details['shortcuts'] as $shortcut): ?>
-			<li><?php echo anchor($shortcut['uri'], lang($shortcut['name'])); ?></li>
+			<?php foreach ($module_details['shortcuts'] as $shortcut):
+				$name 	= $shortcut['name'];
+				$uri	= $shortcut['uri'];
+				unset($shortcut['name']);
+				unset($shortcut['uri']); ?>
+			<li><a <?php foreach($shortcut AS $attr => $value) echo $attr.'="'.$value.'"'; echo 'href="' . site_url($uri) . '">' . lang($name) . '</a>'; ?></li>
 			<?php endforeach; ?>
 		<?php endif; ?>
 	</ul>
