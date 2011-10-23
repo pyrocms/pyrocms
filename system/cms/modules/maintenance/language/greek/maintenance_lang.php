27f611047c801ffbe9a54513a1f417d4a7d77910
Petsagourakis George <petsagouris@gmail.com>
2011-10-23 13:57:43 +0300

    Added Maintenance module Greek translation
    Signed-off-by: Petsagourakis George <petsagouris@gmail.com>

---
+++ system/cms/modules/maintenance/language/greek/maintenance_lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['maintenance.list_label'] 		= 'Συντήρηση Προσωρινής Αποθήκευσης';
+$lang['maintenance.no_items'] 			= 'Δεν Υπάρχουν Φάκελοι Προσωρινής Αποθήκευσης';
+
+$lang['maintenance.count_label'] 		= 'Στοιχεία';
+
+$lang['maintenance.empty_msg'] 			= '%s Στοιχείο(α) αφαιρέθηκαν από τον φάκελο %s';
+$lang['maintenance.remove_msg'] 		= 'Ο φάκελος %2$s διαγράφηκε';
+
+$lang['maintenance.empty_msg_err'] 		= 'Σφάλμα κατά το άδειασμα του φακέλου %s';
+$lang['maintenance.remove_msg_err'] 	= 'Σφάλμα κατά την διαγραφή του φακέλου %s';
