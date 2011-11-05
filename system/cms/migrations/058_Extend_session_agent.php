f939f5db4a3ff3b32ec54f1b024cb62a76ad0892
Phil Sturgeon <email@philsturgeon.co.uk>
2011-11-05 16:14:48 +0000

    Changed the migration for fixing user session.

--- system/cms/migrations/058_Extend_session_agent.php
+++ system/cms/migrations/058_Extend_session_agent.php
@@ -5,7 +5,7 @@ class Migration_Extend_session_agent extends CI_Migration {
 	public function up()
 	{
 		$this->db->query('ALTER TABLE '.$this->db->dbprefix('ci_sessions').' 
-			CHANGE `user_agent` `user_agent` VARCHAR(120) CHARACTER SET utf8 COLLATE utf8_unicode_ci');
+			MODIFY user_agent VARCHAR(120)');
 	}
 
 	public function down()
