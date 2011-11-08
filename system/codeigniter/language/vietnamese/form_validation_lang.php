286aa3aae221694b71f23dab996bdcfddf9e7ed2
Thanh Nguyen <nguyenhuuthanh@gmail.com>
2011-11-08 22:31:09 +0700

    Add Vietnamese to installation process

---
+++ system/codeigniter/language/vietnamese/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= "Bạn phải nhập %s.";
+$lang['isset']				= "Bạn phải nhập giá trị cho %s.";
+$lang['valid_email']		= "Bạn phải nhập email đúng định dạng cho %s.";
+$lang['valid_emails']		= "Bạn phải nhập các email đúng định dạng cho %s.";
+$lang['valid_url']			= "Bạn phải nhập URL đúng định dạng cho %s.";
+$lang['valid_ip']			= "Bạn phải nhập IP đúng định dạng cho %s.";
+$lang['min_length']			= "Trường %s phải có ít nhất %s ký tự.";
+$lang['max_length']			= "Trường %s không được vượt quá %s ký tự.";
+$lang['exact_length']		= "Trường %s chỉ được phép có %s ký tự.";
+$lang['alpha']				= "Trường %s chỉ được phép điền các ký tự chữ cái.";
+$lang['alpha_numeric']		= "Trường %s chỉ được phép điền các ký tự chữ và số.";
+$lang['alpha_dash']			= "Trường %s chỉ được phép điền các ký tự chữ và số, dấu gạch dưới và dấu gạch ngang.";
+$lang['numeric']			= "Trường %s chỉ được phép điền các ký tự số.";
+$lang['is_numeric']			= "Trường %s chỉ được phép điền các ký tự số.";
+$lang['integer']			= "Trường %s chỉ được phép điền số nguyên.";
+$lang['regex_match']		= "Trường %s không đúng định dạng.";
+$lang['matches']			= "Trường %s không khớp với trường %s.";
+$lang['is_unique'] 			= "Trường %s phải có giá trị là duy nhất.";
+$lang['is_natural']			= "Trường %s chỉ được phép điền số tự nhiên.";
+$lang['is_natural_no_zero']	= "Trường %s chỉ được phép điền số dương lớn hơn 0.";
+$lang['decimal']			= "Trường %s chỉ được phép điền số thập phân.";
+$lang['less_than']			= "Trường %s chỉ được phép điền số nhỏ hơn %s.";
+$lang['greater_than']		= "Trường %s chỉ được phép điền số lớn hơn %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/vietnamese/form_validation_lang.php */
