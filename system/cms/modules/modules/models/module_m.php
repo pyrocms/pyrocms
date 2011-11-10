581c5d3dae868ca2f09da7eb5c467263b58391d8
Adam Fairholm <adam.fairholm@gmail.com>
2011-11-10 02:15:23 -0500

    Fix for my previous fix on the module permissions error. That's how I roll. Ref #862

--- system/cms/modules/modules/models/module_m.php
+++ system/cms/modules/modules/models/module_m.php
@@ -175,11 +175,11 @@ class Module_m extends MY_Model
 				'path' 				=> $location,
 				'updated_on'		=> $row->updated_on
 			);
-
+			
 			if ( ! empty($params['is_backend']))
 			{
 				// This user has no permissions for this module
-				if ( $this->current_user->group !== 'admin' AND (!isset($result->slug) OR !isset($this->permissions[$result->slug]) OR empty($this->permissions[$result->slug])))
+				if ( $this->current_user->group !== 'admin' AND empty($this->permissions[$row->slug]) )
 				{
 					continue;
 				}
@@ -187,7 +187,7 @@ class Module_m extends MY_Model
 
 			$modules[$module['name']] = $module;
 		}
-
+		
 		ksort($modules);
 
 		return array_values($modules);
