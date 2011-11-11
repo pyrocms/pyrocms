00291978738189d9aaacb5f1d1da9d5f958b50e7
Adam Jackett <adam@darkhousemedia.com>
2011-11-11 14:55:34 -0500

    Added chunk method to pages plugin

--- system/cms/modules/pages/plugin.php
+++ system/cms/modules/pages/plugin.php
@@ -53,6 +53,24 @@ class Plugin_Pages extends Plugin
 
 		return $this->content() ? $page : $page['body'];
 	}
+
+	/**
+	 * Get a page chunk by page ID and chunk name
+	 *
+	 * @param int 		$id The ID of the page
+	 * @param string 	$slug The name of the chunk
+	 * @return array
+	 */
+	function chunk()
+	{
+		$chunk = $this->db->select('*')
+					->where('page_id', $this->attribute('id'))
+					->where('slug', $this->attribute('name'))
+					->get('page_chunks')
+					->row_array();
+
+		return ($chunk ? ($this->content() ? $chunk : $chunk['body']) : FALSE);
+	}
 	
 	/**
 	 * Children list
