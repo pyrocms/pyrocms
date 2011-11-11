d77c7f0481035dc4bdfbe058f9615535050206f6
jerel <mail@unruhdesigns.com>
2011-11-10 22:42:24 -0600

    The old contact form will now be swapped for the new one during upgrade.

--- system/cms/migrations/060_Convert_lex_tags.php
+++ system/cms/migrations/060_Convert_lex_tags.php
@@ -9,9 +9,18 @@ class Migration_Convert_lex_tags extends CI_Migration {
 		foreach ($page_chunks as $chunk)
 		{
 			$new_body = preg_replace(array(
-				'/{(\/)?(pyro:)?([^}]+)}/'
+				'/{(\/)?(pyro:)?([^}]+)}/',
+				'/{{\scontact:form.*?}}/'
 			), array(
-				'{{ $1$3 }}'
+				'{{ $1$3 }}',
+				'<p>To contact us please fill out the form below.</p>
+				{{ contact:form name="text|required" email="text|required|valid_email" subject="text|required|max_length[100]" message="textarea" attachment="file|zip" }}
+					<div><label for="name">Name:</label>{{ name }}</div>
+					<div><label for="email">Email:</label>{{ email }}</div>
+					<div><label for="subject">Subject:</label>{{ subject }}</div>
+					<div><label for="message">Message:</label>{{ message }}</div>
+					<div><label for="attachment">Attach  a zip file:</label>{{ attachment }}</div>
+				{{ /contact:form }}'
 			), $chunk->body);
 			
 			$this->db
