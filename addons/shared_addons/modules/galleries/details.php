4ebd6330eb579f58fb3ec411ba81e2b93f7787cf
Petsagourakis George <petsagouris@gmail.com>
2011-11-13 12:21:31 +0200

    updated some modules greek title and descriptions

--- addons/shared_addons/modules/galleries/details.php
+++ addons/shared_addons/modules/galleries/details.php
@@ -10,6 +10,7 @@ class Module_Galleries extends Module {
 			'name' => array(
 				'sl' => 'Galerija',
 				'en' => 'Galleries',
+				'en' => 'Γκαλερί',
 				'de' => 'Galerien',
 				'nl' => 'Gallerijen',
 				'fr' => 'Galeries',
@@ -26,6 +27,7 @@ class Module_Galleries extends Module {
 			'description' => array(
 				'sl' => 'Modul galerije vam omogoča da ustvarite albume vaših slik.',
 				'en' => 'The galleries module is a powerful module that lets users create image galleries.',
+				'el' => 'Παρέχει την δυνατότητα στους χρήστες να δημιουργούν άλμπουμ εικόνων.',
 				'de' => 'Mit dem Galerie Modul kannst du Bildergalerien anlegen.',
 				'nl' => 'De gallerij module die gebruikers in staat stelt afbeeldingsgallerijen te maken.',
 				'fr' => 'Galerie est une puissante extension permettant de créer des galeries d\'images.',
