82dfc8323cf9f0dd3e6a95086c4279addd0911e3
jerel <mail@unruhdesigns.com>
2011-11-14 13:32:43 -0600

    Updated navigation ajax form

--- system/cms/modules/navigation/views/admin/ajax/form.php
+++ system/cms/modules/navigation/views/admin/ajax/form.php
@@ -1,4 +1,4 @@
-<div id="details-container" class="form_inputs">
+<div id="details-container">
 	<?php if ($this->method == 'create'): ?>
 		<div class="hidden" id="title-value-<?php echo $navigation_link->navigation_group_id; ?>">
 			<?php echo lang('nav_link_create_title');?>
@@ -9,7 +9,7 @@
 		</div>
 	<?php endif; ?>
 	
-	<?php echo form_open(uri_string(), 'id="nav-' . $this->method . '" class="crud"'); ?>
+	<?php echo form_open(uri_string(), 'id="nav-' . $this->method . '" class="form_inputs"'); ?>
 	
 		<ul>
 <?php if ($this->method == 'edit'): ?>
@@ -50,22 +50,22 @@
 				</p>
 	
 				<div id="navigation-url" style="<?php echo @$navigation_link->link_type == 'url' ? '' : 'display:none'; ?>">
-					<label for="url"><?php echo lang('nav_url_label');?></label>
+					<label class="label" for="url"><?php echo lang('nav_url_label');?></label>
 					<input type="text" id="url" name="url" value="<?php echo empty($navigation_link->url) ? 'http://' : $navigation_link->url; ?>" />
 				</div>
 	
 				<div id="navigation-module" style="<?php echo @$navigation_link->link_type == 'module' ? '' : 'display:none'; ?>">
-					<label for="module_name"><?php echo lang('nav_module_label');?></label>
+					<label class="label" for="module_name"><?php echo lang('nav_module_label');?></label>
 					<?php echo form_dropdown('module_name', array(lang('nav_link_module_select_default'))+$modules_select, $navigation_link->module_name) ?>
 				</div>
 	
 				<div id="navigation-uri" style="<?php echo @$navigation_link->link_type == 'uri' ? '' : 'display:none'; ?>">
-					<label for="uri"><?php echo lang('nav_uri_label');?></label>
+					<label class="label" for="uri"><?php echo lang('nav_uri_label');?></label>
 					<input type="text" id="uri" name="uri" value="<?php echo $navigation_link->uri; ?>" />
 				</div>
 	
 				<div id="navigation-page" style="<?php echo @$navigation_link->link_type == 'page' ? '' : 'display:none'; ?>">
-					<label for="page_id"><?php echo lang('nav_page_label');?></label>
+					<label class="label" for="page_id"><?php echo lang('nav_page_label');?></label>
 					<select name="page_id">
 						<option value=""><?php echo lang('global:select-pick');?></option>
 						<?php echo $tree_select; ?>
