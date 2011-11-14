3c55ad97a5b49e5e4e1d5a28e3187477ecf291c5
Sami Torola <sami.torola@gmail.com>
2011-11-15 00:07:30 +0200

    Added support caching headers for images. It will also support 304 header responces if requesting image has not changed compared to HTTP_IF_MODIFIED_SINCE value. Setting has configuration to change expiration time.

--- system/cms/modules/files/details.php
+++ system/cms/modules/files/details.php
@@ -112,7 +112,22 @@ class Module_Files extends Module {
 			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 		";
 
-		if($this->db->query($files) && $this->db->query($file_folders))
+		$files_cache = array(
+			'slug' =>   'files_cache',
+			'title' => 'Files Cache',
+			'description'   => 'When outputting an image via site.com/files what shall we set the cache expiration for?',
+			'`default`' => '8-hour',
+			'`value`'   => '480',
+			'type' =>   'select',
+			'`options`' => '0=no-cache|1=1-minute|60=1-hour|180=3-hour|480=8-hour|1440=1-day|43200=30-days',
+			'is_required'   => 1,
+			'is_gui' => 1,
+			'module' => 'files'
+		);
+
+		if( $this->db->query($files) &&
+			$this->db->query($file_folders) &&
+			$this->db->insert('settings', $files_cache))
 		{
 			return TRUE;
 		}
