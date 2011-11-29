a14570fead7505b348c3c9cdfd27fe557602098b
KHALIL <KHALIL@NOVIRIS-KHALIL>
2011-11-29 10:20:23 -0400

    French translations

--- system/cms/modules/pages/language/french/permission_lang.php
+++ system/cms/modules/pages/language/french/permission_lang.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 // Blog Permissions
-$lang['pages.role_put_live'] = 'Put pages live';
-$lang['pages.role_edit_live'] = 'Edit live pages';
-$lang['pages.role_delete_live'] = 'Delete live articles';
+$lang['pages.role_put_live'] = 'Publié les pages en ligne';
+$lang['pages.role_edit_live'] = 'Editer les pages publiées';
+$lang['pages.role_delete_live'] = 'Supprimer les pages publiées';
