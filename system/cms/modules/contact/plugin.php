49929daf9c4b412682291203d428f4c913c90afb
jerel <mail@unruhdesigns.com>
2011-12-05 13:45:35 -0600

    Minor updates to contact plugin

--- system/cms/modules/contact/plugin.php
+++ system/cms/modules/contact/plugin.php
@@ -104,9 +104,7 @@ class Plugin_Contact extends Plugin {
 			  $field_list['to'],
 			  $field_list['from'],
 			  $field_list['reply-to'],
-			  $field_list['max-size'],
-			  $field_list['redirect'],
-			  $field_list['action']
+			  $field_list['max-size']
 			  );
 
 		foreach ($field_list AS $field => $rules)
@@ -195,6 +193,13 @@ class Plugin_Contact extends Plugin {
 
 		if ($this->form_validation->run())
 		{
+			// maybe it's a bot?
+			if ($this->input->post('d0ntf1llth1s1n') !== ' ')
+			{
+				$this->session->set_flashdata('error', lang('contact_submit_error'));
+				redirect(current_url());
+			}
+
 			$data = $this->input->post();
 
 			// Add in some extra details about the visitor
@@ -299,6 +304,7 @@ class Plugin_Contact extends Plugin {
 		}
 	
 		$output	 = form_open_multipart($action, 'class="contact-form"').PHP_EOL;
+		$output	.= form_input('d0ntf1llth1s1n', ' ', 'class="default-form" style="display:none"');
 		$output	.= $this->parser->parse_string($this->content(), str_replace('{{', '{ {', $parse_data), TRUE).PHP_EOL;
 		$output .= '<span class="contact-button">'.form_submit('submit-button', ucfirst($button)).'</span>'.PHP_EOL;
 		$output .= form_close();
