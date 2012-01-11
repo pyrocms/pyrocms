a4d90157dc531abf9613493c80549c7131348a0e
toni haryanto <toha.samba@gmail.com>
2012-01-11 17:23:01 +0700

    add indonesian language for module variabel

---
+++ system/cms/modules/variables/language/indonesian/variables_lang.php
@@ -0,0 +1,36 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * English translation.
+ *
+ * @author		-
+ * @package		PyroCMS
+ * @subpackage 	Variables Module
+ * @category	Modules
+ * @link		http://pyrocms.com
+ * @date		2011-03-13
+ * @version		1.1.0
+ */
+
+// labels
+$lang['variables.syntax_label']				= 'Sintaks';
+$lang['variables.data_label'] 			    = 'Nilai';
+
+// titles
+$lang['variables.create_title'] 			= 'Tambah Variabel';
+$lang['variables.edit_title'] 				= 'Edit variabel "%s"';
+$lang['variables.list_title'] 				= 'Daftar variabel';
+
+// messages
+$lang['variables.no_variables'] 			= 'Tidak ada variabel.';
+$lang['variables.add_success'] 				= 'Variabel Anda "%s" telah tersimpan.';
+$lang['variables.add_error'] 				= 'Terjadi kesalahan ketika mencoba menyimpan variabel "%s".';
+$lang['variables.edit_success'] 			= 'Perubahan pada variabel "%s" tersimpan.';
+$lang['variables.edit_error'] 				= 'Terjadi kesalahan ketika mencoba memperbaharui variabel "%s".';
+$lang['variables.delete_success']			= 'Variabel "%s" berhasil dihapus.';
+$lang['variables.delete_error'] 			= 'Terjadi kesalahan ketika mencoba menghapus variabel "%s".';
+$lang['variables.mass_delete_success']		= '%d dari %d variabel berhasil dihapus, yakni "%s dan %s".';
+$lang['variables.mass_delete_error'] 		= 'Terjadi kesalahan ketika mencoba menghapus %d dari %d variabel, yakni "%s dan %s".';
+$lang['variables.no_select_error'] 			= 'Anda harus memilih variabel terlebih dahulu.';
+$lang['variables.already_exist_error']		= 'Variabel dengan nama "%s" sudah ada.';
+
+/* End of file variables_lang.php */
