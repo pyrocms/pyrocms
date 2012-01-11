975d6f26b8a1123d325402b9bae952664840a8a1
toni haryanto <toha.samba@gmail.com>
2012-01-11 13:38:36 +0700

    add indonesian language for module navigation

---
+++ system/cms/modules/navigation/language/indonesian/navigation_lang.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['nav_title_label']                        = 'Judul';
+$lang['nav_parent_label']						= 'Induk';
+$lang['nav_target_label']                       = 'Target';
+$lang['nav_class_label']                        = 'Class';
+$lang['nav_url_label']                          = 'URL';
+$lang['nav_details_label']                      = 'Detail';
+$lang['nav_text_label']                         = 'Teks';
+$lang['nav_group_label']                        = 'Grup';
+$lang['nav_location_label']                     = 'Lokasi';
+$lang['nav_type_label']                         = 'Tipe tautan';
+$lang['nav_uri_label']                          = 'Tautan dalam Situs (URI)';
+$lang['nav_page_label']                         = 'Halaman';
+$lang['nav_module_label']                       = 'Modul';
+$lang['nav_restricted_to']						= 'Terbatas pada';
+$lang['nav_abbrev_label']                       = 'Singkatan';
+
+$lang['nav_link_target_self']                   = 'Jendela saat ini (default)';
+$lang['nav_link_target_blank']                  = 'Jendela baru (_blank)';
+
+// titles
+$lang['nav_link_create_title']                  = 'Tambah Tautan';
+$lang['nav_group_create_title']                 = 'Tambah Grup';
+$lang['nav_link_edit_title']                    = 'Edit tautan navigasi "%s"';
+$lang['nav_link_list_title']                    = 'Tautan';
+$lang['nav_group_list_title']                   = 'Grup';
+
+// messages
+$lang['nav_group_no_links']                     = 'Tidak ada tautan di grup ini.';
+$lang['nav_no_groups']                          = 'Tidak ada grup navigasi.';
+$lang['nav_group_delete_confirm']               = 'Anda yakin akan menghapus grup navigasi ini? Ini akan menghapus SEMUA tautan di dalam grup, dan file layout harus diperbaharui untuk membuang referensi navigasi.';
+$lang['nav_group_add_success']                  = 'Grup navigasi telah disimpan.';
+$lang['nav_group_add_error']                    = 'Terjadi kesalahan.';
+$lang['nav_group_mass_delete_success']          = 'Grup navigasi telah dihapus.';
+$lang['nav_link_add_success']                   = 'Tautan navigasi telah ditambahkan.';
+$lang['nav_link_add_error']                     = 'Terjadi kesalahan yang tidak terduga.';
+$lang['nav_link_not_exist_error']               = 'Tautan navigasi tidak ada.';
+$lang['nav_link_edit_success']                  = 'Tautan navigasi telah disimpan.';
+$lang['nav_link_delete_success']                = 'Tautan navigasi telah dihapus.';
+$lang['nav_choose_value']						= 'Kolom %s tidak boleh kosong.';
+$lang['nav_link_type_desc']                     = 'Silakan pilih tipe tautan untuk memberikan tambahan opsi untuk membuat tautan Anda.';
