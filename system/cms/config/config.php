373649c8e15d812638eba5d8c5ea844d6a957f5d
Phil Sturgeon <email@philsturgeon.co.uk>
2012-01-22 20:57:07 -0700

    Changed migration 73 to 74. Man this sucks.

--- system/cms/config/config.php
+++ system/cms/config/config.php
@@ -184,7 +184,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 1;
+$config['log_threshold'] = 3;
 
 /*
 |--------------------------------------------------------------------------
--- system/cms/config/migration.php
+++ system/cms/config/migration.php
@@ -23,7 +23,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 73;
+$config['migration_version'] = 74;
 
 
 /*
