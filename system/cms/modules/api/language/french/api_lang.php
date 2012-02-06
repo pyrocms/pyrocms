15d3a095935c954862b4f255ad7afb41f0302585
khalil-tabbal <khalil.tabbal@gmail.com>
2012-02-06 16:43:20 -0400

    french translations

---
+++ system/cms/modules/api/language/french/api_lang.php
@@ -0,0 +1,21 @@
+<?php 
+
+$lang['api:recent_activity'] = 'Activité Récente';
+
+$lang['api:overview'] = 'Vue d\'ensemble';
+$lang['api:keys'] = 'Clés';
+
+$lang['api:method'] = 'Méthode';
+$lang['api:params'] = 'Paramètres';
+$lang['api:api_key'] = 'Clé API';
+
+$lang['api:no_logs']          = 'Il n\'y a aucun post pour le moment.';
+
+$lang['api:enable_api'] = 'Statut API';
+$lang['api:enable_api_description'] = 'Ceci permettra à un utilisateur possédant la clé API de requêter l\'interface et obtenir des données. Faites ceci uniquement si vous comprenez les conséquences.';
+
+$lang['api:enable_user_keys'] = 'Clés API Utilisateur';
+$lang['api:enable_user_keys_description'] = 'Ceci permet aux utilisateurs de générer leur propre clés bas-niveau pour qu\'il puissent interagir avec l\'API.';
+
+$lang['api:key_message'] = 'Votre Token API est %s — Garder le <strong>secret</strong>!';
+$lang['api:generate_key'] = 'Générer une clé API';
