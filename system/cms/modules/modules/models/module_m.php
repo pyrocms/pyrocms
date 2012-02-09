22086398ecf997016120cbde8f23391d06d253b3
Phil Sturgeon <email@philsturgeon.co.uk>
2012-02-09 14:48:47 -0800

    Another glob issue with installer.

--- system/cms/modules/modules/models/module_m.php
+++ system/cms/modules/modules/models/module_m.php
@@ -22,7 +22,7 @@ class Module_m extends MY_Model
 	{
 		parent::__construct();
 
-		$this->load->helper(array('date', 'modules/module'));
+		$this->load->helper('modules/module');
 	}
 
 	/**
@@ -474,9 +474,12 @@ class Module_m extends MY_Model
 		foreach (array(APPPATH, ADDONPATH, SHARED_ADDONPATH) as $directory)
     	{
 			// some servers return false instead of an empty array
-			if ( ! $directory) continue;
-
-			foreach (glob($directory.'modules/*', GLOB_ONLYDIR) as $path)
+			if ( ! $directory or ! ($temp_modules = glob($directory.'modules/*', GLOB_ONLYDIR)))
+			{
+				continue;
+			}
+			
+			foreach ($temp_modules as $path)
 			{
 				$slug = basename($path);
 
@@ -531,6 +534,7 @@ class Module_m extends MY_Model
 				// Looks like it installed ok, add a record
 				$this->add($input);
 			}
+			unset($temp_modules);
 
 			// Going back around, 2nd time is addons
 			$is_core = FALSE;
