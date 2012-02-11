076c5529d67d4b087ef83eb57fd0b4d1c04b4ed3
Adam Fairholm <adam.fairholm@gmail.com>
2012-02-10 23:12:34 -0500

    Fixing JS errors on the blog create function. Cause: the JS minifier does not enjoy already minified JS in certain cases. Changes tagsinput.js to full version instead of min.

--- system/cms/modules/blog/controllers/admin.php
+++ system/cms/modules/blog/controllers/admin.php
@@ -222,7 +222,7 @@ class Admin extends Admin_Controller
 		$this->template
 			->title($this->module_details['name'], lang('blog_create_title'))
 			->append_metadata($this->load->view('fragments/wysiwyg', $this->data, TRUE))
-			->append_js('jquery/jquery.tagsinput.min.js')
+			->append_js('jquery/jquery.tagsinput.js')
 			->append_js('module::blog_form.js')
 			->append_css('jquery/jquery.tagsinput.css')
 			->set('post', $post)
