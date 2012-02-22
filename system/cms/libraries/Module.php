91cba59c94d039e0ac6a78a462d3e86479aa2158
George Petsagourakis <petsagouris@gmail.com>
2012-02-22 09:14:08 +0200

    Added Module::install_tables() so that every table installation logic can be handled more centrally.
    Signed-off-by: George Petsagourakis <petsagouris@gmail.com>

--- system/cms/libraries/Module.php
+++ system/cms/libraries/Module.php
@@ -123,6 +123,108 @@ abstract class Module {
 		isset($ci) OR $ci =& get_instance();
 		return $ci->{$var};
 	}
+	
+	/**
+	 * Installs the modules tables in the database.
+	 * 
+	 * Can handle primary keys and FULLTEXT indexes.
+	 *
+	 * @param array $tables The database tables definitions.
+	 */
+	public function install_tables($tables)
+	{
+
+		foreach ($tables as $table_name => $fields)
+		{
+			// First we go ahead and add all the fields.
+			$this->dbforge->add_field($fields);
+
+
+			// Then go ahead and check for our special cases such as, primary
+			// keys, fulltext indices etc.
+			$primary = array();
+			$fulltext = array();
+
+			//So for all the fields of this table:
+			foreach ($fields as $field => $field_data)
+			{
+				// Check every property of the field definition
+				foreach ($field_data as $key => $value)
+				{
+					// Primary keys
+					if ($key === 'primary')
+					{
+						// Add it for later
+						$primary[] = $field;
+					}
+
+					if ($key === 'fulltext')
+					{
+						// Check if we are actually doing multiple FULLTEXT keys
+						if (is_array($value))
+						{
+							foreach ($value as $fulltext_index_name)
+							{
+								// If we dont have a key for this fulltext
+								if (!array_key_exists($fulltext_index_name, $fulltext))
+								{
+									// Go ahead and create it.
+									$fulltext[$fulltext_index_name] = array();
+								}
+								// Register the field for fulltext keying later on.
+								$fulltext[$fulltext_index_name][] = $field;
+							}
+						}
+						// Or this is just a single fulltext key name.
+						elseif (is_string($value))
+						{
+							// If we dont have a key for this fulltext
+							if (!array_key_exists($value, $fulltext))
+							{
+								// Go ahead and create it.
+								$fulltext[$value] = array();
+							}
+
+							// Register the field for fulltext keying later on.
+							$fulltext[$value][] = $field;
+						}
+					}
+				}
+			}
+			// Add primary keys now
+			if (count($primary) > 0)
+			{
+				if (count($primary) > 1)
+				{
+					// Add them all
+					$this->dbforge->add_key($primary, TRUE);
+				}
+				else
+				{
+					// Just add a single one
+					$this->dbforge->add_key($primary[0], TRUE);
+				}
+			}
+
+			// Then we create the table (if not exists).
+			if ($this->dbforge->create_table($table_name, true))
+			{
+				// Add any fulltext indices now
+				if (count($fulltext) > 0)
+				{
+					// FULLTEXT is only available on MyISAM.
+					$this->db->query('ALTER TABLE '.$this->db->dbprefix($table_name).' ENGINE = MyISAM');
+
+					// Add all the fulltext indices we have collected
+					// @todo there is no checking whether the index exists already.
+					foreach ($fulltext as $index_name => $field_names)
+					{
+						$this->db->query('CREATE FULLTEXT INDEX '.$index_name.' ON '.$this->db->dbprefix($table_name).'('.implode(', ', $field_names).')');
+					}
+				}
+			}
+		}
+	}
 }
 
 /* End of file Module.php */
