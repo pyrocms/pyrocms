fbe874e7368dbd77f1c40a98a8ea99becb9e989f
jerel <mail@unruhdesigns.com>
2012-02-24 11:51:45 -0600

    Turned off xss filtering for Email Templates so that inline styles can be used

--- system/cms/modules/templates/details.php
+++ system/cms/modules/templates/details.php
@@ -11,7 +11,7 @@
  */
 class Module_Templates extends Module {
 
-	public $version = '0.1';
+	public $version = '1.0';
 
 	public function info()
 	{
@@ -52,6 +52,7 @@ class Module_Templates extends Module {
 			'backend' => TRUE,
 			'menu' => 'design',
 			'author' => 'Stephen Cozart',
+			'skip_xss' => TRUE,
 			
 			'shortcuts' => array(
 				array(
