ce6fc9afc05d4ccfa601183e82c2f1fc02affb28
ekserver <lennart.stein24@googlemail.com>
2012-02-28 18:09:30 +0100

    Update german language part 2

--- system/cms/modules/users/language/german/ion_auth_lang.php
+++ system/cms/modules/users/language/german/ion_auth_lang.php
@@ -5,7 +5,7 @@
 * Author:       Ben Edmunds
 *                 ben.edmunds@gmail.com
 *               @benedmunds
-* Translation:  Bernd Hückstädt
+* Translation:  Bernd H&uuml;ckst&auml;dt
 *                 akademie@joytopia.net
 *
 *
@@ -14,28 +14,28 @@
 * Created:  04.02.2010
 *
 * Description:      German language file for Ion Auth messages and errors
-* Beschreibung:     Deutsche Sprach-Datei für Ion Auth System- und Fehlermeldungen
+* Beschreibung:     Deutsche Sprach-Datei f&uuml;r Ion Auth System- und Fehlermeldungen
 *
 */
 
 // Account Creation
 $lang['account_creation_successful']            = 'Das Benutzerkonto wurde erfolgreich erstellt';
 $lang['account_creation_unsuccessful']          = 'Das Benutzerkonto konnte nicht erstellt werden';
-$lang['account_creation_duplicate_email']       = 'Die E-Mail-Adresse ist ungültig oder bereits in Gebrauch';
-$lang['account_creation_duplicate_username']    = 'Der Benutzername ist ungültig oder bereits in Gebrauch';
+$lang['account_creation_duplicate_email']       = 'Die E-Mail-Adresse ist ung&uuml;ltig oder bereits in Gebrauch';
+$lang['account_creation_duplicate_username']    = 'Der Benutzername ist ung&uuml;ltig oder bereits in Gebrauch';
 
 
 // Password
-$lang['password_change_successful']             = 'Das Passwort wurde erfolgreich geändert';
-$lang['password_change_unsuccessful']           = 'Das Passwort konnte nicht geändert werden';
-$lang['forgot_password_successful']             = 'Es wurde eine E-Mail zum Zurücksetzen des Passwortes abgesandt';
-$lang['forgot_password_unsuccessful']           = 'Das Passwort konnte nicht zurückgesetzt werden';
+$lang['password_change_successful']             = 'Das Passwort wurde erfolgreich ge&auml;ndert';
+$lang['password_change_unsuccessful']           = 'Das Passwort konnte nicht ge&auml;ndert werden';
+$lang['forgot_password_successful']             = 'Es wurde eine E-Mail zum Zur&uuml;cksetzen des Passwortes abgesandt';
+$lang['forgot_password_unsuccessful']           = 'Das Passwort konnte nicht zur&uuml;ckgesetzt werden';
 
 // Activation
 $lang['activate_successful']                    = 'Das Benutzerkonto wurde aktiviert';
 $lang['activate_unsuccessful']                  = 'Das Benutzerkonto konnte nicht aktiviert werden';
-$lang['deactivate_successful']                  = 'Das Benutzerkonto wurde de-aktiviert';
-$lang['deactivate_unsuccessful']                = 'Das Benutzerkonto konnte nicht de-aktiviert werden';
+$lang['deactivate_successful']                  = 'Das Benutzerkonto wurde deaktiviert';
+$lang['deactivate_unsuccessful']                = 'Das Benutzerkonto konnte nicht deaktiviert werden';
 $lang['activation_email_successful']            = 'Es wurde eine E-Mail zum Aktivieren des Benutzerkontos abgesandt';
 $lang['activation_email_unsuccessful']          = 'Die Aktivierungs-E-Mail konnte nicht abgesandt werden';
 
@@ -45,7 +45,7 @@ $lang['login_unsuccessful']                     = 'Login fehlerhaft';
 $lang['logout_successful']                      = 'Logout erfolgreich';
 
 // Account Changes
-$lang['update_successful']                      = 'Die Konto-Informationen wurden erfolgreich geändert';
-$lang['update_unsuccessful']                    = 'Die Konto-Informationen konnten nicht geändert werden';
-$lang['delete_successful']                      = 'Das Benutzerkonto wurde gelöscht';
-$lang['delete_unsuccessful']                    = 'Das Benutzerkonto konnte nicht gelöscht werden';
+$lang['update_successful']                      = 'Die Konto-Informationen wurden erfolgreich ge&auml;ndert';
+$lang['update_unsuccessful']                    = 'Die Konto-Informationen konnten nicht ge&auml;ndert werden';
+$lang['delete_successful']                      = 'Das Benutzerkonto wurde gel&ouml;scht';
+$lang['delete_unsuccessful']                    = 'Das Benutzerkonto konnte nicht gel&ouml;scht werden';
