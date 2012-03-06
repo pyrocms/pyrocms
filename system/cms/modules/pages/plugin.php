c40195400d7d5eb530b1d05b350251efa769185c
jerel <mail@unruhdesigns.com>
2012-03-06 02:15:45 -0600

    {{ pages:children }} now handles page chunks properly. Closes issue #1162

--- system/cms/modules/pages/plugin.php
+++ system/cms/modules/pages/plugin.php
@@ -96,15 +96,33 @@ class Plugin_Pages extends Plugin
 	 */
 	public function children()
 	{
-		$limit = $this->attribute('limit');
+		$limit = $this->attribute('limit', 10);
 		
-		return $this->db->select('pages.*, page_chunks.body')
+		$pages = $this->db->select('pages.*')
 			->where('pages.parent_id', $this->attribute('id'))
 			->where('status', 'live')
-			->join('page_chunks', 'pages.id = page_chunks.page_id', 'LEFT')
 			->limit($limit)
 			->get('pages')
 			->result_array();
+
+		if ($pages)
+		{
+			foreach ($pages AS &$page)
+			{
+				// Grab all the chunks that make up the body for this page
+				$page['chunks'] = $this->db->get_where('page_chunks', array('page_id' => $page['id']))->result();
+				
+				$page['body'] = '';
+				foreach ($page['chunks'] as $chunk)
+				{
+					$page['body'] .= 	'<div class="page-chunk ' . $chunk->slug . '">' .
+											(($chunk->type == 'markdown') ? $chunk->parsed : $chunk->body) .
+										'</div>'.PHP_EOL;
+				}
+			}
+		}
+
+		return $pages;
 	}
 
 	// --------------------------------------------------------------------------
