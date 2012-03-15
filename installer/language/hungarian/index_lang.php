79dd8fac54840bdde61def0d1391d00befec4cdb
emkiel <sc0p.78@gmail.com>
2012-03-15 10:32:16 +0100

    Hungarian Translations - PyroCMS's Installer
    Signed-off-by: emkiel <sc0p.78@gmail.com>

---
+++ installer/language/hungarian/index_lang.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['header']                  = 'Üdvözöllek';
+$lang['thankyou']                = 'Köszönjük, hogy a PyroCMS-t választottad!';
+$lang['text']                    = 'A PyroCMS telepítése nagyon egyszerű, csak kövesd lépésről-lépésre a képernyőn látott utasításokat. Ha bármilyen probléma lépne fel a telepítés során, akkor se aggódj! A telepítő elmagyarázza a teendőket.';
+$lang['step1']                   = 'Első lépés';
+$lang['link']                    = 'Tedd meg az első lépést!';
+
+/* End of file index_lang.php */
