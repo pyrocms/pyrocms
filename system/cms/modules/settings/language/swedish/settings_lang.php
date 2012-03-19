0db4d260fe42ca90ce7b9aa235333bf91359adac
marju212 <info@incore.se>
2012-03-19 07:05:11 +0100

    More swedish words puh

---
+++ system/cms/modules/settings/language/swedish/settings_lang.php
@@ -0,0 +1,39 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+
+/**
+ * Swedish translation.
+ *
+ * @author		marcus@incore.se
+ * @package		PyroCMS  
+ * @link		http://pyrocms.com
+ * @date		2012-03-13
+ * @version		1.0.0
+ */
+
+
+$lang['settings_site_name']		= 'Webbplatsens namn';
+$lang['settings_site_name_desc']		= 'Webbplatsens namn för sidtitlar och annat.';
+$lang['settings_site_slogan']		= 'Webbplatsens slogan';
+$lang['settings_site_slogan_desc']		= 'Webbplatsens slogan för sidtitlar och annat.';
+$lang['settings_site_lang']		= 'Webbplatsspråk';
+$lang['settings_site_lang_desc']		= 'Grundspråk på webbplatsen, används för att välja mallar för e-postmeddelanden, kontakt formulär, och andra funktioner som inte bör vara beroende av vilket språk en användare valt.';
+$lang['settings_contact_email']		= 'Kontakt e-post';
+$lang['settings_contact_email_desc']		= 'All e-post från användare, gäster och webbplatsenn kommer att gå till denna e-postadress.';
+$lang['settings_server_email']		= 'Server e-post';
+$lang['settings_server_email_desc']		= 'Alla e-post till användarna kommer från den här e-postadressen.';
+$lang['settings_meta_topic']		= 'Metadata, ämne';
+$lang['settings_meta_topic_desc']		= 'Två eller tre ord som beskriver denna typ av företag / webbplats.';
+$lang['settings_currency']		= 'Valuta';
+$lang['settings_currency_desc']		= 'Valutasymbol';
+$lang['settings_dashboard_rss']		= 'Kontrollpanel RSS källa';
+$lang['settings_dashboard_rss_desc']		= 'Länk till RSS källa som ska vara synlig i kontrollpanelen';
+$lang['settings_dashboard_rss_count']		= 'Antal RSS meddelanden';
+$lang['settings_dashboard_rss_count_desc']		= 'Antal RSS meddelanden som ska vara synliga i kontrollpanelen';
+$lang['settings_date_format']		= 'Datumformat';
+$lang['settings_frontend_enabled']		= 'Webbplatsstatus';
+
+
+
+/* End of file settings_lang.php */  
+/* Location: ./system/cms/modules/settings/language/swedish */ 
