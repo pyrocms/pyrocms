a3bc112f021cf98c1e8237a17856ee51f157f39d
marju212 <info@incore.se>
2012-03-19 20:10:16 +0100

    Modify lang-file

--- system/cms/modules/widgets/language/swedish/widgets_lang.php
+++ system/cms/modules/widgets/language/swedish/widgets_lang.php
@@ -1,37 +1,41 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-// sidebar
-$lang['widgets.available_title']        = 'Tillgängliga widgetar';
-$lang['widgets.widget_area_wrapper']    = 'Tillgängliga fält';
-$lang['widgets.instructions']           = 'Dra & släpp för att installera';
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 
-$lang['widgets.instances']            	= 'Instanser';
-$lang['widgets.areas']            		= 'Fält';
+/**
+ * Swedish translation.
+ *
+ * @author		marcus@incore.se
+ * @package		PyroCMS  
+ * @link		http://pyrocms.com
+ * @date		2012-03-13
+ * @version		1.0.0
+ */
 
-// Widgets
-$lang['widgets.widget']                 = 'Widget';
-$lang['widgets.widget_author']          = 'Av';
-$lang['widgets.widget_short_name']      = 'Kortnamn';
-$lang['widgets.widget_version']         = 'Version';
-$lang['widgets.status_label']			= 'Status';
 
-$lang['widgets.inactive_title'] 		= 'Widget inaktiverad';
-$lang['widgets.active_title'] 			= 'Widget aktiverad';
+$lang['widgets.available_title']		= 'Tillgängliga widgetar';
+$lang['widgets.widget_area_wrapper']		= 'Tillgängliga fält';
+$lang['widgets.instructions']		= 'Dra & släpp för att installera';
+$lang['widgets.instances']		= 'Instanser';
+$lang['widgets.areas']		= 'Fält';
+$lang['widgets.widget']		= 'Widget';
+$lang['widgets.widget_author']		= 'Av';
+$lang['widgets.widget_short_name']		= 'Kortnamn';
+$lang['widgets.widget_version']		= 'Version';
+$lang['widgets.status_label']		= 'Status';
+$lang['widgets.inactive_title']		= 'Widget inaktiverad';
+$lang['widgets.active_title']		= 'Widget aktiverad';
+$lang['widgets.add_area']		= 'Lägg till fält';
+$lang['widgets.delete_area']		= 'Radera fält';
+$lang['widgets.edit_area']		= 'Redigera fält';
+$lang['widgets.widget_area']		= 'Fält';
+$lang['widgets.widget_area_title']		= 'Fältnamn';
+$lang['widgets.widget_area_slug']		= 'Fält kortnamn';
+$lang['widgets.view_code']		= 'Visa kod';
+$lang['widgets.instance_title']		= 'Titel';
+$lang['widgets.tag_title']		= 'Tagg';
+$lang['widgets.no_available_widgets']		= 'Det finns inga widgetar tillgängliga.';
 
-// Widget area titles
-$lang['widgets.add_area']               = 'Lägg till fält';
-$lang['widgets.delete_area']            = 'Radera fält';
-$lang['widgets.edit_area']              = 'Redigera fält';
 
-// Widget area field names
-$lang['widgets.widget_area']            = 'Fält';
-$lang['widgets.widget_area_title']      = 'Fältnamn';
-$lang['widgets.widget_area_slug']       = 'Fält kortnamn';
 
-$lang['widgets.view_code']				= 'Visa kodning'; #translate
-
-$lang['widgets.instance_title']         = 'Titel';
-$lang['widgets.tag_title']              = 'Tagg';
-
-$lang['widgets.no_available_widgets']	= 'Det finns inga widgetar tillgängliga.';
+/* End of file widgets_lang.php */  
+/* Location: ./system/cms/modules/widgets/language/swedish */ 
