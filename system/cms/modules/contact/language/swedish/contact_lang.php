c5e1ad3f41a4d97ae73bdcf80b81b837fed3f43d
marju212 <info@incore.se>
2012-03-19 15:19:51 +0100

    More language files...

---
+++ system/cms/modules/contact/language/swedish/contact_lang.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+
+/**
+ * Swedish translation.
+ *
+ * @author		marcus@incore.se
+ * @package		PyroCMS  
+ * @link		http://pyrocms.com
+ * @date		2012-03-13
+ * @version		1.0.0
+ */
+
+
+$lang['contact_submit_error']		= 'Ursäkta. Vi drog slutsatsen att du kunde vara en bot. Vi ber om ursäkt om vi har fel, vänligen kontakta oss på något annat sätt.';
+$lang['contact_sent_text']		= 'Ditt meddelande har skickats. Vi återkommer till dig så snart vi kan.';
+$lang['contact_error_message']		= 'Det uppstod ett problem när meddelandet skulle skickas. Försök igen senare ...';
+
+
+
+/* End of file contact_lang.php */  
+/* Location: ./system/cms/modules/contact/language/swedish */ 
