9b3f129f2efa9abbc60310e2892ee14dc1c835a0
marju212 <info@incore.se>
2012-03-19 16:25:19 +0100

    Last swedish words...

---
+++ installer/language/swedish/step_1_lang.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+
+/**
+ * Swedish translation.
+ *
+ * @author		marcus@incore.se
+ * @package		PyroCMS  
+ * @link		http://pyrocms.com
+ * @date		2012-03-13
+ * @version		1.0.0
+ */
+
+
+$lang['header']		= 'Steg 1: Konfigurera Databas';
+$lang['db_settings']		= 'Inställningar';
+$lang['db_text']		= 'PyroCMS kräver en databas (MySQL) för att lagra sidor och inställningar, det första vi gör är att se till att databaskopplingen fungerar. Ifall du inte förstå vad vi frågar efter så kontakta ditt webbhotell eller serveradministratör.';
+$lang['server']		= 'MySQL Värdnamn';
+$lang['username']		= 'MySQL Användarnamn';
+$lang['password']		= 'MySQL Lösenord';
+$lang['portnr']		= 'MySQL Port';
+$lang['server_settings']		= 'Serverinställningar';
+$lang['httpserver']		= 'HTTP Server';
+$lang['httpserver_text']		= 'PyroCMS requires a HTTP Server to display dynamic content when a user goes to your website. It looks like you already have one by the fact that you can see this page, but if we know exactly which type then PyroCMS can configure itself even better. If you do not know what any of this means just ignore it and carry on with the installation.';
+$lang['rewrite_fail']		= 'PyroCMS kräver en HTTP-server för att visa dynamiskt innehåll när en användare går till din webbplats. Det ser ut som du redan har en sådan av det faktum att du kan se denna sida, men om vi vet exakt vilken typ så kan PyroCMS anpassas. Om du inte vet vad allt detta betyder bara ignorera det och fortsätt med installationen.';
+$lang['mod_rewrite']		= 'Du har valt "(Apache med mod_rewrite)", men din server inte har rewrite-modulen aktiverad. Aktivera denna modul eller installera PyroCMS med hjälp av "Apache (utan mod_rewrite)" alternativet.';
+$lang['step2']		= 'Steg 2';
+$lang['db_success']		= 'Databaskopplingen fungerar';
+$lang['db_failure']		= 'Kan inte kontakta databasen:';
+
+
+
+/* End of file step_1_lang.php */  
+/* Location: ./installer/language/swedish */ 
