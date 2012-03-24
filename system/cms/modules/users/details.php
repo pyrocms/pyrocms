8b4bb6bfa3bb1025e96f6f1af2495f8450770f43
Adam Fairholm <adam.fairholm@gmail.com>
2012-03-24 16:31:36 -0400

    Check for group_has_role function so the installation works. Closes #1237.

--- system/cms/modules/users/details.php
+++ system/cms/modules/users/details.php
@@ -63,32 +63,35 @@ class Module_Users extends Module {
 			'roles'		=> array('admin_profile_fields')
 			);
 
-		if(group_has_role('users', 'admin_profile_fields'))
+		if (function_exists('group_has_role'))
 		{
-			$info['sections'] = array(
-				'users' => array(
-						'name' 	=> 'user_list_title',
-						'uri' 	=> 'admin/users',
-							'shortcuts' => array(
-								'create' => array(
-									'name' 	=> 'user_add_title',
-									'uri' 	=> 'admin/users/create',
-									'class' => 'add'
+			if(group_has_role('users', 'admin_profile_fields'))
+			{
+				$info['sections'] = array(
+					'users' => array(
+							'name' 	=> 'user_list_title',
+							'uri' 	=> 'admin/users',
+								'shortcuts' => array(
+									'create' => array(
+										'name' 	=> 'user_add_title',
+										'uri' 	=> 'admin/users/create',
+										'class' => 'add'
+										)
+									)
+								),
+					'fields' => array(
+							'name' 	=> 'user_profile_fields_label',
+							'uri' 	=> 'admin/users/fields',
+								'shortcuts' => array(
+									'create' => array(
+										'name' 	=> 'user_add_field',
+										'uri' 	=> 'admin/users/fields/create',
+										'class' => 'add'
+										)
 									)
 								)
-							),
-				'fields' => array(
-						'name' 	=> 'user_profile_fields_label',
-						'uri' 	=> 'admin/users/fields',
-							'shortcuts' => array(
-								'create' => array(
-									'name' 	=> 'user_add_field',
-									'uri' 	=> 'admin/users/fields/create',
-									'class' => 'add'
-									)
-								)
-							)
-					);
+						);
+			}
 		}
 
 		return $info;
