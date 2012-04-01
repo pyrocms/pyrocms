d225387f8f66d9d838ef7a30524704262e970d85
Christian <chri788@gmail.com>
2012-04-01 21:48:47 +0300

    Update system/cms/modules/blog/language/italian/blog_lang.php

--- system/cms/modules/blog/language/italian/blog_lang.php
+++ system/cms/modules/blog/language/italian/blog_lang.php
@@ -5,9 +5,9 @@
 // labels
 $lang['blog_posted_label'] 			= 'Pubblicato';
 $lang['blog_posted_label_alt']			= 'Pubblicato alle';
-$lang['blog_written_by_label']				= 'Written by'; #translate
-$lang['blog_author_unknown']				= 'Unknown'; #translate
-$lang['blog_keywords_label']				= 'Keywords'; #translate
+$lang['blog_written_by_label']				= 'Scritto da'; #translate
+$lang['blog_author_unknown']				= 'Sconosciuto'; #translate
+$lang['blog_keywords_label']				= 'Parole chiave'; #translate
 $lang['blog_tagged_label']					= 'Tagged'; #translate
 $lang['blog_category_label'] 			= 'Categoria';
 $lang['blog_post_label'] 			= 'Articolo';
@@ -31,9 +31,9 @@ $lang['blog_rss_name_suffix'] 			= ' Notizie';
 $lang['blog_rss_category_suffix'] 		= ' Notizie';
 $lang['blog_author_name_label'] 		= 'Nome autore';
 $lang['blog_read_more_label'] 			= 'Leggi tutto&nbsp;&raquo;';
-$lang['blog_created_hour']                  = 'Time (Hour)'; #translate
-$lang['blog_created_minute']                = 'Time (Minute)'; #translate
-$lang['blog_comments_enabled_label']         = 'Comments Enabled'; #translate
+$lang['blog_created_hour']                  = 'Time (Ore)'; #translate
+$lang['blog_created_minute']                = 'Time (Minuti)'; #translate
+$lang['blog_comments_enabled_label']         = 'Commenti abilitati';
 
 // titles
 $lang['blog_create_title'] 			= 'Aggiungi un articolo';
