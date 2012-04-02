pyrocms/pyrocms

commit cd72cfe0df25ed602d2b10cefc0195e5de05a2b4

Minor global improvements.
- Removed all the instances of `$this->data` usage.
- Page_m::get($id) now returns also the chunks, use Page_m::get($id, false) to receive only the page info. Furthermore this function now returns an array.
- Fixed documentation and code standards on many files.

Signed-off-by: George Petsagourakis <petsagouris@gmail.com>

system/cms/modules/pages/controllers/admin_layouts.php
@@ -1,22 +1,24 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 /**
- * @author 		Phil Sturgeon
- * @author 		Yorick Peterse
+ * Admin controller for the Page Layouts of the Pages module.
+ *
+ * @author		 Phil Sturgeon
+ * @author		 Yorick Peterse
  * @author		PyroCMS Dev Team
- * @package 	PyroCMS\Core\Modules\Pages\Controllers
+ * @package	 PyroCMS\Core\Modules\Pages\Controllers
  */
 class Admin_layouts extends Admin_Controller
 {
 	/**
 	 * The current active section
-	 * @access protected
+	 *
 	 * @var string
 	 */
 	protected $section = 'layouts';
-	
+
 	/**
 	 * Validation rules used by the form_validation library
-	 * @access private
+	 *
 	 * @var array
 	 */
 	private $validation_rules = array(
@@ -49,8 +51,6 @@ class Admin_layouts extends Admin_Controller
 
 	/**
 	 * Constructor method
-	 * @access public
-	 * @return void
 	 */
 	public function __construct()
 	{
@@ -71,37 +71,33 @@ class Admin_layouts extends Admin_Controller
 
 	/**
 	 * Index methods, lists all page layouts
-	 * @access public
-	 * @return void
 	 */
 	public function index()
 	{
 		// Get all page layouts
-		$this->data->page_layouts = $this->page_layouts_m->get_all();
+		$this->template->page_layouts = $this->page_layouts_m->get_all();
 
 		// Render the view
 		$this->template
 			->title($this->module_details['name'], lang('pages.layout_id_label'))
-			->build('admin/layouts/index', $this->data);
+			->build('admin/layouts/index');
 	}
 
 	/**
 	 * Create method, creates a new page layout
-	 * @access public
-	 * @return void
 	 */
 	public function create()
 	{
 		// Got validation?
 		if ($this->form_validation->run())
-	    {
+		{
 			// Insert the page
-	    	$id = $this->page_layouts_m->insert(array(
-				'title' 	=> $this->input->post('title'),
-				'theme_layout' 	=> $this->input->post('theme_layout'),
-				'body' 		=> $this->input->post('body', FALSE),
-				'css' 		=> $this->input->post('css'),
-				'js' 		=> $this->input->post('js')
+			$id = $this->page_layouts_m->insert(array(
+				'title' => $this->input->post('title'),
+				'theme_layout' => $this->input->post('theme_layout'),
+				'body' => $this->input->post('body', FALSE),
+				'css' => $this->input->post('css'),
+				'js' => $this->input->post('js')
 			));
 
 			// Success or fail?
@@ -110,74 +106,74 @@ class Admin_layouts extends Admin_Controller
 				: $this->session->set_flashdata('notice', lang('page_layouts.create_error'));
 
 			redirect('admin/pages/layouts');
-	    }
+		}
 
 		// Loop through each validation rule
-		foreach($this->validation_rules as $rule)
+		foreach ($this->validation_rules as $rule)
 		{
 			$page_layout->{$rule['field']} = set_value($rule['field']);
 		}
 
 		$theme_layouts = $this->template->get_theme_layouts($this->settings->default_theme);
-		foreach($theme_layouts as $theme_layout)
+		foreach ($theme_layouts as $theme_layout)
 		{
-			$this->data->theme_layouts[$theme_layout] = basename($theme_layout, '.html');
+			$this->template->theme_layouts[$theme_layout] = basename($theme_layout, '.html');
 		}
 
-	    // Assign data for display
-	    $this->load->vars(array(
+		// Assign data for display
+		$this->load->vars(array(
 			'page_layout' => &$page_layout
 		));
 
-	    // Load WYSIWYG editor
+		// Load WYSIWYG editor
 		$this->template
 			->title($this->module_details['name'], lang('pages.layout_id_label'), lang('page_layouts.create_title'))
-			->build('admin/layouts/form', $this->data);
+			->build('admin/layouts/form');
 	}
 
 	/**
 	 * Edit method, edits an existing page layout
-	 * @access public
-	 * @return void
+	 *
+	 * @param int $id The id of the page layout.
 	 */
 	public function edit($id = 0)
 	{
 		empty($id) AND redirect('admin/pages/layouts');
 
-	    // We use this controller property for a validation callback later on
-	    $this->page_layout_id = $id;
+		// We use this controller property for a validation callback later on
+		$this->page_layout_id = $id;
 
-	    // Set data, if it exists
-	    if (!$page_layout = $this->page_layouts_m->get($id))
-	    {
+		// Set data, if it exists
+		if ( ! $page_layout = $this->page_layouts_m->get($id))
+		{
 			$this->session->set_flashdata('error', lang('page_layouts.page_not_found_error'));
 			redirect('admin/pages/layouts/create');
-	    }
+		}
 
-	    // Give validation a try, who knows, it just might work!
+		// Give validation a try, who knows, it just might work!
 		if ($this->form_validation->run())
-	    {
+		{
 			// Run the update code with the POST data
 			$this->page_layouts_m->update($id, array(
-				'title' 	=> $this->input->post('title'),
-				'theme_layout' 	=> $this->input->post('theme_layout'),
-				'body' 		=> $this->input->post('body', FALSE),
-				'css' 		=> $this->input->post('css'),
-				'js'		=> $this->input->post('js')
+				'title' => $this->input->post('title'),
+				'theme_layout' => $this->input->post('theme_layout'),
+				'body' => $this->input->post('body', FALSE),
+				'css' => $this->input->post('css'),
+				'js' => $this->input->post('js')
 			));
 
 			// Wipe cache for this model as the data has changed
 			$this->pyrocache->delete_all('page_layouts_m');
 
 			$this->session->set_flashdata('success', sprintf(lang('page_layouts.edit_success'), $this->input->post('title')));
-			
+
 			redirect('admin/pages/layouts');
-	    }
+		}
 
 		// Loop through each validation rule
-		foreach($this->validation_rules as $rule)
+		foreach ($this->validation_rules as $rule)
 		{
-			if($this->input->post($rule['field']))
+			if ($this->input->post($rule['field']))
 			{
 				$page_layout->{$rule['field']} = set_value($rule['field']);
 			}
@@ -185,7 +181,7 @@ class Admin_layouts extends Admin_Controller
 
 		$theme_layouts = $this->template->get_theme_layouts($this->settings->default_theme);
 		$theme_layouts_options = array();
-		foreach($theme_layouts as $theme_layout)
+		foreach ($theme_layouts as $theme_layout)
 		{
 			$theme_layouts_options[$theme_layout] = basename($theme_layout, '.html');
 		}
@@ -194,17 +190,17 @@ class Admin_layouts extends Admin_Controller
 			->title($this->module_details['name'], lang('pages.layout_id_label'), sprintf(lang('page_layouts.edit_title'), $page_layout->title))
 			->set('theme_layouts', $theme_layouts_options)
 			->set('page_layout', $page_layout)
-			->build('admin/layouts/form', $this->data);
+			->build('admin/layouts/form');
 	}
 
 	/**
-	 * Delete method, deletes an existing page layout
-	 * @access public
-	 * @return void
+	 * Delete a page layout
+	 *
+	 * @param int $id The id of the page layout to delete.
 	 */
 	public function delete($id = 0)
 	{
-		// Attention! Error of no selection not handeled yet.
+		// @todo: Error of no selection not handeled yet.
 		$ids = ($id) ? array($id) : $this->input->post('action_to');
 
 		// Go through the array of slugs to delete

system/cms/controllers/admin.php
@@ -1,51 +1,43 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * The admin class is basically the main controller for the backend.
- * 
- * @author 		PyroCMS Development Team
- * @package 	PyroCMS\Core\Controllers
+ *
+ * @author PyroCMS Development Team
+ * @package	 PyroCMS\Core\Controllers
  */
 class Admin extends Admin_Controller
 {
 	/**
 	 * Constructor method
-	 *
-	 * @access public
-	 * @return void
 	 */
 	public function __construct()
 	{
-  		parent::__construct();
+		parent::__construct();
 
 		$this->load->helper('users/user');
- 	}
+	}
 
- 	/**
- 	 * Show the control panel
-	 *
-	 * @access public
-	 * @return void
- 	 */
- 	public function index()
+	/**
+	 * Show the control panel
+	 */
+	public function index()
 	{
-		$data = array();
-		
-		if (is_dir('./installer'))
-		{
-			$data['messages']['notice'] = lang('cp_delete_installer_message');
-		}
-		
 		$this->template
 			->enable_parser(TRUE)
-			->title(lang('global:dashboard'))
-			->build('admin/dashboard', $data);
+			->title(lang('global:dashboard'));
+
+		if (is_dir('./installer'))
+		{
+			$this->template
+				->set('messages', array('notice' => lang('cp_delete_installer_message')));
+		}
+
+		$this->template
+			->build('admin/dashboard');
 	}
 
 	/**
 	 * Log in
-	 *
-	 * @access public
-	 * @return void
 	 */
 	public function login()
 	{
@@ -53,12 +45,12 @@ class Admin extends Admin_Controller
 		$this->validation_rules = array(
 			array(
 				'field' => 'email',
-				'label'	=> lang('email_label'),
+				'label' => lang('email_label'),
 				'rules' => 'required|callback__check_login'
 			),
 			array(
 				'field' => 'password',
-				'label'	=> lang('password_label'),
+				'label' => lang('password_label'),
 				'rules' => 'required'
 			)
 		);
@@ -66,7 +58,7 @@ class Admin extends Admin_Controller
 		// Call validation and set rules
 		$this->load->library('form_validation');
 		$this->form_validation->set_rules($this->validation_rules);
-	
+
 		// If the validation worked, or the user is already logged in
 		if ($this->form_validation->run() OR $this->ion_auth->logged_in())
 		{
@@ -80,9 +72,6 @@ class Admin extends Admin_Controller
 
 	/**
 	 * Logout
-	 *
-	 * @access public
-	 * @return void
 	 */
 	public function logout()
 	{
@@ -95,38 +84,34 @@ class Admin extends Admin_Controller
 	/**
 	 * Callback From: login()
 	 *
-	 * @access public
 	 * @param string $email The Email address to validate
+	 *
 	 * @return bool
 	 */
 	public function _check_login($email)
 	{
-		$remember = (bool) $this->input->post('remember');
-
-		if ($this->ion_auth->login($email, $this->input->post('password'), $remember))
+		if ($this->ion_auth->login($email, $this->input->post('password'), (bool)$this->input->post('remember')))
 		{
-			return TRUE;
+			return true;
 		}
 
 		$this->form_validation->set_message('_check_login', $this->ion_auth->errors());
-		return FALSE;
+		return false;
 	}
-	
+
 	/**
 	 * Display the help string from a module's
 	 * details.php file in a modal window
 	 *
-	 * @access	public
 	 * @param	string	$slug	The module to fetch help for
+	 *
 	 * @return	void
 	 */
-	
 	public function help($slug)
 	{
-		$this->data->help = $this->module_m->help($slug);
-
 		$this->template
 			->set_layout('modal', 'admin')
-			->build('admin/partials/help', $this->data);
+			->set('help', $this->module_m->help($slug))
+			->build('admin/partials/help');
 	}
 }

system/cms/core/MY_Model.php
@@ -1,7 +1,7 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * A base model to provide the basic CRUD actions for all models that inherit 
+ * A base model to provide the basic CRUD actions for all models that inherit
  * from it.
  *
  * @author Jamie Rumbelow <http://jamierumbelow.net>
@@ -59,7 +59,7 @@ class MY_Model extends CI_Model
 	protected $skip_validation = FALSE;
 
 	/**
-	 * Wrapper to __construct for when loading class is a superclass to a regular 
+	 * Wrapper to __construct for when loading class is a superclass to a regular
 	 * controller, i.e. - extends Base not extends Controller.
 	 *
 	 * @return void
@@ -83,13 +83,13 @@ class MY_Model extends CI_Model
 	}
 
 	/**
-	 * 
+	 *
 	 * @todo Provide short description.
 	 *
 	 * @param string $method
 	 * @param array $arguments
 	 * @return \MY_Model
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public function __call($method, $arguments)
 	{
@@ -114,7 +114,7 @@ class MY_Model extends CI_Model
 	 * Get table name
 	 *
 	 * @param boolean $prefix Whether the table name should be prefixed or not.
-	 * @return string 
+	 * @return string
 	 */
 	public function table_name($prefix = TRUE)
 	{
@@ -133,26 +133,26 @@ class MY_Model extends CI_Model
 	}
 
 	/**
-	 * Get a single record by creating a WHERE clause with a value for your 
+	 * Get a single record by creating a WHERE clause with a value for your
 	 * primary key.
 	 *
 	 * @author Phil Sturgeon
-	 * @param string $primary_value The value of your primary key
+	 * @param string $id The value of your primary key
 	 * @return object
 	 */
-	public function get($primary_value)
+	public function get($id)
 	{
-		return $this->db->where($this->primary_key, $primary_value)
+		return $this->db->where($this->primary_key, $id)
 						->get($this->_table)
 						->row();
 	}
 
 	/**
-	 * Get a single record by creating a WHERE clause with the key of $key and 
+	 * Get a single record by creating a WHERE clause with the key of $key and
 	 * the value of $val.
 	 *
 	 * @todo What are the ghost parameters this accepts?
-	 * 
+	 *
 	 * @author Phil Sturgeon
 	 * @return object
 	 */
@@ -167,12 +167,12 @@ class MY_Model extends CI_Model
 
 	/**
 	 * Get many result objects in an array.
-	 * 
+	 *
 	 * Similar to get(), but returns a result array of many result objects.
-	 * 
+	 *
 	 * @author Phil Sturgeon
 	 * @param string $primary_value The value of your primary key
-	 * @return array 
+	 * @return array
 	 */
 	public function get_many($primary_value)
 	{
@@ -182,12 +182,12 @@ class MY_Model extends CI_Model
 
 	/**
 	 * Similar to get_by(), but returns a result array of many result objects.
-	 * 
+	 *
 	 * The function accepts ghost parameters, fetched via func_get_args().
 	 * Those are:
 	 *  1. string `$key` The key to search by.
 	 *  2. string `$value` The value of that key.
-	 * 
+	 *
 	 * They are used in the query in the where statement something like:
 	 *   <code>[...] WHERE {$key}={$value} [...]</code>
 	 *
@@ -215,15 +215,15 @@ class MY_Model extends CI_Model
 
 	/**
 	 * Similar to get_by(), but returns a result array of many result objects.
-	 * 
+	 *
 	 * The function accepts ghost parameters, fetched via func_get_args().
 	 * Those are:
 	 *  1. string `$key` The key to search by.
 	 *  2. string `$value` The value of that key.
-	 * 
+	 *
 	 * They are used in the query in the where statement something like:
 	 *   <code>[...] WHERE {$key}={$value} [...]</code>
-	 * 
+	 *
 	 * @author Phil Sturgeon
 	 * @return array
 	 */
@@ -247,7 +247,7 @@ class MY_Model extends CI_Model
 	}
 
 	/**
-	 * Insert a new record into the database, calling the before and after 
+	 * Insert a new record into the database, calling the before and after
 	 * create callbacks.
 	 *
 	 * @author Jamie Rumbelow
@@ -259,7 +259,7 @@ class MY_Model extends CI_Model
 	public function insert($data, $skip_validation = FALSE)
 	{
 		$valid = TRUE;
-		
+
 		if ($skip_validation === FALSE)
 		{
 			$valid = $this->_run_validation($data);
@@ -275,14 +275,14 @@ class MY_Model extends CI_Model
 		$this->_run_after_create($data, $this->db->insert_id());
 
 		$this->skip_validation = FALSE;
-		
+
 		return $this->db->insert_id();
 	}
 
 	/**
 	 * Insert multiple rows at once.
-	 * 
-	 * Similar to insert(), just passing an array to insert multiple rows at 
+	 *
+	 * Similar to insert(), just passing an array to insert multiple rows at
 	 * once.
 	 *
 	 * @author Jamie Rumbelow
@@ -327,7 +327,7 @@ class MY_Model extends CI_Model
 	 * @param integer $primary_value The primary key basically the row's ID.
 	 * @param array $data The data to update.
 	 * @param boolean $skip_validation Whether we should skip the validation of the data.
-	 * @return boolean 
+	 * @return boolean
 	 */
 	public function update($primary_value, $data, $skip_validation = FALSE)
 	{
@@ -352,7 +352,7 @@ class MY_Model extends CI_Model
 
 	/**
 	 * Update a record, specified by $key and $val.
-	 * 
+	 *
 	 * The function accepts ghost parameters, fetched via func_get_args().
 	 * Those are:
 	 *  1. string `$key` The key to update with.
@@ -362,7 +362,7 @@ class MY_Model extends CI_Model
 	 *   <code>UPDATE {table} SET {$key}={$data} WHERE {$key}={$value}</code>
 	 *
 	 * @author Jamie Rumbelow
-	 * @return boolean 
+	 * @return boolean
 	 */
 	public function update_by()
 	{
@@ -388,7 +388,7 @@ class MY_Model extends CI_Model
 	 * @param array $primary_values The array of IDs
 	 * @param array $data The data to update
 	 * @param boolean $skip_validation Whether we should skip the validation of the data.
-	 * @return boolean 
+	 * @return boolean
 	 */
 	public function update_many($primary_values, $data, $skip_validation)
 	{
@@ -442,7 +442,7 @@ class MY_Model extends CI_Model
 	 * Delete a row from the database table by the key and value.
 	 *
 	 * @author Phil Sturgeon
-	 * @return bool 
+	 * @return bool
 	 */
 	public function delete_by()
 	{
@@ -457,7 +457,7 @@ class MY_Model extends CI_Model
 	 *
 	 * @author Phil Sturgeon
 	 * @param array $primary_values
-	 * @return bool 
+	 * @return bool
 	 */
 	public function delete_many($primary_values)
 	{
@@ -468,7 +468,7 @@ class MY_Model extends CI_Model
 	/**
 	 * Generate the dropdown options.
 	 *
-	 * @return array The options for the dropdown. 
+	 * @return array The options for the dropdown.
 	 */
 	function dropdown()
 	{
@@ -497,13 +497,13 @@ class MY_Model extends CI_Model
 	}
 
 	/**
-	 * Orders the result set by the criteria, using the same format as 
+	 * Orders the result set by the criteria, using the same format as
 	 * CodeIgniter's AR library.
-	 * 
+	 *
 	 * @author Jamie Rumbelow
 	 * @param string $criteria The criteria to order by
 	 * @param string $order the order direction
-	 * @return \MY_Model 
+	 * @return \MY_Model
 	 */
 	public function order_by($criteria, $order = 'ASC')
 	{
@@ -513,14 +513,14 @@ class MY_Model extends CI_Model
 
 	/**
 	 * Limits the result set.
-	 * 
+	 *
 	 * Pass an integer to set the actual result limit.
 	 * Pass a second integer set the offset.
 	 *
 	 * @author Jamie Rumbelow
 	 * @param int $limit The number of rows
 	 * @param int $offset The offset
-	 * @return \MY_Model 
+	 * @return \MY_Model
 	 */
 	public function limit($limit, $offset = 0)
 	{
@@ -533,7 +533,7 @@ class MY_Model extends CI_Model
 	 * Removes duplicate entries from the result set.
 	 *
 	 * @author Phil Sturgeon
-	 * @return \MY_Model 
+	 * @return \MY_Model
 	 */
 	public function distinct()
 	{
@@ -546,7 +546,7 @@ class MY_Model extends CI_Model
 	 *
 	 * @author Jamie Rumbelow
 	 * @param array $data The array of actions
-	 * @return mixed 
+	 * @return mixed
 	 */
 	private function _run_before_create($data)
 	{
@@ -563,7 +563,7 @@ class MY_Model extends CI_Model
 	 *
 	 * @author Jamie Rumbelow
 	 * @param array $data The array of actions
-	 * @param int $id 
+	 * @param int $id
 	 */
 	private function _run_after_create($data, $id)
 	{
@@ -578,7 +578,7 @@ class MY_Model extends CI_Model
 	 *
 	 * @author Dan Horrigan
 	 * @param array $data
-	 * @return boolean 
+	 * @return boolean
 	 */
 	private function _run_validation($data)
 	{
@@ -612,7 +612,7 @@ class MY_Model extends CI_Model
 
 	/**
 	 * Fetches the table from the pluralised model name.
-	 * 
+	 *
 	 * @author Jamie Rumbelow
 	 */
 	private function _fetch_table()
@@ -628,7 +628,7 @@ class MY_Model extends CI_Model
 	 * Sets where depending on the number of parameters
 	 *
 	 * @author Phil Sturgeon
-	 * @param array $params 
+	 * @param array $params
 	 */
 	private function _set_where($params)
 	{
@@ -646,7 +646,7 @@ class MY_Model extends CI_Model
 	 * Sets limit depending on the number of parameters
 	 *
 	 * @author Phil Sturgeon
-	 * @param array $params 
+	 * @param array $params
 	 */
 	private function _set_limit($params)
 	{

system/cms/libraries/Plugins.php
@@ -4,23 +4,20 @@
  *
  * @author		Phil Sturgeon
  * @author		PyroCMS Dev Team
- * @package 	PyroCMS\Core\Libraries
+ * @package	 PyroCMS\Core\Libraries
  */
 abstract class Plugin
 {
 	private $attributes = array();
 	private $content = array();
 
-	// ------------------------------------------------------------------------
-
 	/**
-	 * Set Data
+	 * Set Data for the plugin.
 	 *
-	 * Set Data for the plugin. Avoid doing this in constructor so we dont force logic on developers
+	 * Avoid doing this in constructor so we do not force logic on developers.
 	 *
-	 * @param	array - Content of the tags if any
-	 * @param	array - Attributes passed to the plugin
-	 * @return 	array
+	 * @param array $content Content of the tags if any
+	 * @param array $attributes Attributes passed to the plugin
 	 */
 	public function set_data($content, $attributes)
 	{
@@ -28,77 +25,67 @@ abstract class Plugin
 		$attributes AND $this->attributes = $attributes;
 	}
 
-	// ------------------------------------------------------------------------
-
+	/**
+	 * Make the Codeigniter object properies & methods accessible to this class.
+	 *
+	 * @param string $var The name of the method/property.
+	 *
+	 * @return mixed
+	 */
 	public function __get($var)
 	{
 		return get_instance()->$var;
 	}
 
-	// ------------------------------------------------------------------------
-
 	/**
-	 * Get param
+	 * Getter for the content.
 	 *
-	 * This is a helper used from the parser files to process a list of params
-	 *
-	 * @param	array - Params passed from view
-	 * @param	array - Array of default params
-	 * @return 	array
+	 * @return array
 	 */
 	public function content()
 	{
 		return $this->content;
 	}
 
-	// ------------------------------------------------------------------------
-
 	/**
-	 * Get param
+	 * Getter for the attributes.
 	 *
-	 * This is a helper used from the parser files to process a list of params
-	 *
-	 * @param	array - Params passed from view
-	 * @param	array - Array of default params
-	 * @return 	array
+	 * @return array
 	 */
 	public function attributes()
 	{
 		return $this->attributes;
 	}
 
-	// ------------------------------------------------------------------------
-
 	/**
-	 * Get param
+	 * Get the value of an attribute.
 	 *
-	 * This is a helper used from the parser files to process a list of params
+	 * @param string $param The name of the attribute.
+	 * @param mixed $default The default value to return if no value can be found.
 	 *
-	 * @param	array - Params passed from view
-	 * @param	array - Array of default params
-	 * @return 	array
+	 * @return mixed The value.
 	 */
 	public function attribute($param, $default = NULL)
 	{
 		return isset($this->attributes[$param]) ? $this->attributes[$param] : $default;
 	}
 
-	// ------------------------------------------------------------------------
-
 	/**
-	 * Get param
+	 * Render a view located in a module.
 	 *
-	 * This is a helper used from the parser files to process a list of params
+	 * @todo Document this better.
 	 *
-	 * @param	array - Params passed from view
-	 * @param	array - Array of default params
-	 * @return 	array
+	 * @param string $module The module to load the view from.
+	 * @param string $view The name of the view to load.
+	 * @param array $vars The array of variables to pass to the view.
+	 *
+	 * @return string The rendered view.
 	 */
 	public function module_view($module, $view, $vars = array())
 	{
-		if (file_exists($this->template->get_views_path() . 'modules/' . $module . '/' . $view . (pathinfo($view, PATHINFO_EXTENSION) ? '' : '.php')))
+		if (file_exists($this->template->get_views_path().'modules/'.$module.'/'.$view.(pathinfo($view, PATHINFO_EXTENSION) ? '' : '.php')))
 		{
-			$path = $this->template->get_views_path() . 'modules/' . $module . '/';
+			$path = $this->template->get_views_path().'modules/'.$module.'/';
 		}
 		else
 		{
@@ -111,7 +98,7 @@ abstract class Plugin
 		// add this view location to the array
 		$this->load->set_view_path($path);
 
-		$content = $this->load->_ci_load(array('_ci_view' => $view, '_ci_vars' => ((array) $vars), '_ci_return' => TRUE));
+		$content = $this->load->_ci_load(array('_ci_view' => $view, '_ci_vars' => ((array)$vars), '_ci_return' => TRUE));
 
 		// Put the old array back
 		$this->load->set_view_path($save_path);
@@ -145,15 +132,17 @@ class Plugins
 				return $this->_process($path, $class, $method, $attributes, $content);
 			}
 
-			else if (defined('ADMIN_THEME') and file_exists($path = APPPATH.'themes/'.ADMIN_THEME.'/plugins/'.$class.'.php'))
-			{
-				return $this->_process($path, $class, $method, $attributes, $content);
+			else {
+				if (defined('ADMIN_THEME') and file_exists($path = APPPATH.'themes/'.ADMIN_THEME.'/plugins/'.$class.'.php'))
+				{
+					return $this->_process($path, $class, $method, $attributes, $content);
+				}
 			}
 
 			// Maybe it's a module
 			if (module_exists($class))
 			{
-				if (file_exists($path = $directory . 'modules/' . $class . '/plugin.php'))
+				if (file_exists($path = $directory.'modules/'.$class.'/plugin.php'))
 				{
 					$dirname = dirname($path).'/';
 
@@ -169,23 +158,25 @@ class Plugins
 			}
 		}
 
-		log_message('debug', 'Unable to load: ' . $class);
+		log_message('debug', 'Unable to load: '.$class);
 
-		return FALSE;
+		return false;
 	}
-
-	 // --------------------------------------------------------------------
 
 	/**
 	 * Process
 	 *
 	 * Just process the class
 	 *
-	 * @access	private
-	 * @param	object
-	 * @param	string
-	 * @param	array
-	 * @return	mixed
+	 * @todo Document this better.
+	 *
+	 * @param string $path
+	 * @param string $class
+	 * @param string $method
+	 * @param array $attributes
+	 * @param array $content
+	 *
+	 * @return bool|mixed
 	 */
 	private function _process($path, $class, $method, $attributes, $content)
 	{
@@ -195,17 +186,17 @@ class Plugins
 		if ( ! isset($this->loaded[$class]))
 		{
 			include $path;
-			$this->loaded[$class] = TRUE;
+			$this->loaded[$class] = true;
 		}
 
 		if ( ! class_exists($class_name))
 		{
-//			throw new Exception('Plugin "' . $class_name . '" does not exist.');
-//			return FALSE;
+			//throw new Exception('Plugin "' . $class_name . '" does not exist.');
+			//return FALSE;
 
-			log_message('error', 'Plugin class "' . $class_name . '" does not exist.');
+			log_message('error', 'Plugin class "'.$class_name.'" does not exist.');
 
-			return FALSE;
+			return false;
 		}
 
 		$class_init = new $class_name;
@@ -216,15 +207,15 @@ class Plugins
 			// But does a property exist by that name?
 			if (property_exists($class_init, $method))
 			{
-				return TRUE;
+				return true;
 			}
 
-//			throw new Exception('Method "' . $method . '" does not exist in plugin "' . $class_name . '".');
-//			return FALSE;
+			//throw new Exception('Method "' . $method . '" does not exist in plugin "' . $class_name . '".');
+			//return FALSE;
 
-			log_message('error', 'Plugin method "' . $method . '" does not exist on class "' . $class_name . '".');
+			log_message('error', 'Plugin method "'.$method.'" does not exist on class "'.$class_name.'".');
 
-			return FALSE;
+			return false;
 		}
 
 		return call_user_func(array($class_init, $method));

system/cms/modules/blog/controllers/admin.php
@@ -8,14 +8,14 @@ class Admin extends Admin_Controller
 {
 	/**
 	 * The current active section
-	 * @access protected
+	 *
 	 * @var string
 	 */
 	protected $section = 'posts';
 
 	/**
 	 * Array that contains the validation rules
-	 * @access protected
+	 *
 	 * @var array
 	 */
 	protected $validation_rules = array(
@@ -82,8 +82,6 @@ class Admin extends Admin_Controller
 
 	/**
 	 * The constructor
-	 * @access public
-	 * @return void
 	 */
 	public function __construct()
 	{
@@ -95,17 +93,20 @@ class Admin extends Admin_Controller
 		$this->load->library(array('keywords/keywords', 'form_validation'));
 
 		// Date ranges for select boxes
-		$this->data->hours = array_combine($hours = range(0, 23), $hours);
-		$this->data->minutes = array_combine($minutes = range(0, 59), $minutes);
+		$this->template
+			->set('hours', array_combine($hours = range(0, 23), $hours))
+			->set('minutes', array_combine($minutes = range(0, 59), $minutes))
+		;
 
-		$this->data->categories = array();
+		$_categories = array();
 		if ($categories = $this->blog_categories_m->order_by('title')->get_all())
 		{
 			foreach ($categories as $category)
 			{
-				$this->data->categories[$category->id] = $category->title;
+				$_categories[$category->id] = $category->title;
 			}
 		}
+		$this->template->set('categories', $_categories);
 	}
 
 	/**
@@ -141,13 +142,15 @@ class Admin extends Admin_Controller
 			->set('pagination', $pagination)
 			->set('blog', $blog);
 
-		$this->input->is_ajax_request() ? $this->template->build('admin/tables/posts', $this->data) : $this->template->build('admin/index', $this->data);
+		$this->input->is_ajax_request()
+			? $this->template->build('admin/tables/posts')
+			: $this->template->build('admin/index');
 
 	}
 
 	/**
 	 * Create new post
-	 * @access public
+	 *
 	 * @return void
 	 */
 	public function create()

commit 3204ea6a58f2dc52c4ab1a479680e30fdd36d8b7

Fixed a bug in Theme Layouts that was introduced by cd72cfe0df25ed602d2b10cefc0195e5de05a2b4

system/cms/modules/pages/controllers/admin_layouts.php
@@ -88,6 +88,8 @@ class Admin_layouts extends Admin_Controller
 	 */
 	public function create()
 	{
+		$data = new stdClass();
+
 		// Got validation?
 		if ($this->form_validation->run())
 		{
@@ -117,7 +119,7 @@ class Admin_layouts extends Admin_Controller
 		$theme_layouts = $this->template->get_theme_layouts($this->settings->default_theme);
 		foreach ($theme_layouts as $theme_layout)
 		{
-			$this->template->theme_layouts[$theme_layout] = basename($theme_layout, '.html');
+			$data->theme_layouts[$theme_layout] = basename($theme_layout, '.html');
 		}
 
 		// Assign data for display
@@ -128,7 +130,7 @@ class Admin_layouts extends Admin_Controller
 		// Load WYSIWYG editor
 		$this->template
 			->title($this->module_details['name'], lang('pages.layout_id_label'), lang('page_layouts.create_title'))
-			->build('admin/layouts/form');
+			->build('admin/layouts/form', $data);
 	}
 
 	/**