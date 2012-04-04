53d3ba2de9cbefa33c2dd56d7c105ea22d80dc69
migueljusto <miguel.justo@me.com>
2012-04-04 04:03:33 +0100

    Added portuguese translation
    The user module don't show the portuguese language, but the rest is
    okay. I don't know why..

---
+++ system/cms/language/portuguese/extra_validation_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['alpha_dot_dash']			= "O campo %s pode conter apenas caracteres alfanuméricos, underscores ( _ ), pontos e traços.";
+$lang['decimal']				= "O campo %s pode conter apenas números decimais.";
+$lang['csrf_bad_token']			= "Token CSRF inválido";
+
+/* End of file extra_validation_lang.php */
---
+++ system/cms/modules/users/language/portuguese/help_lang.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// inline help html. Only 'help_body' is used.
+$lang['help_body'] = "<h4>Visão Geral</h4>
+<p>O módulo Utilizadores trabalha em conjunto com grupos e permissões para dar ao PyroCMS controlo de acesso.</p>
+
+<h4>Adicionar Utilizador</h4>
+<p>Preencha os detalhes do utilizador (incluindo senha) e salve. Se tem os e-mails de ativação ativado nas configurações de e-mail, será enviado para o novo utilizador com um link de ativação.</p>
+
+<h4>Ativar Novos Utilizadores</h4>
+<p>Se os e-mails de ativação estiverem desativados nas Configurações de utilizadores, os utilizadores que se registam no site (front-end), irá aparecer sob o item Utilizadores inativos no menu até aprovar ou excluir a conta. Se os e-mails de ativação estiverem ativados os utilizadores podem registar-se 
+sozinhos, sem a ajuda de um administrador.
+</p>";
