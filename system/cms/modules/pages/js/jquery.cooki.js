85fbbc649323c5dfb01623753dc9d1b0533bfc92
Ryun Shofner <humtech@gmail.com>
2012-04-04 23:28:03 -0700

    Initial commit
    Signed-off-by: Ryun Shofner <humtech@gmail.com>

---
+++ system/cms/modules/pages/js/jquery.cooki.js
@@ -0,0 +1,54 @@
+/*
+ * @todo rebind submit button after..
+ */
+jQuery.cachedScript = function(url, options) {
+
+  // allow user to set any option except for dataType, cache, and url
+	options = $.extend(options || {}, {
+		dataType: 'script',
+		cache: true,
+		url: url
+	});
+
+	if (typeof url === 'object')
+	{
+		var len = url.length;
+		for (i=0; i<len;i++) {
+			options.url = url[i];
+			jQuery.ajax(options);
+		}
+	}
+	return jQuery.ajax(options);
+};
+jQuery.cookie = function (key, value, options) {
+    
+    // key and at least value given, set cookie...
+    if (arguments.length > 1 && String(value) !== "[object Object]") {
+        options = jQuery.extend({}, options);
+
+        if (value === null || value === undefined) {
+            options.expires = -1;
+        }
+
+        if (typeof options.expires === 'number') {
+            var days = options.expires, t = options.expires = new Date();
+            t.setDate(t.getDate() + days);
+        }
+        
+        value = String(value);
+        
+        return (document.cookie = [
+            encodeURIComponent(key), '=',
+            options.raw ? value : encodeURIComponent(value),
+            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+            options.path ? '; path=' + options.path : '',
+            options.domain ? '; domain=' + options.domain : '',
+            options.secure ? '; secure' : ''
+        ].join(''));
+    }
+
+    // key and possibly options given, get cookie...
+    options = value || {};
+    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+};
