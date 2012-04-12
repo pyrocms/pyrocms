e877373842519c01b60262205ac5a2af2224daaa
jerel <mail@unruhdesigns.com>
2012-04-11 22:42:14 -0500

    Fixed a bug with user profile viewing that caused the user's profile to not be displayed when using alternate routes

--- system/cms/modules/users/views/profile/view.php
+++ system/cms/modules/users/views/profile/view.php
@@ -1,4 +1,4 @@
-<h2 class="page-title">{{ user:display_name user_id={url:segments segment="3"} }}</h2>
+<h2 class="page-title">{{ user:display_name user_id= _user:id }}</h2>
 
 
 <!-- Container for the user's profile -->
@@ -9,7 +9,8 @@
 
 		<table>
 	
-			{{ user:profile_fields user_id={url:segments segment="3"} }}
+			{{# we use _user:id as that is the id passed to this view. Different than the logged in user's user:id #}}
+			{{ user:profile_fields user_id= _user:id }}
 				
 				<tr><td><strong>{{ name }}:</strong></td><td>{{ value }}</td></tr>
 
