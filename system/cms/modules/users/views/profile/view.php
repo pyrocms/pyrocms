c46806b7abf0824870394c77468740f9c85363eb
jerel <mail@unruhdesigns.com>
2012-04-12 00:35:43 -0500

    Changed user profile view to hide information like email addresses by default

--- system/cms/modules/users/views/profile/view.php
+++ system/cms/modules/users/views/profile/view.php
@@ -11,8 +11,10 @@
 	
 			{{# we use _user:id as that is the id passed to this view. Different than the logged in user's user:id #}}
 			{{ user:profile_fields user_id= _user:id }}
-				
-				<tr><td><strong>{{ name }}:</strong></td><td>{{ value }}</td></tr>
+				{{#   viewing own profile?    are they an admin?        ok it's a regular user, we'll show the non-sensitive items #}}
+				{{ if user:id === _user:id or user:group === 'admin' or slug != 'email' and slug != 'first_name' and slug != 'last_name' and slug != 'username' and value }}
+					<tr><td><strong>{{ name }}:</strong></td><td>{{ value }}</td></tr>
+				{{ endif }}
 
 			{{ /user:profile_fields }}
 
