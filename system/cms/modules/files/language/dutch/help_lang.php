d1acadfc5a778ac3e971ab8a89dd062c75b8e01b
Patrick Kivits <patrick@patrickkivits.nl>
2012-04-21 19:38:02 +0200

    Dutch languages translated in several modules

---
+++ system/cms/modules/files/language/dutch/help_lang.php
@@ -0,0 +1,30 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// inline help html. Only 'help_body' is used.
+$lang['help_body'] = "
+<h4>Overview</h4><hr>
+<p>De bestanden module is een goede manier om voor de site admin bestanden te beheren.
+Alle afbeeldingen of bestanden die worden geplaatst in pagina's gallerijen of blog berichten worden hier opgeslagen.
+Pagina afbeeldingen kunnen direct van de WYSIWYG editor worden geupload of eerst hier worden geupload en later worden geplaatst via de WYSIWYG editor</p>
+
+<h6>Mappen beheren</h6>
+<p>Nadat u de eerste map of mappen heeft gemaakt kunt u zo veel submappen aanmaken als u wilt. Bijvoorbeeld blog/images/screenshots/image.jpg and pages/audio/sample.mp3.
+De mapnamen zijn alleen voor uw gebruik, de mapnaam wordt niet weergeven in de download link.
+Om een map te wijzigen kunt u Beheer mappen in het linker column selecteren. Klik dan op Wijzig op de map die u wilt wijzigen.</p>
+
+<h6>Bestanden beheren</h6>
+<p>Om bestanden te beheren selecteert u de mapnaam in de linker column. Als het bestand in een submap is kunt u de hoofdmap gebruiken.
+Zodra u in de hoofdmap bent kunt u de submap kiezen en een filter toepassen om op bestandstype te filteren doormiddel van de selectie bovenaan uw scherm.
+Nu in de goede map bent kunt u bestanden uploaden door op de Upload knop te drukken naast het filter.
+Er zijn twee manier op uw bestanden te bekijken: Lijst en Grid. Lijst laat alle bestand details zien terwijl grid een thumbnail laat zien van de afbeelding.</p>
+
+<h6>Bestanden uploaden</h6>
+<p>
+Nadat u op de upload knop heeft gedrukt in de gewenste map komt er een upload scherm.
+U kunt bestanden toevoegen door deze te slepen in het Upload gedeelte of door bestanden te selecteren via het standaard upload scherm.
+De gelecteerde bestanden worden weergegeven in een lijst onder aan uw scherm.
+U kunt eventueele onnodige bestanden verwijderen van de lijst of de bestanden uploaden door op Upload te klikken.
+Als het uploaden is mislukt controleert u dan uw bestandsgrootte. Veel servers laten geen bestanden toe die groter zijn dan 2MB.
+Veel moderne cameras maken afbeeldingen van 5MB of groter.
+Om dit te voorkomen kunt u vragen de upload limit te vergroten of uw afbeeldingen van te voren te verkleinen.
+Het verkleinen van uw afbeeldingen versnelt het uploaden van uw afbeeldingen.</p>";
---
+++ system/cms/modules/sitemap/language/dutch/help_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// inline help html. Only 'help_body' is used.
+$lang['help_body'] = '<h4>Overview</h4>
+<p>De sitemap module genereert een pagina en XML bestand voor zoekmachines. 
+Kijk op <a href="http://sitemaps.org">sitemaps.org</a> voor meer informatie.
+</p>';
