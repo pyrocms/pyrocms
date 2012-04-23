803007fa71992cd3812939e75cc3edb86a9cd8e9
jerel <mail@unruhdesigns.com>
2012-04-23 00:09:10 -0500

    Removed iPad from the mobile array of the user_agent config file. It will now load the full version of a site as our documentation states it will. Closes #1338

--- system/cms/config/user_agents.php
+++ system/cms/config/user_agents.php
@@ -94,7 +94,6 @@ $mobiles = array(
 					'nokia'				=> "Nokia",
 					'palm'				=> "Palm",
 					'iphone'			=> "Apple iPhone",
-					'ipad'				=> "iPad",
 					'ipod'				=> "Apple iPod Touch",
 					'sony'				=> "Sony Ericsson",
 					'ericsson'			=> "Sony Ericsson",
