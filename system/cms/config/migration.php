1ff90be4a0b411358dc11868aadfef7051266bbc
Phil Sturgeon <email@philsturgeon.co.uk>
2012-04-24 20:27:31 +0100

    Improved blog slug and title length.

--- system/cms/config/migration.php
+++ system/cms/config/migration.php
@@ -23,7 +23,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 88;
+$config['migration_version'] = 89;
 
 /*
 |--------------------------------------------------------------------------
