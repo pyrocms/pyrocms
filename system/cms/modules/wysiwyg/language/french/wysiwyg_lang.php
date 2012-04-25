a14d55b3312175c8feaf65f374ea8893c08ad2af
George Petsagourakis <petsagouris@gmail.com>
2012-04-25 23:27:27 +0300

    Making the WYSIWYG module language files uniform.

--- system/cms/modules/wysiwyg/language/french/wysiwyg_lang.php
+++ system/cms/modules/wysiwyg/language/french/wysiwyg_lang.php
@@ -1,17 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang = array(
-    'wysiwyg.meta.width'    =>  'Largeur',
-    'wysiwyg.meta.height'   =>  'Hauteur',
-    'wysiwyg.meta.size'     =>  'Taille',
-    'wysiwyg.label.insert_width'    => 'Inserer la largeur de l\'image:',
-	'wysiwyg.label.no_limit' =>	'(0 pour supprimer la limite)',
-    'wysiwyg.label.float'   =>  'Flottant',
-    'wysiwyg.label.left'    =>  'Gauche',
-    'wysiwyg.label.right'   =>  'Droite',
-    'wysiwyg.label.none'    =>  'Aucun',
-    'wysiwyg.meta.mime'     =>  'Type Mime'
-
-);
-
-/* end of file wysiwyg_lang.php */
+$lang['wysiwyg.meta.width'] = 'Largeur';
+$lang['wysiwyg.meta.height'] = 'Hauteur';
+$lang['wysiwyg.meta.size'] = 'Taille';
+$lang['wysiwyg.label.insert_width'] = 'Inserer la largeur de l\'image:';
+$lang['wysiwyg.label.no_limit'] = 'Full Size'; #translate
+$lang['wysiwyg.label.float'] = 'Flottant';
+$lang['wysiwyg.label.left'] = 'Gauche';
+$lang['wysiwyg.label.right'] = 'Droite';
+$lang['wysiwyg.label.none'] = 'Aucun';
+$lang['wysiwyg.meta.mime'] = 'Type Mime';
--- system/cms/modules/wysiwyg/language/hebrew/wysiwyg_lang.php
+++ system/cms/modules/wysiwyg/language/hebrew/wysiwyg_lang.php
@@ -1,17 +1,12 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang = array(
-    'wysiwyg.meta.width'    =>  'רוחב',
-    'wysiwyg.meta.height'   =>  'גובה',
-    'wysiwyg.meta.size'     =>  'מידה',
-    'wysiwyg.label.insert_width'    => 'הכנס רוחב התמונה:',
-	'wysiwyg.label.no_limit' =>	'Full Size', #translate
-    'wysiwyg.label.float'   =>  'צף',
-    'wysiwyg.label.left'    =>  'שמאל',
-    'wysiwyg.label.right'   =>  'ימין',
-    'wysiwyg.label.none'    =>  'כלום',
-    'wysiwyg.meta.mime'     =>  'Mime Type'
-
-);
-
-/* end of file wysiwyg_lang.php */
+$lang['wysiwyg.meta.width'] = 'רוחב';
+$lang['wysiwyg.meta.height'] = 'גובה';
+$lang['wysiwyg.meta.size'] = 'מידה';
+$lang['wysiwyg.label.insert_width'] = 'הכנס רוחב התמונה:';
+$lang['wysiwyg.label.no_limit'] = 'Full Size'; #translate
+$lang['wysiwyg.label.float'] = 'צף';
+$lang['wysiwyg.label.left'] = 'שמאל';
+$lang['wysiwyg.label.right'] = 'ימין';
+$lang['wysiwyg.label.none'] = 'כלום';
+$lang['wysiwyg.meta.mime'] = 'Mime Type'; #translate
