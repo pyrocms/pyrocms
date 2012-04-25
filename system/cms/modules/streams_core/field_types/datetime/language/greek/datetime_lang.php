7da9acab3de4058274b930ecb4250ec13708396a
Adam Fairholm <adam.fairholm@gmail.com>
2012-04-25 00:15:18 -0400

    Fixing some null state issues with the date/time field.

--- system/cms/modules/streams_core/field_types/datetime/language/greek/datetime_lang.php
+++ system/cms/modules/streams_core/field_types/datetime/language/greek/datetime_lang.php
@@ -7,3 +7,4 @@ $lang['streams.datetime.start_date'] 			= 'Ξεκινά Από';
 $lang['streams.datetime.end_date'] 				= 'Τελείωνει Σε';
 $lang['streams.datetime.rest_instructions'] 	= 'Μπορεί να εισαχθεί μια καταχώριση με προσθαφαίρεση αριθμών με βάση την σημερινή ημερομηνία (πχ: -20) ή ξεχωριστά για μέρες (D), μήνες (M) και χρόνια (Y). πχ: +1D +2Y<';
 $lang['streams.datetime.storage']				= 'Μορφή Αποθήκευσης';
+$lang['streams.invalid_input_for_date_range_check'] = 'Invalid date input for data range check.'; #translate
--- system/cms/modules/streams_core/field_types/datetime/language/lithuanian/datetime_lang.php
+++ system/cms/modules/streams_core/field_types/datetime/language/lithuanian/datetime_lang.php
@@ -7,3 +7,4 @@ $lang['streams.datetime.start_date'] 			= 'Apribojimo pradžia';
 $lang['streams.datetime.end_date'] 				= 'Apribojimo pabaiga';
 $lang['streams.datetime.rest_instructions'] 	= 'Galima vesti skaitinę reikšmę nuo esamos datos (Pvz.: -20) arba skaitinę reikšmę dienoms (D), mėnesiams (M), metams (Y). Pvz: +1D +2Y<';
 $lang['streams.datetime.storage']				= 'Storage Format'; #translate
+$lang['streams.invalid_input_for_date_range_check'] = 'Invalid date input for data range check.'; #translate
