1cf3a90497014f209a577118889d7be07498de5c
jerel <mail@unruhdesigns.com>
2012-05-07 17:25:05 -0500

    Fixed a bug in the files module that showed when the last item was deleted

--- system/cms/modules/files/js/functions.js
+++ system/cms/modules/files/js/functions.js
@@ -685,7 +685,7 @@ jQuery(function($){
 
 					// if they are trying it out and created a folder and then removed it
 					// then we show the no data messages again
-					if ($folders_center.find('li').length === 0) {
+					if ($folders_center.find('li').length === 0 && pyro.files.current_level === 0) {
 						$('.no_data').fadeIn('fast');
 					}
 					
