9b47d3be7e65322127b0c02f9aef6721d7dc7534
Phil Sturgeon <email@philsturgeon.co.uk>
2012-06-03 11:36:37 -0500

    Moved quick links out of analytics if

--- system/cms/themes/pyrocms/views/admin/dashboard.php
+++ system/cms/themes/pyrocms/views/admin/dashboard.php
@@ -1,7 +1,53 @@
+
+<!-- Begin Quick Links -->
+	<?php if ($theme_options->pyrocms_quick_links == 'yes') : ?>
+	<div class="one_full">
+		
+		<section id="quick_links" class="<?php echo isset($rss_items); ?>">
+			<h4><?php //echo lang('cp_admin_quick_links') ?></h4>
+			<ul>
+				<?php if(array_key_exists('comments', $this->permissions) OR $this->current_user->group == 'admin'): ?>
+				<li>
+					<a class="tooltip-s" title="<?php echo lang('cp_manage_comments') ?>" href="<?php echo site_url('admin/comments') ?>">
+						<?php echo Asset::img('icons/comments.png', lang('cp_manage_comments')); ?>
+					</a>
+				</li>
+				<?php endif; ?>
+				
+				<?php if(array_key_exists('pages', $this->permissions) OR $this->current_user->group == 'admin'): ?>
+				<li>
+					<a class="tooltip-s" title="<?php echo lang('cp_manage_pages'); ?>" href="<?php echo site_url('admin/pages') ?>">
+						<?php echo Asset::img('icons/pages.png', lang('cp_manage_pages')); ?>
+					</a>
+				</li>
+				<?php endif; ?>
+				
+				<?php if(array_key_exists('files', $this->permissions) OR $this->current_user->group == 'admin'): ?>
+				<li>
+					<a class="tooltip-s" title="<?php echo lang('cp_manage_files'); ?>" href="<?php echo site_url('admin/files') ?>">
+						<?php echo Asset::img('icons/files.png', lang('cp_manage_files')); ?>
+					</a>
+				</li>
+				<?php endif; ?>
+				
+				<?php if(array_key_exists('users', $this->permissions) OR $this->current_user->group == 'admin'): ?>
+				<li>
+					<a class="tooltip-s" title="<?php echo lang('cp_manage_users'); ?>" href="<?php echo site_url('admin/users') ?>">
+						<?php echo Asset::img('icons/users.png', lang('cp_manage_users')); ?>
+					</a>
+				</li>
+				<?php endif; ?>
+			</ul>
+		</section>
+
+	</div>		
+	<?php endif; ?>
+	<!-- End Quick Links -->
+
 <?php if ((isset($analytic_visits) OR isset($analytic_views)) AND $theme_options->pyrocms_analytics_graph == 'yes'): ?>
 <script type="text/javascript">
 
-$(function($) {
+jQuery(function($) {
 		var visits = <?php echo isset($analytic_visits) ? $analytic_visits : 0; ?>;
 		var views = <?php echo isset($analytic_views) ? $analytic_views : 0; ?>;
 
@@ -60,51 +106,6 @@ $(function($) {
 	
 	});
 </script>
-
-	<!-- Begin Quick Links -->
-	<?php if ($theme_options->pyrocms_quick_links == 'yes') : ?>
-	<div class="one_full">
-		
-		<section id="quick_links" class="<?php echo isset($rss_items); ?>">
-			<h4><?php //echo lang('cp_admin_quick_links') ?></h4>
-			<ul>
-				<?php if(array_key_exists('comments', $this->permissions) OR $this->current_user->group == 'admin'): ?>
-				<li>
-					<a class="tooltip-s" title="<?php echo lang('cp_manage_comments') ?>" href="<?php echo site_url('admin/comments') ?>">
-						<?php echo Asset::img('icons/comments.png', lang('cp_manage_comments')); ?>
-					</a>
-				</li>
-				<?php endif; ?>
-				
-				<?php if(array_key_exists('pages', $this->permissions) OR $this->current_user->group == 'admin'): ?>
-				<li>
-					<a class="tooltip-s" title="<?php echo lang('cp_manage_pages'); ?>" href="<?php echo site_url('admin/pages') ?>">
-						<?php echo Asset::img('icons/pages.png', lang('cp_manage_pages')); ?>
-					</a>
-				</li>
-				<?php endif; ?>
-				
-				<?php if(array_key_exists('files', $this->permissions) OR $this->current_user->group == 'admin'): ?>
-				<li>
-					<a class="tooltip-s" title="<?php echo lang('cp_manage_files'); ?>" href="<?php echo site_url('admin/files') ?>">
-						<?php echo Asset::img('icons/files.png', lang('cp_manage_files')); ?>
-					</a>
-				</li>
-				<?php endif; ?>
-				
-				<?php if(array_key_exists('users', $this->permissions) OR $this->current_user->group == 'admin'): ?>
-				<li>
-					<a class="tooltip-s" title="<?php echo lang('cp_manage_users'); ?>" href="<?php echo site_url('admin/users') ?>">
-						<?php echo Asset::img('icons/users.png', lang('cp_manage_users')); ?>
-					</a>
-				</li>
-				<?php endif; ?>
-			</ul>
-		</section>
-
-	</div>		
-	<?php endif; ?>
-	<!-- End Quick Links -->
 
 <section class="title">
 	<h4>Statistics</h4>
