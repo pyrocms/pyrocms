ae02be1aaf4c6214744537a78ff2657867842cfa
Adam Fairholm <adam.fairholm@gmail.com>
2012-06-10 23:32:57 -0400

    Updating the images field type to work with cloud files.
    Previously, the Streams image field type did everything on its own and integrated with the PyroCMS file record system. With the new Files module handling cloud+local files with abstracted functions, the image file type has been updated so you can upload to cloud folders. It has also been updated to work with the new thumbs URL syntax.

--- system/cms/modules/streams_core/field_types/image/language/czech/image_lang.php
+++ system/cms/modules/streams_core/field_types/image/language/czech/image_lang.php
@@ -7,3 +7,6 @@ $lang['streams.image.resize_height'] 	= 'Výška po zmenšení';
 $lang['streams.image.allowed_types'] 	= 'Povolené typy';
 $lang['streams.image.image_errors']		= 'Objevili se následující problémy při přidávání souboru';
 $lang['streams.image.need_folder']		= 'Musíte zvolit složku pro upload než budete moci nahrát soubory.';
+$lang['streams.image.keep_ratio'] 		= 'Keep Ratio'; #translate
+$lang['streams.image.keep_ratio_instr']		= 'When resizing, should we keep the ratio?'; #translate
+$lang['streams.image.allowed_types_instr'] 	= 'Ex: jpg|jpeg|png'; #translate
