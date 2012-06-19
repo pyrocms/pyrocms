3cc698215500a683d4fe5d6646320c50d8d762f3
Mikael <mikaelfoyn@gmail.com>
2012-06-19 19:22:26 +0200

    Norwegian translation for Files-Module

---
+++ system/cms/modules/files/language/norwegian/permission_lang.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Files Permissions
+$lang['files.role_wysiwyg']			= 'Manage Files From WYSIWYG';
+$lang['files.role_wysiwyg']			= 'Håndter filer fra WYSIWYG';
+$lang['files.role_upload']			= 'Last opp fil';
+$lang['files.role_download_file']	= 'Last ned fil';
+$lang['files.role_edit_file']		= 'Rediger fil';
+$lang['files.role_delete_file'] 	= 'Slett fil';
+
+$lang['files.role_create_folder']	= 'Opprett mappe';
+$lang['files.role_set_location']	= 'Sett som sky-tjeneste';
+$lang['files.role_synchronize']		= 'Synkroniser med skyen';
+$lang['files.role_edit_folder']		= 'Rediger mappe';
+$lang['files.role_delete_folder'] 	= 'Slett mappe';
