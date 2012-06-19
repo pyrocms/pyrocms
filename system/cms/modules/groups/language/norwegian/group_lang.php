78d6a6d30c2d2ea82bbc92ccf40c90137ab1b2ae
Mikael <mikaelfoyn@gmail.com>
2012-06-19 19:27:14 +0200

    Norwegian translation for Groups-Module.

---
+++ system/cms/modules/groups/language/norwegian/group_lang.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['groups.title']					= 'Tittel';
+$lang['groups.name']				    = 'Navn';
+$lang['groups.short_name']				= 'Kort navn';
+
+// titles
+$lang['groups.index_title']             = 'Liste grupper';
+$lang['groups.add_title']               = 'Legg til gruppe';
+$lang['groups.edit_title']              = 'Redigerer gruppe "%s"';
+
+// messages
+$lang['groups.no_groups']               = 'Ingen grupper funnet.';
+$lang['groups.add_success']             = 'Gruppen "%s" har blitt opprettet.';
+$lang['groups.add_error']               = 'Kunne ikke opprette gruppen "%s".';
+$lang['groups.edit_success']            = 'Gruppen "%s" har blitt lagret.';
+$lang['groups.edit_error']              = 'Kunne ikke lagre gruppen "%s".';
+$lang['groups.delete_success']          = 'Gruppen ble slettet.';
+$lang['groups.delete_error']            = 'Det oppstod en feil under sletting av denne gruppen. Du må slette alle brukere tilknyttet denne gruppen før du sletter gruppen.';
+$lang['groups.already_exist_error']     = 'En gruppe med samme navnet "%s" ekisterer allerede.';
+
+/* End of file group_lang.php */
