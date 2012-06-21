5c03789d0eda4d4957fd9cb214c68ed091b33613
Mikael <mikaelfoyn@gmail.com>
2012-06-21 18:06:56 +0200

    Translated modules-module to Norwegian.

---
+++ system/cms/modules/modules/language/norwegian/modules_lang.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['modules.core_list']               = 'Kjernemoduler';
+$lang['modules.addon_list']              = 'Tilleggsmoduler';
+$lang['modules.introduction']            = 'Nedenfor er en liste over installerte moduler og tilhørende informasjon, for eksempel beskrivelsen eller versjon.';
+$lang['modules.core_introduction']		 = 'Nedenfor er en liste over kjernemoduler og tilhørende informasjon. Du kan deaktivere dem, men gjør det med ekstrem forsiktighet. Deaktivering av moduler for eksempel brukere vil gjøre nettstedet ubrukelig.';
+$lang['modules.disable_error']            = 'Kunne ikke deaktivere modulen "%s".';
+$lang['modules.disable_success']         = 'Modulen "%s" har blitt deaktivert.';
+$lang['modules.enable_error']            = 'Kunne ikke aktivere modulen "%s".';
+$lang['modules.enable_success']          = 'Modulen "%s" har blitt aktivert.';
+$lang['modules.install_error']           = 'Kunne ikke installere modulen.';
+$lang['modules.install_success']         = 'Modulen "%s" har blitt installert.';
+$lang['modules.delete_success']			 = 'Modulen "%s" har blitt slettet.';
+$lang['modules.delete_error']			 = 'Kunne ikke slette modulen "%s".';
+$lang['modules.uninstall_error']         = 'Kunne ikke avinstallere modulen "%s".';
+$lang['modules.uninstall_success']       = 'Modulen "%s" har blitt avinstallert.';
+$lang['modules.upgrade_error']           = 'Kunne ikke oppgradere modulen "%s".';
+$lang['modules.upgrade_success']         = 'Modulen "%s" ble oppgradert.';
+$lang['modules.already_exists_error']    = 'En modul med navnet "%s" eksisterer allerede.';
+$lang['modules.module_not_specified']    = 'Du må spesifisere en modul.';
+$lang['modules.details_error']           = 'Det er en feil i details.php filen i %s modulen. Rett feilen, installer deretter modulen på nytt.';
+
+$lang['modules.manually_remove']         = 'Du må manuelt fjerne "%s" for fjerne hele modulen.';
+$lang['modules.upload_title']            = 'Last opp en modul';
+$lang['modules.upload_desc']             = 'Velg en fil og klikke Last opp-knappen';
+
+$lang['modules.confirm_enable']          = 'Er du sikker på at du vil aktivere denne modulen?';
+$lang['modules.confirm_disable']         = 'Er du sikker på at du vil deaktivere denne modulen?';
+$lang['modules.confirm_install']         = 'Er du sikker på at du vil installere denne modulen?';
+$lang['modules.confirm_uninstall']		 = 'Alle databasedatene vil bli fjernet! Er du sikker på at du vil avinstallere denne modulen?';
+$lang['modules.confirm_delete']       	 = 'Alle modul filer og databasedatene vil bli fjernet! Er du sikker på at du vil slette denne modulen?';
+$lang['modules.confirm_upgrade'] 		 = 'Er du sikker på at du ønsker å oppgradere denne modulen?';
