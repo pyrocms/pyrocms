4817c5232177037d3109187c998d6668005db825
Mikael <mikaelfoyn@gmail.com>
2012-06-21 17:49:15 +0200

    Translated Installer to Norwegian Language.

---
+++ installer/language/norwegian/global_lang.php
@@ -0,0 +1,13 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['intro']	=	'Introduksjon';
+$lang['step1']	=	'Steg #1';
+$lang['step2']	=	'Steg #2';
+$lang['step3']	=	'Steg #3';
+$lang['step4']	=	'Steg #4';
+$lang['final']	=	'Siste steg';
+
+$lang['installer.passwords_match']		= "Passordene er identiske";
+$lang['installer.passwords_dont_match']	= "Passordene er ikke identiske.";
+
+/* End of file global_lang.php */
---
+++ installer/language/norwegian/step_4_lang.php
@@ -0,0 +1,31 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+// labels
+$lang['header']			=	'Steg 4: Opprett Database';
+$lang['intro_text']		=	'Fyll ut skjemaet nedenfor og klikk på knappen merket "Install" for å installere PyroCMS. Sørg for å installere PyroCMS i riktig databasen ettersom alle eksisterende endringer vil gå tapt!';
+
+$lang['db_settings']		=	'Database Innstillinger';
+$lang['db_create']		=	'Opprett database';
+$lang['db_notice']		=	'Du må kanskje gjøre dette selv';
+$lang['default_user']		=	'Standard Bruker';
+$lang['database']		=	'Database';
+$lang['site_settings']		= 	'Innstillinger for webområde';
+$lang['site_ref']		=	'Site Ref'; #translate
+$lang['user_name']		= 	'Brukernavn';
+$lang['first_name']		= 	'Fornavn';
+$lang['last_name']		=	'Etternavn';
+$lang['email']			=	'E-postadresse';
+$lang['password']		= 	'Passord';
+$lang['conf_password']		=	'Gjenta Passord';
+$lang['finish']			=	'Installer';
+
+$lang['invalid_db_name'] = 'Database navnet du skrev inn er ugyldig. Vennligst bruk kun bokstaver, tall og understrek.';
+$lang['error_101']		=	'Databasen ble ikke funnet. Hvis du spurt installasjonsprogrammet til å lage denne databasen, kan det ha mislyktes på grunn av manglende tillatelser.';
+$lang['error_102']		=	'Installasjonsprogrammet kunne ikke opprette tabeller i databasen.';
+$lang['error_103']		=	'Installasjonsprogrammet kunne ikke sette inn dataene inn i databasen.';
+$lang['error_104']		=	'Installasjonsprogrammet kunne ikke opprette standard brukeren';
+$lang['error_105']		=	'Database konfigurasjonsfilen kunne ikke bli skrevet, hoppet du over steg 3?';
+$lang['error_106']		=	'Konfigurasjonsfilen kunne ikke bli skrevet, er du sikker på at filen har de riktige rettighetene?';
+$lang['success']		=	'PyroCMS er installert.';
+
+/* End of file step_4_lang.php */
