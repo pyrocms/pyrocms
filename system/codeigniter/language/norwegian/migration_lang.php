dbeda7861baae37f7a78717a4f00dce7b0900c52
Mikael <mikaelfoyn@gmail.com>
2012-06-21 17:11:44 +0200

    CodeIgniter Norwegian Language.

---
+++ system/codeigniter/language/norwegian/migration_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['migration_none_found']			= "Ingen migreringer ble funnet.";
+$lang['migration_not_found']			= "Ingen migrasjon ble funnet med versjonsnummer: %d.";
+$lang['migration_multiple_version']		= "Dette er flere migreringer med samme versjonsnummer: %d.";
+$lang['migration_class_doesnt_exist']	= "Migrasjons klassen \"%s\" ble ikke funnet.";
+$lang['migration_missing_up_method']	= "Migrasjons klassen \"%s\" mangler en 'up' metode.";
+$lang['migration_missing_down_method']	= "Migrasjons klassen \"%s\" mangler en 'down' metode.";
+$lang['migration_invalid_filename']		= "Migrasjon \"%s\" har et ugyldig filnavn.";
+
+
+/* End of file migration_lang.php */
+/* Location: ./system/language/english/migration_lang.php */
---
+++ system/codeigniter/language/norwegian/upload_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['upload_userfile_not_set'] = "Ikke i stand til å finne en POST-variable kalt userfile.";
+$lang['upload_file_exceeds_limit'] = "Størrelsen på filen som ble opplastet overgår maksimum tillatt størrelse i konfigurasjonsfilen til PHP.";
+$lang['upload_file_exceeds_form_limit'] = "Størrelsen på filen som ble opplastet overgår maksimumstørrelsen tillatt i opplastingsskjemaet.";
+$lang['upload_file_partial'] = "Filen ble bare delvis opplastet.";
+$lang['upload_no_temp_directory'] = "Den midlertidige mappen mangler.";
+$lang['upload_unable_to_write_file'] = "Filen kunne ikke bli skrevet til serveren.";
+$lang['upload_stopped_by_extension'] = "Opplastningen ble avbrutt av extension";
+$lang['upload_no_file_selected'] = "Du valgte ikke en fil for opplastning.";
+$lang['upload_invalid_filetype'] = "Filtypen du prøver å laste opp er ikke godtatt.";
+$lang['upload_invalid_filesize'] = "Filen du prøver å laste opp er større enn den godtatte størrelsen.";
+$lang['upload_invalid_dimensions'] = "Bildet du prøver å laste opp overgår maksimum høyde eller bredde.";
+$lang['upload_destination_error'] = "Et problem ble møtt mens filen ble flyttet fra den midlertidige plasseringen til den endelige.";
+$lang['upload_no_filepath'] = "Filbanen til hvor de opplastede filene skal bli plassert er ikke gyldig";
+$lang['upload_no_file_types'] = "Du har ikke spesifisert noen godtatte filtyper.";
+$lang['upload_bad_filename'] = "Filnavnet du oppgav finnes allerede på serveren.";
+$lang['upload_not_writable'] = "Mappen for de opplastede filene ser ikke ut til å være skrivbar.";
+
+
+/* End of file upload_lang.php */
+/* Location: ./system/language/english/upload_lang.php */
