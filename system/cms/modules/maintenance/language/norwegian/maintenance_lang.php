9ce73a9a480ca5ac4b34e6221fecdd14c2de36e9
Mikael <mikaelfoyn@gmail.com>
2012-06-21 17:55:35 +0200

    Translated Maintenance-module to Norwegian.

---
+++ system/cms/modules/maintenance/language/norwegian/maintenance_lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['maintenance:list_label'] 		= 'Mellomlager Vedlikehold';
+$lang['maintenance:no_items'] 			= 'Ingen Mellomlager Mapper';
+
+$lang['maintenance:count_label'] 		= 'Elementer';
+
+$lang['maintenance:empty_msg'] 			= '%s element (er) fjernet fra mappe %s';
+$lang['maintenance:remove_msg'] 		= 'Mappe %2$s slettet';
+
+$lang['maintenance:empty_msg_err'] 		= 'Feil ved tømming av mappe %s';
+$lang['maintenance:remove_msg_err'] 	= 'Feil ved å fjerne mappen %s';
+
+$lang['maintenance:export_data']		= 'Eksporter data';
+$lang['maintenance:export_xml']			= 'Eksporter XML';
+$lang['maintenance:export_csv']			= 'Eksporter CSV';
+$lang['maintenance:export_json']		= 'Eksporter JSON';
+$lang['maintenance:table_label']		= 'Tabeller';
+$lang['maintenance:record_label']		= 'Oppføringer';
