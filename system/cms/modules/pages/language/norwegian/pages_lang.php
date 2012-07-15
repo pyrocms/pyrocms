704c653287dd15f494e065df969c257d7f263e54
Mikael <mikaelfoyn@gmail.com>
2012-07-15 19:17:30 +0200

    Translated Pages-module to Norwegian.

---
+++ system/cms/modules/pages/language/norwegian/pages_lang.php
@@ -0,0 +1,69 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['pages:page']							= 'Page';
+$lang['pages:pages']						= 'Pages';
+
+// tabs
+$lang['pages:content_label']				= 'Page Content';
+$lang['pages:meta_label']					= 'Meta data';
+$lang['pages:design_label']					= 'Design';
+$lang['pages:script_label']					= 'Script';
+$lang['pages:options_label']				= 'Options';
+$lang['pages:detail_label']					= 'Details';
+
+// label
+$lang['pages:add_page_chunk']				= 'Add Page Chunk';
+$lang['pages:page_chunk']					= 'Page Chunk';
+$lang['pages:language_label']				= 'Language';
+$lang['pages:updated_label']				= 'Updated';
+$lang['pages:unknown_label']				= 'Unknown';
+$lang['pages:navigation_label']				= 'Add to Navigation';
+$lang['pages:body_label']					= 'Page body';
+$lang['pages:meta_title_label']				= 'Meta title';
+$lang['pages:meta_keywords_label']			= 'Meta keywords';
+$lang['pages:meta_desc_label']				= 'Meta description';
+$lang['pages:layout_id_label']				= 'Page Layout';
+$lang['pages:css_label']					= 'CSS';
+$lang['pages:js_label']						= 'JavaScript';
+$lang['pages:access_label']					= 'Access';
+$lang['pages:rss_enabled_label']			= 'RSS enabled?';
+$lang['pages:comments_enabled_label']		= 'Comments enabled?';
+$lang['pages:is_home_label']				= 'Is default (home) page?';
+$lang['pages:strict_uri_label']				= 'Require an exact uri match?';
+
+$lang['pages:status_label']					= 'Status';
+$lang['pages:draft_label']					= 'Draft';
+$lang['pages:live_label']					= 'Live';
+$lang['pages:revisions_label']				= 'Revisions';
+$lang['pages:compare_label']				= 'Compare';
+$lang['pages:preview_label']				= 'Preview';
+$lang['pages:current_label']				= 'Current';
+$lang['pages:view_label']					= 'View';
+$lang['pages:create_label']					= 'Add child';
+$lang['pages:duplicate_label']				= 'Duplicate';
+
+// title
+$lang['pages:create_title']					= 'Add Page';
+$lang['pages:edit_title']					= 'Edit Page "%s"';
+$lang['pages:list_title']					= 'Pages';
+$lang['pages:layouts_create_title']			= 'Add Page Layout';
+$lang['pages:layouts_list_title']			= 'Layouts';
+
+// messages
+$lang['pages:no_pages']						= 'There are no pages.';
+$lang['pages_create_success']				= 'The page was created.';
+$lang['pages_create_error']					= 'That page has not been created.';
+$lang['pages_page_not_found_error']			= 'That page does not exist.';
+$lang['pages_edit_success']					= 'The page "%s" was saved.';
+$lang['pages_delete_home_error']			= 'You can not delete the home page!';
+$lang['pages_delete_success']				= 'Page #%s has been deleted.';
+$lang['pages_mass_delete_success']			= '%s pages have been deleted.';
+$lang['pages_delete_none_notice']			= 'No pages were deleted.';
+$lang['pages_page_already_exist_error']		= 'A page with the URL "%s" already exists in %s.';
+$lang['pages_parent_not_exist_error']		= 'The parent page you have selected does not exist.';
+$lang['pages_chunk_slug_length']			= 'Page Chunk slugs may be no more than 30 characters in length.';
+$lang['pages_root_folder']                	= 'the top level';
+
+$lang['pages:tree_explanation_title']	    = 'Explanation';
+$lang['pages:tree_explanation']				= 'The list on the left represents pages on your website. Click the page item to show its children. You may drag pages to order them; drag horizontally to make it a child. When you click the page text you will see all sorts of handy information in this box.';
+$lang['pages:rss_explanation']				= 'Enabling RSS for this page will give users a chance to subscribe to an RSS feed which shows each immediate page child as a new entry.';
