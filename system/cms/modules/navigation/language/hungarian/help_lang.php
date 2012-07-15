bb2073e15a55724d6df9a9f02e3a9cb7e4027e34
Walkman <walkman@walkman.pk>
2012-07-15 12:12:09 +0200

    Full hungarian fixes and translation. I translated every help_lang files too. Spaces insted of tabs in changed files. I can't translate certain pyrostream phrases properly until I see them in context (I don't have it yet).

--- system/cms/modules/navigation/language/hungarian/help_lang.php
+++ system/cms/modules/navigation/language/hungarian/help_lang.php
@@ -3,27 +3,31 @@
 // inline help html. Only 'help_body' is used.
 $lang['help_body'] = "
 <h4>Leírás</h4>
-<p>The Navigation module controls your main navigation area as well as other link groups.</p>
+<p>A Navigáció modul vezérli a fő navigációs részt, ugyanúgy mint más link csoportokat.</p>
+
 
 <h4>Navigációs csoportok</h4>
-<p>Navigation links are displayed according to the group that they are in. In most themes the Header group is the main navigation. 
-Check your theme's documentation to find out which navigation groups are supported in the theme files. 
-If you want to display a group within site content just use this tag: {{ navigation:links group=\"your-group-name\" }}</p>
+<p>A navigációs linkek a csoportjuk szerint lesznek megjelenítve. A legtöbb oldalstílusban a \"Header\" a fő navigációs csoport.
+ Nézd meg az oldalstílusod dokumentációját hogy megtudd melyik navigációs csoportok támogatottak a stílus fájljaiban.
+Ha meg akarsz jeleníteni egy csoportot egy oldal tartalmában, használd a következő tag-et: {{ navigation:links group=\"linkcsoportod-neve\" }}</p>
 
 <h4>Linkek hozzáadása</h4>
-<p>Choose a title for your link, then select the group that you wish for it to display in.
-Link types are as follows:
+<p>Válassz egy címet a linkednek, azután válaszd ki a csoportot amelyikben meg szeretnéd jeleníteni.
+A link típusok a következők:
 <ul>
-<li>URL: an external link - http://google.com</li>
-<li>Site Link: a link within your site - galleries/portfolio-pictures</li>
-<li>Module: takes a visitor to the index page of a module</li>
-<li>Page: link to a page</li>
+<li>URL: egy külső link - http://google.com</li>
+<li>Oldal Link: egy link az oldalon belül - galeria/portfolio-kepek</li>
+<li>Modul: a látogatót a modul index oldalára viszi</li>
+<li>Oldal: link egy oldalra</li>
 </ul>
-Target specifies if this link should open in a new browser window or tab. 
-(Tip: use New Window sparingly to avoid annoying your site visitors.)
-The Class field allows you to add a css class to a single link.</p>
+A cél megadja hogy a link új ablabkan vagy böngészőlapon nyíljon meg.
+(Tipp: használd az új oldalakat mérséklettel, hogy elkerüld a látogatók bosszantását.)
+Az Osztály mezővel egy CSS osztályt adhatsz egyetlen hivatkozáshoz.</p>
+
 <p></p>
 
 <h4>Navigációs linkek elrendezése</h4>
-<p>The order of your links in the admin panel are reflected on the website front-end. 
-To change the order that they appear simply drag and drop them until they are in the order that you like.</p>"; #translate
+<p>A linkjeid sorrendje az admin panelen ugyanolyan mint a weboldal front-endjén.
+A sorrend változtatásához egyszerűen fogd és húzd őket míg nincsenek olyan sorrendben ahogyan szeretnéd.</p>";
+
+/* End of file help_lang.php */
