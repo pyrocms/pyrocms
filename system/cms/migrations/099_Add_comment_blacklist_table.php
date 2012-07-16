b802b3a8bc2b0850c3e9f3535e3446a7dd77615e
Matt Frost <mfrost.design@gmail>
2012-07-15 19:23:02 -0500

    incrementing migration to 99 and creating table with dbforge instead of raw query

--- system/cms/migrations/099_Add_comment_blacklist_table.php
+++ system/cms/migrations/099_Add_comment_blacklist_table.php
@@ -2,21 +2,27 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Migration_Add_comment_blacklist_table extends CI_Migration {
-    
-    public function up()
-    {
-            $table_name = $this->db->dbprefix('comment_blacklists');
-            $this->db->query("
-                CREATE TABLE $table_name (
-                id int(11) NOT NULL primary key auto_increment,
-                website varchar(255) NULL,
-                email varchar(150) NULL)
-            ");
-    }
+	
+	public function up()
+	{
+		$this->dbforge->add_field('id');
+		$fields = array(
+			'website' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 255
+				),
+			'email' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 150
+				),
+			);
+		$this->dbforge->add_field($fields);
+		$this->dbforge->create_table('comment_blacklists');
+	}
 
-     public function down()
-     {
-         $table_name = $this->db->dbprefix('comment_blacklist');
-         $this->db->query("DROP TABLE $table_name");
-     }
+	public function down()
+	{
+		$this->dbforge->drop_table('comment_blacklists');
+	}
+
 }
--- system/cms/modules/comments/models/comment_blacklists_m.php
+++ system/cms/modules/comments/models/comment_blacklists_m.php
@@ -8,39 +8,39 @@
 */
 
 class Comment_blacklists_m extends MY_Model {
-    
-    private $data;
+	
+	private $data;
    
-    public function __construct()
-    {
-        $this->_table = $this->db->dbprefix('comment_blacklists');
-    }
+	public function __construct()
+	{
+		$this->_table = $this->db->dbprefix('comment_blacklists');
+	}
 
-    
-    public function insert($data)
-    {
-        if ($this->_get_count($data)->count < 1)
-        {
-            return parent::insert($data);
-        }
-    }
-    
-    private function _get_count($data)
-    {
-        foreach($data as $k => $v)
-        {
-            $where .= "`$k`='$v' OR "; 
-        }
-        $this->db->select('count(*) as count')->from('comment_blacklists')->where(substr($where,0,-4)); 
-        return $this->db->get()->row();
-    }
+	
+	public function insert($data)
+	{
+		if ($this->_get_count($data)->count < 1)
+		{
+			return parent::insert($data);
+		}
+	}
+	
+	private function _get_count($data)
+	{
+		foreach($data as $k => $v)
+		{
+			$where .= "`$k`='$v' OR "; 
+		}
+		$this->db->select('count(*) as count')->from('comment_blacklists')->where(substr($where,0,-4)); 
+		return $this->db->get()->row();
+	}
 
-    public function is_blacklisted($data)
-    {
-        if ($this->_get_count($data)->count > 0)
-        {
-            return true;
-        }
-        return false;
-    }
+	public function is_blacklisted($data)
+	{
+		if ($this->_get_count($data)->count > 0)
+		{
+			return true;
+		}
+		return false;
+	}
 }
