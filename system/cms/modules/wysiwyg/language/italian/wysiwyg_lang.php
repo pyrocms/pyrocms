f2f0afd14d9125f91c75f24dd4c346b369b80d52
Christian Giupponi <christian.giupponi@stilogo.it>
2012-07-27 18:55:29 +0200

    Some italian update
    Some language update

---
+++ system/cms/modules/wysiwyg/language/italian/wysiwyg_lang.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['wysiwyg.meta.width'] = 'Larghezza';
+$lang['wysiwyg.meta.height'] = 'Altezza';
+$lang['wysiwyg.meta.size'] = 'Dimensione';
+$lang['wysiwyg.label.insert_width'] = 'Inserisci larghezza immagine:';
+$lang['wysiwyg.label.no_limit'] = 'Dimensione intera';
+$lang['wysiwyg.label.float'] = 'Float';
+$lang['wysiwyg.label.left'] = 'Sinistra';
+$lang['wysiwyg.label.right'] = 'Destra';
+$lang['wysiwyg.label.none'] = 'Nulla';
+$lang['wysiwyg.meta.mime'] = 'Mime Type';
