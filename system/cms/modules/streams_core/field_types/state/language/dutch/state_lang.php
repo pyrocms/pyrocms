e7220663ad905848c2449327ab3ab2f9f003e65c
Geoffrey <mgeoffrey@gmail.com>
2012-08-07 20:48:36 +0200

    Add new countries's states
    France & Netherland =)

---
+++ system/cms/modules/streams_core/field_types/state/language/dutch/state_lang.php
@@ -0,0 +1,16 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+  * This is a State Field Types for PyroCMS
+  *
+  * @author		Jaap Jolman, Geoffrey Monté
+  * @package	State Field Types
+  * @copyright	Copyright (c) 2008 - 2013, ODIN-ADDONS
+**/
+
+$lang['streams.state.name'] 			= 'Provincies';
+$lang['streams.state.full']				= 'Volledige naam';
+$lang['streams.state.abbr']				= 'Afgekort';
+$lang['streams.state.state_display']	= 'Toon Provincie';
+
+/* End of file state_lang.php */
+/* Location: ./system/cms/modules/streams_core/field_types/state/language/dutch/state_lang.php */
--- system/cms/modules/streams_core/field_types/state/language/french/state_lang.php
+++ system/cms/modules/streams_core/field_types/state/language/french/state_lang.php
@@ -1,6 +1,21 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+  * This is a State Field Types for PyroCMS
+  *
+  * @author		Jaap Jolman, Geoffrey Monté
+  * @package	State Field Types
+  * @copyright	Copyright (c) 2008 - 2013, ODIN-ADDONS
+**/
 
-$lang['streams.state.name'] 			= "État américain";
+$lang['streams.state.name'] 			= "Département";
 $lang['streams.state.full']				= "Nom complet";
 $lang['streams.state.abbr']				= "Abréviation";
 $lang['streams.state.state_display']	= "Affichage de l'État";
+
+$lang['streams.state.country']			= 'Pays';
+$lang['streams.state.us']				= 'Etats Unis';
+$lang['streams.state.nl']				= 'Pays Bas';
+$lang['streams.state.fr']				= 'France';
+
+/* End of file state_lang.php */
+/* Location: ./system/cms/modules/streams_core/field_types/state/language/french/state_lang.php */
