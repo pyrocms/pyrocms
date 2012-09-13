b07c8cb313328671ec98f9a7658ff41e822e1c2e
Adam Fairholm <adam.fairholm@gmail.com>
2012-09-12 23:57:29 -0400

    Adding DB space for IPv6 in PyroCMS tables.
    Changing from 16 (or 32 in the contact_log case) to 45.
    
    Fixes #1805.

--- system/cms/config/migration.php
+++ system/cms/config/migration.php
@@ -9,7 +9,7 @@
 | and disable it back when you're done.
 |
 */
-$config['migration_enabled'] = TRUE;
+$config['migration_enabled'] = true;
 
 
 /*
@@ -23,7 +23,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 102;
+$config['migration_version'] = 103;
 
 /*
 |--------------------------------------------------------------------------
