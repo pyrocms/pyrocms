0a829462761884bb3a26e410c3a0897850a8782f
Scott Parry <hello@ikreativ.com>
2012-09-26 14:57:40 +0100

    Current module state and changed pipe to dash

--- system/cms/themes/pyrocms/views/admin/partials/header.php
+++ system/cms/themes/pyrocms/views/admin/partials/header.php
@@ -75,7 +75,7 @@
 		<h2><?php echo $module_details['name'] ? anchor('admin/'.$module_details['slug'], $module_details['name']) : lang('global:dashboard'); ?></h2>
 	
 		<small>
-			<?php if ( $this->uri->segment(2) ) { echo '&nbsp; | &nbsp;'; } ?>
+			<?php if ( $this->uri->segment(2) ) { echo '&nbsp; &mdash;&nbsp;'; } ?>
 			<?php echo $module_details['description'] ? $module_details['description'] : ''; ?>
 		</small>
 
