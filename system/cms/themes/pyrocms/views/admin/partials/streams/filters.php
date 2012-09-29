a15142587729c3b658aa97989b9ee0942f624ebb
Ryan Thompson <service@aiwebsystems.com>
2012-09-29 12:13:11 -0500

    These got wiped
    Added filters / tabbed form views back to 2.2

---
+++ system/cms/themes/pyrocms/views/admin/partials/streams/filters.php
@@ -0,0 +1,54 @@
+<?php if ( $filters != null ): ?>
+<fieldset id="filters">
+
+	<legend><?php echo lang('global:filters'); ?></legend>
+
+	<?php echo form_open(); ?>
+
+	<ul>  
+		<?php foreach ( $filters as $k=>$filter ): ?>
+		<li>
+		<?php
+
+			if ( is_array($filter) )
+			{
+
+				// Dropdown type
+				echo '<label>'.$stream_fields->{$k}->field_name.':&nbsp;</label>';
+				echo form_dropdown('f_'.$stream_fields->{$k}->field_slug, $filter, isset($filter_data['filters']['f_'.$stream_fields->{$k}->field_slug]) ? $filter_data['filters']['f_'.$stream_fields->{$k}->field_slug] : null);
+			}
+			else
+			{
+
+				// Switch for the normal ones
+				switch ( $stream_fields->{$filter}->field_type )
+				{
+
+					// Text type fields
+					case 'text':
+					case 'textarea':
+					case 'email':
+					case 'choice';
+					case 'wysiwyg':
+						echo '<label>'.$stream_fields->{$filter}->field_name.':&nbsp;</label>';
+						echo form_input('f_'.$stream_fields->{$filter}->field_slug, isset($filter_data['filters']['f_'.$stream_fields->{$filter}->field_slug]) ? $filter_data['filters']['f_'.$stream_fields->{$filter}->field_slug] : '');
+						break;
+
+					default: break;
+				}
+			}
+
+		?>
+		</li>
+		<?php endforeach; ?>
+
+		<li>
+			<div class="buttons">
+				<?php echo form_submit('filter', lang('buttons.filter'), 'class="button btn"'); ?>
+				<?php echo form_submit('clear_filters', lang('buttons.clear'), 'class="button btn"'); ?>
+			</div>
+		</li>
+	</ul>
+	<?php echo form_close(); ?>
+</fieldset>
+<?php endif; ?>
