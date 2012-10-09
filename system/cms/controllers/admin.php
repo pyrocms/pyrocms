2fb3d2b93e061c6acc2e60721919ef593ff49fb4
Lee Mc Kay <trojan_spikepk@yahoo.co.uk>
2012-10-09 17:56:37 +0100

    Habbit

--- system/cms/controllers/admin.php
+++ system/cms/controllers/admin.php
@@ -38,8 +38,8 @@ class Admin extends Admin_Controller
 			{
 				$this->template
 					->set('messages', array('notice' => lang('cp_delete_installer_message')));
-			} // if(delete_files
-		} // if (is_dir
+			}
+		}
 
 		$this->template
 			->build('admin/dashboard');
