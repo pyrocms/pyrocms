bd871288707a2a0d23d1997f58360a013b12a612
Adam Fairholm <adam.fairholm@gmail.com>
2012-10-20 14:46:58 -0400

    Making default value for text area field type be a text area box.
    Addresses #1815

--- system/cms/modules/streams_core/field_types/textarea/language/french/textarea_lang.php
+++ system/cms/modules/streams_core/field_types/textarea/language/french/textarea_lang.php
@@ -1,3 +1,4 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['streams.textarea.name'] 		= "Zone de texte";
+$lang['streams.textarea.name'] 			= "Zone de texte";
+$lang['streams.textarea.default_text'] 	= 'Default Text'; #translate
