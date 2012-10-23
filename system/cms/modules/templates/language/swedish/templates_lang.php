65e394915998c187a704c3fa6a4c952bb6f5572e
Incore <info@incore.se>
2012-10-23 21:08:59 +0200

    Added swedish

--- system/cms/modules/templates/language/swedish/templates_lang.php
+++ system/cms/modules/templates/language/swedish/templates_lang.php
@@ -1,32 +1,39 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-// Labels
-$lang['templates.language_label']			= 'Språk';
-$lang['templates.choose_lang_label']		= 'Välj språk';
-$lang['templates.subject_label']			= 'Ämne';
-$lang['templates.body_label']				= 'Meddelande';
-$lang['templates.slug_label']				= 'Slugg';
+ /**
+ * Swedish translation.
+ *
+ * @author		marcus@incore.se
+ * @package		PyroCMS  
+ * @link		http://pyrocms.com
+ * @date		2012-10-22
+ * @version		1.1.0
+ */
 
-// Titles
-$lang['templates.create_title']				= 'Lägg till mall';
-$lang['templates.edit_title']				= 'Redigera mall "%s"';
-$lang['templates.clone_title']				= 'Kopiera mall "%s"';
-$lang['templates.list_title']				= 'Mallar';
-$lang['templates.default_title']			= 'Standardmallar';
-$lang['templates.user_defined_title']		= 'Användardefinierade mallar';
+$lang['templates.language_label'] = 'Språk';
+$lang['templates.choose_lang_label'] = 'Välj språk';
+$lang['templates.subject_label'] = 'Ämne';
+$lang['templates.body_label'] = 'Meddelande';
+$lang['templates.slug_label'] = 'Slugg';
+$lang['templates.create_title'] = 'Skapa mall';
+$lang['templates.edit_title'] = 'Redigera mall "%s"';
+$lang['templates.clone_title'] = 'Kopiera mall "%s"';
+$lang['templates.list_title'] = 'Mallar';
+$lang['templates.default_title'] = 'Standardmallar';
+$lang['templates.user_defined_title'] = 'Användardefinierade mallar';
+$lang['templates.tmpl_create_success'] = 'E-postmallen "%s" är sparad.';
+$lang['templates.tmpl_create_error'] = 'E-postmallen "%s" kunde inte sparas.';
+$lang['templates.tmpl_edit_success'] = 'Ändringar av E-postmallen "%s" är sparad.';
+$lang['templates.tmpl_edit_error'] = 'Ändringar av E-postmallen "%s" kunde inte sparas.';
+$lang['templates.tmpl_clone_success'] = '"%s" klonades. Du kan nu redigera E-postmallen.';
+$lang['templates.tmpl_clone_error'] = '"%s" kunde inte klonas. Försök igen.';
+$lang['templates.single_delete_success'] = 'E-postmallen är raderad.';
+$lang['templates.mass_delete_success'] = '%s E-postmallar av %s har raderats.';
+$lang['templates.mass_delete_error'] = 'Ett fel inträffade när du försökte radera "%s".';
+$lang['templates.default_delete_error'] = 'Ett fel inträffade, standardmallen kan inte raderas.';
+$lang['templates.no_select_error'] = 'Du måste välja en E-postmall först.';
+$lang['templates.already_exist_error'] = 'En E-postmall med namnet "%s" finns redan.';
 
-// Messages
-$lang['templates.tmpl_create_success']		= 'E-postmallen "%s" är sparad.';
-$lang['templates.tmpl_create_error']		= 'E-postmallen "%s" kunde inte sparas.';
-$lang['templates.tmpl_edit_success']		= 'Ändringar av E-postmallen "%s" är sparad.';
-$lang['templates.tmpl_edit_error']			= 'Ändringar av E-postmallen "%s" kunde inte sparas.';
-$lang['templates.tmpl_clone_success']		= '"%s" klonades. Du kan nu redigera E-postmallen.';
-$lang['templates.tmpl_clone_error']			= '"%s" kunde inte klonas. Försök igen.';
-$lang['templates.single_delete_success']	= 'E-postmallen är raderad.';
-$lang['templates.mass_delete_success']		= '%s E-postmallar av %s har raderats.';
-$lang['templates.mass_delete_error'] 		= 'Ett fel inträffade när du försökte radera "%s".';
-$lang['templates.default_delete_error'] 	= 'Ett fel inträffade, standardmallen kan inte raderas.';
-$lang['templates.no_select_error'] 			= 'Du måste välja en E-postmall först.';
-$lang['templates.already_exist_error']		= 'En E-postmall med namnet "%s" finns redan.';
 
-/* End of file templates_lang.php */
+/* End of file templates_lang.php */  
+/* Location: system/cms/modules/templates/language/swedish/templates_lang.php */  
