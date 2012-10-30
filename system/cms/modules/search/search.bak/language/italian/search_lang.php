a6185c4e8eb36a3ef6705ca3fd6064775b0e71f4
Kefeng Deng <dkf_genius@hotmail.com>
2012-10-30 15:19:16 +1300

    removed search.bak

--- system/cms/modules/search/search.bak/language/italian/search_lang.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-// titles
-$lang['search:index']            = 'Cerca';
-$lang['search:results']          = 'Risultati ricerca';
-
-// messages
-$lang['search:no_results']            = 'Non sono stati trovati risultati con i termini di ricerca inseriti.';
