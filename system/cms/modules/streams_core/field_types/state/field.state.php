1fe62245582e17ecfe678fd16f4a4b293b1bdc86
geoffrey <mgeoffrey@gmail.com>
2012-11-17 19:51:32 +0100

    Add States  from different countries

--- system/cms/modules/streams_core/field_types/state/field.state.php
+++ system/cms/modules/streams_core/field_types/state/field.state.php
@@ -1,10 +1,14 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+﻿<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
- * PyroStreams US State Field Type
+ * PyroStreams States Field Type
  *
  * @package		PyroCMS\Core\Modules\Streams Core\Field Types
  * @author		Adam Fairholm
+ * @license		http://parse19.com/pyrostreams/docs/license
+ * @license		http://parse19.com/pyrostreams/license
+ * @link		http://parse19.com/pyrostreams
+ * @custom		Geoffrey Monte, Jaap Jolmann
  */
 class Field_state
 {
@@ -16,68 +20,191 @@ class Field_state
 
 	public $author					= array('name' => 'Adam Fairholm', 'url' => 'http://adamfairholm.com');
 	
-	public $custom_parameters		= array('state_display', 'default_state');
+	public $custom_parameters		= array('state_display','country');
 
 	// --------------------------------------------------------------------------
 
 	/**
-	 * Our glorious 50 states!
 	 *
 	 * @access 	public
 	 * @var 	array
 	 */
 	public $raw_states = array(
-		'AL' => 'Alabama',  
-		'AK'=> 'Alaska',  
-		'AZ'=> 'Arizona',  
-		'AR'=> 'Arkansas',
-		'CA'=> 'California',  
-		'CO'=> 'Colorado',  
-		'CT'=> 'Connecticut',  
-		'DE'=> 'Deleware',
-		'DC'=> 'District of Columbia',
-		'FL'=> 'Florida',
-		'GA'=> 'Georgia',
-		'HI'=> 'Hawaii',
-		'ID'=> 'Idaho',
-		'IL'=> 'Illinois',
-		'IN'=> 'Indiana',  
-		'IA'=> 'Iowa',
-		'KS'=> 'Kansas', 
-		'KY'=> 'Kentucky',
-		'LA'=> 'Louisiana',
-		'ME'=> 'Maine',
-		'MD'=> 'Maryland',
-		'MA'=> 'Massachusetts',
-		'MI'=> 'Michigan',  
-		'MN'=> 'Minnesota',
-		'MS'=> 'Mississippi',
-		'MO'=> 'Missouri',
-		'MT'=> 'Montana',
-		'NE'=> 'Nebraska',
-		'NV'=> 'Nevada',
-		'NH'=> 'New Hampshire',
-		'NJ'=> 'New Jersey',
-		'NM'=> 'New Mexico',
-		'NY'=> 'New York',
-		'NC'=> 'North Carolina',
-		'ND'=> 'North Dakota',
-		'OH'=> 'Ohio',
-		'OK'=> 'Oklahoma', 
-		'OR'=> 'Oregon',
-		'PA'=> 'Pennsylvania',
-		'RI'=> 'Rhode Island',  
-		'SC'=> 'South Carolina',
-		'SD'=> 'South Dakota',
-		'TN'=> 'Tennessee',
-		'TX'=> 'Texas',
-		'UT'=> 'Utah',
-		'VT'=> 'Vermont', 
-		'VA'=> 'Virginia',
-		'WA'=> 'Washington',
-		'WV'=> 'West Virginia',
-		'WI'=> 'Wisconsin',
-		'WY'=> 'Wyoming'
+		'US' => array(
+			'AL'	=> 'Alabama',
+			'AK'	=> 'Alaska',
+			'AZ'	=> 'Arizona',
+			'AR'	=> 'Arkansas',
+			'CA'	=> 'California',
+			'CO'	=> 'Colorado',
+			'CT'	=> 'Connecticut',
+			'DE'	=> 'Deleware',
+			'DC'	=> 'District of Columbia',
+			'FL'	=> 'Florida',
+			'GA'	=> 'Georgia',
+			'HI'	=> 'Hawaii',
+			'ID'	=> 'Idaho',
+			'IL'	=> 'Illinois',
+			'IN'	=> 'Indiana',
+			'IA'	=> 'Iowa',
+			'KS'	=> 'Kansas',
+			'KY'	=> 'Kentucky',
+			'LA'	=> 'Louisiana',
+			'ME'	=> 'Maine',
+			'MD'	=> 'Maryland',
+			'MA'	=> 'Massachusetts',
+			'MI'	=> 'Michigan',
+			'MN'	=> 'Minnesota',
+			'MS'	=> 'Mississippi',
+			'MO'	=> 'Missouri',
+			'MT'	=> 'Montana',
+			'NE'	=> 'Nebraska',
+			'NV'	=> 'Nevada',
+			'NH'	=> 'New Hampshire',
+			'NJ'	=> 'New Jersey',
+			'NM'	=> 'New Mexico',
+			'NY'	=> 'New York',
+			'NC'	=> 'North Carolina',
+			'ND'	=> 'North Dakota',
+			'OH'	=> 'Ohio',
+			'OK'	=> 'Oklahoma',
+			'OR'	=> 'Oregon',
+			'PA'	=> 'Pennsylvania',
+			'RI'	=> 'Rhode Island',
+			'SC'	=> 'South Carolina',
+			'SD'	=> 'South Dakota',
+			'TN'	=> 'Tennessee',
+			'TX'	=> 'Texas',
+			'UT'	=> 'Utah',
+			'VT'	=> 'Vermont',
+			'VA'	=> 'Virginia',
+			'WA'	=> 'Washington',
+			'WV'	=> 'West Virginia',
+			'WI'	=> 'Wisconsin',
+			'WY'	=> 'Wyoming'
+		),
+		'NL' => array(
+			'GR'	=> 'Groningen',
+			'FR'	=> 'Friesland',
+			'DR'	=> 'Drenthe',
+			'OV'	=> 'Overijsel',
+			'FL'	=> 'Flevoland',
+			'GE'	=> 'Gelderland',
+			'UT'	=> 'Utrecht',
+			'NH'	=> 'Noord-Holland',
+			'ZH'	=> 'Zuid-Holland',
+			'ZL'	=> 'Zeeland',
+			'BB'	=> 'Brabant',
+			'LM'	=> 'Limburg'
+		),
+		'FR' => array(
+			'01'	=> 'Ain',
+			'02'	=> 'Aisne',
+			'03'	=> 'Allier',
+			'04'	=> 'Alpes de Haute Provence',
+			'05'	=> 'Alpes Hautes',
+			'06'	=> 'Alpes Maritimes',
+			'07'	=> 'Ardèche',
+			'08'	=> 'Ardennes',
+			'09'	=> 'Ariège',
+			'10'	=> 'Aube',
+			'11'	=> 'Aude',
+			'12'	=> 'Aveyron',
+			'13'	=> 'Bouches du Rhône',
+			'14'	=> 'Calvados',
+			'15'	=> 'Cantal',
+			'16'	=> 'Charente',
+			'17'	=> 'Charente Maritime',
+			'18'	=> 'Cher',
+			'19'	=> 'Corrèze',
+			'2A'	=> 'Corse du sud',
+			'2B'	=> 'Corse Haute',
+			'21'	=> 'Côte d\'or',
+			'22'	=> 'Cotes d\'Armor',
+			'23'	=> 'Creuse',
+			'24'	=> 'Dordogne',
+			'25'	=> 'Doubs',
+			'26'	=> 'Drôme',
+			'27'	=> 'Eure',
+			'28'	=> 'Eure et Loir',
+			'29'	=> 'Finistère',
+			'30'	=> 'Gard',
+			'31'	=> 'Garonne',
+			'32'	=> 'Gers',
+			'33'	=> 'Gironde',
+			'34'	=> 'Hérault',
+			'35'	=> 'Ille et Vilaine',
+			'36'	=> 'Indre',
+			'37'	=> 'Indre et Loire',
+			'38'	=> 'Isère',
+			'39'	=> 'Jura',
+			'40'	=> 'Landes',
+			'41'	=> 'Loir et Cher',
+			'42'	=> 'Loire',
+			'43'	=> 'Loire',
+			'44'	=> 'Loire Atlantique',
+			'45'	=> 'Loiret',
+			'46'	=> 'Lot',
+			'47'	=> 'Lot et Garonne',
+			'48'	=> 'Lozère',
+			'49'	=> 'Maine et Loire',
+			'50'	=> 'Manche',
+			'51'	=> 'Marne',
+			'52'	=> 'Marne Haute : Chaumont',
+			'53'	=> 'Mayenne',
+			'54'	=> 'Meurthe et Moselle',
+			'55'	=> 'Meuse',
+			'56'	=> 'Morbihan',
+			'57'	=> 'Moselle : Metz',
+			'58'	=> 'Nièvre',
+			'59'	=> 'Nord',
+			'60'	=> 'Oise',
+			'61'	=> 'Orne',
+			'62'	=> 'Pas de Calais',
+			'63'	=> 'Puy de dôme',
+			'64'	=> 'Pyrénées-Atlantiques',
+			'65'	=> 'Pyrénées',
+			'66'	=> 'Pyrénées Orientales',
+			'67'	=> 'Rhin bas',
+			'68'	=> 'Rhin Haut',
+			'69'	=> 'Rhône',
+			'70'	=> 'Saône haute',
+			'71'	=> 'Saône et loire',
+			'72'	=> 'Sarthe',
+			'73'	=> 'Savoie',
+			'74'	=> 'Savoie haute',
+			'75'	=> 'Paris',
+			'76'	=> 'Seine Maritime',
+			'77'	=> 'Seine et Marne',
+			'78'	=> 'Yvelines',
+			'79'	=> 'Sèvres deux',
+			'80'	=> 'Somme',
+			'81'	=> 'Tarn',
+			'82'	=> 'Tarn et Garonne',
+			'83'	=> 'Var',
+			'84'	=> 'Vaucluse',
+			'85'	=> 'Vendée',
+			'86'	=> 'Vienne',
+			'87'	=> 'Vienne Haute',
+			'88'	=> 'Vosges',
+			'89'	=> 'Yonne',
+			'90'	=> 'Territoire de Belfort',
+			'91'	=> 'Essonne',
+			'92'	=> 'Hauts de seine',
+			'93'	=> 'Seine st Denis',
+			'94'	=> 'Val de Marne',
+			'95'	=> 'Val d\'Oise',
+			'971'	=> 'Guadeloupe',
+			'972'	=> 'Martinique',
+			'973'	=> 'Guyane',
+			'974'	=> 'Réunion',
+			'975'	=> 'St Pierre et Miquelon',
+			'985'	=> 'Nouvelle Calédonie',
+			'986'	=> 'Wallis et Futuna',
+			'987'	=> 'Polynésie Française',
+			'976'	=> 'Mayotte : Mamoutzou',
+			'984'	=> 'Terres australes et Antartiques Françaises'
+		)
 	);
 
 	// --------------------------------------------------------------------------
@@ -92,24 +219,20 @@ class Field_state
 	public function form_output($data, $entry_id, $field)
 	{
 		// Default is abbr for backwards compat.
-		if ( ! isset($data['custom']['state_display']))
-		{
+		if( ! isset($data['custom']['state_display']) ):
+		
 			$data['custom']['state_display'] = 'abbr';
-		}
-
-		// Value
-		// We only use the default value if this is a new
-		// entry.
-		if ( ! $data['value'] and ! $entry_id)
-		{
-			$value = (isset($field->field_data['default_state'])) ? $field->field_data['default_state'] : null;
-		}
-		else
-		{
-			$value = $data['value'];
-		}
 	
-		return form_dropdown($data['form_slug'], $this->states($field->is_required, $data['custom']['state_display']), $value, 'id="'.$data['form_slug'].'"');
+		endif;
+		
+		if( ! isset($data['custom']['country']) ): //Default : US
+		
+			$data['custom']['country'] = 'US';
+	
+		endif;
+	
+	
+		return form_dropdown($data['form_slug'], $this->states($field->is_required, $data['custom']['state_display'], $data['custom']['country']), $data['value'], 'id="'.$data['form_slug'].'"');
 	}
 
 	// --------------------------------------------------------------------------
@@ -129,10 +252,17 @@ class Field_state
 	public function pre_output_plugin($input, $data)
 	{
 		if ( ! $input) return null;
-
+		$full= '';
+		if(isset($this->raw_states['US'][$input])) 
+			{ $full = $this->raw_states['US'][$input]; }
+		elseif(isset($this->raw_states['NL'][$input])) 
+			{ $full = $this->raw_states['NL'][$input]; }
+		elseif(isset($this->raw_states['FR'][$input])) 
+			{ $full = $this->raw_states['FR'][$input]; }
+		
 		return array(
 			'abbr'	=> $input,
-			'full' 	=> $this->raw_states[$input]
+			'full' 	=> $full
 		);
 	}
 
@@ -153,8 +283,13 @@ class Field_state
 			$data['state_display'] = 'abbr';
 	
 		endif;
-
-		$states = $this->states('yes', $data['state_display']);
+		// Default is US.
+		if( ! isset($data['country']) ):
+		
+			$data['country'] = 'US';
+	
+		endif;		
+		$states = $this->states('yes', $data['state_display'], $data['country']);
 	
 		return ( isset($states[$input]) ) ? $states[$input] : null;
 	}
@@ -178,22 +313,28 @@ class Field_state
 	}
 
 	// --------------------------------------------------------------------------
+	
+	// --------------------------------------------------------------------------
 
 	/**
-	 * Default Country Parameter
+	 * Select the country you want. if nothing is defined, it will be US by default
 	 *
-	 * @access 	public
-	 * @return 	string
-	 */
-	public function param_default_state($input)
-	{
-		// Return a drop down of countries
-		// but we don't require them to give one.
-		return form_dropdown('default_state', $this->states('no', 'full'), $input);
+	 * @access	public
+	 * @return	string
+	 */	
+	public function param_country($value = null)
+	{	
+		$options = array(
+			'US' => $this->CI->lang->line('streams.state.us'),
+			'NL' => $this->CI->lang->line('streams.state.nl'),
+			'FR' => $this->CI->lang->line('streams.state.fr')
+		);
+	
+		return form_dropdown('country', $options, $value);
 	}
 
 	// --------------------------------------------------------------------------
-	
+		
 	/**
 	 * State
 	 *
@@ -202,19 +343,16 @@ class Field_state
 	 * @access	private
 	 * @return	array
 	 */
-	private function states($is_required, $state_display = 'abbr')
+	private function states($is_required, $state_display = 'abbr', $country = 'US')
 	{	
 		if( $state_display != 'abbr' and $state_display != 'full') $state_display = 'abbr';
 	
-		$choices = array();
-	
-		if($is_required == 'no') $choices[null] = get_instance()->config->item('dropdown_choose_null');
-	
+
 		$states = array();
 		
 		if($state_display == 'abbr'):
 		
-			foreach($this->raw_states as $abbr => $full):
+			foreach($this->raw_states[$country] as $abbr => $full):
 			
 				$states[$abbr] = $abbr;
 			
@@ -222,11 +360,14 @@ class Field_state
 			
 		else:
 		
-			$states = $this->raw_states;
+			$states = $this->raw_states[$country];
 		
 		endif; 
-		
-		return array_merge($choices, $states);
+		//Array Merge sucks with numeric value
+		 if($is_required == 'no') $states[null]=get_instance()->config->item('dropdown_choose_null');
+		return $states;
 	}
-	
 }
+
+/* End of file field.state.php */
+/* Location: ./system/cms/modules/streams_core/field_types/state/field.state.php */
