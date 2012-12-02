36e8c5e75ddda925e151d250f311dd290714db03
geoffrey <mgeoffrey@gmail.com>
2012-12-02 23:16:17 +0100

    add NL & FR states

--- system/cms/modules/streams_core/field_types/state/language/french/state_lang.php
+++ system/cms/modules/streams_core/field_types/state/language/french/state_lang.php
@@ -1,7 +1,11 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['streams.state.name'] 			= "État américain";
+$lang['streams.state.name'] 			= "Département";
 $lang['streams.state.full']				= "Nom complet";
 $lang['streams.state.abbr']				= "Abréviation";
 $lang['streams.state.state_display']	= "Affichage de l'État";
+$lang['streams.state.country']			= 'Pays';
+$lang['streams.state.us']				= 'United States';
+$lang['streams.state.nl']				= 'Netherland';
+$lang['streams.state.fr']				= 'France';
 $lang['streams.state.default_state']	= 'Default State'; #translate
--- system/cms/modules/streams_core/field_types/state/language/swedish/state_lang.php
+++ system/cms/modules/streams_core/field_types/state/language/swedish/state_lang.php
@@ -14,7 +14,10 @@ $lang['streams.state.name'] = 'US Delstat';
 $lang['streams.state.full'] = 'Namn';
 $lang['streams.state.abbr'] = 'Förkortning';
 $lang['streams.state.state_display'] = 'Visa delstat';
+$lang['streams.state.country']			= 'Country';
+$lang['streams.state.us']				= 'United States';
+$lang['streams.state.nl']				= 'Netherland';
+$lang['streams.state.fr']				= 'France';
 $lang['streams.state.default_state']	= 'Default State'; #translate
-
 /* End of file state_lang.php */  
 /* Location: system/cms/modules/streams_core/field_types/state/language/swedish/state_lang.php */
