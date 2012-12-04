4f2b635626f8528414814498c37fb806b5633c9c
geoffrey <mgeoffrey@gmail.com>
2012-12-04 17:20:49 +0100

    DELETING BOM

--- system/cms/modules/streams_core/libraries/Fields.php
+++ system/cms/modules/streams_core/libraries/Fields.php
@@ -1,4 +1,4 @@
-﻿<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * PyroStreams Core Fields Library
