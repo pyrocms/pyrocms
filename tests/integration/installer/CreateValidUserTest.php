be02c5513a704b5416c43a5d7dfbe450d722c0b7
Matt Frost <mfrost.design@gmail.com>
2012-12-09 13:30:56 -0600

    ValidUserTest

---
+++ tests/integration/installer/CreateValidUserTest.php
@@ -0,0 +1,53 @@
+<?php
+
+require dirname(dirname(__FILE__)) . '/goutte.phar';
+use Goutte\Client;
+
+class CreateValidUserTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->client = new Client();
+    }
+
+    public function tearDown()
+    {
+        unset($this->client);
+    }
+
+    public function navigateToStepFour()
+    {
+        $crawler = $this->client->request('http://localhost/installer');
+        $link = $crawler->selectLink('Step #1')->link();
+        $crawler = $this->client->click($link);
+        $form = $crawler->selectButton('Step #2')->form();
+        $formFields = array(
+            'username'=>'pyro',
+            'password'=>'pyro',
+            'database'=>'pyrocms',
+            'create_db'=>'true',
+        );
+        $crawler = $this->client->submit($form,$formFields);
+        return $crawler;
+    }
+    /**
+     * @test
+     * Given that the database settings are successful
+     * When the user form is filled out
+     * Then the user should be created and user taken to confirmation page
+     */
+    public function CreateAdminUserInInstaller()
+    {
+        $crawler = $this->navigateToStepFour();
+        $form = $crawler->selectButton('Install')->form();
+        $formFields = array(
+            'user_name'=>'admin',
+            'user_firstname'=> 'Pyro',
+            'user_lastname' => 'Admin',
+            'user_email' => 'admin@admin.com',
+            'user_password' => 'administrator'
+        );
+        $crawler = $this->client->submit($form,$formFields);
+        $this->assertContains('')
+    }
+}
