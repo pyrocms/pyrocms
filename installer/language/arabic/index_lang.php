d06ee4436928b4d27f2939b7c091a6209b9292c1
Kasparas Galdikas <foreachas@gmail.com>
2012-12-14 22:36:59 +0000

    Revert "Revert "Hmm, switched from 2.1 branch to 2.2, therefore it got rid of all of""
    This reverts commit beba24a8f3f440b2291ac6e42c4e74ee006b0b15.

--- installer/language/arabic/index_lang.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
-
-$lang['header']		=	'أهلاً بك';
-$lang['thankyou']	= 	'نشكرك على اختيارك PyroCMS!';
-$lang['text']		=	'تثبيت PyroCMS سهل للغاية، فقط اتبع التعليمات على الشاشة. إن واجهت أية مشاكل في عملية التثبيت فلا تقلق حيث أن برنامج التثبيت سيرشدك إلى ما يجب عمله.';
-$lang['step1'] 		= 'الخطوة الأولى';
-$lang['link']		= 'تقدم إلى الخطوة الأولى';
--- installer/language/danish/index_lang.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
-
-$lang['header']		=	'Velkommen';
-$lang['thankyou']	= 	'Tak fordi du valgte PyroCMS!';
-$lang['text']		=	'Det er meget nemt at installere PyroCMS, følg blot trinene og meddelelserne på skærmen. Bliv ikke bekymret i tilfælde af at du har problemer med at installere system, guiden vil forklare hvad du skal gøre.';
-$lang['step1'] 		= 'Trin 1';
-$lang['link']		= 'Fortsæt til første trin';
