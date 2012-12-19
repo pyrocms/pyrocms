ad186a6bb72b255804d50ba935c98eb3718a2ddf
Ryan Thompson <service@aiwebsystems.com>
2012-12-19 13:28:01 -0600

    Typo

--- system/cms/modules/files/controllers/admin.php
+++ system/cms/modules/files/controllers/admin.php
@@ -283,7 +283,7 @@ class Admin extends Admin_Controller {
 
 		$description 	= $this->input->post('description');
 		$keywords_hash	= $this->keywords->process($this->input->post('keywords'), $this->input->post('old_hash'));
-		$alt_attribute	= $this->input->post('alt_atttibute');
+		$alt_attribute	= $this->input->post('alt_attribute');
 
 		if ($id = $this->input->post('file_id'))
 		{
