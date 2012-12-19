cd1a732b453af23f023d90435fe8de0028ca36f0
Adam Fairholm <adam.fairholm@gmail.com>
2012-12-19 13:46:33 -0500

    Fixing some odd english language grammar errors in the installer.

--- installer/language/english/complete_lang.php
+++ installer/language/english/complete_lang.php
@@ -2,11 +2,11 @@
 
 // labels
 $lang['congrats']			= 'Congratulations';
-$lang['intro_text']			= 'PyroCMS is now installed and ready to go! Please log into the admin panel with the following details.';
+$lang['intro_text']			= 'PyroCMS is now installed and ready to go! Please log into the admin panel with the following details:';
 $lang['email']				= 'E-mail';
 $lang['password']			= 'Password';
 $lang['show_password']		= 'Show Password?';
-$lang['outro_text']			= 'Finally, <strong>delete the installer from your server</strong> as if you leave it here it can be used to hack your website.';
+$lang['outro_text']			= 'Finally, <strong>delete the installer from your server</strong>. Leaving it on your server is a secuirity risk.';
 
 $lang['go_website']			= 'Go to Website';
 $lang['go_control_panel']	= 'Go to Control Panel';
