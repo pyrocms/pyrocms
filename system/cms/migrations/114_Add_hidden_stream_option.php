6ae9dc65775caec3181de470f8f46c6c0e10037f
Adam Fairholm <adam.fairholm@gmail.com>
2012-12-20 14:49:43 -0500

    Updating migration 114 to add permissions column to streams.
    This could be added by the streams module, but it's easier here.

--- system/cms/migrations/114_Add_hidden_stream_option.php
+++ system/cms/migrations/114_Add_hidden_stream_option.php
@@ -5,15 +5,31 @@ class Migration_Add_hidden_stream_option extends CI_Migration
 {
     public function up()
     {
-        $columns = array(
-            'is_hidden' => array(
-                        'type' => 'ENUM',
-                        'null' => true,
-                        'constraint' => array('yes', 'no'),
-                        'default' => 'no'
-                    )
-        );
-        $this->dbforge->add_column('data_streams', $columns);
+       if ( ! $this->db->field_exists('is_hidden', 'data_streams'))
+       {
+            $columns = array(
+                'is_hidden' => array(
+                            'type' => 'ENUM',
+                            'null' => true,
+                            'constraint' => array('yes', 'no'),
+                            'default' => 'no'
+                        ),
+
+            );
+            $this->dbforge->add_column('data_streams', $columns);
+        }
+
+       if ( ! $this->db->field_exists('permissions', 'data_streams'))
+       {
+            $columns = array(
+                'permissions' => array(
+                            'type' => 'TEXT',
+                            'null' => true
+                        ),
+
+            );
+            $this->dbforge->add_column('data_streams', $columns);
+        }
     }
 
     public function down()
