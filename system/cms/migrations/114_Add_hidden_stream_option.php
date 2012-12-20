cc1c8ad25c368c182d4d54692ccc8cb907923abd
Adam Fairholm <adam.fairholm@gmail.com>
2012-12-20 14:53:37 -0500

    Adding missing group fill to streams permissions migration.

--- system/cms/migrations/114_Add_hidden_stream_option.php
+++ system/cms/migrations/114_Add_hidden_stream_option.php
@@ -29,6 +29,23 @@ class Migration_Add_hidden_stream_option extends CI_Migration
 
             );
             $this->dbforge->add_column('data_streams', $columns);
+ 
+            // Since this is a new feature, we are going to give accesss
+            // to all use groups first so people all the sudden
+            // don't see any streams.
+            $groups = $this->db
+                            ->select('*, groups.id as group_id')
+                            ->from('groups, permissions')
+                            ->where('groups.id', 'permissions.group_id')
+                            ->where('permissions.module', 'streams')
+                            ->where('groups.name !=', 'admin')->get()->result();
+
+            $groups_arr = array();
+            foreach ($groups as $g)
+            {
+                $groups_arr[] = $g->group_id;
+            }
+            $this->db->update('data_streams', array('permissions' => serialize($groups_arr)));
         }
     }
 
