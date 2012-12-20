6fd025bd5679e181ddce48504f5496ac7b814fe4
Joshua Pekera <joshuapekera@me.com>
2012-12-20 02:12:01 -0600

    Animated Files
    Added some animation through out the files module to make Jerel and Adam drool.
    
    Signed-off-by: Joshua Pekera <joshuapekera@me.com>

--- system/cms/modules/files/views/admin/index.php
+++ system/cms/modules/files/views/admin/index.php
@@ -1,4 +1,4 @@
-<section class="title">
+<section class="title animated bounceInDown">
 	<h4 id="file-title">
 		<?php echo lang('files:files_title') ?>
 	</h4>
@@ -24,7 +24,7 @@
 	</div>
 </section>
 
-<section class="item">
+<section class="item animated bounceInLeft">
 	<section class="side">
 		<ul id="folders-sidebar">
 			<li class="folder places" data-id="0"><a href="#"><?php echo lang('files:places') ?></a></li>
@@ -61,7 +61,7 @@
 
 	</section>
 
-	<section class="side sidebar-right">
+	<section class="side sidebar-right animated fadeInRight" id="file-sidebar">
 		<button class="close" alt="close">Close</button>
 		<ul id="search-results"></ul>
 	</section>
@@ -175,7 +175,7 @@
 
 </section>
 
-<section class="file-path">
+<section class="file-path animated bounceInUp">
 	<h5 id="file-breadcrumbs">
 		<span id="crumb-root">
 			<a data-id="0" href="#"><?php echo lang('files:places') ?></a>
