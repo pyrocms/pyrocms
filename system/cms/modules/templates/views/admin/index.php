bbe8ac72cfeed62600d8068c0a569683c26c6b39
Hugo Dias <hugomrdias@gmail.com>
2012-12-27 17:04:25 +0000

    fixing the grid templates module added one_full and added cellspacing 0 to the bottom table

--- system/cms/modules/templates/views/admin/index.php
+++ system/cms/modules/templates/views/admin/index.php
@@ -1,14 +1,14 @@
 <?php if(!empty($templates)): ?>
-
+<div class="one_full">
 <section class="title">
 	<h4><?php echo lang('templates:default_title') ?></h4>
 </section>
-	
+
 <section class="item">
 	<div class="content">
 
 	    <?php echo form_open('admin/templates/action') ?>
-	
+
 	    <table border="0" class="table-list" cellspacing="0">
 	        <thead>
 	            <tr>
@@ -19,9 +19,9 @@
 	                <th width="220"></th>
 	            </tr>
 	        </thead>
-	
+
 	        <tbody>
-			
+
 	    <?php foreach ($templates as $template): ?>
 			<?php if($template->is_default): ?>
 	            <tr>
@@ -42,41 +42,43 @@
 		</tbody>
 		</table>
 	    <?php echo form_close() ?>
-	 
+
 	 	<div class="table_action_buttons">
 			<?php $this->load->view('admin/partials/buttons', array('buttons' => array('delete') )) ?>
 		</div>
 	</div>
 </section>
+</div>
 
+<div class="one_full">
 <section class="title">
 	<h4><?php echo lang('templates:user_defined_title') ?></h4>
 </section>
 
 <?php echo form_open('admin/templates/delete') ?>
-   
+
 <section class="item">
 	<div class="content">
-		<table border="0" class="table-list clear-both">
+		<table border="0" class="table-list clear-both" cellspacing="0">
 	        <thead>
 	            <tr>
 	                <th><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
 	                <th><?php echo lang('name_label') ?></th>
-	                <th><?php echo lang('global:description') ?></th>
-	                <th><?php echo lang('templates:language_label') ?></th>
+	                <th class="collapse"><?php echo lang('global:description') ?></th>
+	                <th class="collapse"><?php echo lang('templates:language_label') ?></th>
 	                <th width="200"></th>
 	            </tr>
 	        </thead>
-	
+
 	        <tbody>
-		
+
 	    <?php foreach ($templates as $template): ?>
 			<?php if(!$template->is_default): ?>
 	            <tr>
 					<td><?php echo form_checkbox('action_to[]', $template->id);?></td>
 	                <td><?php echo $template->name ?></td>
-	                <td><?php echo $template->description ?></td>
-	                <td><?php echo $template->lang ?></td>
+	                <td class="collapse"><?php echo $template->description ?></td>
+	                <td class="collapse"><?php echo $template->lang ?></td>
 	                <td class="actions">
 					<div class="buttons buttons-small align-center">
 						<?php echo anchor('admin/templates/preview/' . $template->id, lang('buttons:preview'), 'class="button preview modal"') ?>
@@ -87,19 +89,19 @@
 	            </tr>
 			<?php endif ?>
 	    <?php endforeach ?>
-		
-		
+
+
 	        </tbody>
 	    </table>
-	
+
 		<div class="table_action_buttons">
 			<?php $this->load->view('admin/partials/buttons', array('buttons' => array('delete') )) ?>
 		</div>
-	
+
 	    <?php echo form_close() ?>
 	</div>
 </section>
-	
+
 <?php else: ?>
 
 <section class="item">
@@ -109,3 +111,5 @@
 </section>
 
 <?php endif ?>
+
+</div>
