bdb118b3dc09fa84f095e803c42910bd4993d43e
Joshua Pekera <joshuapekera@me.com>
2012-12-28 13:28:01 -0600

    Bug fixes
    updates to css to fix form layouts and various layout bugs.
    
    Signed-off-by: Joshua Pekera <joshuapekera@me.com>

--- system/cms/modules/blog/views/admin/partials/filters.php
+++ system/cms/modules/blog/views/admin/partials/filters.php
@@ -3,23 +3,23 @@
 
 	<?php echo form_open('', '', array('f_module' => $module_details['slug'])) ?>
 		<ul>
-			<li class="one_quarter">
+			<li class="">
         		<label for="f_status"><?php echo lang('blog:status_label') ?></label>
         		<?php echo form_dropdown('f_status', array(0 => lang('global:select-all'), 'draft'=>lang('blog_draft_label'), 'live'=>lang('blog_live_label'))) ?>
     		</li>
 
-			<li class="one_quarter">
+			<li class="">
         		<label for="f_category"><?php echo lang('blog:category_label') ?></label>
        			<?php echo form_dropdown('f_category', array(0 => lang('global:select-all')) + $categories) ?>
     		</li>
 
-			<li class="one_quarter">
+			<li class="">
 				<label for="f_category"><?php echo lang('global:keywords') ?></label>
 				<?php echo form_input('f_keywords', '', 'style="width: 55%;"') ?>
 			</li>
 
-			<li class="one_quarter last">
-				<?php echo anchor(current_url() . '#', lang('buttons:cancel'), 'class="alignright button red"') ?>
+			<li class="">
+				<?php echo anchor(current_url() . '#', lang('buttons:cancel'), 'class="button red"') ?>
 			</li>
 		</ul>
 	<?php echo form_close() ?>
