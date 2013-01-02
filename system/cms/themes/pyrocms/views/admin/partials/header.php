604d3de1e4964901fa5705cb186d0d5f0b5b203c
Joshua Pekera <joshuapekera@me.com>
2013-01-01 19:58:05 -0600

    Retina Stuff
    Updated image replacement for the pyro logo
    
    Signed-off-by: Joshua Pekera <joshuapekera@me.com>

--- system/cms/themes/pyrocms/views/admin/partials/header.php
+++ system/cms/themes/pyrocms/views/admin/partials/header.php
@@ -6,7 +6,8 @@
 	<div class="topbar-inner">
 		<div class="wrapper">
 			<div id="logo">
-				<?php echo anchor('', Asset::img('logo.png', 'view site'), 'target="_blank"') ?>
+				<!-- <?php echo anchor('', Asset::img('logo.png', 'view site'), 'target="_blank"') ?> -->
+				<?php echo anchor('','<span id="pyro-logo"></span>', 'target="_blank"') ?>
 			</div>
 		
 			<nav id="primary">
