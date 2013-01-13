5ab3297256fda1d360ebffa7433add33d3efbc32
Joshua Pekera <joshuapekera@me.com>
2013-01-12 21:51:05 -0600

    Default Theme
    updated lots of style and layout stuff for the default website theme
    
    Signed-off-by: Joshua Pekera <joshuapekera@me.com>

--- system/cms/themes/default/theme.php
+++ system/cms/themes/default/theme.php
@@ -16,7 +16,7 @@ class Theme_Default extends Theme {
 																'is_required'   => true),
 									'layout' => 			array('title' => 'Layout',
 																'description'   => 'Which type of layout shall we use?',
-																'default'       => '2 column',
+																'default'       => 'full-width-home',
 																'type'          => 'select',
 																'options'       => '2 column=Two Column|full-width=Full Width|full-width-home=Full Width Home Page',
 																'is_required'   => true),
