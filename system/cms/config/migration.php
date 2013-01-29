59600d7ca85dca6bc93501b90d56fb8cc84b959e
Adam Fairholm <adam.fairholm@gmail.com>
2013-01-28 20:58:19 -0500

    Bumping migrations to 118.
    This is necessary to make #2297 work.

--- system/cms/config/migration.php
+++ system/cms/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = true;
 |
 */
 
-$config['migration_version'] = 117;
+$config['migration_version'] = 118;
 
 /*
 |--------------------------------------------------------------------------
