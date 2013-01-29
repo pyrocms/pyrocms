55b4df9569977d5820962df5a9009c1d5490fb68
Adam Fairholm <adam.fairholm@gmail.com>
2013-01-28 21:03:19 -0500

    Moving Page types description up under the slug.
    Just seemed to make more sense than at the bottom.

--- system/cms/modules/pages/views/admin/types/form.php
+++ system/cms/modules/pages/views/admin/types/form.php
@@ -42,6 +42,11 @@
 								<?php endif; ?>
 								</div>
 							</li>
+
+                            <li>
+                                <label for="description"><?php echo lang('global:description');?></label>
+                                <div class="input"><?php echo form_input('description', $page_type->description, 'id="description"'); ?></div>
+                            </li>
 		
 							<?php if ($this->method == 'edit'): ?>
 								<?php echo form_hidden('old_slug', $page_type->slug); ?>
@@ -83,10 +88,7 @@
 								<label for="title_label"><?php echo lang('page_types:title_label');?><br><small><?php echo lang('page_types:title_label_instructions'); ?></small></label>
 								<div class="input"><?php echo form_input('title_label', $page_type->title_label, 'id="title_label" maxlength="100"'); ?></div>
 							</li>		
-                            <li>
-                                <label for="description"><?php echo lang('global:description');?></label>
-                                <div class="input"><?php echo form_input('description', $page_type->description, 'id="description"'); ?></div>
-                            </li>
+
 						</ul>
 						
 					</fieldset>
