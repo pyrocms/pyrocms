312951c9a11a2b1f634586813fabf8670b49cb9b
Solinx <wouter.van.dam@gmail.com>
2013-01-30 14:58:30 +0100

    Update system/cms/modules/pages/plugin.php
    Fixing indenting

--- system/cms/modules/pages/plugin.php
+++ system/cms/modules/pages/plugin.php
@@ -94,27 +94,27 @@ class Plugin_Pages extends Plugin
 			->get('pages')
 			->row();
         
-        $page->body = '';
+		$page->body = '';
         
-        // Legacy support for chunks
-        if ($this->db->table_exists('page_chunks'))
-        {
-    		// Grab all the chunks that make up the body
-    		$page->chunks = $this->db->get_where('page_chunks', array('page_id' => $page->id))->result();
+		// Legacy support for chunks
+		if ($this->db->table_exists('page_chunks'))
+		{
+			// Grab all the chunks that make up the body
+			$page->chunks = $this->db->get_where('page_chunks', array('page_id' => $page->id))->result();
     		
-    		if ($page->chunks)
-    		{
-    			foreach ($page->chunks as $chunk)
-    			{
-    				$page->body .= '<div class="page-chunk '.$chunk->slug.'">' .
-    					(($chunk->type == 'markdown') ? $chunk->parsed : $chunk->body) .
-    					'</div>' . PHP_EOL;
-    			}
-    		}
+			if ($page->chunks)
+			{
+    				foreach ($page->chunks as $chunk)
+				{
+					$page->body .= '<div class="page-chunk '.$chunk->slug.'">' .
+					(($chunk->type == 'markdown') ? $chunk->parsed : $chunk->body) .
+					'</div>' . PHP_EOL;
+				}
+			}
     
-    		// we'll unset the chunks array as Lex is grouchy about mixed data at the moment
-    		unset($page->chunks);
-        }
+			// we'll unset the chunks array as Lex is grouchy about mixed data at the moment
+			unset($page->chunks);
+		}
         
 		return $this->content() ? array($page) : $page->body;
 	}
@@ -212,7 +212,7 @@ class Plugin_Pages extends Plugin
 			->get('pages')
 			->result_array();
 
-        // Legacy support for chunks
+		// Legacy support for chunks
 		if ($pages && $this->db->table_exists('page_chunks'))
 		{
 			foreach ($pages as &$page)
