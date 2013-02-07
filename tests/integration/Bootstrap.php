57cddd1312f967f4a5899c9cbd1a04f2784dda42
Matt Frost <mfrost.design@gmail.com>
2013-02-07 11:51:28 -0600

    making changes to the database host

--- tests/integration/Bootstrap.php
+++ tests/integration/Bootstrap.php
@@ -2,5 +2,6 @@
 // Errors on full!
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
-
+require 'goutte.phar';
+define('PYRO_DB_HOST','127.0.0.1');
 define('PYRO_HOST', isset($_SERVER['PYRO_TEST_HOST']) ? $_SERVER['PYRO_TEST_HOST'] : 'localhost');
