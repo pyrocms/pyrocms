1b7b2ed43e168a4b6831a07088b897ba79cd527a
Adam Fairholm <adam.fairholm@gmail.com>
2013-02-19 20:45:00 -0500

    Make "Search" a Lang Variable
    Placeholder was previously hardcoded.
    
    Fixes #2449.

--- system/cms/language/thai/admin_lang.php
+++ system/cms/language/thai/admin_lang.php
@@ -47,6 +47,8 @@ $lang['cp:nav_structure']				= 'Structure'; #translate
 $lang['cp:nav_content']					= 'เนื้อหา';
 $lang['cp:nav_misc']					= 'Miscellaneous'; #translate
 
+$lang['cp:search']                      = 'Search'; #translate
+
 $lang['cp:must_login']	 				= 'คุณต้องล็อกอินเพื่อดูหน้านี้.';
 $lang['cp:access_denied'] 				= 'คุณไม่ได้รับสิทธิ์เพียงพอที่จะดูหน้านี้.';
 $lang['cp:delete_installer_message']              = 'The installer directory still exists! Please delete it from your server! Leaving it online means control of this site could be granted to somebody else.'; #translate
--- system/cms/themes/pyrocms/views/admin/partials/header.php
+++ system/cms/themes/pyrocms/views/admin/partials/header.php
@@ -61,9 +61,8 @@
 					});
 				</script>
 	
-	
 				<form class="topbar-search">
-					<input type="text" class="search-query" id="nav-search" placeholder="Search" ontouchstart="">
+					<input type="text" class="search-query" id="nav-search" placeholder="{{ helper:lang line="cp:search" }}" ontouchstart="">
 				</form>
 			</div>
 			
