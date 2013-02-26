17b483529537843d3214bca29cc98d8b96443acc
Phil Sturgeon <email@philsturgeon.co.uk>
2013-02-25 22:11:40 -0500

    Fixes #2478: IPv6 support backported for 2.1.x

--- system/cms/migrations/097_Remove_theme_option_length.php
+++ system/cms/migrations/097_Remove_theme_option_length.php
@@ -7,6 +7,15 @@ class Migration_Remove_theme_option_length extends CI_Migration
 		$this->dbforge->modify_column('theme_options', array(
 			'options' => array('type' => 'TEXT'),
 		));
+		
+		// Snuck in a fix for IPv6 support
+		$this->dbforge->modify_column('ci_sessions', array(
+			'ip_address' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 45,
+				'null' => false
+			)
+		));
 	}
 
 	public function down()
