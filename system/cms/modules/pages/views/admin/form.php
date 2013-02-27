c8886a63b61e2908916f43bce828f8b1083fdd80
Mikael Kundert <miksu@kundert.fi>
2013-02-27 20:46:37 +0200

    Revert "More fix for #2474 which covers keywords module too."
    This reverts commit ec35b2884942b84e912162f7c2345515e7043ba5.

--- system/cms/modules/pages/views/admin/form.php
+++ system/cms/modules/pages/views/admin/form.php
@@ -108,12 +108,12 @@
 						<label for="meta_title"><?php echo lang('pages:meta_title_label') ?></label>
 						<div class="input"><input type="text" id="meta_title" name="meta_title" maxlength="255" value="<?php echo $page->meta_title ?>" /></div>
 					</li>
-					<?php if (module_enabled('keywords')) : ?>
-						<li>
-							<label for="meta_keywords"><?php echo lang('pages:meta_keywords_label') ?></label>
-							<div class="input"><input type="text" id="meta_keywords" name="meta_keywords" maxlength="255" value="<?php echo $page->meta_keywords ?>" /></div>
-						</li>
-					<?php endif; ?>
+									
+					<li>
+						<label for="meta_keywords"><?php echo lang('pages:meta_keywords_label') ?></label>
+						<div class="input"><input type="text" id="meta_keywords" name="meta_keywords" maxlength="255" value="<?php echo $page->meta_keywords ?>" /></div>
+					</li>
+					
 					<li>
 						<label for="meta_description"><?php echo lang('pages:meta_desc_label') ?></label>
 						<?php echo form_textarea(array('name' => 'meta_description', 'value' => $page->meta_description, 'rows' => 5)) ?>
