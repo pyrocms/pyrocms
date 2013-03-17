cf3af3e2ec1d08fc33a0c562006f5c20b7f11eae
Ahmed Maawy <ultimateprogramer@gmail.com>
2013-03-17 11:42:28 +0300

    Some fixes to the bease theme

--- addons/shared_addons/themes/base/views/modules/blog/view.php
+++ addons/shared_addons/themes/base/views/modules/blog/view.php
@@ -1,31 +1,43 @@
 {{ theme:partial name="aside" }}
 
 <article class="single_post">	
-	<h5>{{ theme:image file="link.png" }} <?php echo $post->title; ?><small><a href="{{ base:url }}blog" title="Back to the blog">&larr; Back</a></small></h5>
+	<h5>{{ theme:image file="link.png" }} <?php echo $post[0]["title"]; ?><small><a href="{{ base:url }}blog" title="Back to the blog">&larr; Back</a></small></h5>
 			
 	<div class="post_date">
 		<span class="date">
 			{{ theme:image file="date.png" }}
-			About <?php $now = time(); $posted = date($post->created_on); echo timespan($posted, $now); ?> ago.
+			About <?php $now = time(); $posted = date($post[0]["created_on"]); echo timespan($posted, $now); ?> ago.
 		</span>
 	</div>
 			
 	<hr>
 	
 	<div class="post_body">
-		<?php echo $post->body; ?>
+		<?php echo $post[0]["body"]; ?>
 	</div>
 			
 	<div class="post_meta">
-		<?php if($post->keywords) : ?>
+		<?php if($post[0]["keywords"]) : ?>
 			{{ theme:image file="tags.png" }}
 			<span class="tags">
-				<?php echo $post->keywords; ?>
+				<?php echo $post[0]["keywords"]; ?>
 			</span>
 		<?php endif; ?>
-	</div>			
+	</div>
+	
+	<?php if (Settings::get('enable_comments')): ?>	
+		
+		<div id="existing-comments">
+			<h4><?php echo lang('comments:title') ?></h4>
+			<?php echo $this->comments->display() ?>
+		</div>		
 
-	<?php if ($post->comments_enabled): ?>
-		<?php echo display_comments($post->id); ?>
+		
+		<?php if ($form_display): ?>
+			<?php echo $this->comments->form() ?>
+		<?php else: ?>
+			<?php echo sprintf(lang('blog:disabled_after'), strtolower(lang('global:duration:'.str_replace(' ', '-', $post[0]['comments_enabled'])))) ?>
+		<?php endif ?>
+	
 	<?php endif; ?>
 </article>
