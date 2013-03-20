d3b3ef44a0e27f03b0e282576cd06c9785b52c5c
Roumen Damianoff <roumen@dawebs.com>
2013-03-20 23:07:24 +0200

    Added Bulgarian language (navigation)

---
+++ system/cms/modules/navigation/language/bulgarian/navigation_lang.php
@@ -0,0 +1,42 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['nav:parent_label']			= 'Родител';
+$lang['nav:target_label']                       = 'Таргет';
+$lang['nav:class_label']                        = 'Клас';
+$lang['nav:url_label']                          = 'URL';
+$lang['nav:details_label']                      = 'Детайли';
+$lang['nav:text_label']                         = 'Текст';
+$lang['nav:group_label']                        = 'Група';
+$lang['nav:location_label']                     = 'Местоположение';
+$lang['nav:type_label']                         = 'Връзка тип';
+$lang['nav:uri_label']                          = 'Връзка (URI)';
+$lang['nav:page_label']                         = 'Страница';
+$lang['nav:module_label']                       = 'Модул';
+$lang['nav:restricted_to']			= 'Ограничен за';
+$lang['nav:abbrev_label']                       = 'Абревиатура';
+
+$lang['nav:link_target_self']                   = 'Същият прозорец (default)';
+$lang['nav:link_target_blank']                  = 'Нов прозорец (_blank)';
+
+// titles
+$lang['nav:link_create_title']                  = 'Добави връзка';
+$lang['nav:group_create_title']                 = 'Добави група';
+$lang['nav:link_edit_title']                    = 'Редактирай връзка "%s"';
+$lang['nav:link_list_title']                    = 'Връзки';
+$lang['nav:group_list_title']                   = 'Групи';
+
+// messages
+$lang['nav:group_no_links']                     = 'Няма връзки в тази група.';
+$lang['nav:no_groups']                          = 'Няма навигационни групи.';
+$lang['nav:group_delete_confirm']               = 'Сигурни ли сте че искате да изтриете навигационната група? Това ще изтрие ВСИЧКИ навигационни връзки в групата и ще се наложи да бъдат редактирани референциите към тях.';
+$lang['nav:group_add_success']                  = 'Вашата група беше запаметена.';
+$lang['nav:group_add_error']                    = 'Възникна грешка.';
+$lang['nav:group_mass_delete_success']          = 'Навигационната група беше изтрита.';
+$lang['nav:link_add_success']                   = 'Навигационната група беше добавена.';
+$lang['nav:link_add_error']                     = 'Възникна неочаквана грешка.';
+$lang['nav:link_not_exist_error']               = 'Навигационната група не съществува.';
+$lang['nav:link_edit_success']                  = 'Навигационната връзка беше запаметена.';
+$lang['nav:link_delete_success']                = 'Навигационната връзка беше изтрита.';
+$lang['nav:choose_value']			= 'Полето %s не трябва да е празно.';
+$lang['nav:link_type_desc']                     = 'Моля, изберете тип на връзката за да получите достъп до допълнителните опции.';
