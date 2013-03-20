501c5c097978790593e84731375cec4a69eca507
Roumen Damianoff <roumen@dawebs.com>
2013-03-20 22:49:52 +0200

    Added Bulgarian language (widgets)

---
+++ system/cms/modules/widgets/language/bulgarian/widgets_lang.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// sidebar
+$lang['widgets:available_title']        = 'Налични джаджи';
+$lang['widgets:widget_area_wrapper']    = 'Налични джаджи';
+$lang['widgets:instructions']           = 'Издърпай и пусни за да инсталираш';
+
+
+$lang['widgets:instances']            	= 'Инстанции';
+$lang['widgets:areas']            	= 'Зони';
+
+// Widgets
+$lang['widgets:widget']                 = 'Джаджа';
+$lang['widgets:widget_author']          = 'Автор';
+$lang['widgets:widget_short_name']      = 'Кратко име';
+$lang['widgets:widget_version']         = 'Версия';
+$lang['widgets:status_label']		= 'Статус';
+
+$lang['widgets:inactive_title'] 	= 'Неактивни джаджи';
+$lang['widgets:active_title'] 		= 'Активни джаджи';
+
+// Widget area titles
+$lang['widgets:add_area']               = 'Добави зона';
+$lang['widgets:delete_area']            = 'Изтрий зона';
+$lang['widgets:edit_area']              = 'Редактирай зона';
+
+// Widget area field names
+$lang['widgets:widget_area']            = 'Зона';
+$lang['widgets:widget_area_title']      = 'Заглавие на зоната';
+$lang['widgets:widget_area_slug']       = 'Slug на зоната';
+
+$lang['widgets:view_code']		= 'Виж кода';
+
+$lang['widgets:instance_title']         = 'Заглавие';
+$lang['widgets:show_title']		= 'Показване на заглавието?';
+$lang['widgets:tag_title']              = 'Таг';
+
+$lang['widgets:no_available_widgets']	= 'Няма достъпни джаджи.';
