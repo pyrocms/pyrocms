acd40f5cf75812cce83a60c51fca02bedb119673
wernerwerks <gordon@wernerwerks.com>
2013-03-26 09:07:06 -0500

    Update help_lang.php
    Swap English and Chinese Traditional help language.

--- system/cms/modules/variables/language/english/help_lang.php
+++ system/cms/modules/variables/language/english/help_lang.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 // inline help html. Only 'help_body' is used.
-$lang['help_body'] = "<h4>概觀</h4>
-<p>透過「系統變數」模組，您將可以新增變數，並將之顯示在網站的任意一處。</p>
+$lang['help_body'] = "<h4>Overview</h4>
+<p>With the Variables module you can add simple values and display them anywhere on the site.</p>
 
-<h4>新增全域變數</h4>
-<p>使用「系統變數」很簡單，只要指定一個名稱和內容。然後您就可以將系統產生的短碼嵌入頁面、文章等，該數值就會被顯示。</p>";
+<h4>Adding Global Variables</h4>
+<p>To use Variables simply choose a short name and assign a value. You can then embed the generated tag in page content, blog posts, etc. and the assigned value will be displayed.</p>"; #translate
