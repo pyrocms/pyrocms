5ae4ea0a71621d5730704096d8af8df5f776a348
jerel <mail@unruhdesigns.com>
2013-03-30 21:28:48 -0500

    Fixes #2599. Removed the default limit of 10 from pages, files, and blog. Unlimited now unless specified which is the expected behavior

--- system/cms/modules/blog/plugin.php
+++ system/cms/modules/blog/plugin.php
@@ -46,7 +46,7 @@ class Plugin_Blog extends Plugin
 					'limit' => array(
 						'type' => 'number',
 						'flags' => '',
-						'default' => '10',
+						'default' => '',
 						'required' => false,
 						),
 					'offset' => array(
@@ -81,7 +81,7 @@ class Plugin_Blog extends Plugin
 					'limit' => array(
 						'type' => 'number',
 						'flags' => '',
-						'default' => '10',
+						'default' => '',
 						'required' => false,
 						),
 					'order-by' => array(
@@ -140,7 +140,7 @@ class Plugin_Blog extends Plugin
 					'limit' => array(
 						'type' => 'number',
 						'flags' => '',
-						'default' => '10',
+						'default' => '',
 						'required' => false,
 						),
 					),
@@ -181,7 +181,7 @@ class Plugin_Blog extends Plugin
 			'sort'			=> 'desc',
 			'show_past'		=> 'no',
 			'date_by'		=> 'created_on',
-			'limit'			=> $this->attribute('limit', 10),
+			'limit'			=> $this->attribute('limit', null),
 			'offset'		=> $this->attribute('offset')
 		);
 		foreach ($overrides as $k => $v)
@@ -305,7 +305,7 @@ class Plugin_Blog extends Plugin
 	 */
 	public function categories()
 	{
-		$limit     = $this->attribute('limit', 10);
+		$limit     = $this->attribute('limit', null);
 		$order_by  = $this->attribute('order-by', 'title');
 		$order_dir = $this->attribute('order-dir', 'ASC');
 
@@ -365,7 +365,7 @@ class Plugin_Blog extends Plugin
 	 */	
 	public function tags()
 	{
-		$limit = $this->attribute('limit', 10);
+		$limit = $this->attribute('limit', null);
 		
 		$this->load->library(array('keywords/keywords'));
 
