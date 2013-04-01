0009abec4bb871058aad21852a48e35bc0dab0f4
Alireza Jahandideh <ar.jahandideh@gmail.com>
2013-04-01 20:21:34 +0430

    CodeIgniter Persian Language

---
+++ system/codeigniter/language/persian/unit_test_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['ut_test_name'] = 'نام تست';
+$lang['ut_test_datatype'] = 'نوع دیتای تست';
+$lang['ut_res_datatype'] = 'نوع دیتای مورد انتظار';
+$lang['ut_result'] = 'نتیجه';
+$lang['ut_undefined'] = 'نام تست نامشخص';
+$lang['ut_file'] = 'نام فایل';
+$lang['ut_line'] = 'شماره خط';
+$lang['ut_passed'] = 'پاس شده';
+$lang['ut_failed'] = 'پاس نشده';
+$lang['ut_boolean'] = 'منطقی';
+$lang['ut_integer'] = 'عددی';
+$lang['ut_float'] = 'عددی اعشاری';
+$lang['ut_double'] = 'عددی اعشاری';
+$lang['ut_string'] = 'رشته';
+$lang['ut_array'] = 'آرایه';
+$lang['ut_object'] = 'آبجکت';
+$lang['ut_resource'] = 'ریسورس';
+$lang['ut_null'] = 'خالی';
+$lang['ut_notes'] = 'یادداشت';
+
+/* End of file unit_test_lang.php */
+/* Location: ./system/language/persian/unit_test_lang.php */
---
+++ system/codeigniter/language/persian/upload_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['upload_userfile_not_set'] = 'متغییر userfile پیدا نشد ، احتمالا پست نشده یا نام فیلد اشتباه است.';
+$lang['upload_file_exceeds_limit'] = 'حجم فایل ارسال شده از حجم مجاز تعیین شده در تنظیمات php بیشتر است.';
+$lang['upload_file_exceeds_form_limit'] = 'حجم فایل ارسال شده از حجم مجاز تعیین شده در فرم ارسال بیشتر است.';
+$lang['upload_file_partial'] = 'فقط بخشی از فایل ارسال شد.';
+$lang['upload_no_temp_directory'] = 'شاخه فایلهای موقت (temporary) یافت نشد.';
+$lang['upload_unable_to_write_file'] = 'امکان نوشتن فایل بر روی سخت دیسک نیست.';
+$lang['upload_stopped_by_extension'] = 'ارسال فایل به دلیل پسوند متوقف شد.';
+$lang['upload_no_file_selected'] = 'فایلی برای ارسال انتخاب نشده.';
+$lang['upload_invalid_filetype'] = 'این نوع فایل برای ارسال مجاز نیست.';
+$lang['upload_invalid_filesize'] = 'حجم فایل ارسالی بیشتر از حجم مجاز است.';
+$lang['upload_invalid_dimensions'] = 'طول یا عرض عکس ارسالی از حد مجاز بیشتر است.';
+$lang['upload_destination_error'] = 'خطایی در هنگام جابجایی فایل به مقصد رخ داده است.';
+$lang['upload_no_filepath'] = 'مسیر ارسال صحیح نیست.';
+$lang['upload_no_file_types'] = 'هنوز نوع فایلهای مجاز تعیین نشده.';
+$lang['upload_bad_filename'] = 'فایلی با همین نام قبلا ارسال شده.';
+$lang['upload_not_writable'] = 'شاخه مقصد قابل نوشتن نیست.';
+
+/* End of file upload_lang.php */
+/* Location: ./system/language/persian/upload_lang.php */
