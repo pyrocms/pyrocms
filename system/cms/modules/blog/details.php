4eff20bf09cfd36caf452d9ec89f97bb5ed1f708
Alireza Jahandideh <ar.jahandideh@gmail.com>
2013-04-01 23:06:54 +0430

    Persian Translation of Blog module
    Blog Module and plugin and widgets are traslated to Persian

--- system/cms/modules/blog/details.php
+++ system/cms/modules/blog/details.php
@@ -15,6 +15,7 @@ class Module_Blog extends Module
 		$info = array(
 			'name' => array(
 				'en' => 'Blog',
+                                'fa' => 'بلاگ',
 				'ar' => 'المدوّنة',
 				'br' => 'Blog',
 				'pt' => 'Blog',
@@ -33,6 +34,7 @@ class Module_Blog extends Module
 			),
 			'description' => array(
 				'en' => 'Post blog entries.',
+                                'fa' => 'مقالات منتشر شده در بلاگ',
 				'ar' => 'أنشر المقالات على مدوّنتك.',
 				'br' => 'Escrever publicações de blog',
 				'pt' => 'Escrever e editar publicações no blog',
