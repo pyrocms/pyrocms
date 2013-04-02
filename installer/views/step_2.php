20e48a7c8b1778f05e561077e609c03aed2ad345
jerel <mail@unruhdesigns.com>
2013-04-01 20:42:45 -0500

    Fixes #2590. Errors in error messages on installer Step 2

--- installer/views/step_2.php
+++ installer/views/step_2.php
@@ -16,9 +16,9 @@
 		<!-- Server -->
 		<li>
 			<h5>{server_settings}</h5>
-			<p class="result <?php echo ($http_server->supported === true) ? 'pass' : 'partial'; ?>">
-				<?php if ($http_server->supported === true): ?>
-					<?php echo $http_server->name; ?>
+			<p class="result <?php echo ($http_server_supported === true) ? 'pass' : 'partial'; ?>">
+				<?php if ($http_server_supported === true): ?>
+					<?php echo $http_server_name; ?>
 				<?php else: ?>{server_fail}<?php endif; ?>
 			</p>
 		</li>
@@ -28,7 +28,7 @@
 			<h5>{php_settings}</h5>
 			<p><?php echo sprintf(lang('php_required'), $php_min_version); ?></p>
 			<p class="result <?php echo ($php_acceptable) ? 'pass' : 'fail'; ?>">
-				{php_version} <strong><?php echo $php_version; ?></strong>.
+				{php_version} <strong><?php echo $php_running_version; ?></strong>.
 				<?php if (!$php_acceptable): ?>
 					<?php echo sprintf(lang('php_fail'), $php_min_version); ?>
 				<?php endif; ?>
@@ -40,14 +40,14 @@
 			<h5><?php echo lang('mysql_settings'); ?></h5>
 			<p><?php echo lang('mysql_required'); ?></p>
 			<!-- Server -->
-			<p class="result <?php echo ($mysql->server_version_acceptable) ? 'pass' : 'fail'; ?>">
-				{mysql_version1} <strong><?php echo $mysql->server_version; ?></strong>.
-				<?php if ($mysql->server_version_acceptable) : ?>{mysql_fail}<?php endif; ?>
+			<p class="result <?php echo ($server_version_acceptable) ? 'pass' : 'fail'; ?>">
+				{mysql_version1} <strong><?php echo $server_version; ?></strong>.
+				<?php if ( ! $server_version_acceptable) : ?>{mysql_fail}<?php endif; ?>
 			</p>
 			<!-- Client -->
-			<p class="result <?php echo ($mysql->client_version_acceptable) ? 'pass' : 'fail'; ?>">
-				{mysql_version2} <strong><?php echo $mysql->client_version; ?></strong>.
-				<?php if ($mysql->client_version_acceptable): ?>{mysql_fail}<?php endif; ?>
+			<p class="result <?php echo ($client_version_acceptable) ? 'pass' : 'fail'; ?>">
+				{mysql_version2} <strong><?php echo $client_version; ?></strong>.
+				<?php if ( ! $client_version_acceptable): ?>{mysql_fail}<?php endif; ?>
 			</p>
 		</li>
 	</ul>
@@ -64,7 +64,7 @@
 			<h5>{gd_settings}</h5>
 			<p>{gd_required}</p>
 			<p class="result <?php echo ($gd_acceptable) ? 'pass' : 'fail'; ?>">
-				{gd_version} <strong><?php echo $gd_version; ?></strong>.
+				{gd_version} <strong><?php echo $gd_running_version; ?></strong>.
 				<?php if (!$gd_acceptable): ?>{gd_fail}<?php endif; ?>
 			</p>
 		</li>
