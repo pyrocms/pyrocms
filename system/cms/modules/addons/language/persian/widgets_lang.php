eba120601bb14bba6bb2fb7041fcb7426c136ae4
Alireza Jahandideh <ar.jahandideh@gmail.com>
2013-04-03 20:59:29 +0430

    Persian language (modules)

---
+++ system/cms/modules/addons/language/persian/widgets_lang.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// sidebar
+$lang['widgets:available_title']        = 'ویجت های موجود';
+$lang['widgets:widget_area_wrapper']    = 'ناحیه های موجود';
+$lang['widgets:instructions']           = 'برای نصب بکشید و بیاندازید';
+
+
+$lang['widgets:instances']            	= 'instance ها';
+$lang['widgets:areas']            	= 'ناحیه ها';
+
+// Widgets
+$lang['widgets:widget']                  = 'ویجت';
+$lang['widgets:widget_author']           = 'پدیدآور';
+$lang['widgets:widget_short_name']      = 'نام کوتاه';
+$lang['widgets:widget_version']          = 'ورژن';
+$lang['widgets:status_label']	      = 'وضعیت';
+
+$lang['widgets:inactive_title'] 		= 'ویجت غیر فعال شد';
+$lang['widgets:active_title'] 		= 'ویجت فعال شد';
+
+// Widget area titles
+$lang['widgets:add_area']               = 'اضافه کردن ناحیه';
+$lang['widgets:delete_area']            = 'حذف ناحیه';
+$lang['widgets:edit_area']              = 'ویرایش ناحیه';
+
+// Widget area field names
+$lang['widgets:widget_area']            = 'ناحیه';
+$lang['widgets:widget_area_title']      = 'نام ناحیه';
+$lang['widgets:widget_area_slug']       = 'نام کوتاه ناحیه';
+
+$lang['widgets:view_code']          = 'مشاهده کد';
+
+$lang['widgets:instance_title']         = 'عنوان';
+$lang['widgets:show_title']		    = 'عنوان ویجت ها نشان داده شوند؟'; #translate
+$lang['widgets:tag_title']              = 'تگ';
+
+$lang['widgets:no_available_widgets']	= 'ویجتی موجود نیست';
