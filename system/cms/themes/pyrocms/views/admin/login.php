9d2e29c37e4e0b03db9fcb6cda47ca1989f9723e
jerel <mail@unruhdesigns.com>
2013-04-04 00:39:55 -0500

    Added noindex,nofollow to the admin login screen

--- system/cms/themes/pyrocms/views/admin/login.php
+++ system/cms/themes/pyrocms/views/admin/login.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name=viewport content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="robots" content="noindex,nofollow">
 	<title><?php echo $this->settings->site_name; ?> - <?php echo lang('login_title');?></title>
 
 	<base href="<?php echo base_url(); ?>"/>
