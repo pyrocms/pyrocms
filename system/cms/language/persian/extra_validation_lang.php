09767581282c91fe1d40365e8feb692e71ba658c
Alireza Jahandideh <ar.jahandideh@gmail.com>
2013-04-04 12:49:30 +0430

    persian lang (main)

---
+++ system/cms/language/persian/extra_validation_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['alpha_dot_dash']			= "فیلد  می تواند حاوی کلمات انگلیسی کوچک، آندرلاین، نقطه و یا خط فاصله باشد";
+$lang['decimal']				= 'فیلد %s می تواند حاوی اعداد باشد';
+$lang['csrf_bad_token']			= "CSRF Token غیر قابل قبول";
+
+/* End of file extra_validation_lang.php */
---
+++ system/cms/language/persian/global_lang.php
@@ -0,0 +1,118 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['global:control-panel'] 			= 'پنل مدیریت';
+$lang['global:profile']				= 'پروفایل';
+
+$lang['global:title'] 				= 'عنوان';
+$lang['name_label'] 				= 'نام';
+$lang['desc_label'] 				= 'شرح';
+$lang['version_label'] 				= 'ورژن';
+$lang['global:author'] 				= 'پدیدآور';
+$lang['global:action'] 				= 'عملیات';
+$lang['global:slug'] 				= 'Slug';
+$lang['global:slug_instructions']                   = 'حروف انگلیسی کوچک و آندرلاین قابل قبول است';
+$lang['global:user'] 				= 'کاربر';
+
+$lang['global:dialog:delete_message']             = 'آیا مطمئن هستید که می خواهید این مورد را حذف کنید؟ این عمل غیر قابل بازگشت است';
+$lang['global:dialog:confirm'] 			= 'تایید';
+
+$lang['global:yes'] 				= 'بله';
+$lang['global:no'] 				= 'خیر';
+
+$lang['global:select-none'] 			= '-- هیچیک --';
+$lang['global:select-all'] 				= '-- همه --';
+$lang['global:select-any'] 				= '-- هر یک --';
+$lang['global:select-pick'] 			= '-- انتخاب --';
+
+$lang['global:check-all']				= 'همه';
+$lang['global:check-none'] 			= 'هیچ';
+
+$lang['global:operators:equal']                     = 'برابر با';
+$lang['global:operators:not-equal']                 = 'نابرابر با';
+$lang['global:operators:less-than']                 = 'کمتر از';
+$lang['global:operators:greater-than']              = 'بیشتر از';
+
+$lang['global:duration:1-day']			= 'یک روز';
+$lang['global:duration:1-week']			= 'یک هفته';
+$lang['global:duration:2-weeks']                    = 'دو هفته';
+$lang['global:duration:1-month']                    = 'یک ماه';
+$lang['global:duration:3-months']                   = 'سه ماه';
+$lang['global:duration:always']			= 'همیشه';
+
+$lang['navigation_headline'] 			= 'راهبری';
+$lang['breadcrumb_base_label'] 			= 'خانه';
+
+$lang['logged_in_welcome']			= '%s خوش آمدید، شما لاگین کردید.';
+$lang['logout_label'] 				= 'خروج';
+$lang['edit_profile_label']                         = 'ویرایش پروفایل';
+$lang['settings_label'] 				= 'تنظیمات';
+
+$lang['close_message'] 				= 'این پیغام را ببندید';
+$lang['general_error_label'] 			= 'خطا';
+$lang['required_error_label'] 			= 'فیلد های اجباری وارد نشده اند';
+$lang['note_label'] 				= 'توجه';
+$lang['success_label'] 				= 'موفقیت آمیز';
+$lang['global:fields']				= 'فیلدها';
+$lang['global:custom_fields']			= 'فیلد های سفارشی';
+
+$lang['login_title'] 				= 'لاگین';
+$lang['global:fatal_error'] 			= 'خطای اساسی، آیا سیستم مدیریت محتوا نصب شده است؟';
+$lang['global:demo_restrictions']                   = 'برخی موارد در این صفحه غیر فعال شده است که به علت دمو بودن است';
+
+$lang['login_error_label'] 				= 'خطا در لاگین';
+$lang['forgot_password_label'] 			= 'پسوردم را فراموش کرده ام';
+$lang['global:email'] 				= 'ایمیل';
+$lang['global:password'] 				= 'پسورد';
+$lang['login_label'] 				= 'لاگین';
+
+$lang['help_label']                               = 'راهنما';
+$lang['save_label'] 				= 'ذخیره';
+$lang['cancel_label'] 				= 'انصراف';
+$lang['update_label'] 				= 'متن آپدیت';
+$lang['global:delete'] 				= 'حذف';
+$lang['activate_label']             		= 'فعالسازی';
+$lang['global:approve']				= 'غیر فعالسازی';
+$lang['global:unapprove']				= 'عدم تایید';
+$lang['publish_label'] 				= 'انتشار';
+$lang['global:upload'] 				= 'آپلود';
+$lang['global:uninstall'] 				= 'حذف';
+$lang['global:install'] 				= 'نصب';
+$lang['global:upgrade'] 				= 'آپگرید';
+$lang['global:enable']				= 'فعال کردن';
+$lang['global:disable'] 				= 'غیر فعال کردن';
+$lang['global:enabled']				= 'فعال شده';
+$lang['global:disabled'] 				= 'غیر فعال شده';
+
+$lang['all_right_reserved']				= 'تمامی حقوق محفوظ است';
+$lang['powered_by_pyrocms']			= 'قدرت گرفته از PyroCMS';
+$lang['top_page']					= 'بالا';
+$lang['back_to_top']				= 'بازگشت به بالا';
+
+$lang['global:filters'] 				= 'فیلتر ها';
+$lang['global:image'] 				= 'تصویر';
+$lang['global:description']				= 'شرح';
+$lang['global:actions']				= 'عملیات';
+$lang['global:view']				= 'مشاهده';
+$lang['global:preview']				= 'پیش نمایش';
+$lang['global:add']				= 'افزودن';
+$lang['global:edit']				= 'ویرایش';
+$lang['global:empty']				= 'خالی';
+$lang['global:delete']				= 'حذف';
+$lang['global:remove']				= 'برداشتن';
+
+$lang['global:keywords']				= 'کلمات کلیدی';
+
+$lang['global:or']					= 'یا';
+$lang['global:and']				= 'و';
+$lang['global:at']					= 'در';
+
+$lang['global:widgets']				= 'ویجت ها';
+$lang['global:themes']				= 'قالب ها'; #translate
+$lang['global:field_types']         		= 'نوع فیلد ها';
+$lang['global:plugins']				= 'پلاگین ها';
+$lang['global:plugin']				= 'پلاگین';
+
+$lang['global:options']				= 'گزینه ها';
+$lang['global:plain-text']                 		= 'متن ساده';
+
+/* End of file global_lang.php */
