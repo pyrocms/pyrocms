ad37f31b3ece240d614a1d4954c17d093c8d6745
Phil Sturgeon <email@philsturgeon.co.uk>
2013-04-05 00:28:46 -0400

    CodeIgniter 3.0 changed the meaning of :any Now this is "Anything that is not another /" which I believe is different to what it used to be. These even crazier rules seem to take care of the issue at least.

--- system/cms/modules/users/config/routes.php
+++ system/cms/modules/users/config/routes.php
@@ -1,5 +1,5 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
 // Admin Routes
-$route['users/admin/fields(/:any)?']    = 'admin_fields$1';
-$route['users/admin/groups(/:any)?']    = 'admin_groups$1';
+$route['users/admin/fields(/:any)*']    = 'admin_fields$1';
+$route['users/admin/groups(/:any)*']    = 'admin_groups$1';
