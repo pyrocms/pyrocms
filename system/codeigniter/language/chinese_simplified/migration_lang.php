6304dbbeeeffa792e1fcc9d405a7cbf6369502fa
lisatiya <lisatiyamiyu@gmail.com>
2013-04-25 21:47:52 +0200

    added number of migration to migration_not_found error

--- system/codeigniter/language/chinese_simplified/migration_lang.php
+++ system/codeigniter/language/chinese_simplified/migration_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['migration_none_found']			= "迁移功能模块不存在.";
-$lang['migration_not_found']			= "未能发现迁移功能模块.";
+$lang['migration_not_found']			= "未能发现迁移功能模块: %d.";
 $lang['migration_multiple_version']		= "多个迁移模块拥有相同的版本号: %d.";
 $lang['migration_class_doesnt_exist']	= "迁移类 \"%s\" 未能被发现.";
 $lang['migration_missing_up_method']	= "迁移类 \"%s\" 丢失 'up' 功能.";
