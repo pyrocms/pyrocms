88f6a68190ad3bd0188784eac15f0357504aef7a
Nicolas Schneider <nico.tcap@gmail.com>
2013-05-16 20:10:59 +0300

    Update page_types_lang.php
    Rename 'Seitenlayout' (used for 'page type') to 'Seitentyp', because Layout is used for the page layout (!= page type).

--- system/cms/modules/pages/language/german/page_types_lang.php
+++ system/cms/modules/pages/language/german/page_types_lang.php
@@ -3,30 +3,30 @@
 // tabs
 $lang['page_types:html_label']                 = 'HTML';
 $lang['page_types:css_label']                  = 'CSS';
-$lang['page_types:basic_info']                 = 'Basic Info'; #translate
+$lang['page_types:basic_info']                 = 'Informationen';
 
 // labels
 $lang['page_types:updated_label']              = 'Aktualisiert';
-$lang['page_types:layout']                     = 'Layout'; #translate
-$lang['page_types:auto_create_stream']         = 'Create New Stream for this Page Type'; #translate
-$lang['page_types:select_stream']              = 'Stream'; #translate
+$lang['page_types:layout']                     = 'Layout'; 
+$lang['page_types:auto_create_stream']         = 'Neuen Stream für diesen Seitentyp erstellen'; 
+$lang['page_types:select_stream']              = 'Stream'; 
 $lang['page_types:theme_layout_label']         = 'Seitenlayout';
-$lang['page_types:save_as_files']              = 'Save as Files'; #translate
-$lang['page_types:content_label']              = 'Content Tab Label'; #translate
-$lang['page_types:title_label']                = 'Title Label'; #translate
-$lang['page_types:sync_files']                 = 'Sync Files'; #translate
+$lang['page_types:save_as_files']              = 'Als Datei speichern'; 
+$lang['page_types:content_label']              = 'Beschriftung des Inhalte-Tabs'; 
+$lang['page_types:title_label']                = 'Beschriftung des Titels'; 
+$lang['page_types:sync_files']                 = 'Dateien synchronisieren'; 
 
 // titles
-$lang['page_types:list_title']                 = 'Seitentypes auflisten';
-$lang['page_types:list_title_sing']            = 'Page Type'; #translate
-$lang['page_types:create_title']               = 'Seitenlayout hinzuf&uuml;gen';
-$lang['page_types:edit_title']                 = 'Seitenlayout "%s" bearbeiten';
+$lang['page_types:list_title']                 = 'Seitentypen auflisten';
+$lang['page_types:list_title_sing']            = 'Seitentyp';
+$lang['page_types:create_title']               = 'Seitentyp hinzuf&uuml;gen';
+$lang['page_types:edit_title']                 = 'Seitentyp "%s" bearbeiten';
 
 // messages
 $lang['page_types:no_pages']                   = 'Es existieren noch keine Seitentypes.';
-$lang['page_types:create_success_add_fields']  = 'You have created a new page type; now add the fields that you want your page to have.'; #translate
+$lang['page_types:create_success_add_fields']  = 'Ein neuer Seitentyp wurde erstellt; es k&ouml;nnen nun Felder hinzugef&uuml;gt werden.'; 
 $lang['page_types:create_success']             = 'Seitenlayout wurde erstellt.';
-$lang['page_types:success_add_tag']            = 'The page field has been added. However before its data will be output you must insert its tag into the Page Type\'s Layout textarea'; #translate
+$lang['page_types:success_add_tag']            = 'Das Feld wurde erstellt. Bevor die Daten dieses Feldes angezeigt werden k&ouml;nnen, muss der Tag des Feldes zum Layout des Seitentyps hinzugefügt werden.';
 $lang['page_types:create_error']               = 'Ein Fehler ist aufgetreten. Seitenlayout konnte nicht erstellt werden.';
 $lang['page_types:page_type.not_found_error']  = 'Seitenlayout existiert nicht.';
 $lang['page_types:edit_success']               = 'Seitenlayout "%s" wurde gesichert.';
@@ -34,23 +34,23 @@ $lang['page_types:delete_home_error']          = 'Das Standard Seitenlayout kann
 $lang['page_types:delete_success']             = 'Seitenlayout #%s wurde gel&ouml;scht.';
 $lang['page_types:mass_delete_success']        = '%s Seitentypes wurden gel&ouml;scht.';
 $lang['page_types:delete_none_notice']         = 'Es wurde(n) kein(e) Seitenlayout(s) gel&ouml;scht.';
-$lang['page_types:already_exist_error']        = 'A table with that name already exists. Please choose a different name for this page type.'; #translate# slug = title?
-$lang['page_types:_check_pt_slug_msg']         = 'Your page type slug must be unique.'; #translate
+$lang['page_types:already_exist_error']        = 'Ein Eintrag mit diesem Namen existiert bereits. Bitte gib diesem Seitentyp einen anderen Namen.'; 
+$lang['page_types:_check_pt_slug_msg']         = 'Der Seitentyp-Slug muss einzigartig sein.';
 
 $lang['page_types:variable_introduction']      = 'In dieser Eingabebox stehen zwei Variablen zur Auswahl';
 $lang['page_types:variable_title']             = 'Enth&auml;lt den Titel der Seite.';
 $lang['page_types:variable_body']              = 'Enth&auml;lt den HTML Inhalt der Seite.';
-$lang['page_types:sync_notice']                = 'Only able to sync %s from the file system.'; #translate
-$lang['page_types:sync_success']               = 'Files synced successfully.'; #translate
-$lang['page_types:sync_fail']                  = 'Unable to sync your files.'; #translate
+$lang['page_types:sync_notice']                = 'Es konnten nur %s aus dem Dateisystem synchronisiert werden.';
+$lang['page_types:sync_success']               = 'Dateien erfolgreich synchronisiert.'; 
+$lang['page_types:sync_fail']                  = 'Dateien konnten nicht synchronisiert werden.'; 
 
 // Instructions
-$lang['page_types:stream_instructions']        = 'This stream will hold the custom fields for your page type. You can choose a new stream, or one will be created for you.'; #translate
-$lang['page_types:saf_instructions']           = 'Checking this will save your page layout file, as well as any custom CSS and JS as flat files in your assets/page_types folder.'; #translate
-$lang['page_types:content_label_instructions'] = 'This renames the tab that holds your page type stream fields.'; #translate
-$lang['page_types:title_label_instructions']   = 'This renames the page title field from "Title". This is useful if you are using "Title" as something else, like "Product Name" or "Team Member Name".'; #translate
+$lang['page_types:stream_instructions']        = 'Dieser Stream beinhaltet die Daten des Seitentyps. Du kannst einen Stream ausw&auml;hlen, oder es wird ein neuer Stream angelegt.'; 
+$lang['page_types:saf_instructions']           = 'Wenn du diese Option anw&auml;hlst, wird das Layout dieses Seitentyps als Datei gespeichert.';
+$lang['page_types:content_label_instructions'] = 'Dieser Name wird für die Beschriftung des Inhalte-Tab verwendet.';
+$lang['page_types:title_label_instructions']   = 'Dieser Name wird anstelle des "Titels" verwendet. Dies ist nütlich für Seitentypen wie "Produkte", wo anstatt eines Titel der "Produktname" angebrachter ist.';
 
 // Misc
-$lang['page_types:delete_message']             = 'Are you sure you want to delete this page type? This will delete <strong>%s</strong> pages using this layout, any page children of these pages, and any stream entries associated with these pages. <strong>This cannot be undone.</strong> '; #translate
+$lang['page_types:delete_message']             = 'Bist du sicher, dass du diesen Seitentyp löschen möchtest? Diese Aktion wird <strong>%s</strong> Seiten, all ihre Unterseiten und Stream-Einträge löschen. <strong>Diese Aktion kann nicht rückgängig gemacht werden!</strong>';
 
-$lang['page_types:delete_streams_message']     = 'This will also delete the <strong>%s stream</strong> associated with this page type.'; #translate
+$lang['page_types:delete_streams_message']     = 'Diese Aktion löscht auch den <strong>%s Stream</strong> dieses Seitentyps.';
