5cb1381f19a6181684226f9d69fdfdcce855a604
Nicolas Schneider <nico.tcap@gmail.com>
2013-05-16 19:13:37 +0200

    Update permission_lang.php

--- system/cms/modules/pages/language/german/permission_lang.php
+++ system/cms/modules/pages/language/german/permission_lang.php
@@ -1,11 +1,11 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // Page Permissions
-$lang['pages:role_put_live']    = 'Put pages live'; #translate
-$lang['pages:role_edit_live']   = 'Edit live pages'; #translate
-$lang['pages:role_delete_live'] = 'Delete live articles'; #translate
+$lang['pages:role_put_live']    = 'Seiten veröffentlichen';
+$lang['pages:role_edit_live']   = 'Veröffentlichte Seiten bearbeiten'; 
+$lang['pages:role_delete_live'] = 'Veröffentlichte Seiten löschen'; 
 
 // Page Type Permissions
-$lang['pages:role_create_types'] = 'Create Types'; #translate
-$lang['pages:role_edit_types']   = 'Edit Types'; #translate
-$lang['pages:role_delete_types'] = 'Delete Types'; #translate
+$lang['pages:role_create_types'] = 'Seitentypen erstellen';
+$lang['pages:role_edit_types']   = 'Seitentypen bearbeiten';
+$lang['pages:role_delete_types'] = 'Seitentypen löschen';
