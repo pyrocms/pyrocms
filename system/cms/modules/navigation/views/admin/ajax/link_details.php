49df56480190c1a1720d1664382dab64521d65cf
Ryan Thompson <service@aiwebsystems.com>
2013-07-21 15:35:21 -0500

    Update Navigation module views

--- system/cms/modules/navigation/views/admin/ajax/link_details.php
+++ system/cms/modules/navigation/views/admin/ajax/link_details.php
@@ -1,5 +1,7 @@
-<div id="details-container">
-	<h4><?php echo $link->title ?></h4>
+<div class="padding-left">
+
+
+	<h4 class="no-margin padding-top padding-bottom"><?php echo $link->title ?></h4>
 
 	<input id="link-id" type="hidden" value="<?php echo $link->id ?>" />
 	<input id="link-uri" type="hidden" value="<?php echo ! empty($link->uri) ? $link->uri : '' ?>" />
@@ -13,31 +15,33 @@
 		<p>
 			<strong><?php echo lang('global:title');?>:</strong> <?php echo $link->title ?>
 		</p>
-
+		
 		<p>
 			<strong><?php echo lang('nav:target_label');?>:</strong> <?php echo (!empty($link->target)) ? lang('nav:link_target_blank') : lang('nav:link_target_self') ?>
 		</p>
-
+		
 		<p>
 			<strong><?php echo lang('nav:class_label');?>:</strong> <?php echo $link->class ?>
 		</p>
-
+		
 		<p>
 			<strong><?php echo lang('nav:type_label');?>:</strong> <?php echo $link->link_type ?>
 		</p>
-
+		
 		<p>
 			<strong><?php echo lang('nav:location_label');?>:</strong>
 			<a target="_blank" href="<?php echo $link->url ?>"><?php echo $link->url ?></a>
 		</p>
-
+		
 		<p>
 			<strong><?php echo lang('nav:restricted_to');?>:</strong> <?php echo $link->restricted_to ?>
 		</p>
-	</fieldset>
+	</fieldset>	
 
-	<div class="buttons">
-		<?php echo anchor('admin/navigation/edit/' . $link->id, lang('global:edit'), 'rel="'.$link->navigation_group_id.'" class="button ajax"') ?>
-		<?php echo anchor('admin/navigation/delete/' . $link->id, lang('global:delete'), 'class="confirm button"') ?>
+	<div class="btn-group padding-top padding-bottom">
+		<?php echo anchor('admin/navigation/edit/' . $link->id, lang('global:edit'), 'rel="'.$link->navigation_group_id.'" class="btn btn-small btn-warning ajax"') ?>
+		<?php echo anchor('admin/navigation/delete/' . $link->id, lang('global:delete'), 'class="confirm btn btn-small btn-danger"') ?>
 	</div>
+
+
 </div>
--- system/cms/modules/navigation/views/admin/index.php
+++ system/cms/modules/navigation/views/admin/index.php
@@ -1,81 +1,119 @@
-<?php if ( ! empty($groups)): ?>
+<section class="padded">
+<div class="container-fluid">
+
+
+	<?php if ( ! empty($groups)): ?>
 	<?php foreach ($groups as $group): ?>
 
-		<section rel="<?php echo $group->id ?>" class="group-<?php echo $group->id ?> box">
-			<div class="one_full">
-				<section class="title files-title">
-					<ul>
-						<li>
-							<h4 class="tooltip" title="<?php echo lang('nav:abbrev_label').': '.$group->abbrev ?>"><?php echo $group->title;?></h4>
-						</li>
 
-						<li><?php echo anchor('admin/navigation/groups/delete/'.$group->id, lang('global:delete'), array('class' => 'tooltip-e confirm btn button red',  'title' => lang('nav:group_delete_confirm'))) ?></li>
-						<li>
-							<!-- <h4 class="form-title group-title-<?php echo $group->id ?>"></h4> -->
-							<?php echo anchor('admin/navigation/create/'.$group->id, lang('nav:link_create_title'), 'rel="'.$group->id.'" class="add ajax button"') ?>
-						</li>
-					</ul>
+		<!-- Box -->
+		<section class="box">
 
-				</section>
+			<!-- Header -->
+			<section class="box-header">
+				<span class="title" title="<?php echo lang('nav:abbrev_label').': '.$group->abbrev ?>"><?php echo $group->title;?></span>
+
+				<!-- Toolbar -->
+				<div class="box-toolbar">
+
+					<div class="btn-group pull-right margin-right">
+						<?php echo anchor('admin/navigation/create/'.$group->id, lang('nav:link_create_title'), 'rel="'.$group->id.'" class="add ajax btn btn-small btn-success"') ?>
+						<?php echo anchor('admin/navigation/groups/delete/'.$group->id, lang('global:delete'), array('class' => 'tooltip-e confirm btn btn-small btn-danger',  'title' => lang('nav:group_delete_confirm'))) ?>
+					</div>
+				</div>
+			</section>
+
+
+			<!-- Box Content -->
+			<section class="box-content">
 
 				<?php if ( ! empty($navigation[$group->id])): ?>
 
-				<section class="item collapsed">
-						<div class="content">
-							<div class="one_half">
-								<div id="link-list">
-									<ul class="sortable">
-										<?php echo tree_builder($navigation[$group->id], '<li id="link_{{ id }}"><div><a href="#" rel="'.$group->id.'" alt="{{ id }}">{{ title }}</a></div>{{ children /}}</li>') ?>
-									</ul>
-								</div>
+
+					<div class="row-fluid">
+				
+						<div class="span6">
+
+							<div class="dd links-list-wrapper padded">
+							
+								<ul class="dd-list">
+
+									<?php echo tree_builder($navigation[$group->id], '<li class="dd-item" id="link_{{ id }}"><div class="dd-handle dd3-handle"></div><div class="dd3-content"><a href="#" rel="'.$group->id.'" alt="{{ id }}">{{ title }}</a></div>{{ children }}</li>') ?>
+
+								</ul>
+
 							</div>
 
-							<div class="one_half last">
-								<div id="link-details" class="group-<?php echo $group->id ?>">
-
-									<p>
-										<?php echo lang('navs.tree_explanation') ?>
-									</p>
-
-								</div>
-							</div>
 						</div>
-					</section>
-				</div>
+
+						<div class="span6">
+
+							<div class="link-details border-left border-color-grayLighter">
+
+								<p><?php echo lang('navs.tree_explanation') ?></p>
+
+							</div>
+
+						</div>
+
+					</div>
+				
 
 				<?php else:?>
 
-				<section class="item collapsed">
-					<div class="content">
-						<div class="one_half">
-							<div id="link-list" class="empty">
-								<ul class="sortable">
+					<div class="row-fluid">
+				
+						<div class="span6">
 
-									<p><?php echo lang('nav:group_no_links');?></p>
+							<p class="alert margin"><?php echo lang('nav:group_no_links');?></p>
 
-								</ul>
-							</div>
 						</div>
 
-						<div class="one_half last">
-							<div id="link-details" class="group-<?php echo $group->id ?>">
+						<div class="span6">
 
-								<p>
-									<?php echo lang('navs.tree_explanation') ?>
-								</p>
+							<div class="link-details border-left border-color-grayLighter">
+
 
 							</div>
+
 						</div>
+
 					</div>
-				</section>
+
 				<?php endif ?>
 
+			</section>
+			<!-- /Box Content -->
+
 		</section>
+		<!-- /Box -->
+
 
 	<?php endforeach ?>
+	<?php else: ?>
 
-<?php else: ?>
-	<div class="blank-slate">
-		<p><?php echo lang('nav:no_groups');?></p>
-	</div>
-<?php endif;
+		<!-- Box -->
+		<section class="box">
+
+			<!-- Header -->
+			<section class="box-header">
+				<span class="title"><?php echo lang('blog:posts_title') ?></span>
+			</section>
+
+
+			<!-- Box Content -->
+			<section class="box-content">
+
+				<p class="alert margin"><?php echo lang('nav:no_groups');?></p>
+
+			</section>
+			<!-- /Box Content -->
+
+		</section>
+		<!-- /Box -->
+
+	<?php endif ?>
+
+
+</div>
+</section>
