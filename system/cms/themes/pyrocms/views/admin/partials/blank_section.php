0bf4a23908e977bb7d64e1c0a3134a9cc7c0fb77
Ryan Thompson <service@aiwebsystems.com>
2013-07-21 15:00:21 -0500

    Bai admin theme

--- system/cms/themes/pyrocms/views/admin/partials/blank_section.php
+++
@@ -1,9 +0,0 @@
-<section class="title">
-	<?php if(isset($template['page_title'])) { echo '<h4>'.lang_label($template['page_title']).'</h4>'; } ?>
-</section>
-
-<section class="item">
-	<div class="content">
-		<?php echo $content; ?>
-	</div>
-</section>
