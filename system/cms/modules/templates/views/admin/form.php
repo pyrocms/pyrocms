2dc17490d8710fc96150bf5902b6055f13d9eda3
Ryan Thompson <service@aiwebsystems.com>
2013-07-21 15:41:22 -0500

    Update Templates module views

--- system/cms/modules/templates/views/admin/form.php
+++ system/cms/modules/templates/views/admin/form.php
@@ -1,64 +1,83 @@
-<div class="one_full">
-<?php if($this->method == 'edit' and ! empty($email_template)): ?>
-	<section class="title">
-    	<h4><?php echo sprintf(lang('templates:edit_title'), $email_template->name) ?></h4>
-	</section>
-<?php else: ?>
-	<section class="title">
-    	<h4><?php echo lang('templates:create_title') ?></h4>
-	</section>
-<?php endif ?>
+<section class="padded">
+<div class="container-fluid">
 
-<section class="item">
-	<div class="content">
-	<?php echo form_open(current_url(), 'class="crud"') ?>
 
-		<div class="form_inputs">
+	<!-- Box -->
+	<section class="box">
 
-			<ul>
-
-				<?php if ( ! $email_template->is_default): ?>
-				<li class="<?php echo alternator('even', '') ?>">
-					<label for="name"><?php echo lang('name_label') ?> <span>*</span></label>
-					<div class="input"><?php echo form_input('name', $email_template->name) ?></div>
-				</li>
-
-				<li  class="<?php echo alternator('even', '') ?>">
-					<label for="slug"><?php echo lang('templates:slug_label') ?> <span>*</span></label>
-					<div class="input"><?php echo form_input('slug', $email_template->slug) ?></div>
-				</li>
-
-				<li class="<?php echo alternator('even', '') ?>">
-					<label for="lang"><?php echo lang('templates:language_label') ?></label>
-					<div class="input"><?php echo form_dropdown('lang', $lang_options, array($email_template->lang)) ?>
-				</li>
-
-				<li class="<?php echo alternator('even', '') ?>">
-					<label for="description"><?php echo lang('desc_label') ?> <span>*</span></label>
-					<div class="input"><?php echo form_input('description', $email_template->description) ?></div>
-				</li>
-
+		<!-- Header -->
+		<section class="box-header">
+			<span class="title">
+				<?php if($this->method == 'edit' and ! empty($email_template)): ?>
+					<?php echo sprintf(lang('templates:edit_title'), $email_template->name) ?>
+				<?php else: ?>
+					<?php echo lang('templates:create_title') ?>
 				<?php endif ?>
-				<li class="<?php echo alternator('even', '') ?>">
-					<label for="subject"><?php echo lang('templates:subject_label') ?> <span>*</span></label>
-					<div class="input"><?php echo form_input('subject', $email_template->subject) ?></div>
-				</li>
+			</span>
+		</section>
 
-				<li class="<?php echo alternator('even', '') ?>">
-					<label for="body"><?php echo lang('templates:body_label') ?> <span>*</span></label>
-					<br style="clear:both" />
-					<?php echo form_textarea('body', $email_template->body, 'class="templates wysiwyg-advanced"') ?>
-				</li>
 
-			</ul>
+		<!-- Box Content -->
+		<section class="box-content">
 
-			<div class="buttons padding-top">
-				<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel') )) ?>
-			</div>
 
-		</div>
+			<?php echo form_open(current_url(), 'class="crud"') ?>
+			
+				<fieldset class="padding-top">
+				
+					<ul>
+					
+						<?php if ( ! $email_template->is_default): ?>
+						<li class="row-fluid input-row">
+							<label class="span3" for="name"><?php echo lang('name_label') ?> <span>*</span></label>
+							<div class="input span9"><?php echo form_input('name', $email_template->name) ?></div>
+						</li>
+						
+						<li class="row-fluid input-row">
+							<label class="span3" for="slug"><?php echo lang('templates:slug_label') ?> <span>*</span></label>
+							<div class="input span9"><?php echo form_input('slug', $email_template->slug) ?></div>
+						</li>
+						
+						<li class="row-fluid input-row">
+							<label class="span3" for="lang"><?php echo lang('templates:language_label') ?></label>
+							<div class="input span9"><?php echo form_dropdown('lang', $lang_options, array($email_template->lang)) ?>
+						</li>
+						
+						<li class="row-fluid input-row">
+							<label class="span3" for="description"><?php echo lang('desc_label') ?> <span>*</span></label>
+							<div class="input span9"><?php echo form_input('description', $email_template->description) ?></div>
+						</li>
+						
+						<?php endif ?>
+						<li class="row-fluid input-row">
+							<label class="span3" for="subject"><?php echo lang('templates:subject_label') ?> <span>*</span></label>
+							<div class="input span9"><?php echo form_input('subject', $email_template->subject) ?></div>
+						</li>
+					
+						<li class="row-fluid input-row">
+							<label for="body"><?php echo lang('templates:body_label') ?> <span>*</span></label>
+							<br style="clear:both" />
+							<div class="padded no-padding-top">
+								<?php echo form_textarea('body', $email_template->body, 'class="templates wysiwyg-advanced"') ?>
+							</div>
+						</li>
+					
+					</ul>
+				
+					<div class="btn-group padded no-padding-bottom">
+						<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel') )) ?>
+					</div>
+			
+				</div>
+						
+			<?php echo form_close() ?>
 
-	<?php echo form_close() ?>
-	</div>
-</section>
+
+		</section>
+		<!-- /Box Content -->
+
+	</section>
+	<!-- /Box -->
+
 </div>
+</section>
--- system/cms/modules/templates/views/admin/index.php
+++ system/cms/modules/templates/views/admin/index.php
@@ -1,102 +1,156 @@
-<div class="one_full">
-	<section class="title">
-		<h4><?php echo lang('templates:default_title') ?></h4>
-	</section>
+<?php if(!empty($templates)): ?>
 
-	<section class="item">
-		<div class="content">
+<section class="padded">
+<div class="container-fluid">
 
-		    <table border="0" class="table-list" cellspacing="0">
+
+	<!-- Box -->
+	<section class="box">
+
+		<!-- Header -->
+		<section class="box-header">
+			<span class="title"><?php echo lang('templates:default_title') ?></span>
+		</section>
+
+
+		<!-- Box Content -->
+		<section class="box-content">
+
+
+		    <?php echo form_open('admin/templates/action') ?>
+		
+		    <table class="table table-hover table-striped">
 		        <thead>
 		            <tr>
+		                <th><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
 		                <th><?php echo lang('name_label') ?></th>
 		                <th class="collapse"><?php echo lang('global:description') ?></th>
 		                <th class="collapse"><?php echo lang('templates:language_label') ?></th>
 		                <th width="220"></th>
 		            </tr>
 		        </thead>
-
+		
 		        <tbody>
-
-		    <?php foreach ($default_templates as $template): ?>
+				
+		    <?php foreach ($templates as $template): ?>
+				<?php if($template->is_default): ?>
 		            <tr>
+						<td><?php echo form_checkbox('action_to[]', $template->id);?></td>
 		                <td><?php echo $template->name ?></td>
 		                <td class="collapse"><?php echo $template->description ?></td>
 		                <td class="collapse"><?php echo $template->lang ?></td>
-		                <td class="actions">
-						<div class="buttons buttons-small align-center">
-							<?php echo anchor('admin/templates/preview/' . $template->id, lang('buttons:preview'), 'class="button preview modal"') ?>
-		                    <?php echo anchor('admin/templates/edit/' . $template->id, lang('buttons:edit'), 'class="button edit"') ?>
-							<?php echo anchor('admin/templates/create_copy/' . $template->id, lang('buttons:clone'), 'class="button clone"') ?>
-						</div>
+		                <td>
+
+							<div class="btn-group pull-right">
+								<?php echo anchor('admin/templates/preview/' . $template->id, lang('buttons:preview'), 'class="btn btn-small" data-toggle="modal"') ?>
+								<?php echo anchor('admin/templates/edit/' . $template->id, lang('buttons:edit'), 'class="btn btn-small btn-warning"') ?>
+								<?php echo anchor('admin/templates/create_copy/' . $template->id, lang('buttons:clone'), 'class="btn btn-small"') ?>
+							</div>
 		                </td>
 		            </tr>
+				<?php endif ?>
 		    <?php endforeach ?>
 			</tbody>
 			</table>
-
-		</div>
-	</section>
-</div>
-
-<?php if(! $defined_templates->isEmpty()): ?>
-<div class="one_full">
-	<section class="title">
-		<h4><?php echo lang('templates:user_defined_title') ?></h4>
-	</section>
-
-	<?php echo form_open('admin/templates/delete') ?>
-
-	<section class="item">
-		<div class="content">
-			<table border="0" class="table-list clear-both" cellspacing="0">
-		        <thead>
-		            <tr>
-		                <th><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
-		                <th><?php echo lang('name_label') ?></th>
-		                <th><?php echo lang('global:description') ?></th>
-		                <th><?php echo lang('templates:language_label') ?></th>
-		                <th width="220"></th>
-		            </tr>
-		        </thead>
-
-		        <tbody>
-
-			    <?php foreach ($defined_templates as $template): ?>
-			            <tr>
-							<td><?php echo form_checkbox('action_to[]', $template->id);?></td>
-			                <td><?php echo $template->name ?></td>
-			                <td><?php echo $template->description ?></td>
-			                <td><?php echo $template->lang ?></td>
-			                <td class="actions">
-							<div class="buttons buttons-small align-center">
-								<?php echo anchor('admin/templates/preview/' . $template->id, lang('buttons:preview'), 'class="button preview modal"') ?>
-			                    <?php echo anchor('admin/templates/edit/' . $template->id, lang('buttons:edit'), 'class="button edit"') ?>
-								<?php echo anchor('admin/templates/delete/' . $template->id, lang('buttons:delete'), 'class="button delete"') ?>
-							</div>
-			                </td>
-			            </tr>
-			    <?php endforeach ?>
-		        </tbody>
-		    </table>
-
-			<div class="table_action_buttons">
+		    <?php echo form_close() ?>
+		 
+		 	<div class="btn-group padded no-padding-top">
 				<?php $this->load->view('admin/partials/buttons', array('buttons' => array('delete') )) ?>
 			</div>
+		
 
-		    <?php echo form_close() ?>
-		</div>
+		</section>
+		<!-- /Box Content -->
+
 	</section>
+	<!-- /Box -->
+
+
+
+
+	<!-- Box -->
+	<section class="box">
+
+		<!-- Header -->
+		<section class="box-header">
+			<span class="title"><?php echo lang('templates:user_defined_title') ?></span>
+		</section>
+
+
+		<!-- Box Content -->
+		<section class="box-content">
+
+
+			<?php echo form_open('admin/templates/delete') ?>
+		   
+				<table class="table table-hover table-striped clear-both">
+			        <thead>
+			            <tr>
+			                <th><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
+			                <th><?php echo lang('name_label') ?></th>
+			                <th><?php echo lang('global:description') ?></th>
+			                <th><?php echo lang('templates:language_label') ?></th>
+			                <th width="220"></th>
+			            </tr>
+			        </thead>
+			
+			        <tbody>
+				
+				    <?php foreach ($templates as $template): ?>
+						<?php if(!$template->is_default): ?>
+				            <tr>
+								<td><?php echo form_checkbox('action_to[]', $template->id);?></td>
+				                <td><?php echo $template->name ?></td>
+				                <td><?php echo $template->description ?></td>
+				                <td><?php echo $template->lang ?></td>
+				                <td class="actions">
+
+									<div class="btn-group pull-right">
+										<?php echo anchor('admin/templates/preview/' . $template->id, lang('buttons:preview'), 'class="btn btn-small" data-toggle="modal"') ?>
+										<?php echo anchor('admin/templates/edit/' . $template->id, lang('buttons:edit'), 'class="btn btn-small btn-warning"') ?>
+										<?php echo anchor('admin/templates/delete/' . $template->id, lang('buttons:delete'), 'class="btn btn-small btn-danger"') ?>
+									</div>
+
+				                </td>
+				            </tr>
+						<?php endif ?>
+				    <?php endforeach ?>
+					
+					
+			        </tbody>
+			    </table>
+			
+				<div class="btn-group padding-left padding-right">
+					<?php $this->load->view('admin/partials/buttons', array('buttons' => array('delete') )) ?>
+				</div>
+			
+			<?php echo form_close() ?>
+
+		</section>
+		<!-- /Box Content -->
+
+	</section>
+	<!-- /Box -->
+
+
 </div>
+</section>
 
 <?php else: ?>
 
-<div class="one_full">
-	<section class="item">
-		<div class="content">
-	    <div class="no_data"><?php echo lang('templates:currently_no_templates') ?></div>
-		</div>
+	<!-- Box -->
+	<section class="box">
+
+		<!-- Box Content -->
+		<section class="box-content">
+
+		    <p class="alert margin"><?php echo lang('templates:currently_no_templates') ?></p>
+
+		</section>
+		<!-- /Box Content -->
+
 	</section>
-</div>
+	<!-- /Box -->		   
+
 
 <?php endif ?>
