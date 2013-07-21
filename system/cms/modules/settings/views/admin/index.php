a82a63a244c02812d7a051b3e876144ff00525bb
Ryan Thompson <service@aiwebsystems.com>
2013-07-21 15:40:39 -0500

    Update Settings module views

--- system/cms/modules/settings/views/admin/index.php
+++ system/cms/modules/settings/views/admin/index.php
@@ -1,57 +1,82 @@
-<section class="title">
-	<h4><?php echo $module_details['name'] ?></h4>
-</section>
+<section class="padded">
+<div class="container-fluid">
 
-<section class="item">
-	<div class="content">
-		<?php if ($setting_sections): ?>
-			<?php echo form_open('admin/settings/edit', 'class="crud"');?>
 
-				<div class="tabs">
+	<!-- Box -->
+	<section class="box">
 
-					<ul class="tab-menu">
+		<!-- Header -->
+		<section class="box-header">
+			<span class="title"><?php echo $module_details['name'] ?></span>
+		</section>
+
+
+		<!-- Box Content -->
+		<section class="box-content">
+
+
+			<?php if ($setting_sections): ?>
+				<?php echo form_open('admin/settings/edit', 'class="crud"');?>
+			
+					
+					<!-- Tabs -->
+					<ul class="nav nav-tabs padded no-padding-bottom grayLightest-bg">
 						<?php foreach ($setting_sections as $section_slug => $section_name): ?>
-						<li>
-							<a href="#<?php echo $section_slug ?>" title="<?php printf(lang('settings:section_title'), $section_name) ?>">
+						<li <?php echo current(array_keys($setting_sections)) == $section_slug ? 'class="active"' : null;?>>
+							<a href="#<?php echo $section_slug ?>" title="<?php printf(lang('settings:section_title'), $section_name) ?>" data-toggle="tab">
 								<span><?php echo $section_name ?></span>
 							</a>
 						</li>
 						<?php endforeach ?>
 					</ul>
+					<!-- /Tabs -->
 
-					<?php foreach ($setting_sections as $section_slug => $section_name): ?>
-					<div class="form_inputs" id="<?php echo $section_slug;?>">
-						<fieldset>
-							<ul>
-							<?php $section_count = 1; foreach ($settings[$section_slug] as $setting): ?>
-								<li id="<?php echo $setting->slug ?>" class="<?php echo $section_count++ % 2 == 0 ? 'even' : '' ?>">
-									<label for="<?php echo $setting->slug ?>">
-										<?php echo $setting->title ?>
-										<?php if($setting->description): echo '<small>'.$setting->description.'</small>'; endif ?>
-									</label>
+			
+					<!-- Tab Content -->
+					<div class="tab-content">
 
-									<div class="input <?php echo 'type-'.$setting->type ?>">
-										<?php echo $setting->form_control ?>
-									</div>
-									<span class="move-handle"></span>
-								</li>
-							<?php endforeach ?>
-							</ul>
-						</fieldset>
+						<?php foreach ($setting_sections as $section_slug => $section_name): ?>
+						<div class="tab-pane <?php echo current(array_keys($setting_sections)) == $section_slug ? 'active' : null;?>" id="<?php echo $section_slug;?>">
+							<fieldset>
+								<ul>
+								<?php $section_count = 1; foreach ($settings[$section_slug] as $setting): ?>
+									<li class="row-fluid input-row">
+										<label class="span3" for="<?php echo $setting->slug ?>">
+											<?php echo $setting->title ?>
+											<?php if($setting->description): echo '<small>'.$setting->description.'</small>'; endif ?>
+										</label>
+			
+										<div class="input span9 <?php echo 'type-'.$setting->type ?>">
+											<?php echo $setting->form_control ?>
+										</div>
+										<span class="move-handle"></span>
+									</li>
+								<?php endforeach ?>
+								</ul>
+							</fieldset>
+						</div>
+						<?php endforeach ?>
+			
 					</div>
-					<?php endforeach ?>
-
+					<!-- /Tab Content -->
+			
+					<div class="btn-group padded no-padding-bottom">
+						<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save') )) ?>
+					</div>
+			
+				<?php echo form_close() ?>
+			<?php else: ?>
+				<div>
+					<p><?php echo lang('settings:no_settings');?></p>
 				</div>
+			<?php endif ?>
 
-				<div class="buttons padding-top">
-					<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save') )) ?>
-				</div>
 
-			<?php echo form_close() ?>
-		<?php else: ?>
-			<div>
-				<p><?php echo lang('settings:no_settings');?></p>
-			</div>
-		<?php endif ?>
-	</div>
+		</section>
+		<!-- /Box Content -->
+
+	</section>
+	<!-- /Box -->
+
+</div>
 </section>
