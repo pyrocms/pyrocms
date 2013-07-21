db53300828a639ae7a41c2fbe2545c822dd1befb
Ryan Thompson <service@aiwebsystems.com>
2013-07-21 15:39:49 -0500

    Update Redirects module views

--- system/cms/modules/redirects/views/admin/index.php
+++ system/cms/modules/redirects/views/admin/index.php
@@ -1,60 +1,78 @@
-<section class="title">
-	<h4><?php echo lang('redirects:list_title') ?></h4>
+<section class="padded">
+<div class="container-fluid">
+
+
+	<!-- Box -->
+	<section class="box">
+
+		<!-- Header -->
+		<section class="box-header">
+			<span class="title"><?php echo lang('redirects:list_title') ?></span>
+		</section>
+
+
+		<!-- Box Content -->
+		<section class="box-content">
+
+
+			<?php if ($redirects): ?>
+
+				
+			    <?php echo form_open('admin/redirects/delete') ?>
+					
+					<table class="table table-hover table-striped">
+					    
+					    <thead>
+							<tr>
+								<th width="15"><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
+								<th width="25"><?php echo lang('redirects:type');?></th>
+								<th width="25%"><?php echo lang('redirects:from');?></th>
+								<th><?php echo lang('redirects:to');?></th>
+								<th width="200"></th>
+							</tr>
+					    </thead>
+						
+						<tbody>
+						<?php foreach ($redirects as $redirect): ?>
+						    <tr>
+							<td><?php echo form_checkbox('action_to[]', $redirect->id) ?></td>
+							<td><?php echo $redirect->type;?></td>
+							<td><?php echo str_replace('%', '*', $redirect->from);?></td>
+							<td><?php echo $redirect->to;?></td>
+							<td class="text-center">
+							<div class="actions">
+							    <?php echo anchor('admin/redirects/edit/' . $redirect->id, lang('redirects:edit'), 'class="button edit"');?>
+								<?php echo anchor('admin/redirects/delete/' . $redirect->id, lang('redirects:delete'), array('class'=>'confirm button delete'));?>
+							</div>
+							</td>
+						    </tr>
+						<?php endforeach ?>
+					    </tbody>
+
+					</table>
+				
+					<div class="btn-group">
+						<?php $this->load->view('admin/partials/buttons', array('buttons' => array('delete') )) ?>
+					</div>
+
+			    <?php echo form_close() ?>
+
+
+			    <?php $this->load->view('admin/partials/pagination') ?>
+
+
+			<?php else: ?>
+				
+				<div class="alert margin"><?php echo lang('redirects:no_redirects');?></div>
+				
+			<?php endif ?>
+
+
+		</section>
+		<!-- /Box Content -->
+
+	</section>
+	<!-- /Box -->
+
+</div>
 </section>
-
-<?php if ( ! $redirects->isEmpty()): ?>
-
-	<section class="item">
-		<div class="content">
-
-	    <?php echo form_open('admin/redirects/delete') ?>
-		<table border="0" class="table-list" cellspacing="0">
-		    <thead>
-				<tr>
-					<th width="15"><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
-					<th width="25"><?php echo lang('redirects:type');?></th>
-					<th width="25%"><?php echo lang('redirects:from');?></th>
-					<th><?php echo lang('redirects:to');?></th>
-					<th width="200"></th>
-				</tr>
-		    </thead>
-			<tfoot>
-				<tr>
-					<td colspan="5">
-						<div class="inner"><?php $this->load->view('admin/partials/pagination') ?></div>
-					</td>
-				</tr>
-			</tfoot>
-		    <tbody>
-			<?php foreach ($redirects as $redirect): ?>
-			    <tr>
-				<td><?php echo form_checkbox('action_to[]', $redirect->id) ?></td>
-				<td><?php echo $redirect->type;?></td>
-				<td><?php echo str_replace('%', '*', $redirect->from);?></td>
-				<td><?php echo $redirect->to;?></td>
-				<td class="align-center">
-				<div class="actions">
-				    <?php echo anchor('admin/redirects/edit/' . $redirect->id, lang('redirects:edit'), 'class="button edit"');?>
-					<?php echo anchor('admin/redirects/delete/' . $redirect->id, lang('redirects:delete'), array('class'=>'confirm button delete'));?>
-				</div>
-				</td>
-			    </tr>
-			<?php endforeach ?>
-		    </tbody>
-		</table>
-
-		<div class="table_action_buttons">
-			<?php $this->load->view('admin/partials/buttons', array('buttons' => array('delete') )) ?>
-		</div>
-	    <?php echo form_close() ?>
-
-		</div>
-	</section>
-
-<?php else: ?>
-	<section class="item">
-		<div class="content">
-			<div class="no_data"><?php echo lang('redirects:no_redirects');?></div>
-		</div>
-	</section>
-<?php endif ?>
