bd6a2fe4e562029d53fa4cfd947cf24bc7b51ed7
Ryan Thompson <service@aiwebsystems.com>
2013-07-21 15:31:51 -0500

    Update Keywords module views

--- system/cms/modules/keywords/views/admin/index.php
+++ system/cms/modules/keywords/views/admin/index.php
@@ -1,43 +1,63 @@
-<section class="title">
-	<h4><?php echo $module_details['name'] ?></h4>
-</section>
+<section class="padded">
+<div class="container-fluid">
 
-<section class="item">
-<div class="content">
 
-<?php if ( ! $keywords->isEmpty()): ?>
-    <table class="table-list" cellspacing="0">
-		<thead>
-			<tr>
-				<th width="40%"><?php echo lang('keywords:name');?></th>
-				<th width="200"></th>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td colspan="3">
-					<div class="inner"><?php $this->load->view('admin/partials/pagination') ?></div>
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-		<?php foreach ($keywords as $keyword):?>
-			<tr>
-				<td><?php echo $keyword->name ?></td>
-				<td class="actions">
-				<?php echo anchor('admin/keywords/edit/'.$keyword->id, lang('global:edit'), 'class="button edit"') ?>
-				<?php if ( ! in_array($keyword->name, array('user', 'admin'))): ?>
-					<?php echo anchor('admin/keywords/delete/'.$keyword->id, lang('global:delete'), 'class="confirm button delete"') ?>
-				<?php endif ?>
-				</td>
-			</tr>
-		<?php endforeach;?>
-		</tbody>
-    </table>
+	<!-- Box -->
+	<section class="box">
 
-<?php else: ?>
-	<div class="no_data"><?php echo lang('keywords:no_keywords');?></div>
-<?php endif;?>
+		<!-- Header -->
+		<section class="box-header">
+			<span class="title"><?php echo $module_details['name'] ?></span>
+		</section>
+
+
+		<!-- Box Content -->
+		<section class="box-content">
+
+
+			<?php if ($keywords): ?>
+			    <table class="table table-hover table-striped">
+					
+					<thead>
+						<tr>
+							<th width="40%"><?php echo lang('keywords:name');?></th>
+							<th width="200"></th>
+						</tr>
+					</thead>
+					
+					<tbody>
+					<?php foreach ($keywords as $keyword):?>
+						<tr>
+							<td><?php echo $keyword->name ?></td>
+							<td>
+
+								<div class="btn-group pull-right">
+									<?php echo anchor('admin/keywords/edit/'.$keyword->id, lang('global:edit'), 'class="btn btn-small btn-warning edit"') ?>
+									<?php if ( ! in_array($keyword->name, array('user', 'admin'))): ?>
+										<?php echo anchor('admin/keywords/delete/'.$keyword->id, lang('global:delete'), 'class="confirm btn btn-small btn-danger"') ?>
+									<?php endif ?>
+								</div>
+
+							</td>
+						</tr>
+					<?php endforeach;?>
+					</tbody>
+
+			    </table>
+
+			<?php else: ?>
+				<div class="alert margin"><?php echo lang('keywords:no_keywords');?></div>
+			<?php endif;?>
+
+
+			<?php $this->load->view('admin/partials/pagination') ?>
+			
+
+		</section>
+		<!-- /Box Content -->
+
+	</section>
+	<!-- /Box -->
 
 </div>
 </section>
