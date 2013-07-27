81e4c92f2a52b6e5a65ec34fd5131065a23fa495
Osvaldo Brignoni <obrignoni@gmail.com>
2013-07-27 18:23:14 -0400

    Updated the Variables module to use Streams

--- system/cms/modules/variables/details.php
+++ system/cms/modules/variables/details.php
@@ -10,88 +10,181 @@ use Pyro\Module\Addons\AbstractModule;
  */
 class Module_Variables extends AbstractModule
 {
-    public $version = '0.4';
+	public $version = '1.0.0';
 
-    public function info()
-    {
-        return array(
-            'name' => array(
-                'en' => 'Variables',
-                'ar' => 'المتغيّرات',
-                'br' => 'Variáveis',
-                'pt' => 'Variáveis',
-                'cs' => 'Proměnné',
-                'da' => 'Variable',
-                'de' => 'Variablen',
-                'el' => 'Μεταβλητές',
-                'es' => 'Variables',
+	public function info()
+	{
+		return array(
+			'name' => array(
+				'en' => 'Variables',
+				'ar' => 'المتغيّرات',
+				'br' => 'Variáveis',
+				'pt' => 'Variáveis',
+				'cs' => 'Proměnné',
+				'da' => 'Variable',
+				'de' => 'Variablen',
+				'el' => 'Μεταβλητές',
+				'es' => 'Variables',
                 'fa' => 'متغییرها',
-                'fi' => 'Muuttujat',
-                'fr' => 'Variables',
-                'he' => 'משתנים',
-                'id' => 'Variabel',
-                'it' => 'Variabili',
-                'lt' => 'Kintamieji',
-                'nl' => 'Variabelen',
-                'pl' => 'Zmienne',
-                'ru' => 'Переменные',
-                'sl' => 'Spremenljivke',
-                'zh' => '系統變數',
-                'th' => 'ตัวแปร',
-                'se' => 'Variabler',
-                'hu' => 'Változók',
-            ),
-            'description' => array(
-                'en' => 'Manage global variables that can be accessed from anywhere.',
-                'ar' => 'إدارة المُتغيّرات العامة لاستخدامها في أرجاء الموقع.',
-                'br' => 'Gerencia as variáveis globais acessíveis de qualquer lugar.',
-                'pt' => 'Gerir as variáveis globais acessíveis de qualquer lugar.',
-                'cs' => 'Spravujte globální proměnné přístupné odkudkoliv.',
-                'da' => 'Håndtér globale variable som kan tilgås overalt.',
-                'de' => 'Verwaltet globale Variablen, auf die von überall zugegriffen werden kann.',
-                'el' => 'Διαχείριση μεταβλητών που είναι προσβάσιμες από παντού στον ιστότοπο.',
-                'es' => 'Manage global variables to access from everywhere.',
-                'fa' => 'مدیریت متغییر های جامع که می توانند در هر جای سایت مورد استفاده قرار بگیرند',
-                'fi' => 'Hallitse globaali muuttujia, joihin pääsee käsiksi mistä vain.',
-                'fr' => 'Manage global variables to access from everywhere.',
-                'he' => 'ניהול משתנים גלובליים אשר ניתנים להמרה בכל חלקי האתר',
-                'id' => 'Mengatur variabel global yang dapat diakses dari mana saja.',
-                'it' => 'Gestisci le variabili globali per accedervi da ogni parte.',
-                'lt' => 'Globalių kintamujų tvarkymas kurie yra pasiekiami iš bet kur.',
-                'nl' => 'Beheer globale variabelen die overal beschikbaar zijn.',
-                'pl' => 'Zarządzaj globalnymi zmiennymi do których masz dostęp z każdego miejsca aplikacji.',
-                'ru' => 'Управление глобальными переменными, которые доступны в любом месте сайта.',
-                'sl' => 'Urejanje globalnih spremenljivk za dostop od kjerkoli',
-                'th' => 'จัดการตัวแปรทั่วไปโดยที่สามารถเข้าถึงได้จากทุกที่.',
-                'zh' => '管理此網站內可存取的全局變數。',
-                'hu' => 'Globális változók kezelése a hozzáféréshez, bárhonnan.',
-                'se' => 'Hantera globala variabler som kan avändas över hela webbplatsen.',
-            ),
-            'frontend'  => false,
-            'backend'   => true,
-            'menu'      => 'data',
-            'shortcuts' => array(
-                array(
-                    'name' => 'variables:create_title',
-                    'uri' => 'admin/variables/create',
-                    'class' => 'add',
-                ),
-            ),
-        );
-    }
+				'fi' => 'Muuttujat',
+				'fr' => 'Variables',
+				'he' => 'משתנים',
+				'id' => 'Variabel',
+				'it' => 'Variabili',
+				'lt' => 'Kintamieji',
+				'nl' => 'Variabelen',
+				'pl' => 'Zmienne',
+				'ru' => 'Переменные',
+				'sl' => 'Spremenljivke',
+				'tw' => '系統變數',
+				'cn' => '系统变数',
+				'th' => 'ตัวแปร',
+				'se' => 'Variabler',
+				'hu' => 'Változók',
+			),
+			'description' => array(
+				'en' => 'Manage global variables that can be accessed from anywhere. ',
+				'ar' => 'إدارة المُتغيّرات العامة لاستخدامها في أرجاء الموقع.',
+				'br' => 'Gerencia as variáveis globais acessíveis de qualquer lugar.',
+				'pt' => 'Gerir as variáveis globais acessíveis de qualquer lugar.',
+				'cs' => 'Spravujte globální proměnné přístupné odkudkoliv.',
+				'da' => 'Håndtér globale variable som kan tilgås overalt.',
+				'de' => 'Verwaltet globale Variablen, auf die von überall zugegriffen werden kann.',
+				'el' => 'Διαχείριση μεταβλητών που είναι προσβάσιμες από παντού στον ιστότοπο.',
+				'es' => 'Manage global variables to access from everywhere.',
+                            'fa' => 'مدیریت متغییر های جامع که می توانند در هر جای سایت مورد استفاده قرار بگیرند',
+				'fi' => 'Hallitse globaali muuttujia, joihin pääsee käsiksi mistä vain.',
+				'fr' => 'Gérer des variables globales pour pouvoir y accéder depuis n\'importe quel endroit du site.',
+				'he' => 'ניהול משתנים גלובליים אשר ניתנים להמרה בכל חלקי האתר',
+				'id' => 'Mengatur variabel global yang dapat diakses dari mana saja.',
+				'it' => 'Gestisci le variabili globali per accedervi da ogni parte.',
+				'lt' => 'Globalių kintamujų tvarkymas kurie yra pasiekiami iš bet kur.',
+				'nl' => 'Beheer globale variabelen die overal beschikbaar zijn.',
+				'pl' => 'Zarządzaj globalnymi zmiennymi do których masz dostęp z każdego miejsca aplikacji.',
+				'ru' => 'Управление глобальными переменными, которые доступны в любом месте сайта.',
+				'sl' =>	'Urejanje globalnih spremenljivk za dostop od kjerkoli',
+				'th' => 'จัดการตัวแปรทั่วไปโดยที่สามารถเข้าถึงได้จากทุกที่.',
+				'tw' => '管理此網站內可存取的全局變數。',
+				'cn' => '管理此网站内可存取的全局变数。',
+				'hu' => 'Globális változók kezelése a hozzáféréshez, bárhonnan.',
+				'se' => 'Hantera globala variabler som kan avändas över hela webbplatsen.',
 
-    public function install($pdb, $schema)
-    {
-        $schema->dropIfExists('variables');
+			),
+			'frontend'	=> false,
+			'backend'	=> true,
+			'menu'		=> 'data',
 
-        $schema->create('variables', function($table) {
-            $table->increments('id');
-            $table->string('name', 250)->nullable();
-            $table->string('data', 250)->nullable();
-        });
+			'sections' => array(
+				'variables' => array(
+					'name' => 'variables:name',
+					'uri' => 'admin/variables',
+				),
+				/* @todo - We will bring this back once we finish the improvements to the Streams CP and finish the Admin_fields controller
+				'fields' => array(
+					'name' => 'streams:fields',
+					'uri' => 'admin/variables/fields'
+				)
+				*/
+			)
+		);
+	}
 
-        return true;
-    }
+	public function install($pdb, $schema)
+	{
+		ci()->load->driver('Streams');
+
+		ci()->streams->utilities->remove_namespace('variables');
+
+		ci()->lang->load('variables/variables');
+
+		if ( ! ci()->type->load_single_type('field'))
+		{
+			ci()->session->set_flashdata('notice', lang('variables:field_field_type_required'));
+			
+			return false;
+		}
+
+		if ( ! ci()->type->load_single_type('template'))
+		{
+			ci()->session->set_flashdata('notice', lang('variables:template_field_type_required'));
+
+			return false;
+		}
+
+		ci()->streams->streams->add_stream('lang:variables:name', 'variables', 'variables');
+
+		ci()->load->model('files/file_folders_m');
+
+        // Create the Variables folder. For the image field
+        $folder = Folder::create(array(
+            'parent_id' => 0,
+            'slug' => 'variables',
+            'name' => 'lang:variables:variables',
+            'location' => 'local',
+            'remote_container' => '',
+            'date_added' => time(),
+            'sort' => time(),
+            'hidden' => 1,
+        ));
+
+		$fields = array(
+			// Fields assigned to Variables
+			array(
+				'name'			=> 'lang:variables:name_label',
+				'slug'			=> 'name',
+				'type'			=> 'slug',
+				'title_column'	=> true,
+				'required'		=> true,
+				'unique'		=> true,
+				'namespace'		=> 'variables',
+				'assign'		=> 'variables',
+				'extra'			=> array('max_length' => 100),
+			),
+			array(
+				'name'			=> 'lang:variables:data_label',
+				'slug'			=> 'data',
+				'type'			=> 'field',
+				'required'		=> true,
+				'namespace'		=> 'variables',
+				'assign'		=> 'variables',
+				'extra'			=> array('max_length' => 100, 'namespace' => 'variables', 'field_slug' => 'data'),
+			),
+			array(
+				'name'			=> 'lang:variables:syntax_label',
+				'slug'			=> 'syntax',
+				'type'			=> 'template',
+				'namespace'		=> 'variables',
+				'assign'		=> 'variables',
+				'extra'			=> 
+					array('template' => '<span class="syntax">{{ noparse }} {{ {{ /noparse }} variables:{{ name }} {{ noparse }} }} {{ /noparse }}</span>'),
+			),
+		    // A default set of selectable fields
+			array('namespace' => 'variables','name' => 'lang:streams:country.name','slug' => 'country','type' => 'country'),
+			array('namespace' => 'variables','name' => 'lang:streams:datetime.name','slug' => 'datetime','type' => 'datetime', 'extra' => array('use_time' => 'no', 'storage' => 'datetime', 'input_type' => 'dropdown')),
+			array('namespace' => 'variables','name' => 'lang:streams:email.name','slug' => 'email','type' => 'email'),
+			array('namespace' => 'variables','name' => 'lang:streams:encrypt.name','slug' => 'encrypt','type' => 'encrypt', 'extra' => array('hide_typing' => 'yes')),
+			array('namespace' => 'variables','name' => 'lang:streams:image.name','slug' => 'image','type' => 'image', 'extra' => array('folder' => $folder->id)),
+			array('namespace' => 'variables','name' => 'lang:streams:integer.name','slug' => 'number','type' => 'integer'),
+			array('namespace' => 'variables','name' => 'lang:streams:keywords.name','slug' => 'keywords','type' => 'keywords'),
+			array('namespace' => 'variables','name' => 'lang:streams:pyro_lang.name','slug' => 'pyro_lang','type' => 'pyro_lang'),
+			array('namespace' => 'variables','name' => 'lang:streams:state.name','slug' => 'state','type' => 'state'),
+			array('namespace' => 'variables','name' => 'lang:streams:text.name','slug' => 'text','type' => 'text'),
+			array('namespace' => 'variables','name' => 'lang:streams:textarea.name','slug' => 'textarea','type' => 'textarea', 'extra' => array('allow_tags' => 'yes', 'content_type' => 'md')),
+			array('namespace' => 'variables','name' => 'lang:streams:url.name','slug' => 'url','type' => 'url'),
+			array('namespace' => 'variables','name' => 'lang:streams:user.name','slug' => 'user','type' => 'user'),
+			array('namespace' => 'variables','name' => 'lang:streams:wysiwyg.name','slug' => 'wysiwyg','type' => 'wysiwyg', 'extra' => array('editor_type' => 'advanced', 'allow_tags' => 'y')),
+		);
+
+		ci()->streams->fields->add_fields($fields);
+		
+		$update_data = array(
+		    'view_options'  => array('name', 'data', 'syntax')
+		);
+
+		ci()->streams->streams->update_stream('variables', 'variables', $update_data);
+
+		return true;
+	}
 
     public function uninstall($pdb, $schema)
     {
@@ -103,5 +196,4 @@ class Module_Variables extends AbstractModule
     {
         return true;
     }
-
 }
