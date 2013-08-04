b01ced811fe9f1303c88b077e91bde4232505225
Osvaldo Brignoni <obrignoni@gmail.com>
2013-08-04 15:01:07 -0400

    We can eloquently create streams

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Stream.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Stream.php
@@ -84,7 +84,7 @@ class Stream extends Eloquent
 	public static function create(array $attributes = array())
 	{
 		// Slug and namespace are required attributes
-		if (isset($attributes['stream_slug']) and isset($attributes['stream_namespace']))
+		if ( ! isset($attributes['stream_slug']) and ! isset($attributes['stream_namespace']))
 		{
 			return false;
 		}
@@ -95,7 +95,117 @@ class Stream extends Eloquent
 			return false;
 		}
 
+		$attributes['stream_prefix']	= isset($attributes['stream_prefix']) ? $attributes['stream_prefix'] : null;
+		$attributes['is_hidden']		= isset($attributes['is_hidden']) ? $attributes['is_hidden'] : false;
+		$attributes['sorting']			= isset($attributes['sorting']) ? $attributes['sorting'] : 'title';
+		$attributes['view_options']		= isset($attributes['view_options']) ? $attributes['view_options'] : array('id', 'created');
+
+		$schema = ci()->pdb->getSchemaBuilder();
+
+		// See if table exists. You never know if it sneaked past validation
+		if ($schema->hasTable($attributes['stream_prefix'].$attributes['stream_slug']))
+		{
+			return false;
+		}
+
+		// Create the table for our new stream
+		$schema->create($attributes['stream_prefix'].$attributes['stream_slug'], function($table) {
+            $table->increments('id');
+            $table->datetime('created');
+            $table->datetime('updated');
+            $table->integer('created_by')->nullable();
+            $table->integer('ordering_count')->nullable();
+        });
+
+		// Create the stream in the data_streams table
 		return parent::create($attributes);
+	}
+
+	/**
+	 * Update Stream
+	 *
+	 * @param	int
+	 * @param	array - attributes
+	 * @return	bool
+	 */
+	public function update(array $attributes = array())
+	{
+		// See if the stream slug is different
+		//$stream = static::find($id);
+
+		if ($stream->stream_slug != $data['stream_slug'] or (isset($data['stream_prefix']) and $stream->stream_prefix != $data['stream_prefix'])) {
+			// Use the right DB prefix
+			if (isset($data['stream_prefix'])) {
+				$prefix = $data['stream_prefix'];
+				$attributes['stream_prefix'] = $prefix;
+			} else {
+				$prefix = $stream->stream_prefix;
+			}
+
+			// Use the right stream slug
+			if (isset($data['stream_slug'])) {
+				$stream_slug = $data['stream_slug'];
+				$attributes['stream_slug'] = $stream_slug;
+			} else {
+				$stream_slug = $stream->stream_slug;
+			}
+
+			// Okay looks like we need to alter the table name.
+			// Check to see if there is a table, then alter it.
+			if ($this->db->table_exists($prefix.$stream_slug)) {
+				show_error(sprintf(lang('streams:table_exists'), $data['stream_slug']));
+			}
+
+			$this->load->dbforge();
+
+			// Using the PyroStreams DB prefix because rename_table
+			// does not prefix the table name properly, it would seem
+			if ( ! $this->dbforge->rename_table($stream->stream_prefix.$stream->stream_slug, $prefix.$stream_slug)) {
+				return false;
+			}
+		}
+
+		if (isset($data['stream_name']))		$attributes['stream_name']		= $data['stream_name'];
+		if (isset($data['about']))				$attributes['about']			= $data['about'];
+		if (isset($data['sorting']))			$attributes['sorting']			= $data['sorting'];
+		if (isset($data['title_column']))		$attributes['title_column']		= 	$data['title_column'];
+		if (isset($data['is_hidden']))			$attributes['is_hidden']		= $data['is_hidden'];
+		if (isset($data['sorting']))			$attributes['sorting']			= $data['sorting'];
+		if (isset($data['menu_path']))			$attributes['menu_path']		= $data['menu_path'];
+
+		// Extra enum data checks
+
+
+		if (isset($attributes['sorting']) and $attributes['sorting'] != 'title' and $attributes['sorting'] != 'custom')
+		{
+			$attributes['sorting'] = 'title';
+		}
+
+		// Permissions
+		if (isset($data['permissions']) and is_array($data['permissions'])) {
+			$attributes['permissions']		= serialize($data['permissions']);
+		}
+
+		// View options.
+		if (isset($extra['view_options']) and is_array($extra['view_options'])) {
+			$insert_data['view_options']		= serialize($extra['view_options']);
+		} else {
+			// Since this is a new stream, we are going to add a basic view profile
+			// with data we know will be there.
+			$insert_data['view_options']		= serialize(array('id', 'created'));
+		}
+
+		return parent::update($attributes);
+	}
+
+	public function getIsHiddenAttribute($is_hidden)
+	{
+		return $is_hidden == 'yes' ? true : false;
+	}
+
+	public function setIsHiddenAttribute($is_hidden)
+	{
+		$this->attributes['is_hidden'] = ! $is_hidden ? 'no' : 'yes';
 	}
 
 	public function getViewOptionsAttribute($view_options)
@@ -104,13 +214,21 @@ class Stream extends Eloquent
     }
 
 	public function setViewOptionsAttribute($view_options)
-    {
-        $this->attributes['view_options'] = serialize($view_options);
+    {	
+    	$this->attributes['view_options'] = serialize($view_options);
     }
 
+	public function getPermissionsAttribute($permissions)
+    {
+        return unserialize($permissions);
+    }
 
+	public function setPermissionsAttribute($permissions)
+    {
+        $this->attributes['permissions'] = serialize($permissions);
+    }
 
-	public function assingments()
+	public function assignments()
 	{
 		return $this->hasMany('Pyro\Module\Streams_core\Model\FieldAssignment', 'stream_id');
 	}
