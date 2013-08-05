7559c32b516ee8b0578a70e9b5ca9282ac319347
Osvaldo Brignoni <obrignoni@gmail.com>
2013-08-04 23:54:45 -0400

    Adding the ability to return the entry collection formatted or unformatted. See comments for details.

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Collection/EntryCollection.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Collection/EntryCollection.php
@@ -4,5 +4,24 @@ use Pyro\Collection\EloquentCollection;
 
 class EntryCollection extends EloquentCollection
 {
+	
+	public $unformatted_entries = array();
+
+	public function __construct(array $entries = array(), array $unformatted_entries = array())
+	{
+		parent::__construct($entries);
+		$this->unformatted_entries = $unformatted_entries;
+	}
+
 	// We can add custom methods here that will be available in the collection returned by the Entry model
+	public function enhanced()
+	{
+		// add odd / even boolean and add extra stuff to the models
+	}
+
+	public function unformatted()
+	{
+		return new static($this->unformatted_entries);
+	}
+
 }
