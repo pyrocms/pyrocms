6a80e376395ddab4d7760cd84d7ed2955b453684
Ryan Thompson <service@aiwebsystems.com>
2013-08-07 12:15:14 -0500

    Remove old dependencies

--- system/cms/modules/pages/controllers/admin.php
+++ system/cms/modules/pages/controllers/admin.php
@@ -46,9 +46,6 @@ class Admin extends Admin_Controller
 
             ->title($this->module_details['name'])
 
-            ->append_js('jquery/jquery.ui.nestedSortable.js')
-            ->append_js('jquery/jquery.cooki.js')
-            ->append_js('jquery/jquery.stickyscroll.js')
             ->append_js('module::index.js')
 
             ->append_css('module::index.css')
@@ -554,10 +551,7 @@ class Admin extends Admin_Controller
         $this->template->group_options = Users\Model\Group::getGroupOptions();
 
         $this->template
-            ->append_js('jquery/jquery.tagsinput.js')
-            ->append_js('jquery/jquery.cooki.js')
-            ->append_js('module::form.js')
-            ->append_css('jquery/jquery.tagsinput.css');
+            ->append_js('module::form.js');
     }
 
     /**
