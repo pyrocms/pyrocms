315a4768b52b8d40150d38e2dc3a0172d35834c9
Osvaldo Brignoni <obrignoni@gmail.com>
2013-08-17 10:14:37 -0400

    Get standard columns in the EntryBuilder

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Query/EntryBuilder.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Query/EntryBuilder.php
@@ -14,12 +14,10 @@ class EntryBuilder extends Builder
 	 */
 	public function get($columns = array('*'), $exclude = false)
 	{
-		$this->stream = $this->model->getStream();
 
 		if ($exclude)
 		{
-			$columns = $this->stream->assignments->getFields()->getFieldsSlugsExclusive($columns);
-			// @todo - restore the non-field columns like id, updated, 
+			$columns = array_merge($columns, $this->model->getFields()->getFieldSlugsExclude($columns));
 		}
 
 		$this->entries = $this->getModels($columns);
@@ -48,35 +46,34 @@ class EntryBuilder extends Builder
 		return $formatted;
 	}
 
-	public function formatEntry($entry = null, $properties = array())
+	public function formatEntry($entry = null)
 	{
 		// Replicate the model to keep the original intact
 		$clone = $entry->replicate();
-
-		foreach (array_keys($clone->getAttributes()) as $field_slug)
-		{
-			// Get the field type instance from the entry
-			if ($type = $entry->getFieldType($field_slug))
-			{
-				// Set the unformatted value, we might need it
-				$clone->setUnformattedValue($field_slug, $entry->{$field_slug});
-				
-				// If there exist a field for the corresponding attribute, format it
-				$clone->{$field_slug} = $type->getFormattedValue();
-
-			}
-			else
-			{
-				// If not replicate the raw value
-				$clone->{$field_slug} = $entry->{$field_slug};
-			}
-		};
 
 		// Restore the primary key to the replicated model, only if it is set
 		if (isset($entry->{$this->model->getKeyName()}))
 		{
 			$clone->{$this->model->getKeyName()} = $entry->{$this->model->getKeyName()};	
 		}
+
+		foreach (array_keys($clone->getAttributes()) as $field_slug)
+		{
+			// Get the field type instance from the entry
+			if (in_array($field_slug, $this->model->getStandardColumns()))
+			{
+				// If not replicate the raw value
+				$clone->{$field_slug} = $entry->{$field_slug};
+			}
+			elseif ($type = $entry->getFieldType($field_slug, $entry))
+			{
+				// Set the unformatted value, we might need it
+				$clone->setUnformattedValue($field_slug, $entry->{$field_slug});
+				
+				// If there exist a field for the corresponding attribute, format it
+				$clone->{$field_slug} = $type->getFormattedValue();
+			}
+		};
 
 		return $clone;	
 	}
