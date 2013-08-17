8a0a970fc39f5a4c562220354e43911a82dde594
Osvaldo Brignoni <obrignoni@gmail.com>
2013-08-17 10:33:14 -0400

    Progress

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Data/Entries.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Data/Entries.php
@@ -1,8 +1,14 @@
 <?php namespace Pyro\Module\Streams_core\Data;
 
-// This will be similar to the old Entries driver
+use Pyro\Module\Streams_core\Core\Model;
 
 class Entries
 {
+
+	public static function getEntry($id = null, $stream_slug = null, $stream_namespace = null, $format = true, $plugin = true)
+	{
+		return Model\Entry::stream($stream_slug, $stream_namespace)->setFormat($format)->setPlugin($plugin)->getEntry($id);
+	}
+
 
 }
