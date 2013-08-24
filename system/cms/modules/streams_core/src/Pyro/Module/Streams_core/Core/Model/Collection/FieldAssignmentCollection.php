c31e3545f929524ad0a523aa7d0b05de024c7975
Osvaldo Brignoni <obrignoni@gmail.com>
2013-08-24 19:48:24 -0400

    Added getStreamIds() method to FieldAssignmentCollection

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Collection/FieldAssignmentCollection.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Collection/FieldAssignmentCollection.php
@@ -4,5 +4,15 @@ use Pyro\Collection\EloquentCollection;
 
 class FieldAssignmentCollection extends EloquentCollection
 {
+	public function getStreamIds()
+	{
+		$stream_ids = array();
+		
+		foreach ($this->items as $key => $assignment)
+		{
+			$stream_ids[] = $assignment->stream_id;
+		}
 
+		return $stream_ids;
+	}
 }
