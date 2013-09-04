b53e2ef12dac08d954ba05e8accdfcf51d816c65
Osvaldo Brignoni <obrignoni@gmail.com>
2013-09-03 21:28:10 -0400

    More exceptions

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Exception/ClassNotInstanceOfEntryException.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Exception/ClassNotInstanceOfEntryException.php
@@ -1,6 +1,6 @@
 <?php namespace Pyro\Module\Streams_core\Core\Model\Exception;
 
-class ClassNotInstanceOfEntry extends Exception
+class ClassNotInstanceOfEntryException extends Exception
 {
 	protected $message = 'The class is not an instance of Pyro\Module\Streams_core\Core\Model\Entry.';
 }
