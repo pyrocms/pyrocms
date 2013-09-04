20a0eb842d7e7d13cb70284caa0f916ace8af824
Osvaldo Brignoni <obrignoni@gmail.com>
2013-09-03 21:36:49 -0400

    Removed ViewCollection

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Collection/ViewCollection.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Pyro\Module\Streams_core\Core\Model\Collection;
-
-use Pyro\Collection\EloquentCollection;
-
-class ViewCollection extends EloquentCollection
-{
-	// We can add custom methods here that will be available in the collection returned by the View model
-}
