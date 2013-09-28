793364e0c541119a76b3d359063711aac74fc3be
Osvaldo Brignoni <obrignoni@gmail.com>
2013-09-28 13:50:39 -0400

    Update merge_tags field type language

--- system/cms/modules/streams_core/field_types/merge_tags/language/english/merge_tags_lang.php
+++ system/cms/modules/streams_core/field_types/merge_tags/language/english/merge_tags_lang.php
@@ -1,3 +1,5 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 $lang['streams:merge_tags.name'] 			= 'Merge Tags';
+$lang['streams:merge_tags.pattern'] 		= 'Pattern';
+$lang['streams:merge_tags.pattern.instructions'] 		= 'Define the pattern that is going to be parsed by Lex.';
