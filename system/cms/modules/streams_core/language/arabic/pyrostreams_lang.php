448da2c266ab7e8868b44158039119d687abd323
Osvaldo Brignoni <obrignoni@gmail.com>
2013-09-29 16:22:44 -0400

    Remove duplicate line

--- system/cms/modules/streams_core/language/arabic/pyrostreams_lang.php
+++ system/cms/modules/streams_core/language/arabic/pyrostreams_lang.php
@@ -189,7 +189,6 @@ $lang['streams:max_length'] 							= "أقصى طول";
 $lang['streams:upload_location'] 						= "موضع الرفع";
 $lang['streams:default_value'] 							= "القيمة الافتراضية";
 $lang['streams:allow_tags'] 							= "Allow Tags";
-$lang['streams:allow_tags'] 							= "Allow Tags";
 $lang['streams:link_uri'] 								= "Link URI";
 $lang['streams:relation_class'] 						= "Relation Class";
 $lang['streams:menu_path']								= 'مسار القائمة';
