07f140b034ea80c4cded072d93335d57c3d47cc5
Ryan Thompson <service@aiwebsystems.com>
2013-10-15 13:08:33 -0500

    Use ACE for page type layouts

--- system/cms/modules/pages/views/admin/types/form.php
+++ system/cms/modules/pages/views/admin/types/form.php
@@ -112,7 +112,7 @@
 								<ul>
 									<li>
 										<label for="html_editor"><?php echo lang('page_types:layout'); ?> <span>*</span></label>
-										<?php echo form_textarea(array('id'=>'html_editor', 'name'=>'body', 'value' => ($page_type->body == '' ? '<h2>{{ title }}</h2>' : $page_type->body), 'rows' => 50)); ?>
+										<?php echo form_textarea(array('id'=>'html_editor', 'name'=>'body', 'value' => ($page_type->body == '' ? '<h2>{{ title }}</h2>' : $page_type->body), 'rows' => 50, 'data-editor' => 'html')); ?>
 									</li>
 								</ul>
 
