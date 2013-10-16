66b7e4123bd811d9833c06bf49298bb20ccbb0a3
Osvaldo Brignoni <obrignoni@gmail.com>
2013-10-15 22:00:54 -0400

    Fallback language for column names that don''t have a corresponding field

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Entry.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Entry.php
@@ -761,7 +761,14 @@ class Entry extends EntryOriginal
 
         foreach ($this->getViewOptions() as $column)
         {
-            $field_names[$fields[$column]->field_slug] = isset($fields[$column]) ? $fields[$column]->field_name : lang('streams:column_'.$column); 
+            if (isset($fields[$column]))
+            {
+                $field_names[$fields[$column]->field_slug] = isset($fields[$column]) ? $fields[$column]->field_name : lang('streams:column_'.$column);     
+            }
+            else
+            {
+                $field_names[$column] = lang('streams:column_'.$column);
+            }
         }
 
         return $field_names;
