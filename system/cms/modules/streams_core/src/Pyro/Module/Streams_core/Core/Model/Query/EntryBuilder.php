96407ff4d597736d79b5eaa50af4968bef92a78c
Osvaldo Brignoni <obrignoni@gmail.com>
2013-10-27 02:33:21 -0400

    Improve view option defaults and avoid method name collisions when parsing attributes

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Query/EntryBuilder.php
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Core/Model/Query/EntryBuilder.php
@@ -349,9 +349,9 @@ class EntryBuilder extends Builder
 		}
 		elseif (method_exists($this->getModel(), $relation) and $instance = $this->getModel()->$relation())
 		{
-			 if ($instance instanceof Relation) {
-			 	return $instance;
-			 }
+			if ($instance instanceof Relation) {
+				return $instance;
+			}
 		}
 
 		return $this;
@@ -466,6 +466,10 @@ class EntryBuilder extends Builder
     {
     	$relations = array();
     	
+    	$entry = new Entry;
+
+    	$entry_methods = get_class_methods($entry);
+
     	if ($this->model->isEnableAutoEagerLoading() and $view_options = $this->model->getViewOptions())
     	{
     		if (in_array('created_by', $view_options))
@@ -474,7 +478,7 @@ class EntryBuilder extends Builder
     		}
 
 	    	foreach ($view_options as $column) {
-	    		if ($this->hasRelation($column)) {
+	    		if ( ! in_array($column, $entry_methods) and $this->hasRelation($column)) {
 	    			$relations[] = $column;
 	    		}
 	    	} 		
