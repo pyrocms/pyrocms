8ecf88fe9cf613ec1e09d8058a062edbd858d336
Ryan Thompson <service@aiwebsystems.com>
2013-10-30 00:08:21 -0500

    HTTP_HOST is much more reliable

--- system/cms/config/config.php
+++ system/cms/config/config.php
@@ -273,8 +273,8 @@ $config['sess_time_to_update']	= 300;
 |
 */
 // for multi-site logins to work properly we have to set a prefix. We use the subdomain for that or default_ if none exists.
-$config['cookie_prefix']	= (substr_count($_SERVER['SERVER_NAME'], '.') > 1) ? substr($_SERVER['SERVER_NAME'], 0, strpos($_SERVER['SERVER_NAME'], '.')) . '_' : 'default_';
-$config['cookie_domain']	= ($_SERVER['SERVER_NAME'] == 'localhost') ? '' : $_SERVER['SERVER_NAME'];
+$config['cookie_prefix']	= (substr_count($_SERVER['HTTP_HOST'], '.') > 1) ? substr($_SERVER['HTTP_HOST'], 0, strpos($_SERVER['HTTP_HOST'], '.')) . '_' : 'default_';
+$config['cookie_domain']	= ($_SERVER['HTTP_HOST'] == 'localhost') ? '' : $_SERVER['HTTP_HOST'];
 $config['cookie_path']		= BASE_URI;
 $config['cookie_secure']	= false;
 $config['cookie_httponly']  = false;
