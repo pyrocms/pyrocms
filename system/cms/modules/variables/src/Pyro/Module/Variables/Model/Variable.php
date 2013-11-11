88cc995a87d65bbbddc28974f4b21d0573bb2698
Osvaldo Brignoni <obrignoni@gmail.com>
2013-11-10 21:58:16 -0500

    Add Variable model that extends Entry

---
+++ system/cms/modules/variables/src/Pyro/Module/Variables/Model/Variable.php
@@ -0,0 +1,12 @@
+<?php namespace Pyro\Module\Variables\Model;
+
+use Pyro\Module\Streams_core\Core\Model\Entry;
+
+class Variable extends Entry
+{
+
+	protected $stream_slug = 'variables';
+
+	protected $stream_namespace = 'variables';
+
+}
