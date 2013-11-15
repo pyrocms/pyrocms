f0478f8552a7c9fc5fc345e786b033da7f8e2552
Ryan Thompson <service@aiwebsystems.com>
2013-11-15 10:04:04 -0600

    Correct hidden trigger in form.php

--- system/cms/modules/streams_core/views/entries/form.php
+++ system/cms/modules/streams_core/views/entries/form.php
@@ -11,7 +11,7 @@
 
 	<?php foreach ($fields as $field): ?>
 
-		<div class="form-group <?php  echo in_array($field['input_slug'], $hidden) ? 'hidden' : null;  ?>">
+		<div class="form-group <?php  echo in_array(str_replace($stream->stream_namespace.'-'.$stream->stream_slug.'-', '', $field['input_slug']), $hidden) ? 'hidden' : null;  ?>">
 		<div class="row">
 			
 			<label class="col-lg-2" for="<?php echo $field['input_slug'];?>">
