e51a71adab47b138ce73511dafb95a57195d95d1
Osvaldo Brignoni <obrignoni@gmail.com>
2013-11-17 17:53:33 -0500

    Update exceptions

--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Exception/InvalidFieldException.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Pyro\Module\Streams_core\Exception;
-
-class InvalidFieldException extends Exception
-{
-	protected $message = 'Invalid field.';
-}
--- system/cms/modules/streams_core/src/Pyro/Module/Streams_core/Exception/InvalidStreamException.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Pyro\Module\Streams_core\Exception;
-
-class InvalidStreamException extends Exception
-{
-	protected $message = 'Invalid stream.';
-}
