48c788b2bbd7ab2c1bc93c0d8afee5cf9dc4a6dd
Ryan Thompson <service@aiwebsystems.com>
2013-11-27 17:27:49 -0600

    Clean up yo shizzle when you uninstizzle or delizzle

---
+++ system/cms/modules/comments/events.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Comments Events Class
+ * 
+ * @package			CMS
+ * @subpackage    	Comments Module
+ * @category    	Events
+ * @author        	Ryan Thompson - AI Web Systems, Inc.
+ * @website       	http://aiwebsystems.com/
+ */
+class Events_Comments {
+
+	public function __construct()
+	{
+		// Log defualts
+		Events::register('module_uninstalled', array($this, 'remove_comments'));
+		Events::register('module_disabled', array($this, 'remove_settings'));
+	}
+
+	/**
+	 *	Remove module comments
+	 *
+	 *	object $module
+	 *	return void
+	 */
+	public function remove_comments($module)
+	{
+		ci()->pdb->table('comments')->where('module', '=', $module->slug)->delete();
+	}
+}
