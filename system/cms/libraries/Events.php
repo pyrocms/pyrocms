4bdb51f4a38484b48f41f6c089fb5a39f17ade18
Phil Sturgeon <email@philsturgeon.co.uk>
2013-12-27 18:32:54 -0500

    Moar PSR-2 for libraries

--- system/cms/libraries/Events.php
+++ system/cms/libraries/Events.php
@@ -15,196 +15,197 @@
 
 class Events
 {
-	/**
-	 * An array of listeners
-	 *
-	 * @var	array
-	 */
-	protected static $_listeners = array();
+    /**
+     * An array of listeners
+     *
+     * @var	array
+     */
+    protected static $_listeners = array();
 
-	/**
-	 * Constructor
-	 *
-	 * Load up the modules.
-	 */
-	public function __construct()
-	{
-		self::_load_modules();
-	}
+    /**
+     * Constructor
+     *
+     * Load up the modules.
+     */
+    public function __construct()
+    {
+        self::_load_modules();
+    }
 
-	/**
-	 * Load Modules
-	 *
-	 * Loads all active modules
-	 */
-	private static function _load_modules()
-	{
-		$_ci = get_instance();
+    /**
+     * Load Modules
+     *
+     * Loads all active modules
+     */
+    private static function _load_modules()
+    {
+        $_ci = get_instance();
 
-		$is_core = true;
+        $is_core = true;
 
-		if (( ! $results = $_ci->moduleManager->getAllEnabled())) {
-			return false;
-		}
+        if (( ! $results = $_ci->moduleManager->getAllEnabled())) {
+            return false;
+        }
 
-		foreach ($results as $row) {
-			// This does not have a valid details.php file! :o
-			if (( ! $details_class = self::spawnClass($row['slug'], $row['is_core']))) {
-				continue;
-			}
-		}
+        foreach ($results as $row) {
+            // This does not have a valid details.php file! :o
+            if (( ! $details_class = self::spawnClass($row['slug'], $row['is_core']))) {
+                continue;
+            }
+        }
 
-		return true;
-	}
-	
-	/**
-	 * Return all registered event keys
-	 */
-	static function registered() {
-		return array_keys(self::$_listeners);
-	}
+        return true;
+    }
 
-	/**
-	 * Spawn Class
-	 *
-	 * Checks to see if a events.php exists and returns a class
-	 *
-	 * @param string $slug The folder name of the module.
-	 * @param boolean $is_core Whether the module is a core module.
-	 * @return object|boolean
-	 */
-	private static function spawnClass($slug, $is_core = false)
-	{
-		$path = $is_core ? APPPATH : ADDONPATH;
+    /**
+     * Return all registered event keys
+     */
+    public static function registered()
+    {
+        return array_keys(self::$_listeners);
+    }
 
-		// Before we can install anything we need to know some details about the module
-		$events_file = $path.'modules/'.$slug.'/events.php';
+    /**
+     * Spawn Class
+     *
+     * Checks to see if a events.php exists and returns a class
+     *
+     * @param string $slug The folder name of the module.
+     * @param boolean $is_core Whether the module is a core module.
+     * @return object|boolean
+     */
+    private static function spawnClass($slug, $is_core = false)
+    {
+        $path = $is_core ? APPPATH : ADDONPATH;
 
-		// Check the details file exists
-		if (!is_file($events_file)) {
-			$events_file = SHARED_ADDONPATH.'modules/'.$slug.'/events.php';
+        // Before we can install anything we need to know some details about the module
+        $events_file = $path.'modules/'.$slug.'/events.php';
 
-			if (!is_file($events_file)) {
-				return false;
-			}
-		}
+        // Check the details file exists
+        if (!is_file($events_file)) {
+            $events_file = SHARED_ADDONPATH.'modules/'.$slug.'/events.php';
 
-		// Sweet, include the file
-		include_once $events_file;
+            if (!is_file($events_file)) {
+                return false;
+            }
+        }
 
-		// Now call the details class
-		$class = 'Events_'.ucfirst(strtolower($slug));
+        // Sweet, include the file
+        include_once $events_file;
 
-		// Now we need to talk to it
-		return class_exists($class) ? new $class : false;
-	}
+        // Now call the details class
+        $class = 'Events_'.ucfirst(strtolower($slug));
 
-	/**
-	 * Register
-	 *
-	 * Registers a Callback for a given event
-	 *
-	 * @param string $event The name of the event.
-	 * @param array $callback The callback for the event.
-	 */
-	public static function register($event, array $callback)
-	{
-		$key = get_class($callback[0]).'::'.$callback[1];
-		self::$_listeners[$event][$key] = $callback;
-		log_message('debug', 'Events::register() - Registered "'.$key.' with event "'.$event.'"');
-	}
+        // Now we need to talk to it
+        return class_exists($class) ? new $class : false;
+    }
 
-	/**
-	 * Triggers an event and returns the results.
-	 *
-	 * The results can be returned in the following formats:
-	 *  - 'array'
-	 *  - 'json'
-	 *  - 'serialized'
-	 *  - 'string'
-	 *
-	 * @param string $event The name of the event
-	 * @param string $data Any data that is to be passed to the listener
-	 * @param string $return_type The return type
-	 * @return string|array The return of the listeners, in the return type
-	 */
-	public static function trigger($event, $data = '', $return_type = 'string')
-	{
-		log_message('debug', 'Events::trigger() - Triggering event "'.$event.'"');
+    /**
+     * Register
+     *
+     * Registers a Callback for a given event
+     *
+     * @param string $event The name of the event.
+     * @param array $callback The callback for the event.
+     */
+    public static function register($event, array $callback)
+    {
+        $key = get_class($callback[0]).'::'.$callback[1];
+        self::$_listeners[$event][$key] = $callback;
+        log_message('debug', 'Events::register() - Registered "'.$key.' with event "'.$event.'"');
+    }
 
-		$calls = array();
+    /**
+     * Triggers an event and returns the results.
+     *
+     * The results can be returned in the following formats:
+     *  - 'array'
+     *  - 'json'
+     *  - 'serialized'
+     *  - 'string'
+     *
+     * @param string $event The name of the event
+     * @param string $data Any data that is to be passed to the listener
+     * @param string $return_type The return type
+     * @return string|array The return of the listeners, in the return type
+     */
+    public static function trigger($event, $data = '', $return_type = 'string')
+    {
+        log_message('debug', 'Events::trigger() - Triggering event "'.$event.'"');
 
-		if (self::has_listeners($event)) {
-			foreach (self::$_listeners[$event] as $listener) {
-				if (is_callable($listener)) {
-					$calls[] = call_user_func($listener, $data);
-				}
-			}
-		}
+        $calls = array();
 
-		return self::_format_return($calls, $return_type);
-	}
+        if (self::has_listeners($event)) {
+            foreach (self::$_listeners[$event] as $listener) {
+                if (is_callable($listener)) {
+                    $calls[] = call_user_func($listener, $data);
+                }
+            }
+        }
 
-	/**
-	 * Format Return
-	 *
-	 * Formats the return in the given type
-	 *
-	 * @param array $calls The array of returns
-	 * @param string $return_type The return type
-	 * @return array|null The formatted return
-	 */
-	protected static function _format_return(array $calls, $return_type)
-	{
-		log_message('debug', 'Events::_format_return() - Formating calls in type "'.$return_type.'"');
+        return self::_format_return($calls, $return_type);
+    }
 
-		switch ($return_type) {
-			case 'array':
-				return $calls;
-				break;
-			case 'json':
-				return json_encode($calls);
-				break;
-			case 'serialized':
-				return serialize($calls);
-				break;
-			case 'string':
-				$str = '';
-				foreach ($calls as $call) {
-					$str .= $call;
-				}
-				return $str;
-				break;
-			default:
-				return $calls;
-				break;
-		}
+    /**
+     * Format Return
+     *
+     * Formats the return in the given type
+     *
+     * @param array $calls The array of returns
+     * @param string $return_type The return type
+     * @return array|null The formatted return
+     */
+    protected static function _format_return(array $calls, $return_type)
+    {
+        log_message('debug', 'Events::_format_return() - Formating calls in type "'.$return_type.'"');
 
-		// Does not do anything, so send null. false would suggest an error
-		return null;
-	}
+        switch ($return_type) {
+            case 'array':
+                return $calls;
+                break;
+            case 'json':
+                return json_encode($calls);
+                break;
+            case 'serialized':
+                return serialize($calls);
+                break;
+            case 'string':
+                $str = '';
+                foreach ($calls as $call) {
+                    $str .= $call;
+                }
+                return $str;
+                break;
+            default:
+                return $calls;
+                break;
+        }
 
-	/**
-	 *
-	 * @param	string
-	 * @return	bool
-	 */
+        // Does not do anything, so send null. false would suggest an error
+        return null;
+    }
 
-	/**
-	 * Checks if the event has listeners
-	 *
-	 * @param string $event The name of the event
-	 * @return boolean Whether the event has listeners
-	 */
-	public static function has_listeners($event)
-	{
-		log_message('debug', 'Events::has_listeners() - Checking if event "'.$event.'" has listeners.');
+    /**
+     *
+     * @param	string
+     * @return	bool
+     */
 
-		if (isset(self::$_listeners[$event]) and count(self::$_listeners[$event]) > 0) {
-			return true;
-		}
+    /**
+     * Checks if the event has listeners
+     *
+     * @param string $event The name of the event
+     * @return boolean Whether the event has listeners
+     */
+    public static function has_listeners($event)
+    {
+        log_message('debug', 'Events::has_listeners() - Checking if event "'.$event.'" has listeners.');
 
-		return false;
-	}
+        if (isset(self::$_listeners[$event]) and count(self::$_listeners[$event]) > 0) {
+            return true;
+        }
+
+        return false;
+    }
 
 }
--- system/cms/libraries/MY_Image_lib.php
+++ system/cms/libraries/MY_Image_lib.php
@@ -10,73 +10,73 @@
  */
 class MY_Image_lib extends CI_Image_lib
 {
-	/**
-	 * Image Process Using GD/GD2
-	 *
-	 * This function will resize or crop
-	 *
-	 * @param	string
-	 * @return	bool
-	 */
-	public function image_process_gd($action = 'resize')
-	{
-		$v2_override = false;
+    /**
+     * Image Process Using GD/GD2
+     *
+     * This function will resize or crop
+     *
+     * @param	string
+     * @return	bool
+     */
+    public function image_process_gd($action = 'resize')
+    {
+        $v2_override = false;
 
-		// If the target width/height match the source, AND if the new file name is not equal to the old file name
-		// we'll simply make a copy of the original with the new name... assuming dynamic rendering is off.
-		if ($this->dynamic_output === false) {
-			if ($this->orig_width == $this->width and $this->orig_height == $this->height) {
- 				if ($this->source_image != $this->new_image) {
-					if (@copy($this->full_src_path, $this->full_dst_path)) {
-						@chmod($this->full_dst_path, DIR_WRITE_MODE);
-					}
-				}
+        // If the target width/height match the source, AND if the new file name is not equal to the old file name
+        // we'll simply make a copy of the original with the new name... assuming dynamic rendering is off.
+        if ($this->dynamic_output === false) {
+            if ($this->orig_width == $this->width and $this->orig_height == $this->height) {
+                 if ($this->source_image != $this->new_image) {
+                    if (@copy($this->full_src_path, $this->full_dst_path)) {
+                        @chmod($this->full_dst_path, DIR_WRITE_MODE);
+                    }
+                }
 
-				return true;
-			}
-		}
+                return true;
+            }
+        }
 
-		// Let's set up our values based on the action
-		if ($action == 'crop') {
-			//  Reassign the source width/height if cropping
-			$this->orig_width  = $this->width;
-			$this->orig_height = $this->height;
+        // Let's set up our values based on the action
+        if ($action == 'crop') {
+            //  Reassign the source width/height if cropping
+            $this->orig_width  = $this->width;
+            $this->orig_height = $this->height;
 
-			// GD 2.0 has a cropping bug so we'll test for it
-			if ($this->gd_version() !== false) {
-				$gd_version = str_replace('0', '', $this->gd_version());
-				$v2_override = ($gd_version == 2) ? true : false;
-			}
-		} else {
-			// If resizing the x/y axis must be zero
-			$this->x_axis = 0;
-			$this->y_axis = 0;
-		}
+            // GD 2.0 has a cropping bug so we'll test for it
+            if ($this->gd_version() !== false) {
+                $gd_version = str_replace('0', '', $this->gd_version());
+                $v2_override = ($gd_version == 2) ? true : false;
+            }
+        } else {
+            // If resizing the x/y axis must be zero
+            $this->x_axis = 0;
+            $this->y_axis = 0;
+        }
 
-		//  Create the image handle
-		if ( ! ($src_img = $this->image_create_gd())) {
-			return false;
-		}
+        //  Create the image handle
+        if ( ! ($src_img = $this->image_create_gd())) {
+            return false;
+        }
 
- 		//  Create The Image
-		//
-		//  old conditional which users report cause problems with shared GD libs who report themselves as "2.0 or greater"
-		//  it appears that this is no longer the issue that it was in 2004, so we've removed it, retaining it in the comment
-		//  below should that ever prove inaccurate.
-		//
-		//  if ($this->image_library == 'gd2' and function_exists('imagecreatetruecolor') and $v2_override == false)
- 		if ($this->image_library == 'gd2' and function_exists('imagecreatetruecolor')) {
-			$create	= 'imagecreatetruecolor';
-			$copy	= 'imagecopyresampled';
-		} else {
-			$create	= 'imagecreate';
-			$copy	= 'imagecopyresized';
-		}
+         //  Create The Image
+        //
+        //  old conditional which users report cause problems with shared GD libs who report themselves as "2.0 or greater"
+        //  it appears that this is no longer the issue that it was in 2004, so we've removed it, retaining it in the comment
+        //  below should that ever prove inaccurate.
+        //
+        //  if ($this->image_library == 'gd2' and function_exists('imagecreatetruecolor') and $v2_override == false)
+         if ($this->image_library == 'gd2' and function_exists('imagecreatetruecolor')) {
+            $create	= 'imagecreatetruecolor';
+            $copy	= 'imagecopyresampled';
+        } else {
+            $create	= 'imagecreate';
+            $copy	= 'imagecopyresized';
+        }
 
-		$dst_img = $create($this->width, $this->height);
+        $dst_img = $create($this->width, $this->height);
 
-		// Fix image transparency. Taken from http://codeigniter.com/forums/viewthread/150527/
-		if ($this->image_library == 'gd2') {
+        // Fix image transparency. Taken from http://codeigniter.com/forums/viewthread/150527/
+        if ($this->image_library == 'gd2') {
             $transparencyIndex = imagecolortransparent($src_img);
             $transparencyColor = array('red' => 255, 'green' => 255, 'blue' => 255);
 
@@ -89,25 +89,25 @@ class MY_Image_lib extends CI_Image_lib
             imagecolortransparent($dst_img, $transparencyIndex);
         }
 
-		$copy($dst_img, $src_img, 0, 0, $this->x_axis, $this->y_axis, $this->width, $this->height, $this->orig_width, $this->orig_height);
+        $copy($dst_img, $src_img, 0, 0, $this->x_axis, $this->y_axis, $this->width, $this->height, $this->orig_width, $this->orig_height);
 
-		//  Show the image
-		if ($this->dynamic_output == true) {
-			$this->image_display_gd($dst_img);
-		} else {
-			// Or save it
-			if ( ! $this->image_save_gd($dst_img)) {
-				return false;
-			}
-		}
+        //  Show the image
+        if ($this->dynamic_output == true) {
+            $this->image_display_gd($dst_img);
+        } else {
+            // Or save it
+            if ( ! $this->image_save_gd($dst_img)) {
+                return false;
+            }
+        }
 
-		//  Kill the file handles
-		imagedestroy($dst_img);
-		imagedestroy($src_img);
+        //  Kill the file handles
+        imagedestroy($dst_img);
+        imagedestroy($src_img);
 
-		// Set the file to 777
-		@chmod($this->full_dst_path, DIR_WRITE_MODE);
+        // Set the file to 777
+        @chmod($this->full_dst_path, DIR_WRITE_MODE);
 
-		return true;
-	}
+        return true;
+    }
 }
