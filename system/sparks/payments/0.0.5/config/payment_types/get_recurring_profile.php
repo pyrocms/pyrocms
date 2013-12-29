d3dbd73fbd0ad6e68aa07db85210326bbead590f
Phil Sturgeon <email@philsturgeon.co.uk>
2013-12-28 19:00:52 -0500

    Removed Payments spark. Use omnipay instead.

--- system/sparks/payments/0.0.5/config/payment_types/get_recurring_profile.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-$config['get_recurring_profile'] = array(
-			'identifier'	=>	'', //Required.  Should have been returned when you created the profile.
-		);
