59ca8e20732caf0575facd4e3351801bdec535b2
Osvaldo Brignoni <obrignoni@gmail.com>
2014-01-21 05:54:02 -0500

    Update use plugin

--- system/cms/plugins/user.php
+++ system/cms/plugins/user.php
@@ -334,7 +334,7 @@ class Plugin_User extends Plugin
 		foreach ($user->profile->getModel()->getAllColumns() as $field_key => $field_data) {
 			if ($plugin_call) {
 				if ( ! isset($this->user_profile_data[$user_id]['plugin'][$field_key]) and $user->{$field_key}) {
-					$this->user_profile_data[$user_id]['plugin'][$field_key] = $user->profile->getPluginOutput($var);
+					$this->user_profile_data[$user_id]['plugin'][$field_key] = $user->profile->getFormatter()->getPluginOutput($var);
 				}
 
 				if ($user->$field_key) {
@@ -378,7 +378,7 @@ class Plugin_User extends Plugin
 			$this->user_profile_data[$user_id] = $this->user_data[$user_id]->profile;	
 		}
 
-		return isset($this->user_profile_data[$user_id]) ? $this->user_profile_data[$user_id]->getPluginOutput($var) : null;
+		return isset($this->user_profile_data[$user_id]) ? $this->user_profile_data[$user_id]->getFormatter()->getPluginOutput($var) : null;
 	}
 
 	/**
