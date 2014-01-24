b2e4b8545b0999f916daa604f99a98b2f202455d
Osvaldo Brignoni <obrignoni@gmail.com>
2014-01-23 22:06:25 -0500

    Its just Fluent now

--- system/cms/src/Pyro/Support/Fluent.php
+++ system/cms/src/Pyro/Support/Fluent.php
@@ -1,9 +1,28 @@
 <?php namespace Pyro\Support;
 
 use Illuminate\Support\Fluent as IlluminateFluent;
+use Illuminate\Support\Str;
 
 class Fluent extends IlluminateFluent
 {
+	/**
+	 * Registered callbacks
+	 * @var array
+	 */
+	protected $callbacks = array();
+
+    /**
+     * Callback trigger prefix
+     * @var string
+     */
+    const CALLBACK_TRIGGER_PREFIX = 'fire_';
+
+    /**
+     * Chain trigger method prefix
+     * @var string
+     */
+    const TRIGGER_PREFIX = 'trigger';
+
 	/**
 	 * Construct
 	 */ 
@@ -38,9 +57,55 @@ class Fluent extends IlluminateFluent
 	 * @param attributes - array
 	 * @return Pyro\Support\Fluent
 	 */ 
-    protected function mergeAttributes(array $attributes = array())
+    public function mergeAttributes(array $attributes = array())
     {
         $this->attributes = array_merge($this->attributes, $attributes);
+
+        return $this;
+    }
+
+    /**
+     * Get trigger method
+     * @return string
+     */
+    protected function getTriggerMethod()
+    {
+        return static::TRIGGER_PREFIX.Str::studly($this->triggerMethod);
+    }
+
+	/**
+	 * Add a callback
+	 * @param string $method_name     
+	 * @param boolean $method_callable
+     *
+     * The call back will be like fire[CamelCaseCallback]() and the Closure will be [camelCaseCallback]()
+     *
+     * i.e. fireOnSave() is the callback for the onSave() Closure
+     * 
+	 */
+    protected function addCallback($method_name, $method_callable)
+    {
+        if (is_callable($method_callable))
+        {
+        	$this->callbacks[camel_case(static::CALLBACK_TRIGGER_PREFIX.$method_name)] = \Closure::bind($method_callable, $this, get_class());
+        }
+    }
+ 
+ 	/**
+ 	 * Handle dynamic calls to the container to set attributes 
+     * or Hook into PHP calls to look for callbacks
+ 	 * @param  strign $method 
+ 	 * @param  array  $parameters        
+ 	 * @return function|\Illuminate\Support\Fluent      
+ 	 */
+	public function __call($method, $parameters)
+    {
+        if (isset($this->callbacks[$method]))
+        {
+            return call_user_func_array($this->callbacks[$method], $parameters);
+        }
+
+        $this->attributes[$method] = count($parameters) > 0 ? $parameters[0] : true;
 
         return $this;
     }
