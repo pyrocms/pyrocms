5f5a47e3a8b89cc7e9505cff4bda5450cb2410d5
Osvaldo Brignoni <obrignoni@gmail.com>
2014-01-28 23:12:54 -0500

    Update the Blog models

--- system/cms/modules/blog/src/Pyro/Module/Blog/BlogCategoryModel.php
+++ system/cms/modules/blog/src/Pyro/Module/Blog/BlogCategoryModel.php
@@ -1,32 +1,56 @@
 <?php namespace Pyro\Module\Blog;
 
 use Pyro\Model\Eloquent;
+use Pyro\FieldType\RelationshipInterface;
 
-class BlogCategoryModel extends Eloquent
+class BlogCategoryModel extends Eloquent implements RelationshipInterface
 {
 	/**
 	 * The model table
+	 * 
 	 * @var string
 	 */
 	protected $table = 'blog_categories';
 
 	/**
 	 * Enable timestamps
+	 * 
 	 * @var boolean
 	 */
 	public $timestamps = false;
 
     /**
      * Cache minutes
+     * 
      * @var int
      */
     public $cacheMinutes = 30;
 
     /**
      * Get the title column value
+     * 
+     * @return string
      */
-	public function getTitleColumnValue()
+	public function getFieldTypeRelationshipTitle()
 	{
 		return $this->title;
 	}
+
+	/**
+	 * Get options
+	 * 
+	 * @return array
+	 */
+	public function getFieldTypeRelationshipOptions()
+	{
+		return $this->get(array('id', 'title'))->lists('title', 'id');
+	}
+
+	public function posts($take = null, $skip = null)
+	{
+		return $this->hasMany('Pyro\Module\Blog\BlogEntryModel', 'category_id')
+			->published($take, $skip);
+	}
+
+
 }
--- system/cms/modules/blog/src/Pyro/Module/Blog/BlogEntryModel.php
+++ system/cms/modules/blog/src/Pyro/Module/Blog/BlogEntryModel.php
@@ -3,4 +3,45 @@
 use Pyro\Streams\Model\BlogsBlogEntryModel;
 
 class BlogEntryModel extends BlogsBlogEntryModel
-{}
+{
+
+	protected $appends = array('url');
+
+	public function findBySlug($slug)
+	{
+
+	}
+
+	public static function getMutatorCache()
+	{
+		return static::$mutatorCache;
+	}
+
+	public function getUrlAttribute()
+	{
+		return site_url('blog/'.date('Y/m', strtotime($this->created_at)).'/'.$this->slug);
+	}
+
+	public static function findManyByCategoryId($categoryId = null, $take = null, $skip = null)
+	{
+		if (! $categoryId) return false;
+
+		return static::published($take, $skip)
+			->where('category_id', '=', $categoryId)
+			->get();
+	}
+
+	public function scopeLive($query)
+	{
+		return $query->where('status', 'live');
+	}
+
+	public function scopePublished($query, $take = null, $skip = null)
+	{
+		return $query->live()
+			->orderBy('created_at', 'DESC')
+			->take($take)
+			->skip($skip);
+	}
+
+}
