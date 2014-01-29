ce9010fca6e12af5f98cdf3b5e3c1f7aa5440ca9
Osvaldo Brignoni <obrignoni@gmail.com>
2014-01-28 19:06:16 -0500

    Extend Presenters with our own

---
+++ system/cms/modules/streams_core/src/Pyro/Module/Streams_core/EntryPresenterDecorator.php
@@ -0,0 +1,52 @@
+<?php namespace Pyro\Module\Streams_core;
+
+use McCool\LaravelAutoPresenter\PresenterDecorator;
+
+class EntryPresenterDecorator extends PresenterDecorator
+{
+	protected $viewOptions = array();
+
+	public function viewOptions($viewOptions = array(), $defaultFormat = null)
+	{
+		$this->viewOptions = $viewOptions;
+		
+		$this->defaultFormat = $defaultFormat;
+
+		return $this;
+	}
+
+    /**
+     * decorate an individual class
+     *
+     * @param mixed $atom
+     * @return mixed
+     */
+    protected function decorateAtom($atom)
+    {
+
+    	if ( ! $this->viewOptions instanceof EntryViewOptions) {
+    		$this->viewOptions = new EntryViewOptions($atom, $this->viewOptions, $this->defaultFormat);
+    	}
+
+        if ( ! isset($atom->presenter)) {
+            return $atom;
+        }
+
+        if ($atom instanceOf BasePresenter) {
+            return $atom;
+        }
+
+        $presenterClass = $atom->presenter;
+
+        if ( ! class_exists($presenterClass)) {
+            throw new PresenterNotFoundException($presenterClass);
+        }
+
+        if ($atom instanceOf Model) {
+            $atom = $this->decorateRelations($atom);
+        }
+
+        return new $presenterClass($atom, $this->viewOptions);
+    }
+
+}
