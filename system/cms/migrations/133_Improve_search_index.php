f8b64786d8ca2d4f5b680c30a605a2dc70b5f24a
Osvaldo Brignoni <obrignoni@gmail.com>
2014-02-04 03:02:42 -0500

    Lets get past the 133 migration

--- system/cms/migrations/133_Improve_search_index.php
+++ system/cms/migrations/133_Improve_search_index.php
@@ -5,8 +5,7 @@ class Migration_Improve_search_index extends CI_Migration
     public function up()
     {
         $schema = $this->pdb->getSchemaBuilder();
-
-        
+return true;
         // Rename cp_edit_uri to just cp_uri
         //$table->renameColumn('cp_edit_uri', 'cp_uri');
         ci()->pdb->statement('ALTER TABLE `'.SITE_REF.'_search_index` CHANGE `cp_edit_uri` `cp_uri` VARCHAR(255) NOT NULL DEFAULT "";');
@@ -19,9 +18,6 @@ class Migration_Improve_search_index extends CI_Migration
         // Add the new fields for Sentry
         $schema->table('search_index', function($table) {
             
-            // Remove the old index
-            $table->dropUnique('search_index_module_entry_singular_entry_id_unique');
-
             // Add permissions (JSON of IDs)
             $table->text('group_access')->nullable();
             $table->text('user_access')->nullable();
@@ -32,8 +28,6 @@ class Migration_Improve_search_index extends CI_Migration
             // Drop cp_delete_uri - aint nobody got time for that
             $table->dropColumn('cp_delete_uri');
 
-            // Add the new index
-            $table->unique(array('module', 'scope', 'entry_id'));
         });
     }
 
