b922b900bb8d6b2c764d902e8ce45a6840a2c764
Osvaldo Brignoni <obrignoni@gmail.com>
2014-02-10 00:12:10 -0500

    Update users

--- system/cms/modules/users/src/Pyro/Module/Users/Ui/ProfileEntryUi.php
+++ system/cms/modules/users/src/Pyro/Module/Users/Ui/ProfileEntryUi.php
@@ -6,65 +6,69 @@ class ProfileEntryUi extends EntryUi
 {
     /**
      * Get default attributes
+     *
      * @return array
      */
-    public function getDefaultAttributes()
+    public function boot()
     {
+        parent::boot();
+
         // Filters to display on our table
-        // Filters
-        $filters = array(
-            'user' => array(
-                'type' => 'text',
-                'title' => 'lang:global:user',
-                'slug' => 'user',
-            ),
-            'email' => array(
-                'type' => 'text',
-                'title' => 'lang:global:email',
-                'slug' => 'email',
-            ),
-            'is_activated' => array(
-                'type' => 'select',
-                'title' => 'lang:user:active',
-                'slug' => 'is_activated',
-                'options' => array(
-                    null => '-- '.lang('user:active').' --',
-                    '1' => lang('global:yes'),
-                    '0' => lang('global:no'),
-                ),
-            ),
-        );
-
-        // Fields to display in our table
-        $fields = array(
-            'first_name',
-            'last_name',
-            'lang:user:activated_account_title' => '{{ if entry:is_activated }}{{ helper:lang line="global:yes" }}{{ else }}{{ helper:lang line="global:no" }}{{ endif }}',
-            'lang:global:user' => '{{ entry:username }}',
-            'lang:global:email' => '{{ entry:email }}',
-        );
-
-        // Buttons to display in our table
-        $buttons = array(
-            array(
-                'label' => lang('global:edit'),
-                'url' => 'admin/users/edit/{{ id }}',
-                'class' => 'btn-sm btn-warning',
-            ),
-            array(
-                'label' => lang('global:delete'),
-                'url' => 'admin/users/delete/{{ id }}',
-                'class' => 'btn-sm btn-danger confirm',
-            ),
-        );
-
-        return array_merge(
-            parent::getDefaultAttributes(), array(
-                'filters' => $filters,
-                'fields' => $fields,
-                'buttons' => $buttons,
-                'skips' => array(),
+        $this
+            ->filters(
+                array(
+                    'user'         => array(
+                        'type'  => 'text',
+                        'title' => 'lang:global:user',
+                        'slug'  => 'user',
+                    ),
+                    'email'        => array(
+                        'type'  => 'text',
+                        'title' => 'lang:global:email',
+                        'slug'  => 'email',
+                    ),
+                    'is_activated' => array(
+                        'type'    => 'select',
+                        'title'   => 'lang:user:active',
+                        'slug'    => 'is_activated',
+                        'options' => array(
+                            null => '-- ' . lang('user:active') . ' --',
+                            '1'  => lang('global:yes'),
+                            '0'  => lang('global:no'),
+                        ),
+                    ),
+                )
             )
-        );
+            // Fields to display in our table
+            ->fields(
+                array(
+                    'first_name',
+                    'last_name',
+                    'lang:user:activated_account_title' => '{{ if entry:is_activated }}{{ helper:lang line="global:yes" }}{{ else }}{{ helper:lang line="global:no" }}{{ endif }}',
+                    'lang:global:user'                  => '{{ entry:username }}',
+                    'lang:global:email'                 => '{{ entry:email }}',
+                )
+            )
+            // Buttons to display in our table
+            ->buttons(
+                array(
+                    array(
+                        'label' => lang('global:edit'),
+                        'url'   => 'admin/users/edit/{{ id }}',
+                        'class' => 'btn-sm btn-warning',
+                    ),
+                    array(
+                        'label' => lang('global:delete'),
+                        'url'   => 'admin/users/delete/{{ id }}',
+                        'class' => 'btn-sm btn-danger confirm',
+                    ),
+                )
+            )
+            ->messages(
+                array(
+                    'success' => 'User saved.'
+                )
+            ) // @todo - language
+            ->redirects('admin/users');
     }
 }
