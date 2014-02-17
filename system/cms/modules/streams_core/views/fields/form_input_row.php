074875b3b965a0a7ab3f42eb6e3f0c081db5caf8
Ryan Thompson <service@aiwebsystems.com>
2014-02-16 22:23:22 -0600

    remove is_ prefix

--- system/cms/modules/streams_core/views/fields/form_input_row.php
+++ system/cms/modules/streams_core/views/fields/form_input_row.php
@@ -1,6 +1,6 @@
 <label class="col-lg-2 control-label" for="<?php echo $field_type->form_slug;?>">
 	<?php echo lang_label($field_type->getField()->field_name);?>
-	<?php if($field_type->getField()->is_required): ?><span class="required">*</span><?php endif; ?>
+	<?php if($field_type->getField()->required): ?><span class="required">*</span><?php endif; ?>
 
 	<?php if( lang_label($field_type->getField()->instructions) != '' ): ?>
 		<p class="help-block c-gray-light"><?php echo lang_label($field_type->getField()->instructions); ?></p>
