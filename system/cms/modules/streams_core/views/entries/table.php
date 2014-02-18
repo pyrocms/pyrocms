07b8f7597026e5c931c9cee339b3e0e2795b7145
Ryan Thompson <service@aiwebsystems.com>
2014-02-17 20:36:24 -0600

    Add class support

--- system/cms/modules/streams_core/views/entries/table.php
+++ system/cms/modules/streams_core/views/entries/table.php
@@ -1,156 +1,195 @@
 <!-- .panel-body -->
 <!--<div class="panel-body">-->
 
-	<?php if (! empty($filters)): ?>
-		<?php $this->load->view('streams_core/entries/filters'); ?>
-	<?php endif; ?>
+<?php if (!empty($filters)): ?>
+    <?php $this->load->view('streams_core/entries/filters'); ?>
+<?php endif; ?>
 
 
-	<?php if ($entries->count() > 0): ?>
+<?php if ($entries->count() > 0): ?>
 
-		<table class="table table-hover n-m">
-			<thead>
-				<tr>
-					<?php if ($stream->sorting == 'custom'): ?><th></th><?php endif; ?>
-					<?php foreach ($fieldNames as $fieldSlug => $fieldName): ?>
-					<?php
+    <section class="table-responsive">
+        <table class="table table-hover n-m">
+            <thead>
+            <tr>
+                <?php if ($stream->sorting == 'custom'): ?>
+                    <th></th><?php endif; ?>
+                <?php foreach ($fieldNames as $fieldSlug => $fieldName): ?>
+                    <?php
 
-						// Replace relation: from Cp voodoo
-						$fieldSlug = str_replace('relation:', '', $fieldSlug);
+                    $class = isset($fields[$fieldSlug]['class']) ? $fields[$fieldSlug]['class'] : null;
 
-						// Get our query string
-						$query_string = array();
+                    // Replace relation: from Cp voodoo
+                    $fieldSlug = str_replace('relation:', '', $fieldSlug);
 
-						// Parse it into above array
-						parse_str($_SERVER['QUERY_STRING'], $query_string);
+                    // Get our query string
+                    $query_string = array();
 
-						$original_query_string = $query_string;
+                    // Parse it into above array
+                    parse_str($_SERVER['QUERY_STRING'], $query_string);
 
-						// Set the order slug
-						$query_string['order-'.$stream->stream_namespace.'-'.$stream->stream_slug] = $fieldSlug;
+                    $original_query_string = $query_string;
 
-						// Set the sort string
-						$query_string['sort-'.$stream->stream_namespace.'-'.$stream->stream_slug] = 
-							isset($query_string['sort-'.$stream->stream_namespace.'-'.$stream->stream_slug])
-								? ($query_string['sort-'.$stream->stream_namespace.'-'.$stream->stream_slug] == 'ASC'
-									? 'DESC'
-									: 'ASC')
-								: 'ASC';
+                    // Set the order slug
+                    $query_string['order-' . $stream->stream_namespace . '-' . $stream->stream_slug] = $fieldSlug;
 
-						// Determine our caret for this item
-						$caret = false;
+                    // Set the sort string
+                    $query_string['sort-' . $stream->stream_namespace . '-' . $stream->stream_slug] =
+                        isset($query_string['sort-' . $stream->stream_namespace . '-' . $stream->stream_slug])
+                            ? ($query_string['sort-' . $stream->stream_namespace . '-' . $stream->stream_slug] == 'ASC'
+                            ? 'DESC'
+                            : 'ASC')
+                            : 'ASC';
 
-						if (isset($original_query_string['order-'.$stream->stream_namespace.'-'.$stream->stream_slug]) and $original_query_string['order-'.$stream->stream_namespace.'-'.$stream->stream_slug] == $fieldSlug)
-							if (isset($original_query_string['sort-'.$stream->stream_namespace.'-'.$stream->stream_slug]))
-								if ($original_query_string['sort-'.$stream->stream_namespace.'-'.$stream->stream_slug] == 'ASC')
-									$caret = '<span class="fa fa-caret-up"></span>';
-								else
-									$caret = '<span class="fa fa-caret-down"></span>';
-							else
-								$caret = '<span class="fa fa-caret-up"></span>';
+                    // Determine our caret for this item
+                    $caret = false;
 
-						?>
-						<th>
-							<a href="<?php echo site_url(uri_string()).'?'.http_build_query($query_string); ?>">
-								<?php echo $fieldName; ?>
-                                <?php if ($caret) echo $caret; ?>
-							</a>
-						</th>
+                    if (isset($original_query_string['order-' . $stream->stream_namespace . '-' . $stream->stream_slug]) and $original_query_string['order-' . $stream->stream_namespace . '-' . $stream->stream_slug] == $fieldSlug) {
+                        if (isset($original_query_string['sort-' . $stream->stream_namespace . '-' . $stream->stream_slug])) {
+                            if ($original_query_string['sort-' . $stream->stream_namespace . '-' . $stream->stream_slug] == 'ASC') {
+                                $caret = '<span class="fa fa-caret-up"></span>';
+                            } else {
+                                $caret = '<span class="fa fa-caret-down"></span>';
+                            }
+                        } else {
+                            $caret = '<span class="fa fa-caret-up"></span>';
+                        }
+                    }
 
-					<?php endforeach; ?>
-				    <th></th>
-				</tr>
-			</thead>
-			<tbody>
-			<?php foreach ($entries as $entry) { ?>
+                    ?>
+                    <th class="<?php echo $class; ?>">
+                        <a href="<?php echo site_url(uri_string()) . '?' . http_build_query($query_string); ?>">
+                            <?php echo $fieldName; ?>
+                            <?php if ($caret) {
+                                echo $caret;
+                            } ?>
+                        </a>
+                    </th>
 
-				<tr>
+                <?php endforeach; ?>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($entries as $entry) { ?>
 
-					<?php if ($stream->sorting == 'custom'): ?><td width="30" class="handle"><?php echo Asset::img('icons/drag_handle.gif', 'Drag Handle'); ?></td><?php endif; ?>
+                <tr>
 
-					<?php if (! empty($viewOptions)): foreach($viewOptions as $viewOption): ?>
-					<td>
+                    <?php if ($stream->sorting == 'custom'): ?>
+                        <td width="30" class="handle"><?php echo Asset::img(
+                                'icons/drag_handle.gif',
+                                'Drag Handle'
+                            ); ?></td>
+                    <?php endif; ?>
 
-						<input type="hidden" name="action_to[]" value="<?php echo $entry->getKey();?>" />
+                    <?php if (!empty($viewOptions)): ?>
+                        <?php foreach ($viewOptions as $viewOption): ?>
+                            <?php $class = isset($fields[$viewOption]['class']) ? $fields[$viewOption]['class'] : null; ?>
+                            <td class="<?php echo $class; ?>">
 
-						<?php echo $entry->{$viewOption}; ?>
+                                <input type="hidden" name="action_to[]" value="<?php echo $entry->getKey(); ?>"/>
 
-					</td>
-					<?php endforeach; endif; ?>
-					<td class="text-right">
+                                <?php echo $entry->{$viewOption}; ?>
 
-						<?php
+                            </td>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <td class="text-right">
 
-							if (isset($buttons)) {
-								$all_buttons = array();
+                        <?php
 
-								foreach ($buttons as $button) {
+                        if (isset($buttons)) {
+                            $all_buttons = array();
 
-                                    // Html?
-                                    if (isset($button['html'])) {
-                                        $all_buttons[] = ci()->parser->parse_string($button['html'], $entry->getAttributes(), true, false, false);
-                                        continue;
-                                    }
+                            foreach ($buttons as $button) {
 
-									// The second is kept for backwards compatibility
-									$url = ci()->parser->parse_string($button['url'], $entry->getAttributes(), true, false, false);
-									$url = str_replace('-entry_id-', $entry->getKey(), $url);
+                                // Html?
+                                if (isset($button['html'])) {
+                                    $all_buttons[] = ci()->parser->parse_string(
+                                        $button['html'],
+                                        $entry->getAttributes(),
+                                        true,
+                                        false,
+                                        false
+                                    );
+                                    continue;
+                                }
 
-									// Label
-									$label = lang_label($button['label']);
+                                // The second is kept for backwards compatibility
+                                $url = ci()->parser->parse_string(
+                                    $button['url'],
+                                    $entry->getAttributes(),
+                                    true,
+                                    false,
+                                    false
+                                );
+                                $url = str_replace('-entry_id-', $entry->getKey(), $url);
 
-									// Remove URL
-									unset($button['url'], $button['label']);
+                                // Label
+                                $label = lang_label($button['label']);
 
-									// Parse variables in attributes
-									foreach ($button as $key => &$value)
-										$value = ci()->parser->parse_string($value, $entry->toArray(), true);
+                                // Remove URL
+                                unset($button['url'], $button['label']);
 
-									$all_buttons[] = anchor($url, $label, $button);
-								}
+                                // Parse variables in attributes
+                                foreach ($button as $key => &$value) {
+                                    $value = ci()->parser->parse_string($value, $entry->toArray(), true);
+                                }
 
-								echo implode('&nbsp;', $all_buttons);
-								unset($all_buttons);
-							}
+                                $all_buttons[] = anchor($url, $label, $button);
+                            }
 
-						?>
-					</td>
-				</tr>
-			<?php } ?>
-			</tbody>
-	    </table>
+                            echo implode('&nbsp;', $all_buttons);
+                            unset($all_buttons);
+                        }
 
-	    <div class="panel-footer">
-			
-			<?php if ($pagination): ?>
-                <?php echo $pagination['links']; ?>
+                        ?>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </section>
 
-                <?php echo form_dropdown(null, array(5 => 5, 10 => 10, 25 => 25, 50 => 50, 100 => 100), ci()->input->get('limit-'.$stream->stream_namespace.'-'.$stream->stream_slug) ?: Settings::get('records_per_page'), 'class="pull-right" style="width: 100px;" onchange="$(\'select#limit-'.$stream->stream_namespace.'-'.$stream->stream_slug.'\').val($(this).val()).closest(\'form\').submit();"'); ?>
-            <?php endif; ?>
-            <div class="clearfix"></div>
-		</div>
+    <div class="panel-footer">
 
-	<?php else: ?>
+        <?php if ($pagination): ?>
+            <?php echo $pagination['links']; ?>
 
-		<div class="alert alert-info m">
-			<?php
+            <?php echo form_dropdown(
+                null,
+                array(5 => 5, 10 => 10, 25 => 25, 50 => 50, 100 => 100),
+                ci()->input->get('limit-' . $stream->stream_namespace . '-' . $stream->stream_slug) ? : Settings::get(
+                    'records_per_page'
+                ),
+                'class="pull-right" style="width: 100px;" onchange="$(\'select#limit-' . $stream->stream_namespace . '-' . $stream->stream_slug . '\').val($(this).val()).closest(\'form\').submit();"'
+            ); ?>
+        <?php endif; ?>
+        <div class="clearfix"></div>
+    </div>
 
-				if (isset($no_entries_message) and $no_entries_message) {
-					echo lang_label($no_entries_message);
-				} else {
-					echo lang('streams:no_entries');
-				}
+<?php else: ?>
 
-			?>
-		</div><!--.no_data-->
+    <div class="alert alert-info m">
+        <?php
 
-	<?php endif; ?>
+        if (isset($no_entries_message) and $no_entries_message) {
+            echo lang_label($no_entries_message);
+        } else {
+            echo lang('streams:no_entries');
+        }
+
+        ?>
+    </div><!--.no_data-->
+
+<?php endif; ?>
 
 <!--</div>-->
 <!-- /.panel-body -->
 
 <div class="stats" style="margin-bottom: -45px;">
     <small class="c-gray m-l" style="line-height: 40px;">
-        Showing results <?php echo ($pagination['offset']+1).' - '.($pagination['current_page']*$pagination['per_page']).' of '.$pagination['total']; ?>
+        Showing
+        results <?php echo ($pagination['offset'] + 1) . ' - ' . ($pagination['current_page'] * $pagination['per_page']) . ' of ' . $pagination['total']; ?>
     </small>
 </div>
