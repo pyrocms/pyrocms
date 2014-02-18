14b1567e6d6dc521376a0c6c9f4829564d4af1ed
Ryan Thompson <service@aiwebsystems.com>
2014-02-18 13:27:31 -0600

    Clean up the row input view

--- system/cms/modules/streams_core/views/fields/form_input_row.php
+++ system/cms/modules/streams_core/views/fields/form_input_row.php
@@ -1,12 +1,17 @@
-<label class="col-lg-2 control-label" for="<?php echo $field_type->form_slug;?>">
-	<?php echo lang_label($field_type->getField()->field_name);?>
-	<?php if($field_type->getField()->required): ?><span class="required">*</span><?php endif; ?>
+<label class="col-lg-2 control-label" for="<?php echo $field_type->form_slug; ?>">
+    <?php echo $field_type->getField()->field_name; ?>
 
-	<?php if( lang_label($field_type->getField()->instructions) != '' ): ?>
-		<p class="help-block c-gray-light"><?php echo lang_label($field_type->getField()->instructions); ?></p>
-	<?php endif; ?>
+    <?php if ($field_type->getField()->required): ?>
+        <span class="required">*</span>
+    <?php endif; ?>
+
+    <?php if ($field_type->getField()->instructions != null): ?>
+        <p class="help-block c-gray-light">
+            <?php echo lang($field_type->getField()->instructions); ?>
+        </p>
+    <?php endif; ?>
 </label>
 
-<div  class="col-lg-10">
-	<?php echo $field_type->getInput(); ?>
+<div class="col-lg-10">
+    <?php echo $field_type->getInput(); ?>
 </div>
