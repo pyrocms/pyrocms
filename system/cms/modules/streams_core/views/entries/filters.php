0a50b8705769fb1c4bd31d8fb82afd1320057ea7
Ryan Thompson <service@aiwebsystems.com>
2014-02-20 13:21:31 -0600

    PSR

--- system/cms/modules/streams_core/views/entries/filters.php
+++ system/cms/modules/streams_core/views/entries/filters.php
@@ -1,21 +1,30 @@
-<?php if (! empty($filters)): ?>
-<section id="filters">
+<?php if (!empty($filters)): ?>
+    <section id="filters">
 
-	<?php echo form_open(null, array('method' => 'get', 'class' => 'form-inline'), array('filter-'.$stream->stream_namespace.'-'.$stream->stream_slug => 'y')); ?>
+        <?php echo form_open(null, array('method' => 'post', 'class' => 'form-inline')); ?>
 
-		<?php foreach ($filters as $filter): ?>
+        <?php foreach ($filters as $filter): ?>
+            <?php if ($field = $assignments->findBySlug($filter)): ?>
+                <?php echo $field->getType()->setStream($stream)->filterInput(); ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
 
-			<?php if ($field = $assignments->findBySlug($filter)) echo $field->getType()->setStream($stream)->filterInput(); ?>
 
-		<?php endforeach; ?>
-		
-		
-		<button class="button green"><?php echo lang('buttons:filter'); ?></button>
-		<a class="button" href="<?php echo site_url(uri_string()); ?>"><?php echo lang('buttons:clear'); ?></a>
+        <button class="button green" type="submit"
+                name="<?php echo 'filter-' . $stream->stream_namespace . '-' . $stream->stream_slug; ?>"
+                value="apply">
+            <?php echo lang('buttons:filter'); ?>
+        </button>
 
-		<br/><br/>
+        <button class="button" type="submit"
+                name="<?php echo 'filter-' . $stream->stream_namespace . '-' . $stream->stream_slug; ?>"
+                value="clear">
+            <?php echo lang('buttons:clear'); ?>
+        </button>
 
-	<?php echo form_close(); ?>
+        <br/><br/>
 
-</section>
+        <?php echo form_close(); ?>
+
+    </section>
 <?php endif; ?>
