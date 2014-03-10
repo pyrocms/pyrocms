4b5a04ecd6f75ee05110e0090b510b017f080415
Ryan Thompson <service@aiwebsystems.com>
2014-03-10 02:11:16 -0500

    PSR

--- system/cms/modules/streams_core/field_types/textarea/src/Pyro/FieldType/Textarea.php
+++ system/cms/modules/streams_core/field_types/textarea/src/Pyro/FieldType/Textarea.php
@@ -5,125 +5,123 @@ use Pyro\Module\Streams\FieldType\FieldTypeAbstract;
 /**
  * PyroStreams Textarea Field Type
  *
- * @package		PyroCMS\Core\Modules\Streams Core\Field Types
- * @author		Adam Fairholm
- * @copyright	Copyright (c) 2011 - 2012, Adam Fairholm
+ * @package        PyroCMS\Core\Modules\Streams Core\Field Types
+ * @author         Adam Fairholm
+ * @copyright      Copyright (c) 2011 - 2012, Adam Fairholm
  */
 class Textarea extends FieldTypeAbstract
 {
-	public $field_type_slug = 'textarea';
+    public $field_type_slug = 'textarea';
 
-	public $db_col_type = 'text';
+    public $db_col_type = 'text';
 
-	public $admin_display = 'full';
+    public $admin_display = 'full';
 
-	public $version = '1.1.0';
+    public $version = '1.1.0';
 
-	public $author = array(
-		'name' => 'Ryan Thompson - PyroCMS',
-		'url' => 'http://pyrocms.com/'
-		);
+    public $author = array(
+        'name' => 'Ryan Thompson - PyroCMS',
+        'url'  => 'http://pyrocms.com/'
+    );
 
-	public $custom_parameters = array(
-		'allow_tags',
-		'content_type',
-		'placeholder',
-		);
-	
-	/**
-	 * Output form input
-	 *
-	 * @param	array
-	 * @param	array
-	 * @return	string
-	 */
-	public function formInput()
-	{
-		return form_textarea(array(
-			'name' => $this->form_slug,
-			'id' => $this->form_slug,
-			'value' => $this->value,
-			'class' => 'form-control',
-			'placeholder' => lang_label($this->getParameter('placeholder')),
-		));
-	}
+    public $custom_parameters = array(
+        'allow_tags',
+        'content_type',
+        'placeholder',
+    );
 
-	/**
-	 * Pre-Ouput content
-	 *
-	 * @return 	string
-	 */
-	public function stringOutput()
-	{
-		$parse_tags		= $this->getParameter('allow_tags', 'n');
-		$content_type 	= $this->getParameter('content_type', 'html');
+    /**
+     * Output form input
+     *
+     * @param    array
+     * @param    array
+     * @return    string
+     */
+    public function formInput()
+    {
+        return form_textarea(
+            array(
+                'name'        => $this->form_slug,
+                'id'          => $this->form_slug,
+                'value'       => addslashes($this->value),
+                'class'       => 'form-control',
+                'placeholder' => lang_label($this->getParameter('placeholder')),
+            )
+        );
+    }
 
-		// If this is the admin, show only the source
-		// @TODO This is hacky, there will be times when the admin wants to see a preview or something
-		if (defined('ADMIN_THEME'))
-		{
-			return $this->value;
-		}
+    /**
+     * Pre-Ouput content
+     *
+     * @return    string
+     */
+    public function stringOutput()
+    {
+        $parse_tags   = $this->getParameter('allow_tags', 'n');
+        $content_type = $this->getParameter('content_type', 'html');
 
-		// If this isn't the admin and we want to allow tags,
-		// let it through. Otherwise we will escape them.
-		if ($parse_tags == 'y')
-		{
-			$content = ci()->parser->parse_string($this->value, array(), true);
-		}
-		else
-		{
-			ci()->load->helper('text');
-			$content = escape_tags($this->value);
-		}
+        // If this is the admin, show only the source
+        // @TODO This is hacky, there will be times when the admin wants to see a preview or something
+        if (defined('ADMIN_THEME')) {
+            return $this->value;
+        }
 
-		// Not that we know what content is there, what format should we treat is as?
-		switch ($content_type)
-		{
-			case 'md':
-				ci()->load->helper('markdown');
-				return parse_markdown($content);
+        // If this isn't the admin and we want to allow tags,
+        // let it through. Otherwise we will escape them.
+        if ($parse_tags == 'y') {
+            $content = ci()->parser->parse_string($this->value, array(), true);
+        } else {
+            ci()->load->helper('text');
+            $content = escape_tags($this->value);
+        }
 
-			case 'html':
-				return $content;
+        // Not that we know what content is there, what format should we treat is as?
+        switch ($content_type) {
+            case 'md':
+                ci()->load->helper('markdown');
+                return parse_markdown($content);
 
-			default:
-				return strip_tags($content);
-		}
+            case 'html':
+                return $content;
 
-	}
-	
-	/**
-	 * Content Type
-	 *
-	 * Is this plain text, HTML or Markdown?
-	 */
-	public function paramContentType($value = null)
-	{
-		$options = array(
-			'text' => lang('global:plain-text'),
-			'html' => 'HTML',
-			'md'   => 'Markdown',
-		);
+            default:
+                return strip_tags($content);
+        }
 
-		// Defaults to Plain Text
-		$value or $value = 'text';
-	
-		return form_dropdown('content_type', $options, $value);
-	}
+    }
 
-	/**
-	 * Default Textarea Value
-	 *
-	 * @param	[string - value]
-	 * @return	string
-	 */
-	public function paramDefaultValue($value = null)
-	{
-		return form_textarea(array(
-			'name'		=> 'default_value',
-			'id'		=> 'default_value',
-			'value'		=> $value,
-		));
-	}
+    /**
+     * Content Type
+     * Is this plain text, HTML or Markdown?
+     */
+    public function paramContentType($value = null)
+    {
+        $options = array(
+            'text' => lang('global:plain-text'),
+            'html' => 'HTML',
+            'md'   => 'Markdown',
+        );
+
+        // Defaults to Plain Text
+        $value or $value = 'text';
+
+        return form_dropdown('content_type', $options, $value);
+    }
+
+    /**
+     * Default Textarea Value
+     *
+     * @param    [string - value]
+     * @return    string
+     */
+    public function paramDefaultValue($value = null)
+    {
+        return form_textarea(
+            array(
+                'name'  => 'default_value',
+                'id'    => 'default_value',
+                'value' => $value,
+            )
+        );
+    }
 }
