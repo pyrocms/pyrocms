864a7213ef93ce4d86794282c50ea6dad24be7e1
Ryan Thompson <service@aiwebsystems.com>
2014-03-13 21:45:35 -0500

    https://github.com/pyrocms/pyrocms/pull/3207

--- system/cms/modules/streams_core/field_types/image/src/Pyro/FieldType/Image.php
+++ system/cms/modules/streams_core/field_types/image/src/Pyro/FieldType/Image.php
@@ -181,6 +181,7 @@ class Image extends FieldTypeAbstract
             return null;
         } else {
             // Return the ID of the file DB entry
+            Events::trigger('file_uploaded', $return);
             return $return['data']['id'];
         }
     }
