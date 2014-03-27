58b62f2916a75d3e9542bcc9acaa8f39516f4ef6
Ryan Thompson <service@aiwebsystems.com>
2014-03-27 01:51:04 -0500

    Fixed widgets
    Let fire and brimstone hail down upon whomever is guilty of this. For all eternity.

--- system/cms/modules/widgets/views/admin/instances/form.php
+++ system/cms/modules/widgets/views/admin/instances/form.php
@@ -1,38 +1,47 @@
-<?php echo form_open() ?>
+<?php echo form_open(); ?>
 
-    <header><h3><?php echo $widget->name ?></h3></header>
+<header>
+    <h3><?php echo $widget->name ?></h3>
+</header>
 
-    <?php if ($instance): ?>
-        <?php echo form_hidden('id', $instance->id) ?>
-    <?php endif ?>
+<?php if ($instance): ?>
+    <?php echo form_hidden('id', $instance->id); ?>
+<?php endif; ?>
 
-    <?php echo form_hidden('widget_id', $widget->id) ?>
+<?php echo form_hidden('widget_id', $widget->id); ?>
 
-    <?php if (! empty($error)) echo $error ?>
+<?php if (!empty($error)): ?>
+    <?php echo $error; ?>
+<?php endif; ?>
 
-    <ol>
-        <li>
-            <label for="name"><?php echo lang('name_label') ?>:</label>
-            <?php echo form_input('name', set_value('name', $instance->name)) ?>
-            <span class="required-icon tooltip"><?php echo lang('required_label') ?></span>
-        </li>
+<ol>
+    <li>
+        <label for="name"><?php echo lang('name_label') ?>:</label>
+        <?php echo form_input('name', set_value('name', $instance->name)); ?>
+        <span class="required-icon tooltip"><?php echo lang('required_label'); ?></span>
+    </li>
 
-        <li>
-            <label for="show_title"><?php echo lang('widgets:show_title') ?>:</label>
-            <?php echo form_checkbox('show_title', true, isset($widget->options['show_title']) ? $widget->options['show_title'] : false) ?>
-        </li>
+    <li>
+        <label for="show_title"><?php echo lang('widgets:show_title') ?>:</label>
+        <?php echo form_checkbox(
+            'show_title',
+            true,
+            isset($widget->options['show_title']) ? $widget->options['show_title'] : false
+        ) ?>
+    </li>
 
-        <?php if (isset($areas)): ?>
+    <?php if (isset($areas)): ?>
         <li>
             <label for="widget_area_id"><?php echo lang('widgets:widget_area') ?>:</label>
-            <?php echo form_dropdown('widget_area_id', $areas, $widget->widget_area_id) ?>
+            <?php echo form_dropdown('widget_area_id', $areas, $widget->widget_area_id); ?>
         </li>
-        <?php endif ?>
-    </ol>
-    <?php echo $form ?: '' ?>
+    <?php endif; ?>
+</ol>
 
-    <div id="instance-actions" class="buttons buttons-small">
-        <?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel'))) ?>
-    </div>
+<?php echo $form ? : ''; ?>
 
-<?php echo form_close()
+<div id="instance-actions" class="buttons buttons-small">
+    <?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel'))); ?>
+</div>
+
+<?php echo form_close(); ?>
