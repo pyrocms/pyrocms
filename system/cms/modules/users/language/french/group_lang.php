de29bb9dfefbb4568386525acac95ade52178b98
Khalil Tabbal <khalil.tabbal@gmail.com>
2014-03-29 10:31:05 -0400

    french translations for module users

--- system/cms/modules/users/language/french/group_lang.php
+++ system/cms/modules/users/language/french/group_lang.php
@@ -1,5 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+$lang['users:group']                        = 'Groupe';
+$lang['users:groups']                       = 'Groupes';
+
 // labels
 $lang['groups:title']					= 'Titre';
 $lang['groups:name']				    = 'Nom';
