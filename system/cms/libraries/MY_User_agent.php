482dc5acb431ce9d6caac03ce92ea92170c7ee6c
Brennon Loveless <brennon.loveless@gmail.com>
2014-04-16 15:44:38 -0600

    Added the ability to override ->agent->is_mobile using the querystring parameter ?mobile_override using an immediately expiring cookie so the site will go back to rending in mobile after the browser has been closed and re-opened

---
+++ system/cms/libraries/MY_User_agent.php
@@ -0,0 +1,57 @@
+<?php
+
+# If the user is viewing the mobile site and is requesting to view the full site
+# We will set a cookie here and then redirect them to the page they were requesting
+if(strpos($_SERVER['REQUEST_URI'], 'mobile_override') !== false) {
+	// first set the mobile_override cookie
+	setcookie('mobile_override', true, 0, '/', '.control4.com');	
+	// remove the mobile override get parameter
+	$new_uri = str_replace('mobile_override', '', $_SERVER['REQUEST_URI']);
+	$new_link = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$new_uri}";
+
+	// if the new link ends in just a ? we will stip it off. Otherwise we will leave it.
+	if(substr($new_link, -1) === '?') {
+		$new_link = substr($new_link, 0, -1);
+	}
+	
+	// then redirect to the current page to activate the cookie
+	header('Location: ' . $new_link);
+	exit;
+}
+
+/**
+ * MY_User_agent
+ * Fix to allow users to bounce back to the full site if they are on a mobile device
+ *
+ * @author      Brennon Loveless
+ */
+class MY_User_agent extends CI_User_agent
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+
+	public function is_mobile($key = null)
+	{
+		// If the mobile override cookie is set then ignore is_mobile and just return false
+		if(isset($_COOKIE['mobile_override'])) {
+			return FALSE;
+		}
+
+		if ( ! $this->is_mobile)
+		{
+			return FALSE;
+		}
+
+		// No need to be specific, it's a mobile
+		if ($key === NULL)
+		{
+			return TRUE;
+		}
+
+		// Check for a specific robot
+		return (isset($this->mobiles[$key]) && $this->mobile === $this->mobiles[$key]);
+	}
+}
