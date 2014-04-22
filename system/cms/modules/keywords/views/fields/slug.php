42582d7a95897f140eb83f67e42d0be0e3849017
Ryan Thompson <service@aiwebsystems.com>
2014-04-22 18:14:54 -0500

    Styled for 3.0

--- system/cms/modules/keywords/views/fields/slug.php
+++ system/cms/modules/keywords/views/fields/slug.php
@@ -1,14 +1,22 @@
-<label for="<?php echo $field_type->form_slug; ?>"><?php echo lang_label($field_type->getField()->field_name); ?>
+<label class="col-lg-2 control-label" for="<?php echo $field_type->form_slug; ?>">
+    <?php echo $field_type->getField()->field_name; ?>
 
-    <?php if ($field_type->getField()->is_required): ?><span class="required">*</span><?php endif; ?>
-
-    <?php if (!empty($field_type->getField()->instructions)): ?>
-        <br/>
-        <small><?php echo lang_label($field_type->getField()->instructions); ?></small>
+    <?php if ($field_type->getField()->is_required): ?>
+        <span class="required">*</span>
     <?php endif; ?>
 
+    <?php if ($field_type->getField()->instructions != null): ?>
+        <p class="help-block c-gray-light">
+            <?php echo lang_label($field_type->getField()->instructions); ?>
+        </p>
+    <?php endif; ?>
 </label>
 
-<div class="input">
-    <?php echo site_url(); ?>/<?php echo $field_type->getInput(); ?>
+<div class="col-lg-10">
+    <div class="d-inline-block">
+        <?php echo site_url(); ?>/
+    </div>
+    <div class="d-inline-block">
+        <?php echo $field_type->getInput(); ?>
+    </div>
 </div>
