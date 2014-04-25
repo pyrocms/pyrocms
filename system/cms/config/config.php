de793c781124010e0849997beb9cfbc7ad01d0f5
Ryan Thompson <service@aiwebsystems.com>
2014-04-25 12:12:49 -0500

    Key

--- system/cms/config/config.php
+++ system/cms/config/config.php
@@ -230,7 +230,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'FvB3Xmf59WOcmiYoSIWl';
 
 /*
 |--------------------------------------------------------------------------
