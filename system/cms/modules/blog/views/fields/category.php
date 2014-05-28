c7b7bb59576906adea3be071cc994f0f207d65b0
Ryan Thompson <service@aiwebsystems.com>
2014-05-28 11:19:33 -0500

    Correct view path for category override

---
+++ system/cms/modules/blog/views/fields/category.php
@@ -0,0 +1,15 @@
+<label for="<?php echo $field_type->form_slug;?>"><?php echo lang_label($field_type->getField()->field_name);?>
+
+    <?php if($field_type->getField()->is_required): ?><span class="required">*</span><?php endif; ?>
+
+    <?php if( ! empty($field_type->getField()->instructions)): ?>
+        <br /><small><?php echo lang_label($field_type->getField()->instructions); ?></small>
+    <?php endif; ?>
+
+</label>
+
+<div class="input">
+    <?php echo $field_type->getInput(); ?>
+
+    [ <?php echo anchor('admin/blog/categories/create_ajax', lang('blog:new_category_label'), 'target="_blank" class="modal category-shortcut"') ?> ]
+</div>
