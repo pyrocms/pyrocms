1b26f3e1ec514d423940acc69956f5df5127c392
james2doyle <james2doyle@gmail.com>
2014-07-01 11:39:06 -0400

    revert pages admin

--- system/cms/modules/pages/controllers/admin.php
+++ system/cms/modules/pages/controllers/admin.php
@@ -100,7 +100,7 @@ class Admin extends Admin_Controller {
     			$html .= '</a></li>';
     		}
     		
-    		if (ob_get_contents()) ob_end_clean();
+    		ob_end_clean();
     		echo $html .= '</ul>';
             return;
         }
