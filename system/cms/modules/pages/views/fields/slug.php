217469e00c03a13fe787f2de29b1ed5ff65e5a88
Ryan Thompson <service@aiwebsystems.com>
2014-07-29 23:00:34 -0500

    Proper slug display for child pages

--- system/cms/modules/pages/views/fields/slug.php
+++ system/cms/modules/pages/views/fields/slug.php
@@ -1,4 +1,7 @@
-<label for="<?php echo $field_type->form_slug; ?>"><?php echo lang_label($field_type->getField()->field_name); ?>
+<label for="<?php
+use Pyro\Module\Pages\Model\Page;
+
+echo $field_type->form_slug; ?>"><?php echo lang_label($field_type->getField()->field_name); ?>
 
     <?php if ($field_type->getField()->is_required): ?><span class="required">*</span><?php endif; ?>
 
@@ -10,5 +13,7 @@
 </label>
 
 <div class="input">
-    <?php echo site_url(); ?><?php echo $field_type->getInput(); ?>
+    <?php $page = Page::find(ci()->input->get('parent')); ?>
+    <?php echo site_url($page ? $page->uri : null) . ($page ? '/' : null); ?><?php echo $field_type->getInput(
+    ); ?>
 </div>
