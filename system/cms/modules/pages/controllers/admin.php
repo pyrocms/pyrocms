f6789786fe06af0a7edec197482596bf523c289d
Robin <robingomez05@gmail.com>
2014-08-08 20:47:57 -0500

    Update admin.php
    fuente: https://forum.pyrocms.com/discussion/24730/message-ob_end_clean-failed-to-delete-buffer-no-buffer-to-delete

--- system/cms/modules/pages/controllers/admin.php
+++ system/cms/modules/pages/controllers/admin.php
@@ -100,7 +100,7 @@ class Admin extends Admin_Controller {
     			$html .= '</a></li>';
     		}
     		
-    		ob_end_clean();
+    		if (ob_get_length() > 0) { ob_end_clean(); }
     		echo $html .= '</ul>';
             return;
         }
