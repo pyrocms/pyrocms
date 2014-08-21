f0506fee8fc18ad0bf70ae80447755e18696c220
RodrigoBalest <r.balest@gmail.com>
2014-08-21 12:33:56 -0300

    Language strings for file type validation
    A new translation string was added due to file type
    validation. Also, another string's key was corrected.

--- system/cms/modules/streams_core/field_types/file/language/lithuanian/file_lang.php
+++ system/cms/modules/streams_core/field_types/file/language/lithuanian/file_lang.php
@@ -4,4 +4,5 @@ $lang['streams:file.name'] 		= 'Failas';
 $lang['streams:file.folder']		= 'Įkėlimo katalogas';
 $lang['streams:file.allowed_types']	= 'Leidžiami formatai';
 $lang['streams:file.folder_notice']     = 'Privalote nurodyti įkėlimo katalogą, prieš įkeliant failą.';
-$lang['streams:file.allowed_types_instrcutions'] = 'Failo formatus atskirkite vertikaliu brūkšniu (|). Pvz.: pdf|png';
+$lang['streams:file.allowed_types_instructions'] = 'Failo formatus atskirkite vertikaliu brūkšniu (|). Pvz.: pdf|png';
+$lang['streams:file.allowed_types_error']        = 'The field %s contains an invalid file type.'; #translate
