1f06892398eda3df02f91d41050df75cd98db021
Daniel Richards <danrichardsri@gmail.com>
2014-08-23 13:42:39 -0400

    Support Streams forms and UI Components
    http://i.xomf.com/vwpwm.png

--- system/cms/themes/pyrocms/views/admin/layouts/modal.php
+++ system/cms/themes/pyrocms/views/admin/layouts/modal.php
@@ -6,19 +6,36 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<?php 
-		Asset::js('jquery/jquery.js');
-		Asset::js('jquery/jquery.js');
+		
+		Asset::css('plugins.css'); 
+		Asset::css('workless/workless.css'); 
+		Asset::css('webdujour.css'); 
+		Asset::css('codemirror.css'); 
+		Asset::css('animate/animate.css'); 
 		Asset::css('admin/basic_layout.css'); 
+		Asset::js('jquery/jquery.js');
+		Asset::js('jquery/jquery-ui.min.js');
+		Asset::js('jquery/jquery.cooki.js');
+		Asset::js('jquery/jquery.slugify.js');
+		Asset::js('plugins.js');
+		Asset::js('scripts.js');
 		echo Asset::render();
 	?>
+
 	
 	<!-- metadata needs to load before some stuff -->
 	<?php file_partial('metadata'); ?>
 
 	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
-<body>
+<body style="background: none;">
 	<?php $this->load->view('admin/partials/notices') ?>
-	<?php echo $template['body']; ?>
+	<div id="container">
+		<section id="content" style="margin-top:0px!important;">
+			<div id="content-body">
+				<?php echo $template['body']; ?>
+			</div>
+		</section>
+	</div>
 </body>
 </html>
