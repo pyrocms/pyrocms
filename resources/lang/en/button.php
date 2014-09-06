bfa7663845760cb387b5165cb81de2a6f181b517
Ryan Thompson <service@aiwebsystems.com>
2014-09-06 01:52:21 -0500

    Lang

---
+++ resources/lang/en/button.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'cancel'        => 'Cancel',
+    'delete'        => 'Delete',
+    'edit'          => 'Edit',
+    'save'          => 'Save',
+    'save_exit'     => 'Save &amp; Exit',
+    'save_create'   => 'Save &amp; Create',
+    'save_continue' => 'Save &amp; Continue',
+];
---
+++ resources/lang/en/misc.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'untitled' => 'Untitled',
+];
