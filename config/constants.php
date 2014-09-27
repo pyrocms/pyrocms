80cdd14dbece47dd343b66537d1c78538bda976a
Ryan Thompson <service@aiwebsystems.com>
2014-09-26 22:10:24 -0500

    Constants, yay!

---
+++ config/constants.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Version
+	|--------------------------------------------------------------------------
+	|
+	| This is the version of Streams Platform.
+	| @todo - move this to the platform package.
+	|
+	*/
+
+	'version' => '1.0-Alpha',
+
+];
