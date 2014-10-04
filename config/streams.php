9b4629e70631d6f5cc332100e23991eb0027f5a5
Ryan Thompson <service@aiwebsystems.com>
2014-10-03 21:46:04 -0500

    Streams config

---
+++ config/streams.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Supported Addon Types
+    |--------------------------------------------------------------------------
+    |
+    | Streams will look for and load addons only of these types.
+    |
+    */
+
+    'addons' => [
+        'types' => [
+            'distribution',
+            'field_type',
+            'extension',
+            'module',
+            'block',
+            'theme',
+            'tag',
+        ]
+    ],
+];
