30b8bd69c7010aaed0934b01ead611a4b5776630
Ryan Thompson <service@aiwebsystems.com>
2014-11-05 00:17:43 -0600

    Streams

---
+++ config/streams.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+	|--------------------------------------------------------------------------
+	| Distribution
+	|--------------------------------------------------------------------------
+	|
+	| This controls the primary distribution used by Streams. The Streams
+    | distribution is like the DNA of the application's unique characteristics.
+	|
+	*/
+
+    'distribution' => 'streams',
+
+    /*
+	|--------------------------------------------------------------------------
+	| Available Locales
+	|--------------------------------------------------------------------------
+	|
+	| This controls the top level locales that are available to the system.
+    | This will prevent from locales where no translations exist.
+	|
+	*/
+
+    'available_locales' => [
+        'en' => 'English',
+        'fr' => 'Français',
+    ],
+];
