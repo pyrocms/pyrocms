89a8f411a0b4fdb71f6dc61abb6dd11dbd1b86b6
Ryan Thompson <service@aiwebsystems.com>
2014-12-07 14:53:00 -0600

    Hmm

--- config/streams.php
+++ config/streams.php
@@ -12,8 +12,7 @@ return [
 	|
 	*/
 
-    'distribution' => 'streams',
-
+    'distribution'      => 'streams',
     /*
 	|--------------------------------------------------------------------------
 	| Available Locales
@@ -28,59 +27,74 @@ return [
         'en',
         'fr',
     ],
-
     /*
 	|--------------------------------------------------------------------------
 	| Entry Management
 	|--------------------------------------------------------------------------
 	*/
 
-    'entries' => [
-        'model' => 'Anomaly\Streams\Platform\Entry\EntryModel',
+    'entries'           => [
+        'model'      => 'Anomaly\Streams\Platform\Entry\EntryModel',
         'repository' => 'Anomaly\Streams\Platform\Entry\EntryRepository',
     ],
-
     /*
 	|--------------------------------------------------------------------------
 	| Stream Management
 	|--------------------------------------------------------------------------
 	*/
 
-    'streams' => [
-        'model' => 'Anomaly\Streams\Platform\Stream\StreamModel',
+    'streams'           => [
+        'model'      => 'Anomaly\Streams\Platform\Stream\StreamModel',
         'repository' => 'Anomaly\Streams\Platform\Stream\StreamRepository',
     ],
-
     /*
 	|--------------------------------------------------------------------------
 	| Field Management
 	|--------------------------------------------------------------------------
 	*/
 
-    'fields' => [
-        'model' => 'Anomaly\Streams\Platform\Field\FieldModel',
+    'fields'            => [
+        'model'      => 'Anomaly\Streams\Platform\Field\FieldModel',
         'repository' => 'Anomaly\Streams\Platform\Field\FieldRepository',
     ],
-
     /*
 	|--------------------------------------------------------------------------
 	| Assignment Management
 	|--------------------------------------------------------------------------
 	*/
 
-    'assignments' => [
-        'model' => 'Anomaly\Streams\Platform\Assignment\AssignmentModel',
+    'assignments'       => [
+        'model'      => 'Anomaly\Streams\Platform\Assignment\AssignmentModel',
         'repository' => 'Anomaly\Streams\Platform\Assignment\AssignmentRepository',
     ],
-
     /*
 	|--------------------------------------------------------------------------
 	| Module Management
 	|--------------------------------------------------------------------------
 	*/
 
-    'modules' => [
-        'model' => 'Anomaly\Streams\Platform\Addon\Module\ModuleModel',
+    'modules'           => [
+        'model'      => 'Anomaly\Streams\Platform\Addon\Module\ModuleModel',
         'repository' => 'Anomaly\Streams\Platform\Addon\Module\ModuleRepository',
+    ],
+    /*
+	|--------------------------------------------------------------------------
+	| Buttons - This facilitates easy standardization between theme frameworks.
+	|--------------------------------------------------------------------------
+	*/
+
+    'buttons'           => [
+        'button'     => 'Anomaly\Streams\Platform\Ui\Button\Button',
+        'repository' => 'Anomaly\Streams\Platform\Ui\Button\ButtonRepository',
+    ],
+    /*
+	|--------------------------------------------------------------------------
+	| Icons - This facilitates easy standardization between theme frameworks.
+	|--------------------------------------------------------------------------
+	*/
+
+    'icons'             => [
+        'icon'       => 'Anomaly\Streams\Platform\Ui\Icon\Icon',
+        'repository' => 'Anomaly\Streams\Platform\Ui\Icon\IconRepository',
     ],
 ];
