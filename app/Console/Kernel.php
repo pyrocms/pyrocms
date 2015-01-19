570f87cdd62fd285a864c4de955cceed76f725d9
Ryan Thompson <service@aiwebsystems.com>
2015-01-19 17:33:29 -0600

    Required

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,4 +5,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel {
 
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [];
+
 }
