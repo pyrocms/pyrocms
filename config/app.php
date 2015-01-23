bcd85839bb4dc4816ad253e2dd905b5cd07c1f57
Ryan Thompson <service@aiwebsystems.com>
2015-01-23 00:00:48 -0600

    Revert

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
 	|
 	*/
 
-	'debug' => true,
+	'debug' => env('APP_DEBUG'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -110,36 +110,36 @@ return [
 
 	'providers' => [
 
-	/*
-	* Laravel Framework Service Providers...
-	*/
-	'Illuminate\Foundation\Providers\ArtisanServiceProvider',
-	'Illuminate\Auth\AuthServiceProvider',
-	'Illuminate\Bus\BusServiceProvider',
-	'Illuminate\Cache\CacheServiceProvider',
-	'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
-	'Illuminate\Routing\ControllerServiceProvider',
-	'Illuminate\Cookie\CookieServiceProvider',
-	'Illuminate\Database\DatabaseServiceProvider',
-	'Illuminate\Encryption\EncryptionServiceProvider',
-	'Illuminate\Filesystem\FilesystemServiceProvider',
-	'Illuminate\Foundation\Providers\FoundationServiceProvider',
-	'Illuminate\Hashing\HashServiceProvider',
-	'Illuminate\Mail\MailServiceProvider',
-	'Illuminate\Pagination\PaginationServiceProvider',
-	'Illuminate\Pipeline\PipelineServiceProvider',
-	'Illuminate\Queue\QueueServiceProvider',
-	'Illuminate\Redis\RedisServiceProvider',
-	'Illuminate\Auth\Passwords\PasswordResetServiceProvider',
-	'Illuminate\Session\SessionServiceProvider',
-	'Illuminate\Translation\TranslationServiceProvider',
-	'Illuminate\Validation\ValidationServiceProvider',
-	'Illuminate\View\ViewServiceProvider',
+		/*
+        * Laravel Framework Service Providers...
+        */
+		'Illuminate\Foundation\Providers\ArtisanServiceProvider',
+		'Illuminate\Auth\AuthServiceProvider',
+		'Illuminate\Bus\BusServiceProvider',
+		'Illuminate\Cache\CacheServiceProvider',
+		'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
+		'Illuminate\Routing\ControllerServiceProvider',
+		'Illuminate\Cookie\CookieServiceProvider',
+		'Illuminate\Database\DatabaseServiceProvider',
+		'Illuminate\Encryption\EncryptionServiceProvider',
+		'Illuminate\Filesystem\FilesystemServiceProvider',
+		'Illuminate\Foundation\Providers\FoundationServiceProvider',
+		'Illuminate\Hashing\HashServiceProvider',
+		'Illuminate\Mail\MailServiceProvider',
+		'Illuminate\Pagination\PaginationServiceProvider',
+		'Illuminate\Pipeline\PipelineServiceProvider',
+		'Illuminate\Queue\QueueServiceProvider',
+		'Illuminate\Redis\RedisServiceProvider',
+		'Illuminate\Auth\Passwords\PasswordResetServiceProvider',
+		'Illuminate\Session\SessionServiceProvider',
+		'Illuminate\Translation\TranslationServiceProvider',
+		'Illuminate\Validation\ValidationServiceProvider',
+		'Illuminate\View\ViewServiceProvider',
 
-	/*
-	* Streams Platform Service Provider...
-	*/
-	'Anomaly\Streams\Platform\StreamsServiceProvider'
+		/*
+        * Streams Platform Service Provider...
+        */
+		'Anomaly\Streams\Platform\StreamsServiceProvider'
 
 	],
 
@@ -156,36 +156,36 @@ return [
 
 	'aliases' => [
 
-	'App'       => 'Illuminate\Support\Facades\App',
-	'Artisan'   => 'Illuminate\Support\Facades\Artisan',
-	'Auth'      => 'Illuminate\Support\Facades\Auth',
-	'Blade'     => 'Illuminate\Support\Facades\Blade',
-	'Cache'     => 'Illuminate\Support\Facades\Cache',
-	'Config'    => 'Illuminate\Support\Facades\Config',
-	'Cookie'    => 'Illuminate\Support\Facades\Cookie',
-	'Crypt'     => 'Illuminate\Support\Facades\Crypt',
-	'DB'        => 'Illuminate\Support\Facades\DB',
-	'Event'     => 'Illuminate\Support\Facades\Event',
-	'File'      => 'Illuminate\Support\Facades\File',
-	'Hash'      => 'Illuminate\Support\Facades\Hash',
-	'Input'     => 'Illuminate\Support\Facades\Input',
-	'Inspiring' => 'Illuminate\Foundation\Inspiring',
-	'Lang'      => 'Illuminate\Support\Facades\Lang',
-	'Log'       => 'Illuminate\Support\Facades\Log',
-	'Mail'      => 'Illuminate\Support\Facades\Mail',
-	'Paginator' => 'Illuminate\Support\Facades\Paginator',
-	'Password'  => 'Illuminate\Support\Facades\Password',
-	'Queue'     => 'Illuminate\Support\Facades\Queue',
-	'Redirect'  => 'Illuminate\Support\Facades\Redirect',
-	'Redis'     => 'Illuminate\Support\Facades\Redis',
-	'Request'   => 'Illuminate\Support\Facades\Request',
-	'Response'  => 'Illuminate\Support\Facades\Response',
-	'Route'     => 'Illuminate\Support\Facades\Route',
-	'Schema'    => 'Illuminate\Support\Facades\Schema',
-	'Session'   => 'Illuminate\Support\Facades\Session',
-	'URL'       => 'Illuminate\Support\Facades\URL',
-	'Validator' => 'Illuminate\Support\Facades\Validator',
-	'View'      => 'Illuminate\Support\Facades\View',
+		'App'       => 'Illuminate\Support\Facades\App',
+		'Artisan'   => 'Illuminate\Support\Facades\Artisan',
+		'Auth'      => 'Illuminate\Support\Facades\Auth',
+		'Blade'     => 'Illuminate\Support\Facades\Blade',
+		'Cache'     => 'Illuminate\Support\Facades\Cache',
+		'Config'    => 'Illuminate\Support\Facades\Config',
+		'Cookie'    => 'Illuminate\Support\Facades\Cookie',
+		'Crypt'     => 'Illuminate\Support\Facades\Crypt',
+		'DB'        => 'Illuminate\Support\Facades\DB',
+		'Event'     => 'Illuminate\Support\Facades\Event',
+		'File'      => 'Illuminate\Support\Facades\File',
+		'Hash'      => 'Illuminate\Support\Facades\Hash',
+		'Input'     => 'Illuminate\Support\Facades\Input',
+		'Inspiring' => 'Illuminate\Foundation\Inspiring',
+		'Lang'      => 'Illuminate\Support\Facades\Lang',
+		'Log'       => 'Illuminate\Support\Facades\Log',
+		'Mail'      => 'Illuminate\Support\Facades\Mail',
+		'Paginator' => 'Illuminate\Support\Facades\Paginator',
+		'Password'  => 'Illuminate\Support\Facades\Password',
+		'Queue'     => 'Illuminate\Support\Facades\Queue',
+		'Redirect'  => 'Illuminate\Support\Facades\Redirect',
+		'Redis'     => 'Illuminate\Support\Facades\Redis',
+		'Request'   => 'Illuminate\Support\Facades\Request',
+		'Response'  => 'Illuminate\Support\Facades\Response',
+		'Route'     => 'Illuminate\Support\Facades\Route',
+		'Schema'    => 'Illuminate\Support\Facades\Schema',
+		'Session'   => 'Illuminate\Support\Facades\Session',
+		'URL'       => 'Illuminate\Support\Facades\URL',
+		'Validator' => 'Illuminate\Support\Facades\Validator',
+		'View'      => 'Illuminate\Support\Facades\View',
 
 	],
 
