0a11a8e5bcc71b8e3dbe0ac1b69b857727eb26e7
Ryan Thompson <service@aiwebsystems.com>
2015-02-27 12:29:20 -0600

    YAY! f

--- database/migrations/2015_02_27_074307_create_applications_table.php
+++ database/migrations/2015_02_27_074307_create_applications_table.php
@@ -1,9 +1,7 @@
 <?php
 
-use Anomaly\Streams\Platform\Application\Application;
 use Anomaly\Streams\Platform\Database\Migration\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
 
 /**
  * Class CreateApplicationsTable
@@ -16,37 +14,14 @@ class CreateApplicationsTable extends Migration
 {
 
     /**
-     * The streams application.
-     *
-     * @param Application
-     */
-    protected $application;
-
-    /**
-     * The streams application.
-     *
-     * @param Application $application
-     */
-    function __construct(Application $application)
-    {
-        $this->application = $application;
-    }
-
-    /**
      * Run the migrations.
      *
      * @return void
      */
     public function up()
     {
-        /* @var Builder $schema */
-        $schema = app('db')->connection()->getSchemaBuilder();
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix(null);
-        $schema->getConnection()->setTablePrefix(null);
-
-        if (!$schema->hasTable('applications')) {
-            $schema->create(
+        if (!Schema::connection('core')->hasTable('applications')) {
+            Schema::connection('core')->create(
                 'applications',
                 function (Blueprint $table) {
 
@@ -58,9 +33,6 @@ class CreateApplicationsTable extends Migration
                 }
             );
         }
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix($this->application->getReference() . '_');
-        $schema->getConnection()->setTablePrefix($this->application->getReference() . '_');
     }
 
     /**
@@ -70,15 +42,6 @@ class CreateApplicationsTable extends Migration
      */
     public function down()
     {
-        /* @var Builder $schema */
-        $schema = app('db')->connection()->getSchemaBuilder();
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix(null);
-        $schema->getConnection()->setTablePrefix(null);
-
-        $schema->dropIfExists('applications');
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix($this->application->getReference() . '_');
-        $schema->getConnection()->setTablePrefix($this->application->getReference() . '_');
+        Schema::connection('core')->dropIfExists('applications');
     }
 }
--- database/migrations/2015_02_27_075558_create_applications_domains_table.php
+++ database/migrations/2015_02_27_075558_create_applications_domains_table.php
@@ -1,9 +1,7 @@
 <?php
 
-use Anomaly\Streams\Platform\Application\Application;
 use Anomaly\Streams\Platform\Database\Migration\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
 
 /**
  * Class CreateApplicationsDomainsTable
@@ -16,37 +14,14 @@ class CreateApplicationsDomainsTable extends Migration
 {
 
     /**
-     * The streams application.
-     *
-     * @param Application
-     */
-    protected $application;
-
-    /**
-     * The streams application.
-     *
-     * @param Application $application
-     */
-    function __construct(Application $application)
-    {
-        $this->application = $application;
-    }
-
-    /**
      * Run the migrations.
      *
      * @return void
      */
     public function up()
     {
-        /* @var Builder $schema */
-        $schema = app('db')->connection()->getSchemaBuilder();
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix(null);
-        $schema->getConnection()->setTablePrefix(null);
-
-        if (!$schema->hasTable('applications_domains')) {
-            $schema->create(
+        if (!Schema::connection('core')->hasTable('applications_domains')) {
+            Schema::connection('core')->create(
                 'applications_domains',
                 function (Blueprint $table) {
 
@@ -57,9 +32,6 @@ class CreateApplicationsDomainsTable extends Migration
                 }
             );
         }
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix($this->application->getReference() . '_');
-        $schema->getConnection()->setTablePrefix($this->application->getReference() . '_');
     }
 
     /**
@@ -69,15 +41,6 @@ class CreateApplicationsDomainsTable extends Migration
      */
     public function down()
     {
-        /* @var Builder $schema */
-        $schema = app('db')->connection()->getSchemaBuilder();
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix(null);
-        $schema->getConnection()->setTablePrefix(null);
-
-        $schema->dropIfExists('applications_domains');
-
-        $schema->getConnection()->getSchemaGrammar()->setTablePrefix($this->application->getReference() . '_');
-        $schema->getConnection()->setTablePrefix($this->application->getReference() . '_');
+        Schema::connection('core')->dropIfExists('applications_domains');
     }
 }
