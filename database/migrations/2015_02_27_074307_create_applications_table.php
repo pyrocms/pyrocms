a90ef4597ba88525dfeac8297306eff9a1375ffd
Ryan Thompson <service@aiwebsystems.com>
2015-02-27 03:56:31 -0600

    Migrations

---
+++ database/migrations/2015_02_27_074307_create_applications_table.php
@@ -0,0 +1,86 @@
+<?php
+
+use Anomaly\Streams\Platform\Application\Application;
+use Anomaly\Streams\Platform\Database\Migration\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+/**
+ * Class CreateApplicationsTable
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ */
+class CreateApplicationsTable extends Migration
+{
+
+    /**
+     * The streams application.
+     *
+     * @param Application
+     */
+    protected $application;
+
+    /**
+     * The streams application.
+     *
+     * @param Application $application
+     */
+    function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /* @var Builder $schema */
+        $schema = app('db')->connection()->getSchemaBuilder();
+
+        $schema->getConnection()->getSchemaGrammar()->setTablePrefix(null);
+        $schema->getConnection()->setTablePrefix(null);
+
+        if (!$schema->hasTable('applications')) {
+            return;
+        }
+
+        $schema->create(
+            'applications',
+            function (Blueprint $table) {
+
+                $table->increments('id');
+                $table->string('name');
+                $table->string('reference');
+                $table->string('domain');
+                $table->boolean('enabled');
+            }
+        );
+
+        $schema->getConnection()->getSchemaGrammar()->setTablePrefix($this->application->getReference() . '_');
+        $schema->getConnection()->setTablePrefix($this->application->getReference() . '_');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        /* @var Builder $schema */
+        $schema = app('db')->connection()->getSchemaBuilder();
+
+        $schema->getConnection()->getSchemaGrammar()->setTablePrefix(null);
+        $schema->getConnection()->setTablePrefix(null);
+
+        $schema->dropIfExists('applications');
+
+        $schema->getConnection()->getSchemaGrammar()->setTablePrefix($this->application->getReference() . '_');
+        $schema->getConnection()->setTablePrefix($this->application->getReference() . '_');
+    }
+}
