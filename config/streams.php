e8d1f7a0d86760172d67e40c626848cee0622f36
Ryan Thompson <service@aiwebsystems.com>
2015-03-06 13:53:31 -0600

    Couple changes

--- config/streams.php
+++ config/streams.php
@@ -63,5 +63,48 @@ return [
 
     'enabled_locales' => [
         'en' => 'English'
-    ]
+    ],
+
+    /*
+	|--------------------------------------------------------------------------
+	| Site Enabled
+	|--------------------------------------------------------------------------
+	|
+	| This controls whether the site is enabled to the public or not. When the
+    | site is disabled you may only access the frontend if you are in the IP
+    | white-list.
+    |
+    | NOTE: This configuration may be overridden by the Settings module.
+	|
+	*/
+
+    'site_enabled' => true,
+
+    /*
+	|--------------------------------------------------------------------------
+	| IP Whitelist
+	|--------------------------------------------------------------------------
+	|
+	| If the site is disabled, only these IPs will be allowed to view public
+    | facing content.
+    |
+    | NOTE: This configuration may be overridden by the Settings module.
+	|
+	*/
+
+    'ip_whitelist' => [],
+
+    /*
+	|--------------------------------------------------------------------------
+	| Force HTTPS
+	|--------------------------------------------------------------------------
+	|
+	| You may opt to force an SSL connection when accessing the application.
+    | Supported options are "none", "all", "public", "admin"
+    |
+    | NOTE: This configuration may be overridden by the Settings module.
+	|
+	*/
+
+    'force_https' => "none",
 ];
