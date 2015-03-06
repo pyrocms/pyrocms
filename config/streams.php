615f723d98356f410317767c288b144fde12ec19
Ryan Thompson <service@aiwebsystems.com>
2015-03-06 13:09:04 -0600

    Available locales

--- config/streams.php
+++ config/streams.php
@@ -11,7 +11,7 @@ return [
     | distribution is like the DNA of the application's unique characteristics.
 	|
 	*/
-    
+
     'distribution' => 'Anomaly\StreamsDistribution\StreamsDistribution',
 
     /*
@@ -23,7 +23,7 @@ return [
     | are loaded in the order in which they appear here.
     |
     */
-    
+
     'addon_types' => [
         'distribution',
         'field_type',
@@ -41,11 +41,27 @@ return [
 	|
 	| This controls the top level locales that are available to the system.
     | This will prevent from locales where no translations exist.
+    |
+    | NOTE: This configuration may be overridden by the Localization module.
 	|
 	*/
-    
+
     'available_locales' => [
-        'en',
-        'fr'
+        'en'
+    ],
+
+    /*
+	|--------------------------------------------------------------------------
+	| Enabled Locales
+	|--------------------------------------------------------------------------
+	|
+	| This controls the translations available to save when editing entries.
+    |
+    | NOTE: This configuration may be overridden by the Localization module.
+	|
+	*/
+
+    'enabled_locales' => [
+        'en'
     ]
 ];
