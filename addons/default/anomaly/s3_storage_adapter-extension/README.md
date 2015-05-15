3c26d15e141c7090ea0372e4f8f30e782d4bba03
Ryan Thompson <service@aiwebsystems.com>
2015-05-14 22:40:13 -0500

    No handler just yet

---
+++ addons/default/anomaly/s3_storage_adapter-extension/README.md
@@ -0,0 +1,3 @@
+# local_storage_adapter-extension
+
+A local storage adapter for the Files module.
---
+++ addons/default/anomaly/s3_storage_adapter-extension/resources/lang/en/addon.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'        => 'S3 Storage Adapter',
+    'description' => 'An Amazon S3 storage adapter for the files module.'
+];
