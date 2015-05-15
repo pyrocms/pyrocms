479b5266fc6184b3fe7230b171904b7521b35e89
Ryan Thompson <service@aiwebsystems.com>
2015-05-14 23:02:30 -0500

    Fucking git..

--- addons/default/anomaly/s3_storage_adapter-extension/README.md
+++
@@ -1,3 +0,0 @@
-# local_storage_adapter-extension
-
-A local storage adapter for the Files module.
--- addons/default/anomaly/s3_storage_adapter-extension/resources/lang/en/addon.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'name'        => 'S3 Storage Adapter',
-    'description' => 'An Amazon S3 storage adapter for the files module.'
-];
