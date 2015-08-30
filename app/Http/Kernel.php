7b379fb4c0732cafd7efd78081b50602873024ef
Ryan Thompson <service@aiwebsystems.com>
2015-08-30 13:16:10 -0500

    We handle this ourselves

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,7 +12,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
