8bc41e32be3168679e080a1d9a8b85214d082a68
Brennon Loveless <brennon.loveless@gmail.com>
2015-10-23 14:47:57 -0600

    Rename migration to follow suite

---
+++ system/cms/migrations/131_Add_custom_to_navigation.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Migration 131: Add custom to navigation
+ *
+ * This will allow us to set use larger option strings
+ * 
+ * Added October 20th, 2015
+ */
+
+class Migration_Add_custom_to_navigation extends CI_Migration {
+
+  public function up()
+  {
+	  $this->dbforge->add_column('navigation_links', array('custom' => array('type' => 'VARCHAR', 'constraint' => 255, 'default' => '')));
+  }
+
+  public function down()
+  {
+	  $this->dbforge->drop-column('navigation_links', 'custom');
+  }
+}
