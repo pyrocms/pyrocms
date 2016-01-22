169203fb18aedd73046573aa61850bdd9124fad9
Ryan Thompson <ryan@pyrocms.com>
2016-01-21 23:26:12 -0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -133,7 +133,6 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
