b3b71099ecef9eab34d2570c660750c32fb939b5
Ryan Thompson <service@aiwebsystems.com>
2016-02-17 23:56:03 -0600

    Default themes

---
+++ resources/core/config/streams/themes.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Active Themes
+    |--------------------------------------------------------------------------
+    |
+    | These values specify the admin and public theme to use.
+    |
+    */
+
+    'admin'    => env('ADMIN_THEME', 'pyrocms.theme.pyrocms'),
+    'standard' => env('STANDARD_THEME', 'anomaly.theme.starter')
+];
