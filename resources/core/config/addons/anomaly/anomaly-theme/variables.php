2768f65c2f51305673f2d674675af05239b540e4
Ryan Thompson <service@aiwebsystems.com>
2016-02-17 23:38:50 -0600

    more self docs

--- resources/core/config/addons/anomaly/anomaly-theme/variables.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'secondary' => '#f2364b',
-    'icon'      => 'public::icon.png',
-    'favicon'   => 'public::favicon.png'
-];
--- resources/core/config/streams/addons.php
+++ resources/core/config/streams/addons.php
@@ -4,16 +4,28 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Eager Loaded Addons
+    | Eager Addons
     |--------------------------------------------------------------------------
     |
     | Eager loaded addons are registered first and can be defined
-    | here by specifying their relative path to the project root.
+    | here by specifying their relative path to the addon's root.
     |
     */
 
     'eager' => [
         'core/anomaly/redirects-module'
-    ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Deferred Addons
+    |--------------------------------------------------------------------------
+    |
+    | Deferred addons are registered last and can be defined
+    | here by specifying their relative path to the addon's root.
+    |
+    */
+
+    'deferred' => []
 
 ];
