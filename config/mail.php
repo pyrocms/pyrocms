9ea5b196c03b732e8a7955e967e6a44a965cb348
Ryan Thompson <ryan@pyrocms.com>
2016-08-30 02:11:09 -0500

    Mail for env

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'mail'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => env('FROM_ADDRESS', 'hello@example.com'),
+        'name'    => env('FROM_NAME', 'Example'),
     ],
 
     /*
