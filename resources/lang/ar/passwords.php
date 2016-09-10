c00ab59a738822d9209d8c91399b1e8fffe3d28f
aladinfajr <aladinfajr@gmail.com>
2016-09-10 21:54:57 +0300

    translate pyrocms to arabic

---
+++ resources/lang/ar/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "كلمة المرور يجب أن تكون على الأقل 6 أحرف وتتطابق التأكيد.",
+    "user"     => "لا يمكن إيجاد مستخدم بهذا البريد الالكترني.",
+    "token"    => "رمز إعادة تعين كلمة المرور غير صالح.",
+    "sent"     => "تم ارسال مذكر كلمة المرور!",
+    "reset"    => "تم اعادة تعين كلمة المرور!",
+
+];
