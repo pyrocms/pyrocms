73d5977d3caa61fb619a41845920968060f1acb4
Ryan Thompson <ryan@pyrocms.com>
2017-03-02 13:58:02 -0600

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 class ExampleTest extends TestCase
 {
 
