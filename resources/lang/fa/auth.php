eb8d14b662e338b3795ceb41da78486894822867
Samsam Babadi <samsambabadi@gmail.com>
2017-05-24 19:43:39 +0430

    translate pyrocms to Persian(Fa)

---
+++ resources/lang/fa/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'اطلاعات ورود صحیح نمیباشد.',
+    'throttle' => 'تعداد دفعات تلاش برای ورود از حد مجاز بیشتر است. لطفا پس از  :seconds ثانیه مجددا تلاش فرمایید.',
+
+];
