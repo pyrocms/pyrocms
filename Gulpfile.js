e76f4b43f0e60b4ab4f9f6edb40719653595cde0
Denis Efremov <efremov.a.denis@gmail.com>
2017-07-08 03:23:29 +0300

    Gulpfile especially for PyroCMS artisan generated theme edition

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,62 @@
-const elixir = require('laravel-elixir');
+// You would need to fill 3 fields below
+var
+  appReference = '',
+  vendor = '',
+  themeSlug = '',
+  resources = `addons/${appReference}/${vendor}/${themeSlug}-theme/resources/`,
+  Elixir = require('laravel-elixir');
 
+require('laravel-elixir-webpack-official');
 require('laravel-elixir-vue-2');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
- */
+Elixir.config.sourcemaps = true;
 
-elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+Elixir.webpack.mergeConfig({
+  babel: {
+    presets: ['es2015', 'stage-2'],
+    plugins: ['transform-runtime'],
+  },
+  resolve: {
+    alias: {
+      $: 'jquery/src/jquery',
+      vue: 'vue/dist/vue.js',
+      jquery: 'jquery/src/jquery',
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        includePaths: [
+          './node_modules/bootstrap-sass/assets/stylesheets'
+        ],
+        loader: 'style!css!sass',
+      },
+      {
+        test: /\.styl(us)?$/,
+        loader: 'style!css!stylus',
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/,
+        loader: 'file?name=/img/[name].[ext]',
+      },
+      {
+        test: /\.(ttf|eot|svg|woff2?)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file?name=/fonts/[name].[ext]',
+      },
+    ],
+  },
+});
+
+Elixir(mix => {
+  mix
+    .copy(`${resources}fonts`, 'public/fonts')
+    .copy(`${resources}img`, 'public/img')
+    .stylus(`./${resources}stylus/main.styl`, 'public/css', `${resources}stylus`)
+    .sass('theme.scss', 'public/css', `${resources}scss/theme`)
+    .webpack('main.js', 'public/js', `${resources}js`);
 });
