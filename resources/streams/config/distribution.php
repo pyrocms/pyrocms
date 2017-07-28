a4c286563794953ac56ce20d838c13c952790216
Ryan Thompson <ryan@pyrocms.com>
2017-07-27 23:39:02 -0500

    Bump

--- resources/streams/config/distribution.php
+++ resources/streams/config/distribution.php
@@ -3,5 +3,5 @@
 return [
     'name'        => 'PyroCMS',
     'description' => 'Build better Laravel websites and applications faster with Pyro.',
-    'version'     => 'v3.3.3',
+    'version'     => 'v3.4.0-beta',
 ];
