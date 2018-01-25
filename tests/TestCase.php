5e322bc0f3f102519d78c1534c9484996fa65cf7
Ryan Thompson <service@aiwebsystems.com>
2018-01-25 11:39:54 -0600

    Testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,13 +11,6 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
 
     /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
-
-    /**
      * Creates the application.
      *
      * @return \Illuminate\Foundation\Application
