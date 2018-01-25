09567f64fa93114bb4a0e6d67b802f9299e9774a
Ryan Thompson <service@aiwebsystems.com>
2018-01-25 10:56:51 -0600

    Example Test

---
+++ tests/BootstrapTest.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Contracts\Console\Kernel;
+
+/**
+ * Class BootstrapTest
+ *
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
+ */
+class BootstrapTest extends TestCase
+{
+
+    /**
+     * Make sure the application
+     * was created as it should.
+     */
+    public function testCreatesApplication()
+    {
+        $this->assertInstanceOf(\Illuminate\Contracts\Foundation\Application::class, $this->createApplication());
+    }
+
+    /**
+     * Make sure the console kernel
+     * is from the Streams Platform.
+     */
+    public function testCreatesConsoleKernel()
+    {
+        $this->assertInstanceOf(\Anomaly\Streams\Platform\Console\Kernel::class, $this->app[Kernel::class]);
+    }
+}
