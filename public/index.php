187e70bd0f6a4be3e258dc95859c0ccd0a909c65
Ryan Thompson <ryan@pyrocms.com>
2018-11-09 23:15:15 -0600

    Implement HTTP cache

--- public/index.php
+++ public/index.php
@@ -49,6 +49,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+$kernel = \Barryvdh\HttpCache\CacheKernel::wrap($kernel);
+
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
