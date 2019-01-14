3ade757381af299bf022a638f48333adf9732b0d
Ryan Thompson <ryan@pyrocms.com>
2019-01-14 17:40:00 -0600

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -13,6 +13,6 @@ use Illuminate\Foundation\Inspiring;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-});
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//});
