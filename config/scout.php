9f6c55c240d70b1d6f801da40556be36b58f966f
Ryan Thompson <ryan@pyrocms.com>
2019-04-02 12:30:31 -0500

    Disable search by default as there are environmental issues sometimes.
    Needs prior thought before enabling.

--- config/scout.php
+++ config/scout.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'driver' => env('SCOUT_DRIVER', 'tntsearch'),
+    'driver' => env('SCOUT_DRIVER', null),
 
     /*
     |--------------------------------------------------------------------------
