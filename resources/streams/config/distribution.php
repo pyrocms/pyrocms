6f17a11f623f6fa0216449fc5558ea1ce9b595cd
Ryan Thompson <ryan@pyrocms.com>
2021-08-25 09:16:12 -0500

    o_O

--- resources/streams/config/distribution.php
+++ resources/streams/config/distribution.php
@@ -3,5 +3,5 @@
 return [
     'name'        => 'PyroCMS',
     'description' => 'Build better Laravel websites and applications faster with Pyro.',
-    'version'     => 'v3.8',
+    'version'     => 'v3.9',
 ];
