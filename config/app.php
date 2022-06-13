c76516c8a1ddd2148948b63f8903c2d198339a7d
Ryan Thompson <ryan@pyrocms.com>
2022-06-13 09:13:58 -0500

    Remove blade icons

--- config/app.php
+++ config/app.php
@@ -194,8 +194,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        //\BladeUI\Icons\BladeIconsServiceProvider::class,
     ],
 
     /*
