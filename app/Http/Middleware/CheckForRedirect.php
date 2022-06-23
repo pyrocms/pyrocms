1d514b350ca6ac783b018e00dfe1fa0b543b1ab4
Ryan Thompson <ryan@pyrocms.com>
2022-06-23 15:03:38 -0500

    Tweaks

---
+++ app/Http/Middleware/CheckForRedirect.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Streams\Core\Support\Facades\Streams;
+
+class CheckForRedirect
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $redirects = Streams::entries('redirects')->get();
+
+        $match = $redirects->first(
+            fn ($entry) => Str::is($entry->from, $request->path())
+        );
+
+        if ($match) {
+            return Redirect::to($match->to, $match->status);
+        }
+
+        return $next($request);
+    }
+}
